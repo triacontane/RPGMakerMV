a9c00e16968ceeeabcfb978c67bd5779d891085a
triacontane <triacontanetakenoko@gmail.com>
2015-12-21 22:44:16 +0900

    update

--- PictureAnimation.js
+++ PictureAnimation.js
@@ -6,6 +6,8 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.1 2015/12/21 ピクチャのファイル名を連番方式で指定できる機能を追加
+//                  アニメーションの強制終了の機能を追加
 // 1.0.0 2015/12/19 初版
 // ----------------------------------------------------------------------------
 // [Blog]   : http://triacontane.blogspot.jp/
@@ -18,7 +20,7 @@
  * @author トリアコンタン
  * 
  * @help 指定したフレーム間隔でピクチャをアニメーションします。
- * アニメーションしたいセルを縦もしくは横一列に並べた画像を用意の上
+ * アニメーションしたいセル画像（※）を用意の上
  * 以下のコマンドを入力してください。
  *
  * 1. ピクチャのアニメーション準備（プラグインコマンド）
@@ -26,17 +28,25 @@
  * 3. ピクチャのアニメーション開始（プラグインコマンド）
  * 4. ピクチャのアニメーション終了（プラグインコマンド）
  *
+ * ※配置方法は以下の3通りがあります。
+ *  縦　：セルを縦に並べて全体を一つのファイルにします。
+ *  横　：セルを横に並べて全体を一つのファイルにします。
+ *  連番：連番のセル画像を複数用意します。(original部分は任意の文字列)
+ *   original00.png(ピクチャの表示で指定するオリジナルファイル)
+ *   original01.png
+ *   original02.png...
+ *
  * プラグインコマンド詳細
  *  イベントコマンド「プラグインコマンド」から実行。
  *  （パラメータの間は半角スペースで区切る）
  *
  *  PA_INIT or
- *  ピクチャのアニメーション準備 [セル数] [フレーム数] [セル配置方向] :
+ *  ピクチャのアニメーション準備 [セル数] [フレーム数] [セル配置方法] :
  *  　このコマンドの次に実行される「ピクチャの表示」をアニメーション対象にします。
  *  　セル数　　　：アニメーションするセル画の数
  *  　フレーム数　：アニメーションする間隔のフレーム数
- *  　セル配置方向：セルの配置（縦 or 横）※省略すると縦になります。
- *  　例：PA_INIT 4 10 横
+ *  　セル配置方向：セルの配置（縦 or 横 or 連番）※省略すると縦になります。
+ *  　例：PA_INIT 4 10 連番
  *
  *  PA_START or
  *  ピクチャのアニメーション開始 [ピクチャ番号] [アニメーションタイプ]
@@ -58,10 +68,16 @@
  *  　指定したピクチャ番号のピクチャをアニメーションを終了します。
  *  　一番上のセルに戻った時点でアニメーションが止まります。
  *
+ *  PA_STOP_FORCE or
+ *  ピクチャのアニメーション強制終了 [ピクチャ番号]
+ *  　指定したピクチャ番号のピクチャをアニメーションを終了します。
+ *  　現在表示しているセルでアニメーションが止まります。
+ *
  *  PA_SET_CELL or
  *  ピクチャのアニメーションセル設定 [ピクチャ番号] [セル番号]
  *  　アニメーションのセルを直接設定します。（開始位置は 1 です）
- *  　主にアニメーションが停止している場合に有効です。
+ *  　任意のタイミングでアニメーションしたい場合に有効です。
+ *  　セル番号を指定しなかった場合は、アニメーションを一つ先に進めます。
  *
  * このプラグインにはプラグインコマンドはありません。
  *
@@ -145,9 +161,9 @@
         switch (getCommandName(command)) {
             case 'PA_INIT' :
             case 'ピクチャのアニメーション準備':
-                cellNumber  = getArgNumber(args[0], 1, 100);
+                cellNumber  = getArgNumber(args[0], 1, 99);
                 frameNumber = getArgNumber(args[1], 1, 9999);
-                direction   = getArgString(args[2], true);
+                direction   = getArgString(args[2], true) || '縦';
                 $gameScreen.setPicturesAnimation(cellNumber, frameNumber, direction);
                 break;
             case 'PA_START' :
@@ -170,12 +186,18 @@
                 picture       = $gameScreen.picture($gameScreen.realPictureId(pictureNum));
                 if (picture) picture.stopAnimation(false);
                 break;
+            case 'PA_STOP_FORCE' :
+            case 'ピクチャのアニメーション強制終了':
+                pictureNum    = getArgNumber(args[0], 1, 100);
+                picture       = $gameScreen.picture($gameScreen.realPictureId(pictureNum));
+                if (picture) picture.stopAnimation(true);
+                break;
             case 'PA_SET_CELL' :
             case 'ピクチャのアニメーションセル設定':
                 pictureNum    = getArgNumber(args[0], 1, 100);
-                cellNumber    = getArgNumber(args[1], 1, 100) - 1;
+                cellNumber    = getArgNumber(args[1], 0, 100) - 1;
                 picture       = $gameScreen.picture($gameScreen.realPictureId(pictureNum));
-                if (picture) picture.cell = cellNumber;
+                if (picture) cellNumber === -1 ? picture.addCellCount() : picture.cell = cellNumber;
                 break;
         }
     };
@@ -225,7 +247,6 @@
         this._frameCount    = 0;
         this._animationType = 0;
         this._loopFlg       = false;
-        this._animation     = false;
         this._direction     = '';
     };
 
@@ -258,12 +279,16 @@
         if (!this.isAnimation()) return;
         this._frameCount = (this._frameCount + 1) % this._frameNumber;
         if (this._frameCount === 0) {
-            this._cellCount = (this._cellCount + 1) %
-                (this._animationType === 2 ? (this._cellNumber - 1) * 2 : this._cellNumber);
+            this.addCellCount();
             if (this.cell === 0 && !this._loopFlg) {
                 this._animationType = 0;
             }
         }
+    };
+
+    Game_Picture.prototype.addCellCount = function() {
+        this._cellCount = (this._cellCount + 1) %
+            (this._animationType === 2 ? (this._cellNumber - 1) * 2 : this._cellNumber);
     };
 
     Game_Picture.prototype.setAnimationInit = function(cellNumber, frameNumber, direction) {
@@ -295,19 +320,65 @@
     var _Sprite_Picture_update = Sprite_Picture.prototype.update;
     Sprite_Picture.prototype.update = function() {
         _Sprite_Picture_update.call(this);
-        if (this.visible) this.updateAnimation();
+        if (this.visible && this.isBitmapReady()) this.updateAnimation();
+    };
+
+    var _Sprite_Picture_updateBitmap = Sprite_Picture.prototype.updateBitmap;
+    Sprite_Picture.prototype.updateBitmap = function() {
+        _Sprite_Picture_updateBitmap.call(this);
+        var picture = this.picture();
+        if (!picture) this._bitmap = null;
     };
 
     Sprite_Picture.prototype.updateAnimation = function() {
-        var dir = this.picture()._direction;
-        if (dir !== '横' && dir !== 'H') {
-            var height = this.bitmap.height / this.picture()._cellNumber;
-            var y      = this.picture().cell * height;
-            this.setFrame(0, y, this.bitmap.width, height);
-        } else {
-            var width = this.bitmap.width / this.picture()._cellNumber;
-            var x      = this.picture().cell * width;
-            this.setFrame(x, 0, width, this.bitmap.height);
+        switch (this.picture()._direction) {
+            case '連番':
+            case 'N':
+                this.bitmap = this._bitmaps[this.picture().cell];
+                this.setFrame(0, 0, this.bitmap.width, this.bitmap.height);
+                break;
+            case '縦':
+            case 'V':
+                var height = this.bitmap.height / this.picture()._cellNumber;
+                var y      = this.picture().cell * height;
+                this.setFrame(0, y, this.bitmap.width, height);
+                break;
+            case '横':
+            case 'H':
+                var width = this.bitmap.width / this.picture()._cellNumber;
+                var x     = this.picture().cell * width;
+                this.setFrame(x, 0, width, this.bitmap.height);
+                break;
         }
+    };
+
+    var _Sprite_Picture_loadBitmap = Sprite_Picture.prototype.loadBitmap;
+    Sprite_Picture.prototype.loadBitmap = function() {
+        _Sprite_Picture_loadBitmap.call(this);
+        var cellNumber = this.picture()._cellNumber;
+        var dir = this.picture()._direction;
+        if (cellNumber > 1 && (dir === '連番' || dir === 'N')) {
+            this._bitmaps = [this.bitmap];
+            for (var i = 1; i < cellNumber; i++) {
+                var filename = this._pictureName.substr(0, this._pictureName.length - 2) + i.padZero(2);
+                this._bitmaps[i] = ImageManager.loadPicture(filename);
+            }
+        }
+        this._bitmapReady = false;
+    };
+
+    Sprite_Picture.prototype.isBitmapReady = function() {
+        if (this._bitmapReady) return true;
+        var dir = this.picture()._direction;
+        var result;
+        if (dir === '連番' || dir === 'N') {
+            result = this._bitmaps.every(function(bitmap) {
+                return bitmap.isReady();
+            })
+        } else {
+            result = this.bitmap.isReady();
+        }
+        if (result) this._bitmapReady = true;
+        return result;
     };
 })();
