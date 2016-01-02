a7097e1bd785438bbc8ad92673e445f681ba7bdd
トリアコンタン <triacontanetakenoko@gmail.com>
2016-01-02 11:00:48 +0900

    update

--- MessageWindowHidden.js
+++ MessageWindowHidden.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.0.2 2016/01/02 競合対策
 // 1.0.1 2015/12/31 コメント追加＋英語対応（仕様に変化なし）
 // 1.0.0 2015/12/30 初版
 // ----------------------------------------------------------------------------
@@ -78,12 +79,14 @@
             if (this.visible) {
                 this.hide();
                 this.subWindows().forEach(function(subWindow) {
+                    subWindow.prevVisible = subWindow.visible;
                     subWindow.hide();
                 });
             } else {
                 this.show();
                 this.subWindows().forEach(function(subWindow) {
-                    subWindow.show();
+                    if (subWindow.prevVisible) subWindow.show();
+                    subWindow.prevVisible = undefined;
                 });
             }
         }
