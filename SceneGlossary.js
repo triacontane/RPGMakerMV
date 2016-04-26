0df70f442cb30e1edfa509bd492a9724008e05bd
triacontane <triacontanetakenoko@gmail.com>
2016-04-26 22:02:39 +0900

    update

--- SceneGlossary.js
+++ SceneGlossary.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.3.1 2016/04/26 ピクチャ名を変数で指定できる機能を追加
 // 1.3.0 2016/04/23 用語の種別ごとに、複数の用語画面を作成できる機能を追加
 //                  用語をアイテムとして使用できる機能を追加
 // 1.2.1 2016/04/21 複数ページ送りをタッチ操作に対応
@@ -391,6 +392,11 @@ function Scene_Glossary() {
         if (arguments.length < 2) min = -Infinity;
         if (arguments.length < 3) max = Infinity;
         return convertEscapeCharactersAndEval(arg, true).clamp(min, max);
+    };
+
+    var getArgString = function (arg, upperFlg) {
+        arg = convertEscapeCharactersAndEval(arg, false);
+        return upperFlg ? arg.toUpperCase() : arg;
     };
 
     var convertEscapeCharactersAndEval = function(text, evalFlg) {
@@ -911,7 +917,7 @@ function Scene_Glossary() {
         if (!this._itemData) return;
         var pictureName = getMetaValues(this._itemData, ['ピクチャ', 'Picture'], index);
         if (pictureName) {
-            var bitmap = ImageManager.loadPicture(pictureName, 0);
+            var bitmap = ImageManager.loadPicture(getArgString(pictureName), 0);
             bitmap.addLoadListener(this.drawItemSub.bind(this, bitmap));
         } else {
             this.drawItemSub(null);
