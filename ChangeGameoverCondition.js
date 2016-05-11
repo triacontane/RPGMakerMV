d448ff283caf866ba8a5d6752ba3243a6dc51090
triacontane <triacontanetakenoko@gmail.com>
2016-05-11 23:14:35 +0900

    update

--- ChangeGameoverCondition.js
+++ ChangeGameoverCondition.js
@@ -19,6 +19,13 @@
  *
  * @help ゲームオーバー条件をいずれかの味方が戦闘不能になった場合
  * に変更します。
+ * 対象となるアクターIDを指定することもできます。
+ *
+ * 条件を変更するにはスクリプトで以下を実行してください。
+ *
+ * $gameParty.setVip(1);   // アクターID[1]が戦闘不能になるとゲームオーバー
+ * $gameParty.setVipAll(); // いずれか一人のアクターが戦闘不能になるとゲームオーバー
+ * $gameParty.removeVip(); // ゲームオーバーの設定を解除
  *
  * このプラグインにはプラグインコマンドはありません。
  *
@@ -31,11 +38,33 @@
 (function () {
     'use strict';
 
+    var _Game_Party_initialize = Game_Party.prototype.initialize;
+    Game_Party.prototype.initialize = function() {
+        _Game_Party_initialize.apply(this, arguments);
+        this._vipActor = null;
+        this._vipAll   = true;
+    };
+
+    Game_Party.prototype.setVip = function(id) {
+        this._vipActor = id;
+        this._vipAll   = false;
+    };
+
+    Game_Party.prototype.setVipAll = function() {
+        this._vipAll   = true;
+    };
+
+    Game_Party.prototype.removeVip = function() {
+        this._vipActor = null;
+        this._vipAll   = false;
+    };
+
     var _Game_Party_isAllDead = Game_Party.prototype.isAllDead;
     Game_Party.prototype.isAllDead = function() {
         var result = _Game_Party_isAllDead.apply(this, arguments);
-        if (!result) {
-            return (this.inBattle() || !this.isEmpty()) && this.deadMembers().length > 0;
+        if (!result && this.deadMembers().length > 0 && (this.inBattle() || !this.isEmpty())) {
+            return this._vipAll ? true :
+                this._vipActor ? this.deadMembers().contains($gameActors.actor(this._vipActor)) : false;
         }
         return result;
     };
