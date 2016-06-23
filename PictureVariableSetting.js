152748b41747d4497cfada2834300f96a01ca3c7
triacontane <triacontanetakenoko@gmail.com>
2016-06-23 23:58:18 +0900

    update

--- PictureVariableSetting.js
+++ PictureVariableSetting.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.4.0 2016/06/23 ピクチャをシェイクさせるコマンドを追加
 // 1.3.0 2016/04/29 ピクチャを指定した角度まで回転させるコマンドを追加
 // 1.2.1 2016/04/14 処理を適用するピクチャを範囲指定もしくは複数指定する機能を追加
 // 1.2.0 2016/03/19 表示中のすべてのピクチャに処理を適用するコマンドを追加
@@ -104,12 +105,29 @@
  *  例 P_SPIN_RELATIVE \v[1] -90 0
  *  -> 変数「1」の値のピクチャを、現在の角度から-90度まで即座に回転させる。
  *
+ *  P_SHAKE [ピクチャ番号] [強さ] [速さ] [角度] [時間(フレーム)] [完了までウェイト]
+ *  対象のピクチャをシェイクさせます。強さと速さはイベント「画面のシェイク」と
+ *  同様の9段階の数値で指定します。角度(degree)はシェイク方向を0から360までの
+ *  値で指定します。(90で縦方向にシェイクします)
+ *  時間を指定しないと、停止するまでシェイクし続けます。
+ *  一番後ろに「WAIT」と付けると、回転が完了するまでウェイトします。
+ *  このコマンドには「P_TARGET_ALL」等が適用されます。
+ *  例 P_SHAKE 1 3 9 0 60 WAIT
+ *  -> ピクチャ番号「1」を60フレームのあいだ、強さ3、速さ9で横方向にシェイクします。
+ *
+ *  P_STOP_SHAKE [ピクチャ番号]
+ *  対象のピクチャをシェイクを止めます。P_SHAKEで時間を指定しないと
+ *  ずっとシェイクし続けるので当コマンドで無効にします。
+ *  このコマンドには「P_TARGET_ALL」等が適用されます。
+ *　例 P_STOP_SHAKE \v[1]
+ *  -> 変数「1」の値のピクチャのシェイクを止めます。
+ *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
  *  についても制限はありません。
  *  このプラグインはもうあなたのものです。
  */
-(function () {
+(function() {
     'use strict';
     var pluginName = 'PictureVariableSetting';
 
@@ -119,7 +137,7 @@
     //=============================================================================
     var getParamBoolean = function(paramNames) {
         var value = getParamOther(paramNames);
-        return (value || '').toUpperCase() == 'ON';
+        return (value || '').toUpperCase() === 'ON';
     };
 
     var getParamNumber = function(paramNames, min, max) {
@@ -128,7 +146,6 @@
         if (arguments.length < 3) max = Infinity;
         return (parseInt(value, 10) || 0).clamp(min, max);
     };
-
 
     var getParamOther = function(paramNames) {
         if (!Array.isArray(paramNames)) paramNames = [paramNames];
@@ -139,28 +156,28 @@
         return null;
     };
 
-    var getCommandName = function (command) {
+    var getCommandName = function(command) {
         return (command || '').toUpperCase();
     };
 
-    var getArgString = function (arg, upperFlg) {
+    var getArgString = function(arg, upperFlg) {
         arg = convertEscapeCharactersAndEval(arg, false);
         return upperFlg ? arg.toUpperCase() : arg;
     };
 
-    var getArgNumber = function (arg, min, max) {
+    var getArgNumber = function(arg, min, max) {
         if (arguments.length < 2) min = -Infinity;
         if (arguments.length < 3) max = Infinity;
         return (parseInt(convertEscapeCharactersAndEval(arg, true), 10) || 0).clamp(min, max);
     };
 
-    var getArgArrayString = function (args, upperFlg) {
+    var getArgArrayString = function(args, upperFlg) {
         var values = getArgString(args, upperFlg).split(',');
         for (var i = 0; i < values.length; i++) values[i] = values[i].trim();
         return values;
     };
 
-    var getArgArrayNumber = function (args, min, max) {
+    var getArgArrayNumber = function(args, min, max) {
         var values = getArgArrayString(args, false);
         if (arguments.length < 2) min = -Infinity;
         if (arguments.length < 3) max = Infinity;
@@ -185,7 +202,7 @@
     //  ピクチャ番号を変数で指定するよう変更します。
     //=============================================================================
     var _Game_Interpreter_pluginCommand      = Game_Interpreter.prototype.pluginCommand;
-    Game_Interpreter.prototype.pluginCommand = function (command, args) {
+    Game_Interpreter.prototype.pluginCommand = function(command, args) {
         _Game_Interpreter_pluginCommand.call(this, command, args);
         switch (getCommandName(command)) {
             case 'P_VARIABLE_VALID':
@@ -209,37 +226,48 @@
             case 'P_SPIN':
                 var spinDuration = getArgNumber(args[2], 0);
                 $gameScreen.spinPicture(getArgNumber(args[0], 1), getArgNumber(args[1]), spinDuration);
-                if(args[3]) this.wait(spinDuration);
+                if (args[3]) this.wait(spinDuration);
                 break;
             case 'P_SPIN_RELATIVE':
                 var spinRelativeDuration = getArgNumber(args[2], 0);
                 $gameScreen.spinPictureRelative(getArgNumber(args[0], 1), getArgNumber(args[1]), spinRelativeDuration);
-                if(args[3]) this.wait(spinRelativeDuration);
+                if (args[3]) this.wait(spinRelativeDuration);
+                break;
+            case 'P_SHAKE':
+                var shakeDuration = getArgNumber(args[4], 0) || Infinity;
+                var power         = getArgNumber(args[1], 1, 9);
+                var speed         = getArgNumber(args[2], 1, 9);
+                var rotation      = getArgNumber(args[3], 0, 360);
+                $gameScreen.shakePicture(getArgNumber(args[0], 1), power, speed, rotation, shakeDuration);
+                if (args[5] && shakeDuration !== Infinity) this.wait(shakeDuration);
+                break;
+            case 'P_STOP_SHAKE':
+                $gameScreen.shakePicture(getArgNumber(args[0], 1), 0, 0, 0, 0);
                 break;
         }
     };
 
-    var _Game_Interpreter_command231 = Game_Interpreter.prototype.command231;
+    var _Game_Interpreter_command231      = Game_Interpreter.prototype.command231;
     Game_Interpreter.prototype.command231 = function() {
         return this.transPictureNumber(_Game_Interpreter_command231.bind(this));
     };
 
-    var _Game_Interpreter_command232 = Game_Interpreter.prototype.command232;
+    var _Game_Interpreter_command232      = Game_Interpreter.prototype.command232;
     Game_Interpreter.prototype.command232 = function() {
         return this.transPictureNumber(_Game_Interpreter_command232.bind(this));
     };
 
-    var _Game_Interpreter_command233 = Game_Interpreter.prototype.command233;
+    var _Game_Interpreter_command233      = Game_Interpreter.prototype.command233;
     Game_Interpreter.prototype.command233 = function() {
         return this.transPictureNumber(_Game_Interpreter_command233.bind(this));
     };
 
-    var _Game_Interpreter_command234 = Game_Interpreter.prototype.command234;
+    var _Game_Interpreter_command234      = Game_Interpreter.prototype.command234;
     Game_Interpreter.prototype.command234 = function() {
         return this.transPictureNumber(_Game_Interpreter_command234.bind(this));
     };
 
-    var _Game_Interpreter_command235 = Game_Interpreter.prototype.command235;
+    var _Game_Interpreter_command235      = Game_Interpreter.prototype.command235;
     Game_Interpreter.prototype.command235 = function() {
         return this.transPictureNumber(_Game_Interpreter_command235.bind(this));
     };
@@ -247,9 +275,9 @@
     Game_Interpreter.prototype.transPictureNumber = function(handler) {
         var result;
         if ($gameSystem.pictureNumVariable) {
-            var oldValue = this._params[0];
+            var oldValue    = this._params[0];
             this._params[0] = $gameVariables.value(this._params[0]).clamp(1, $gameScreen.maxPictures());
-            result = handler();
+            result          = handler();
             this._params[0] = oldValue;
         } else {
             result = handler();
@@ -261,7 +289,7 @@
     // Game_System
     //  ピクチャ番号の変数指定フラグを追加定義します。
     //=============================================================================
-    var _Game_System_initialize = Game_System.prototype.initialize;
+    var _Game_System_initialize      = Game_System.prototype.initialize;
     Game_System.prototype.initialize = function() {
         _Game_System_initialize.call(this);
         this.pictureNumVariable = getParamBoolean('初期値');
@@ -271,12 +299,12 @@
     // Game_Screen
     //  動的ファイル名指定用のプロパティを追加定義します。
     //=============================================================================
-    var _Game_Screen_clear = Game_Screen.prototype.clear;
+    var _Game_Screen_clear      = Game_Screen.prototype.clear;
     Game_Screen.prototype.clear = function() {
         _Game_Screen_clear.call(this);
-        this.dPictureFileName = null;
-        this._pictureTargetStart = null;
-        this._pictureTargetEnd = null;
+        this.dPictureFileName      = null;
+        this._pictureTargetStart   = null;
+        this._pictureTargetEnd     = null;
         this._pictureTargetNumbers = null;
     };
 
@@ -285,17 +313,17 @@
     };
 
     Game_Screen.prototype.setPictureTargetRange = function(start, end) {
-        this._pictureTargetAll = 2;
+        this._pictureTargetAll   = 2;
         this._pictureTargetStart = start;
-        this._pictureTargetEnd = end;
+        this._pictureTargetEnd   = end;
     };
 
     Game_Screen.prototype.setPictureTargetMulti = function(args) {
-        this._pictureTargetAll = 3;
+        this._pictureTargetAll     = 3;
         this._pictureTargetNumbers = args;
     };
 
-    var _Game_Screen_movePicture = Game_Screen.prototype.movePicture;
+    var _Game_Screen_movePicture      = Game_Screen.prototype.movePicture;
     Game_Screen.prototype.movePicture = function(pictureId, origin, x, y, scaleX,
                                                  scaleY, opacity, blendMode, duration) {
         if (this._pictureTargetAll > 0) {
@@ -308,7 +336,7 @@
         }
     };
 
-    var _Game_Screen_rotatePicture = Game_Screen.prototype.rotatePicture;
+    var _Game_Screen_rotatePicture      = Game_Screen.prototype.rotatePicture;
     Game_Screen.prototype.rotatePicture = function(pictureId, speed) {
         if (this._pictureTargetAll > 0) {
             this.iteratePictures(function(picture) {
@@ -320,7 +348,7 @@
         }
     };
 
-    var _Game_Screen_tintPicture = Game_Screen.prototype.tintPicture;
+    var _Game_Screen_tintPicture      = Game_Screen.prototype.tintPicture;
     Game_Screen.prototype.tintPicture = function(pictureId, tone, duration) {
         if (this._pictureTargetAll > 0) {
             this.iteratePictures(function(picture) {
@@ -332,11 +360,11 @@
         }
     };
 
-    var _Game_Screen_erasePicture = Game_Screen.prototype.erasePicture;
+    var _Game_Screen_erasePicture      = Game_Screen.prototype.erasePicture;
     Game_Screen.prototype.erasePicture = function(pictureId) {
         if (this._pictureTargetAll) {
             this.iteratePictures(function(picture, pictureId) {
-                var realPictureId = this.realPictureId(pictureId);
+                var realPictureId             = this.realPictureId(pictureId);
                 this._pictures[realPictureId] = null;
             }.bind(this));
             this._pictureTargetAll = 0;
@@ -373,6 +401,20 @@
         }
     };
 
+    Game_Screen.prototype.shakePicture = function(pictureId, power, speed, rotation, duration) {
+        if (this._pictureTargetAll > 0) {
+            this.iteratePictures(function(picture) {
+                picture.shake(power, speed, rotation, duration);
+            }.bind(this));
+            this._pictureTargetAll = 0;
+        } else {
+            var picture = this.picture(pictureId);
+            if (picture) {
+                picture.shake(power, speed, rotation, duration);
+            }
+        }
+    };
+
     Game_Screen.prototype.iteratePictures = function(callBack) {
         for (var i = 1, n = this.maxPictures(); i <= n; i++) {
             var picture = this.picture(i);
@@ -393,7 +435,7 @@
         }
     };
 
-    var _Game_Screen_maxPictures = Game_Screen.prototype.maxPictures;
+    var _Game_Screen_maxPictures      = Game_Screen.prototype.maxPictures;
     Game_Screen.prototype.maxPictures = function() {
         var max = getParamNumber('ピクチャ表示最大数', 0);
         return max > 0 ? max : _Game_Screen_maxPictures.apply(this, arguments);
@@ -403,19 +445,29 @@
     // Game_Picture
     //  ファイル名の動的生成処理を追加定義します。
     //=============================================================================
-    var _Game_Picture_show = Game_Picture.prototype.show;
+    var _Game_Picture_show      = Game_Picture.prototype.show;
     Game_Picture.prototype.show = function(name, origin, x, y, scaleX,
                                            scaleY, opacity, blendMode) {
         if ($gameScreen.dPictureFileName != null) {
-            arguments[0] = $gameScreen.dPictureFileName;
+            arguments[0]                 = $gameScreen.dPictureFileName;
             $gameScreen.dPictureFileName = null;
         }
         _Game_Picture_show.apply(this, arguments);
     };
 
+    var _Game_Picture_x      = Game_Picture.prototype.x;
+    Game_Picture.prototype.x = function() {
+        return _Game_Picture_x.apply(this, arguments) + this.getShakeX();
+    };
+
+    var _Game_Picture_y      = Game_Picture.prototype.y;
+    Game_Picture.prototype.y = function() {
+        return _Game_Picture_y.apply(this, arguments) + this.getShakeY();
+    };
+
     Game_Picture.prototype.spin = function(targetRotation, duration) {
         this._targetRotation = targetRotation;
-        this._angle = (this._angle % 360);
+        this._angle          = (this._angle % 360);
         if (duration === 0) {
             this._angle = this._targetRotation;
         } else {
@@ -428,18 +480,57 @@
         this.spin(this._angle + targetRelativeRotation, duration);
     };
 
-    var _Game_Picture_update = Game_Picture.prototype.update;
+    Game_Picture.prototype.shake = function(power, speed, rotation, duration) {
+        this.stopShake();
+        this._shakePower     = power;
+        this._shakeSpeed     = speed;
+        this._shakeDuration  = duration;
+        this._shakeRotation  = rotation * Math.PI / 180;
+    };
+
+    Game_Picture.prototype.stopShake = function() {
+        this._shakeDirection = 1;
+        this._shake          = 0;
+    };
+
+    var _Game_Picture_update      = Game_Picture.prototype.update;
     Game_Picture.prototype.update = function() {
         _Game_Picture_update.apply(this, arguments);
         this.updateSpin();
+        this.updateShake();
     };
 
     Game_Picture.prototype.updateSpin = function() {
         if (this._spinDuration > 0) {
-            var d = this._spinDuration;
+            var d       = this._spinDuration;
             this._angle = (this._angle * (d - 1) + this._targetRotation) / d;
             this._spinDuration--;
         }
     };
 
+    Game_Picture.prototype.updateShake = function() {
+        if (this._shakeDuration > 0) {
+            var delta = (this._shakePower * this._shakeSpeed * this._shakeDirection) / 10;
+            if (this._shakeDuration <= 1 && this._shake * (this._shake + delta) < 0) {
+                this._shake = 0;
+            } else {
+                this._shake += delta;
+            }
+            if (this._shake > this._shakePower * 2) {
+                this._shakeDirection = -1;
+            }
+            if (this._shake < -this._shakePower * 2) {
+                this._shakeDirection = 1;
+            }
+            this._shakeDuration--;
+        }
+    };
+
+    Game_Picture.prototype.getShakeX = function() {
+        return this._shake ? this._shake * Math.cos(this._shakeRotation) : 0;
+    };
+
+    Game_Picture.prototype.getShakeY = function() {
+        return this._shake ? this._shake * Math.sin(this._shakeRotation) : 0;
+    };
 })();
