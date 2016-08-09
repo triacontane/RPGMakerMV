b7e5246c833b685597b094acf74c206ad4a3be4f
トリアコンタン <triacontanetakenoko@gmail.com>
2016-08-09 20:22:07 +0900

    update

---
+++ EventReSpawn.js
@@ -0,0 +1,266 @@
+//=============================================================================
+// EventReSpawn.js
+// ----------------------------------------------------------------------------
+// Copyright (c) 2015-2016 Triacontane
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+// ----------------------------------------------------------------------------
+// Version
+// 1.0.0 2016/08/08 初版
+// ----------------------------------------------------------------------------
+// [Blog]   : http://triacontane.blogspot.jp/
+// [Twitter]: https://twitter.com/triacontane/
+// [GitHub] : https://github.com/triacontane/
+//=============================================================================
+
+/*:
+ * @plugindesc イベント動的生成プラグイン
+ * @author トリアコンタン
+ *
+ * @help イベントをコピーして動的に生成します。
+ * コピーした一時イベントは、イベントコマンド「イベントの一時消去」
+ * によって完全に削除することができます。
+ * セルフスイッチは個別に管理され、生成されるたびに初期化されます。
+ *
+ * プラグインコマンド詳細
+ *  イベントコマンド「プラグインコマンド」から実行。
+ *  （パラメータの間は半角スペースで区切る）
+ *
+ * ERS_生成 1 5 10 # イベントID[1]のイベントをコピーしてX[5] Y[10]に配置
+ * ERS_MAKE 1 5 10 # 同上
+ *
+ * 利用規約：
+ *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
+ *  についても制限はありません。
+ *  このプラグインはもうあなたのものです。
+ */
+
+(function() {
+    'use strict';
+    var metaTagPrefix = 'ERS_';
+
+    var getCommandName = function(command) {
+        return (command || '').toUpperCase();
+    };
+
+    var getArgNumber = function(arg, min, max) {
+        if (arguments.length < 2) min = -Infinity;
+        if (arguments.length < 3) max = Infinity;
+        return (parseInt(convertEscapeCharacters(arg), 10) || 0).clamp(min, max);
+    };
+
+    var convertEscapeCharacters = function(text) {
+        if (text == null) text = '';
+        var windowLayer = SceneManager._scene._windowLayer;
+        return windowLayer ? windowLayer.children[0].convertEscapeCharacters(text) : text;
+    };
+
+    //=============================================================================
+    // Game_Interpreter
+    //  プラグインコマンドを追加定義します。
+    //=============================================================================
+    var _Game_Interpreter_pluginCommand      = Game_Interpreter.prototype.pluginCommand;
+    Game_Interpreter.prototype.pluginCommand = function(command, args) {
+        _Game_Interpreter_pluginCommand.apply(this, arguments);
+        var commandPrefix = new RegExp('^' + metaTagPrefix);
+        if (!command.match(commandPrefix)) return;
+        try {
+            this.pluginCommandEventReSpawn(command.replace(commandPrefix, ''), args);
+        } catch (e) {
+            if ($gameTemp.isPlaytest() && Utils.isNwjs()) {
+                var window = require('nw.gui').Window.get();
+                if (!window.isDevToolsOpen()) {
+                    var devTool = window.showDevTools();
+                    devTool.moveTo(0, 0);
+                    devTool.resizeTo(window.screenX + window.outerWidth, window.screenY + window.outerHeight);
+                    window.focus();
+                }
+            }
+            console.log('プラグインコマンドの実行中にエラーが発生しました。');
+            console.log('- コマンド名 　: ' + command);
+            console.log('- コマンド引数 : ' + args);
+            console.log('- エラー原因   : ' + e.stack || e.toString());
+        }
+    };
+
+    Game_Interpreter.prototype.pluginCommandEventReSpawn = function(command, args) {
+        switch (getCommandName(command)) {
+            case '生成' :
+            case 'MAKE' :
+                $gameMap.spawnEvent(getArgNumber(args[0], 1), getArgNumber(args[1], 1), getArgNumber(args[2], 1));
+                break;
+        }
+    };
+
+    //=============================================================================
+    // Game_Map
+    //  イベントのスポーン処理を追加定義します。
+    //=============================================================================
+    var _Game_Map_setupEvents = Game_Map.prototype.setupEvents;
+    Game_Map.prototype.setupEvents = function() {
+        _Game_Map_setupEvents.apply(this, arguments);
+        this._eventIdSequence = this._events.length;
+    };
+
+    Game_Map.prototype.spawnEvent = function(originalEventId, x, y) {
+        if (this.event(originalEventId) && $gameMap.isValid(x, y)) {
+            var eventId = this.getEventIdSequence();
+            var event = new Game_PrefabEvent(this._mapId, eventId, originalEventId, x, y);
+            this._events.push(event);
+        } else {
+            throw new Error('無効なイベントIDもしくは座標のためイベントを作成できませんでした。');
+        }
+    };
+
+    var _Game_Map_eraseEvent = Game_Map.prototype.eraseEvent;
+    Game_Map.prototype.eraseEvent = function(eventId) {
+        _Game_Map_eraseEvent.apply(this, arguments);
+        if (this._events[eventId].isExtinct()) {
+            this._events[eventId] = undefined;
+        }
+    };
+
+    Game_Map.prototype.getEventIdSequence = function() {
+        return this._eventIdSequence++;
+    };
+
+    Game_Map.prototype.getPrefabEvents = function() {
+        return this.events().filter(function(event) {
+            return event.isPrefab();
+        });
+    };
+
+    Game_Map.prototype.resetSelfSwitchForPrefabEvent = function() {
+        this.getPrefabEvents().forEach(function(prefabEvent) {
+            prefabEvent.eraseSelfSwitch();
+        });
+    };
+
+    //=============================================================================
+    // Game_CharacterBase
+    //   プレハブイベントとそうでないキャラクターを区別します。
+    //=============================================================================
+    Game_CharacterBase.prototype.isPrefab = function() {
+        return false;
+    };
+
+    Game_CharacterBase.prototype.isExtinct = function() {
+        return this.isPrefab() && this._erased;
+    };
+
+    //=============================================================================
+    // Game_PrefabEvent
+    //  動的に生成されるイベントオブジェクトです。
+    //=============================================================================
+    function Game_PrefabEvent() {
+        this.initialize.apply(this, arguments);
+    }
+
+    Game_PrefabEvent.prototype.constructor = Game_PrefabEvent;
+    Game_PrefabEvent.prototype = Object.create(Game_Event.prototype);
+
+    Game_PrefabEvent.prototype.initialize = function(mapId, eventId, originalEventId, x, y) {
+        $dataMap.events[eventId] = $dataMap.events[originalEventId];
+        Game_Event.prototype.initialize.call(this, mapId, eventId);
+        this.locate(x, y);
+        this._spritePrepared = false;
+    };
+
+    Game_PrefabEvent.prototype.isPrefab = function() {
+        return true;
+    };
+
+    Game_PrefabEvent.prototype.erase = function() {
+        Game_Event.prototype.erase.call(this);
+        this.eraseSelfSwitch();
+        delete $dataMap.events[this._eventId];
+    };
+
+    Game_PrefabEvent.prototype.isSpritePrepared = function() {
+        return this._spritePrepared;
+    };
+
+    Game_PrefabEvent.prototype.setSpritePrepared = function() {
+        this._spritePrepared = true;
+    };
+
+    Game_PrefabEvent.prototype.eraseSelfSwitch = function() {
+        ['A', 'B', 'C', 'D'].forEach(function(swCode) {
+            var key = [this._mapId, this._eventId, swCode];
+            $gameSelfSwitches.setValue(key, undefined);
+        }.bind(this));
+    };
+
+    //=============================================================================
+    // Sprite
+    //  SpriteIDの付与に使用するカウンターを取得します。
+    //=============================================================================
+    Sprite.getCounter = function() {
+        return this._counter;
+    };
+
+    //=============================================================================
+    // Sprite_Character
+    //  対象キャラクターの消去判定を追加定義します。
+    //=============================================================================
+    Sprite_Character.prototype.isCharacterExtinct = function() {
+        return this._character.isExtinct();
+    };
+
+    //=============================================================================
+    // Spriteset_Map
+    //  プレハブイベントのスプライトを管理します。
+    //=============================================================================
+    var _Spriteset_Map_createCharacters = Spriteset_Map.prototype.createCharacters;
+    Spriteset_Map.prototype.createCharacters = function() {
+        this._prefabSpriteId = Sprite.getCounter() + 1;
+        _Spriteset_Map_createCharacters.apply(this, arguments);
+    };
+
+    var _Spriteset_Map_update = Spriteset_Map.prototype.update;
+    Spriteset_Map.prototype.update = function() {
+        _Spriteset_Map_update.apply(this, arguments);
+        this.updatePrefabEvent();
+    };
+
+    Spriteset_Map.prototype.updatePrefabEvent = function() {
+        $gameMap.getPrefabEvents().forEach(function(event) {
+            if (!event.isSpritePrepared()) {
+                this.makePrefabEvent(event);
+            }
+        }.bind(this));
+        for (var i = 0; i < this._characterSprites.length; i++) {
+            var sprite = this._characterSprites[i];
+            if (!sprite) p (i + ':' + this._characterSprites.length);
+            if (sprite.isCharacterExtinct()) {
+                this.removePrefabEvent(i);
+                i--;
+            }
+        }
+    };
+
+    Spriteset_Map.prototype.makePrefabEvent = function(event) {
+        event.setSpritePrepared();
+        var sprite = new Sprite_Character(event);
+        sprite.spriteId = this._prefabSpriteId;
+        this._characterSprites.push(sprite);
+        this._tilemap.addChild(sprite);
+    };
+
+    Spriteset_Map.prototype.removePrefabEvent = function(index) {
+        var sprite = this._characterSprites[index];
+        this._characterSprites.splice(index, 1);
+        this._tilemap.removeChild(sprite);
+    };
+
+    //=============================================================================
+    // Scene_Map
+    //  場所移動時にセルフスイッチの情報を初期化します。
+    //=============================================================================
+    var _Scene_Map_create = Scene_Map.prototype.create;
+    Scene_Map.prototype.create = function() {
+        $gameMap.resetSelfSwitchForPrefabEvent();
+        _Scene_Map_create.apply(this, arguments);
+    };
+})();
+
