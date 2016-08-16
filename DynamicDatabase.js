0275450d4bc7dbfb4f3994fa17d553113613cc43
triacontane <triacontanetakenoko@gmail.com>
2016-08-16 22:14:24 +0900

    update

--- DynamicDatabase.js
+++ DynamicDatabase.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2016/08/15 コード整形、ドロップアイテムにドロップアイテムIDの定義が抜けていたのを修正
 // 1.0.2 2016/05/16 特徴と効果にデータIDの定義を追加
 // 1.0.1 2016/01/03 iterateとisEmptyが重複定義されないよう対応
 // 1.0.0 2015/12/28 初版
@@ -54,7 +55,7 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function () {
+(function() {
     'use strict';
 
     //=============================================================================
@@ -63,8 +64,8 @@
     //=============================================================================
     if (!Object.prototype.hasOwnProperty('iterate')) {
         Object.defineProperty(Object.prototype, 'iterate', {
-            value : function (handler) {
-                Object.keys(this).forEach(function (key, index) {
+            value: function(handler) {
+                Object.keys(this).forEach(function(key, index) {
                     handler.call(this, key, this[key], index);
                 }, this);
             }
@@ -73,21 +74,22 @@
 
     if (!Object.prototype.hasOwnProperty('isEmpty')) {
         Object.defineProperty(Object.prototype, 'isEmpty', {
-            value : function () {
+            value: function() {
                 return Object.keys(this).length <= 0;
             }
         });
     }
 
     Number.prototype.times = function(handler) {
-        var i = 0; while(i < this) handler.call(this, i++);
+        var i = 0;
+        while (i < this) handler.call(this, i++);
     };
 
     //=============================================================================
     // Scene_Boot
     //  ダイナミックデータベースの構築開始
     //=============================================================================
-    var _Scene_Boot_start = Scene_Boot.prototype.start;
+    var _Scene_Boot_start      = Scene_Boot.prototype.start;
     Scene_Boot.prototype.start = function() {
         _Scene_Boot_start.call(this);
         DynamicDatabaseManager.makeDynamicDatabase();
@@ -103,82 +105,82 @@ function DynamicDatabaseManager() {
 }
 
 DynamicDatabaseManager._columnMapper = null;
-DynamicDatabaseManager._targetData = null;
+DynamicDatabaseManager._targetData   = null;
 
 DynamicDatabaseManager._columnMapperCoomon = {
-    animationId : 'アニメーション',
-    hitType : '命中タイプ',
-    iconIndex : 'アイコン',
-    occasion : '使用可能時',
-    repeats : '連続回数',
-    scope:'範囲',
-    speed:'速度補正',
-    successRate:'成功率',
-    tpCost:'消費TP',
-    tpGain:'得TP',
-    mpCost:'消費MP',
-    damage_elementId:'属性',
-    damage_type:'タイプ',
-    damage_variance:'分散度',
-    price:'価格',
-    params_0:'最大HP',
-    params_1:'最大MP',
-    params_2:'攻撃力',
-    params_3:'防御力',
-    params_4:'魔法力',
-    params_5:'魔法防御',
-    params_6:'敏捷性',
-    params_7:'運'
+    animationId     : 'アニメーション',
+    hitType         : '命中タイプ',
+    iconIndex       : 'アイコン',
+    occasion        : '使用可能時',
+    repeats         : '連続回数',
+    scope           : '範囲',
+    speed           : '速度補正',
+    successRate     : '成功率',
+    tpCost          : '消費TP',
+    tpGain          : '得TP',
+    mpCost          : '消費MP',
+    damage_elementId: '属性',
+    damage_type     : 'タイプ',
+    damage_variance : '分散度',
+    price           : '価格',
+    params_0        : '最大HP',
+    params_1        : '最大MP',
+    params_2        : '攻撃力',
+    params_3        : '防御力',
+    params_4        : '魔法力',
+    params_5        : '魔法防御',
+    params_6        : '敏捷性',
+    params_7        : '運'
 };
 
 DynamicDatabaseManager._columnMapperSkills = {
-    stypeId:'スキルタイプ',
-    requiredWtypeId1:'武器タイプ1',
-    requiredWtypeId2:'武器タイプ2'
+    stypeId         : 'スキルタイプ',
+    requiredWtypeId1: '武器タイプ1',
+    requiredWtypeId2: '武器タイプ2'
 };
 
 DynamicDatabaseManager._columnMapperItems = {
-    itypeId:'アイテムタイプ',
-    consumable:'消耗'
+    itypeId   : 'アイテムタイプ',
+    consumable: '消耗'
 };
 
 DynamicDatabaseManager._columnMapperWeapons = {
-    wtypeId:'武器タイプ'
+    wtypeId: '武器タイプ'
 };
 
 DynamicDatabaseManager._columnMapperArmors = {
-    atypeId:'防具タイプ',
-    etypeId:'装備タイプ'
+    atypeId: '防具タイプ',
+    etypeId: '装備タイプ'
 };
 
 DynamicDatabaseManager._columnMapperEnemies = {
-    battlerHue : '色相',
-    exp : '経験値',
-    gold : '所持金'
+    battlerHue: '色相',
+    exp       : '経験値',
+    gold      : '所持金'
 };
 
 DynamicDatabaseManager._columnMapperStates = {
-    autoRemovalTiming:'自動解除のタイミング',
-    chanceByDamage:'ダメージで解除_ダメージ',
-    maxTurns:'継続ターン数_最大',
-    minTurns:'継続ターン数_最小',
-    motion:'[SV] モーション',
-    overlay:'[SV] 重ね合わせ',
-    priority: '優先度',
-    removeAtBattleEnd:'戦闘終了時に解除',
-    removeByDamage: 'ダメージで解除',
+    autoRemovalTiming  : '自動解除のタイミング',
+    chanceByDamage     : 'ダメージで解除_ダメージ',
+    maxTurns           : '継続ターン数_最大',
+    minTurns           : '継続ターン数_最小',
+    motion             : '[SV] モーション',
+    overlay            : '[SV] 重ね合わせ',
+    priority           : '優先度',
+    removeAtBattleEnd  : '戦闘終了時に解除',
+    removeByDamage     : 'ダメージで解除',
     removeByRestriction: '行動制約で解除',
-    removeByWalking: '歩数で解除',
-    restriction: '行動制約',
-    stepsToRemove: '歩数で解除_歩数'
+    removeByWalking    : '歩数で解除',
+    restriction        : '行動制約',
+    stepsToRemove      : '歩数で解除_歩数'
 };
 
 DynamicDatabaseManager._targetDynamicDatabase = {
     $dataSkills : DynamicDatabaseManager._columnMapperSkills,
-    $dataItems : DynamicDatabaseManager._columnMapperItems,
-    $dataWeapons : DynamicDatabaseManager._columnMapperWeapons,
+    $dataItems  : DynamicDatabaseManager._columnMapperItems,
+    $dataWeapons: DynamicDatabaseManager._columnMapperWeapons,
     $dataArmors : DynamicDatabaseManager._columnMapperArmors,
-    $dataEnemies : DynamicDatabaseManager._columnMapperEnemies,
+    $dataEnemies: DynamicDatabaseManager._columnMapperEnemies,
     $dataStates : DynamicDatabaseManager._columnMapperStates
 };
 
@@ -210,6 +212,7 @@ DynamicDatabaseManager._setColumnMapperDynamic = function() {
     }.bind(this));
     Number(3).times(function(i) {
         this._columnMapperEnemies['dropItems_%1_denominator'.format(i)] = 'ドロップアイテム%1_ドロップアイテム'.format(i + 1);
+        this._columnMapperEnemies['dropItems_%1_dataId'.format(i)]      = 'ドロップアイテム%1_ドロップアイテムID'.format(i + 1);
         this._columnMapperEnemies['dropItems_%1_kind'.format(i)]        = 'ドロップアイテム%1_出現率'.format(i + 1);
     }.bind(this));
 };
@@ -230,7 +233,7 @@ DynamicDatabaseManager._makeDynamicData = function(dataArray, columnMap) {
     dataArray.forEach(function(data) {
         if (data != null) {
             this._targetData = data;
-            data.iterate(function(key, value) {this._makeProperty(data, key, key, value)}.bind(this));
+            data.iterate(function(key, value) {this._makeProperty(data, key, key, value);}.bind(this));
         }
     }.bind(this));
 };
@@ -246,7 +249,7 @@ DynamicDatabaseManager._makeProperty = function(parent, keyPath, key, child) {
         case 'number':
             if (parent.meta.isEmpty()) return;
             var propName = this._columnMapper[keyPath] || this._columnMapperCoomon[keyPath];
-            var metaTag = 'DD' + propName;
+            var metaTag  = 'DD' + propName;
             if (propName != null && parent.meta[metaTag] != null)
                 this._makePropertyFormula(parent, key, child, metaTag);
             parent[propName] = child;
@@ -264,9 +267,9 @@ DynamicDatabaseManager._makeProperty = function(parent, keyPath, key, child) {
 DynamicDatabaseManager._makePropertyFormula = function(parent, key, child, metaTag) {
     var data = this._targetData, データ = this._targetData;
     Object.defineProperty(parent, key, {
-        get: function() {
+        get         : function() {
             try {
-                var prev = child, 元の値 = child;
+                var prev   = child, 元の値 = child;
                 var result = eval(DynamicDatabaseManager._convertEscapeCharacters(this.meta[metaTag]));
                 return typeof child === 'number' ? DynamicDatabaseManager._parseIntStrict(result) : result == true;
             } catch (e) {
@@ -279,7 +282,7 @@ DynamicDatabaseManager._makePropertyFormula = function(parent, key, child, metaT
 
 DynamicDatabaseManager._makePropertyString = function(parent, key, child) {
     Object.defineProperty(parent, key, {
-        get: function() {
+        get         : function() {
             return DynamicDatabaseManager._convertEscapeCharacters(child);
         },
         configurable: false
@@ -300,12 +303,12 @@ DynamicDatabaseManager._convertEscapeCharacters = function(text) {
         return $gameSwitches.value(parseInt(arguments[1], 10)) ? '1' : '0';
     });
     text = text.replace(/\x1bN\[(\d+)\]/gi, function() {
-        var n = parseInt(arguments[1]);
+        var n     = parseInt(arguments[1]);
         var actor = n >= 1 ? $gameActors.actor(n) : null;
         return actor ? actor.name() : '';
     });
     text = text.replace(/\x1bP\[(\d+)\]/gi, function() {
-        var n = parseInt(arguments[1]);
+        var n     = parseInt(arguments[1]);
         var actor = n >= 1 ? $gameParty.members()[n - 1] : null;
         return actor ? actor.name() : '';
     });
