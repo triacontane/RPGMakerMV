21236cff5f28975c2fb9e86cbd2ad9d042541c4e
トリアコンタン <triacontanetakenoko@gmail.com>
2016-10-25 04:48:16 +0900

    update

--- BattleLogToMessage.js
+++ BattleLogToMessage.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.3.0 2016/10/25 バトルイベント実行中などでログウィンドウを閉じることのできる機能を追加
 // 1.2.0 2016/10/18 メッセージスピードの調整機能と、行動終了後に追加ウェイトする機能を追加
 // 1.1.0 2016/10/14 ダメージのポップアップを抑制する機能を追加
 // 1.0.0 2016/10/12 初版
@@ -63,7 +64,19 @@
  *
  * @help バトルログを画面下部のメッセージウィンドウ内に表示するよう変更します。
  *
- * このプラグインにはプラグインコマンドはありません。
+ * プラグインコマンド詳細
+ *  イベントコマンド「プラグインコマンド」から実行。
+ *  （パラメータの間は半角スペースで区切る）
+ *
+ *  BLM_バトルログ閉じる
+ *  BLM_BATTLE_LOG_CLOSE
+ *
+ *  バトルログのウィンドウを閉じます。バトルイベントから実行します。
+ *
+ *  BLM_バトルログ開く
+ *  BLM_BATTLE_LOG_OPEN
+ *
+ *  バトルログのウィンドウを再度開きます。バトルイベントから実行します。
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -74,6 +87,11 @@
 (function() {
     'use strict';
     var pluginName = 'BattleLogToMessage';
+    var metaTagPrefix = 'BLM';
+
+    var getCommandName = function(command) {
+        return (command || '').toUpperCase();
+    };
 
     var getParamOther = function(paramNames) {
         if (!Array.isArray(paramNames)) paramNames = [paramNames];
@@ -110,7 +128,7 @@
         var commandPrefix = new RegExp('^' + metaTagPrefix);
         if (!command.match(commandPrefix)) return;
         try {
-            this.pluginCommandBackLogWithEffect(command.replace(commandPrefix, ''), args);
+            this.pluginCommandBattleLogToMessage(command.replace(commandPrefix, ''), args);
         } catch (e) {
             if ($gameTemp.isPlaytest() && Utils.isNwjs()) {
                 var window = require('nw.gui').Window.get();
@@ -128,11 +146,35 @@
         }
     };
 
-    Game_Interpreter.prototype.pluginCommandBackLogWithEffect = function(command, args) {
+    Game_Interpreter.prototype.pluginCommandBattleLogToMessage = function(command) {
         switch (getCommandName(command)) {
-            case 'XXXXX' :
+            case '_BATTLE_LOG_OPEN' :
+            case '_バトルログ開く' :
+                BattleManager.setBattleLogClose(false);
+                break;
+            case '_BATTLE_LOG_CLOSE' :
+            case '_バトルログ閉じる' :
+                BattleManager.setBattleLogClose(true);
                 break;
         }
+    };
+
+    //=============================================================================
+    // BattleManager
+    //  ウィンドウの開閉状態を管理します。
+    //=============================================================================
+    var _BattleManager_initMembers = BattleManager.initMembers;
+    BattleManager.initMembers = function() {
+        _BattleManager_initMembers.apply(this, arguments);
+        this._battleLogClose = false;
+    };
+
+    BattleManager.setBattleLogClose = function(value) {
+        this._battleLogClose = value;
+    };
+
+    BattleManager.isBattleLogClose = function() {
+        return this._battleLogClose;
     };
 
     //=============================================================================
@@ -162,7 +204,7 @@
 
     var _Scene_Battle_updateWindowPositions      = Scene_Battle.prototype.updateWindowPositions;
     Scene_Battle.prototype.updateWindowPositions = function() {
-        if (BattleManager.isInputting()) {
+        if (BattleManager.isInputting() || BattleManager.isBattleLogClose()) {
             this._logWindow.close();
         } else {
             this._logWindow.open();
