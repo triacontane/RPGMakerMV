a181e6d3956e562c05be864b52e3ba0aff22f74f
トリアコンタン <triacontanetakenoko@gmail.com>
2016-11-20 15:32:44 +0900

    update

--- FixImageLoading.js
+++ FixImageLoading.js
@@ -6,6 +6,8 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.1 2016/11/20 ロード完了時にframeが更新されない不具合を修正
+//                  ロード中にframeが変更された場合に、ロード完了まで反映を遅らせる仕様を追加
 // 1.1.0 2016/11/16 liply_GC.jsとの競合を解消 by 奏 ねこま様
 // 1.0.0 2016/05/02 初版
 // ----------------------------------------------------------------------------
@@ -42,22 +44,40 @@
             if (value) {
                 value.__liply_attachedToScene = true;               // added by nekoma
                 if (!this._bitmap) {
-                    this._bitmap = value;
-                    this._bitmap.__liply_attachedToScene = false;   // added by nekoma
-                    this.setFrame(0, 0, 0, 0);
+                    this._setBitmapObject(value);
                 }
+                this._changeingBitmap = value;
                 value.addLoadListener(function () {
+                    this._changeingBitmap = null;
                     if (!this._bitmap) return;
-                    this._bitmap = value;
-                    this._bitmap.__liply_attachedToScene = false;   // added by nekoma
+                    this._setBitmapObject(value);
                     this._onBitmapLoad();
                 }.bind(this));
             } else {
-                this._bitmap = value;
+                this._bitmap = null;
                 this.texture.frame = Rectangle.emptyRectangle;      // modified by nekoma
             }
         },
         configurable: true
     });
+
+    /**
+     * @method _setBitmapObject
+     * @private
+     */
+    Sprite.prototype._setBitmapObject = function(bitmap) {
+        this._bitmap = bitmap;
+        this._bitmap.__liply_attachedToScene = false;   // added by nekoma
+        this.setFrame(0, 0, 0, 0);
+    };
+
+    var _Sprite_setFrame = Sprite.prototype.setFrame;
+    Sprite.prototype.setFrame = function(x, y, width, height) {
+        if (this._changeingBitmap) {
+            this._changeingBitmap.addLoadListener(_Sprite_setFrame.bind(this, x, y, width, height));
+        } else {
+            _Sprite_setFrame.apply(this, arguments);
+        }
+    };
 })();
 
