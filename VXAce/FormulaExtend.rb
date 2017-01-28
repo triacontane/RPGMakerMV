d69ef911819491c8aedad47538873a1cb310f4ce
triacontane <triacontanetakenoko@gmail.com>
2017-01-28 16:17:07 +0900

    update

--- VXAce/FormulaExtend.rb
+++ VXAce/FormulaExtend.rb
@@ -5,27 +5,27 @@
 # 　計算式から使用できる関数群です。要望により随時追加します。
 #
 # ●使い方
-# 1. スキルの計算式から呼び出します。例(a.additional_atk)
+# 1. スキルの計算式から呼び出します。例(a.plus_atk)
 #
 # ●関数一覧
-#  1. additional_atk(percent, value)
+#  1. plus_atk(percent, value)
 #   [percent]の確率で元の攻撃力に[value]を加算して計算します。
-#  ex:a.additional_atk(50, 100) # 50%の確率で実行者の攻撃力を100加算します。
+#  ex:a.plus_atk(50, 100) # 50%の確率で実行者の攻撃力を100加算します。
 #
-#  2. additional_def(percent, value)
+#  2. plus_def(percent, value)
 #   [percent]の確率で元の防御力に[value]を加算して計算します。
-#  ex:b.additional_def(30, -100) # 30%の確率で対象者の防御力を100減算します。
+#  ex:b.plus_def(30, -100) # 30%の確率で対象者の防御力を100減算します。
 #
-#  3. additional_mat(percent, value)
+#  3. plus_mat(percent, value)
 #   [percent]の確率で元の魔法力に[value]を加算して計算します。
 #
-#  4. additional_mdf(percent, value)
+#  4. plus_mdf(percent, value)
 #   [percent]の確率で元の魔法防御力に[value]を加算して計算します。
 #
-#  5. additional_agi(percent, value)
+#  5. plus_agi(percent, value)
 #   [percent]の確率で元の敏捷性に[value]を加算して計算します。
 #
-#  6. additional_luk(percent, value)
+#  6. plus_luk(percent, value)
 #   [percent]の確率で元の運に[value]を加算して計算します。
 #
 # ●利用規約
@@ -49,43 +49,43 @@ class Game_Battler < Game_BattlerBase
   #--------------------------------------------------------------------------
   # ● 追加パラメータ
   #--------------------------------------------------------------------------
-  def additional_param(original, percent, adjustment)
+  def plus_param(original, percent, adjustment)
     return [original + (rand(100) < percent ? adjustment : 0), 0].max
   end
   #--------------------------------------------------------------------------
   # ● 追加攻撃力
   #--------------------------------------------------------------------------
-  def additional_atk(percent, adjustment)
-    return additional_param(self.atk, percent, adjustment)
+  def plus_atk(percent, adjustment)
+    return plus_param(self.atk, percent, adjustment)
   end
   #--------------------------------------------------------------------------
   # ● 追加防御力
   #--------------------------------------------------------------------------
-  def additional_def(percent, adjustment)
-    return additional_param(self.def, percent, adjustment)
+  def plus_def(percent, adjustment)
+    return plus_param(self.def, percent, adjustment)
   end
   #--------------------------------------------------------------------------
   # ● 追加魔法力
   #--------------------------------------------------------------------------
-  def additional_mat(percent, adjustment)
-    return additional_param(self.mat, percent, adjustment)
+  def plus_mat(percent, adjustment)
+    return plus_param(self.mat, percent, adjustment)
   end
   #--------------------------------------------------------------------------
   # ● 追加魔法防御力
   #--------------------------------------------------------------------------
-  def additional_mdf(percent, adjustment)
-    return additional_param(self.mdf, percent, adjustment)
+  def plus_mdf(percent, adjustment)
+    return plus_param(self.mdf, percent, adjustment)
   end
   #--------------------------------------------------------------------------
   # ● 追加敏捷性
   #--------------------------------------------------------------------------
-  def additional_agi(percent, adjustment)
-    return additional_param(self.agi, percent, adjustment)
+  def plus_agi(percent, adjustment)
+    return plus_param(self.agi, percent, adjustment)
   end
   #--------------------------------------------------------------------------
   # ● 追加運
   #--------------------------------------------------------------------------
-  def additional_luk(percent, adjustment)
-    return additional_param(self.luk, percent, adjustment)
+  def plus_luk(percent, adjustment)
+    return plus_param(self.luk, percent, adjustment)
   end
 end
