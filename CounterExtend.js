4127c8cf001404a9bbe6ec52d566eb24770e1101
トリアコンタン <triacontanetakenoko@gmail.com>
2017-04-09 11:34:04 +0900

    update

--- CounterExtend.js
+++ CounterExtend.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.3.0 2017/04/09 反撃に成功した時点で相手の行動をキャンセルできる機能を追加
 // 1.2.2 2017/02/07 端末依存の記述を削除
 // 1.2.1 2017/01/12 メモ欄の値が空で設定された場合にエラーが発生するかもしれない問題を修正
 // 1.2.0 2016/11/27 反撃スキルIDを複数設定できる機能を追加。条件に応じたスキルで反撃します。
@@ -60,6 +61,7 @@
  *
  * 3. 反撃条件をJavaScript計算式の評価結果を利用できます。
  * 反撃条件を満たさない場合は反撃は実行されません。
+ * 特定のスキルに対してのみ反撃したり、特定の条件下でのみ反撃したりできます。
  * 特徴を有するメモ欄に以下の通り入力してください。
  *
  * <CE_反撃条件:v(1) &lt; 100>    # 変数[1]が100より小さければ反撃します。
@@ -68,8 +70,8 @@
  * <CE_MagicCounterCond:s(1)>     # 同上
  *
  * 実行したスキルの情報はローカル変数「skill」で参照できます。
- * <CE_反撃条件:skill.id === 10>              # スキルIDが[10]なら反撃します。
- * <CE_反撃条件:skill.damage.elementId === 3> # スキル属性が[3]なら反撃します。
+ * <CE_反撃条件:skill.id === 10> # スキルIDが[10]なら反撃します。
+ * <CE_反撃条件:elementId === 3> # スキル属性IDが[3]なら反撃します。
  *
  * 対象のバトラー情報は「this」で、相手のバトラー情報は「target」で参照できます。
  * <CE_反撃条件:this.hpRate() &lt; 0.5> # 自分のHPが50%を下回ると反撃します。
@@ -78,10 +80,30 @@
  * < → &lt;
  * > → &gt;
  *
- * 4. 反撃実行時に専用のアニメーションIDを再生します。
+ * 4. 複数の反撃スキルおよび反撃条件を設定できます。
+ * 複数定義する場合は、末尾に「_1」を付与してください。
+ * <CE_反撃スキルID:4>   # スイッチ[1]がONならスキルID[4]で反撃
+ * <CE_反撃条件:s(1)>    #
+ * <CE_反撃スキルID_1:5> # スイッチ[2]がONならスキルID[5]で反撃
+ * <CE_反撃条件_1:s(2)>  #
+ * <CE_反撃スキルID_2:6> # スイッチ[3]がONならスキルID[6]で反撃
+ * <CE_反撃条件_2:s(3)>  #
+ *
+ * 複数の条件を一度に満たした場合は、インデックスの小さい方が
+ * 優先して使用されます。
+ *
+ * 5. 反撃実行時に専用のアニメーションIDを再生します。
  * 特徴を有するメモ欄に以下の通り入力してください。
  * <CE_反撃アニメID:20>       # 反撃実行前にアニメーション[20]を再生します。
  * <CE_CounterAnimationId:20> # 同上
+ *
+ * 6. 反撃成功時に相手の行動をキャンセル（中断）できます。
+ * 相手が全体攻撃のスキルを使った場合に、反撃成功時点で以降の相手には
+ * 当たらなくなります。特徴を有するメモ欄に以下の通り入力してください。
+ * <CE_キャンセル> # 反撃成功時に相手の行動をキャンセル
+ * <CE_Cancel>     # 同上
+ *
+ * このプラグインにはプラグインコマンドはありません。
  *
  * This plugin is released under the MIT License.
  */
@@ -164,6 +186,12 @@
  * <CE_反撃アニメID:20>       # 反撃実行前にアニメーション[20]を再生します。
  * <CE_CounterAnimationId:20> # 同上
  *
+ * 6. 反撃成功時に相手の行動をキャンセル（中断）できます。
+ * 相手が全体攻撃のスキルを使った場合に、反撃成功時点で以降の相手には
+ * 当たらなくなります。特徴を有するメモ欄に以下の通り入力してください。
+ * <CE_キャンセル> # 反撃成功時に相手の行動をキャンセル
+ * <CE_Cancel>     # 同上
+ *
  * このプラグインにはプラグインコマンドはありません。
  *
  * 利用規約：
@@ -219,11 +247,15 @@ var Imported = Imported || {};
     };
 
     var convertEscapeCharacters = function(text) {
-        if (text == null || text === true) text = '';
+        if (isNotAString(text)) text = '';
         text = text.replace(/&gt;?/gi, '>');
         text = text.replace(/&lt;?/gi, '<');
         var windowLayer = SceneManager._scene._windowLayer;
         return windowLayer ? windowLayer.children[0].convertEscapeCharacters(text) : text;
+    };
+
+    var isNotAString = function(args) {
+        return String(args) !== args;
     };
 
     //=============================================================================
@@ -239,6 +271,12 @@ var Imported = Imported || {};
     Game_BattlerBase.prototype.isValidMagicCounter = function() {
         return this.traitObjects().some(function(traitObject) {
             return getMetaValues(traitObject, ['魔法反撃', 'MagicCounter']);
+        });
+    };
+
+    Game_BattlerBase.prototype.isCounterCancel = function() {
+        return this.traitObjects().some(function(traitObject) {
+            return getMetaValues(traitObject, ['キャンセル', 'Cancel']);
         });
     };
 
@@ -390,6 +428,9 @@ var Imported = Imported || {};
             action.apply(subject);
             this._logWindow.displaySkillCounter(target, action, [subject]);
             this._logWindow.displayActionResults(target, subject);
+        }
+        if (target.isCounterCancel()) {
+            this._targets = [];
         }
     };
 
