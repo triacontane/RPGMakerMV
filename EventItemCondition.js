cb708b74762dc5745022e030b7cbf77253f3da59
トリアコンタン <triacontanetakenoko@gmail.com>
2017-05-04 20:53:27 +0900

    1.1.0 2017/05/04 ウィンドウを表示中にリフレッシュする機能を追加
    1.1.0 2017/05/04 ウィンドウを表示中にリフレッシュする機能を追加

--- EventItemCondition.js
+++ EventItemCondition.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2017/05/04 ウィンドウを表示中にリフレッシュする機能を追加
 // 1.0.1 2017/01/12 メモ欄の値が空で設定された場合にエラーが発生するかもしれない問題を修正
 // 1.0.0 2016/09/16 初版
 // ----------------------------------------------------------------------------
@@ -21,6 +22,10 @@
  * @param DefaultVisible
  * @desc メモ欄が指定されていないアイテムの表示可否です。OFFにするとメモ欄に指定がないアイテムは表示されません。
  * @default ON
+ *
+ * @param RefreshSwitchId
+ * @desc 指定したIDのスイッチがONになるとウィンドウを再描画します。描画後、スイッチは自動でOFFになります。
+ * @default 0
  *
  * @help イベントコマンド「アイテム選択の処理」において
  * 表示可否の条件をアイテムごとに設定できます。
@@ -42,6 +47,10 @@
  * @param デフォルト表示可否
  * @desc メモ欄が指定されていないアイテムの表示可否です。OFFにするとメモ欄に指定がないアイテムは表示されません。
  * @default ON
+ *
+ * @param 再描画スイッチID
+ * @desc 指定したIDのスイッチがONになるとウィンドウを再描画します。描画後、スイッチは自動でOFFになります。
+ * @default 0
  *
  * @help イベントコマンド「アイテム選択の処理」において
  * 表示可否の条件をアイテムごとに設定できます。
@@ -69,18 +78,25 @@
     var pluginName    = 'EventItemCondition';
     var metaTagPrefix = 'EIC';
 
-    var getParamOther = function(paramNames) {
+    var getParamString = function(paramNames) {
         if (!Array.isArray(paramNames)) paramNames = [paramNames];
         for (var i = 0; i < paramNames.length; i++) {
             var name = PluginManager.parameters(pluginName)[paramNames[i]];
             if (name) return name;
         }
-        return null;
+        return '';
+    };
+
+    var getParamNumber = function(paramNames, min, max) {
+        var value = getParamString(paramNames);
+        if (arguments.length < 2) min = -Infinity;
+        if (arguments.length < 3) max = Infinity;
+        return (parseInt(value) || 0).clamp(min, max);
     };
 
     var getParamBoolean = function(paramNames) {
-        var value = getParamOther(paramNames);
-        return (value || '').toUpperCase() === 'ON';
+        var value = getParamString(paramNames);
+        return value.toUpperCase() === 'ON';
     };
 
     var getArgString = function(arg, upperFlg) {
@@ -110,8 +126,8 @@
 
     var convertEscapeCharacters = function(text) {
         if (text == null || text === true) text = '';
-        text = text.replace(/&gt;?/gi, '>');
-        text = text.replace(/&lt;?/gi, '<');
+        text            = text.replace(/&gt;?/gi, '>');
+        text            = text.replace(/&lt;?/gi, '<');
         var windowLayer = SceneManager._scene._windowLayer;
         return windowLayer ? windowLayer.children[0].convertEscapeCharacters(text) : text;
     };
@@ -119,18 +135,19 @@
     //=============================================================================
     // パラメータの取得と整形
     //=============================================================================
-    var paramDefaultVisible = getParamBoolean(['DefaultVisible', 'デフォルト表示可否']);
+    var paramDefaultVisible  = getParamBoolean(['DefaultVisible', 'デフォルト表示可否']);
+    var paramRefreshSwitchId = getParamNumber(['RefreshSwitchId', '再描画スイッチID'], 0);
 
     //=============================================================================
     // Window_EventItem
     //  アイテム選択ウィンドウに条件を設定します。
     //=============================================================================
-    var _Window_EventItem_includes = Window_EventItem.prototype.includes;
+    var _Window_EventItem_includes      = Window_EventItem.prototype.includes;
     Window_EventItem.prototype.includes = function(item) {
         var result = _Window_EventItem_includes.apply(this, arguments);
         if (result) {
             this._existCondition = false;
-            result = this.isOkEventItemSwitch(item) && this.isOkEventItemScript(item);
+            result               = this.isOkEventItemSwitch(item) && this.isOkEventItemScript(item);
             if (!this._existCondition) result = paramDefaultVisible;
         }
         return result;
@@ -146,12 +163,26 @@
     };
 
     Window_EventItem.prototype.isOkEventItemScript = function(item) {
-        var metaValue = getMetaValues(item, ['スクリプト', 'Script']);
+        var metaValue = getMetaValues(item, ['スクリプト', 'RefreshSwitchId']);
         if (metaValue) {
             this._existCondition = true;
             return eval(getArgString(metaValue));
         }
         return true;
+    };
+
+    var _Window_EventItem_update      = Window_EventItem.prototype.update;
+    Window_EventItem.prototype.update = function() {
+        _Window_EventItem_update.apply(this, arguments);
+        this.updateAutoRefresh();
+    };
+
+    Window_EventItem.prototype.updateAutoRefresh = function() {
+        if ($gameSwitches.value(paramRefreshSwitchId) && this.openness === 255) {
+            $gameSwitches.setValue(paramRefreshSwitchId, false);
+            this.refresh();
+            this.select(0);
+        }
     };
 })();
 
