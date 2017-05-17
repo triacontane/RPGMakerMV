ca5ad714bdb961dfd2df414292a867bd7191a506
triacontane <triacontanetakenoko@gmail.com>
2017-05-18 00:04:22 +0900

    1.2.0 2017/05/18 ホイールクリックしたときにスイッチを切り替えられる機能を追加
    1.2.0 2017/05/18 ホイールクリックしたときにスイッチを切り替えられる機能を追加

--- MouseWheelExtend.js
+++ MouseWheelExtend.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.2.0 2017/05/18 ホイールクリックしたときにスイッチを切り替えられる機能を追加
 // 1.1.0 2016/07/04 マウスホイールの状態をスイッチや変数に格納する機能など4種類の機能を追加
 // 1.0.0 2016/07/03 初版
 // ----------------------------------------------------------------------------
@@ -36,6 +37,10 @@
  *
  * @param WheelSwitch
  * @desc マウスホイールのクリックで任意のスイッチをONにします。マップ画面でのみ有効です。
+ * @default 0
+ *
+ * @param WheelToggle
+ * @desc マウスホイールのクリックで任意のスイッチをON/OFFを切り替えます。マップ画面でのみ有効です。
  * @default 0
  *
  * @param ScrollDirection
@@ -71,9 +76,10 @@
  * マウスホイールのクリックに決定、キャンセルボタンと同等の機能を持たせます。
  * 両方指定すると決定ボタンが優先されます。
  *
- * ・クリックでスイッチ
+ * ・クリックでスイッチ、トグル
  * マウスホイールのクリックで任意のスイッチをONにします。マップ画面でのみ有効です。
  * コモンイベントのトリガー等に使用できます。
+ * トグルの場合はクリックするたびにスイッチのON/OFFが切り替わります。
  *
  * ・スクロールで十字キー
  * マウスホイールのスクロールに十字キーと同等の機能を持たせます。
@@ -110,6 +116,10 @@
  * @desc マウスホイールのクリックで任意のスイッチをONにします。マップ画面でのみ有効です。
  * @default 0
  *
+ * @param クリックでトグル
+ * @desc マウスホイールのクリックで任意のスイッチをON/OFFを切り替えます。マップ画面でのみ有効です。
+ * @default 0
+ *
  * @param スクロールで十字キー
  * @desc マウスホイールのスクロールに十字キーと同等の機能を持たせます。
  * @default OFF
@@ -143,9 +153,10 @@
  * マウスホイールのクリックに決定、キャンセルボタンと同等の機能を持たせます。
  * 両方指定すると決定ボタンが優先されます。
  *
- * ・クリックでスイッチ
+ * ・クリックでスイッチ、トグル
  * マウスホイールのクリックで任意のスイッチをONにします。マップ画面でのみ有効です。
  * コモンイベントのトリガー等に使用できます。
+ * トグルの場合はクリックするたびにスイッチのON/OFFが切り替わります。
  *
  * ・スクロールで十字キー
  * マウスホイールのスクロールに十字キーと同等の機能を持たせます。
@@ -195,6 +206,7 @@
     var paramWheelOk         = getParamBoolean(['WheelOk', 'クリックで決定']);
     var paramWheelCancel     = getParamBoolean(['WheelCancel', 'クリックでキャンセル']);
     var paramWheelSwitch     = getParamNumber(['WheelSwitch', 'クリックでスイッチ'], 0);
+    var paramWheelToggle     = getParamNumber(['WheelToggle', 'クリックでトグル'], 0);
     var paramScrollDirection = getParamBoolean(['ScrollDirection', 'スクロールで十字キー']);
     var paramScrollVariable  = getParamNumber(['ScrollVariable', 'スクロールで変数'], 0);
     var paramSensitivityY    = getParamNumber(['SensitivityY', '感度Y'], 1);
@@ -207,9 +219,13 @@
     };
 
     Game_Map.prototype.updateWheelTrigger = function() {
-        if (paramWheelSwitch) {
-            if (TouchInput.isMiddleTriggered()) {
+        if (TouchInput.isMiddleTriggered()) {
+            if (paramWheelSwitch) {
                 $gameSwitches.setValue(paramWheelSwitch, true);
+            }
+            if (paramWheelToggle) {
+                var prevValue = $gameSwitches.value(paramWheelToggle);
+                $gameSwitches.setValue(paramWheelToggle, !prevValue);
             }
         }
         if (paramScrollVariable) {
@@ -279,6 +295,13 @@
     // TouchInput
     //  ホイールクリックを決定ボタンにリンクします。
     //=============================================================================
+    var _TouchInput_update2 = TouchInput.update;
+    TouchInput.update = function() {
+        _TouchInput_update2.apply(this, arguments);
+        this._middleTriggered = this._events.middleTriggered;
+        this._events.middleTriggered = false;
+    };
+
     var _TouchInput__onMiddleButtonDown = TouchInput._onMiddleButtonDown;
     TouchInput._onMiddleButtonDown      = function(event) {
         _TouchInput__onMiddleButtonDown.apply(this, arguments);
@@ -287,7 +310,7 @@
         } else if (paramWheelCancel) {
             Input.setCurrentStateForWheelExtendCancel(true);
         }
-        this._middleTriggered = true;
+        this._events.middleTriggered = true;
     };
 
     var _TouchInput__onMouseUp = TouchInput._onMouseUp;
@@ -299,7 +322,6 @@
             } else if (paramWheelCancel) {
                 Input.setCurrentStateForWheelExtendCancel(false);
             }
-            this._middleTriggered = false;
         }
     };
 
