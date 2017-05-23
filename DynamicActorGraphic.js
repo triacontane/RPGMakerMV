6fe2f67859940d3a309a82bcf62d7e0bfff0885a
triacontane <triacontanetakenoko@gmail.com>
2017-05-24 02:40:33 +0900

    1.1.0 2017/05/24 特定のスイッチがONのときにグラフィックを変更する機能を追加
    1.1.0 2017/05/24 特定のスイッチがONのときにグラフィックを変更する機能を追加

--- DynamicActorGraphic.js
+++ DynamicActorGraphic.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2017/05/24 特定のスイッチがONのときにグラフィックを変更する機能を追加
 // 1.0.2 2017/02/07 端末依存の記述を削除
 // 1.0.1 2017/01/21 ステートアイコンの並び順が逆になっていた不具合を修正
 // 1.0.0 2016/12/31 初版
@@ -20,21 +21,27 @@
  * @author triacontane
  *
  * @help アクターの歩行、顔、バトルグラフィックを動的に変化させます。
- * HPの残量、特定のステート有効時に変化します。
+ * HPの残量、特定のステート有効時、スイッチON時に変化します。
  *
  * アクターのメモ欄に以下の通り入力してください。
  * <DAG_CHARACTER_HP50:test,2> # HP残量50%以下でtest.pngの2番目の画像を
  *                             # 歩行グラフィックに指定します。(HPは10%刻みで指定)
- * <DAG_FACE_HP10:test,1>      # HP残量10%以下でtest.pngの2番目の画像を
+ * <DAG_FACE_HP10:test,2>      # HP残量10%以下でtest.pngの2番目の画像を
  *                             # 顔グラフィックに指定します。
  * <DAG_BATTLER_HP30:test>     # HP残量30%以下でtest.pngを
  *                             # バトラーグラフィックに指定します。
  * <DAG_CHARACTER_ST5:test,2>  # ステート[5]有効時にtest.pngの2番目の画像を
  *                             # 歩行グラフィックに指定します。
- * <DAG_FACE_ST5:test,1>       # ステート[5]有効時にtest.pngの2番目の画像を
+ * <DAG_FACE_ST5:test,2>       # ステート[5]有効時にtest.pngの2番目の画像を
  *                             # 顔グラフィックに指定します。
  * <DAG_BATTLER_ST5:test>      # ステート[5]有効時にtest.pngを
  *                             # バトラーグラフィックに指定します。
+ * <DAG_CHARACTER_SWITCH1:test, 2> # スイッチ[1]ON時にtest.pngの2番目の画像を
+ *                                 # 歩行グラフィックに指定します。
+ * <DAG_FACE_SWITCH1:test,2>       # スイッチ[1]ON時にtest.pngの2番目の画像を
+ *                                 # 顔グラフィックに指定します。
+ * <DAG_BATTLER_SWITCH1:test>      # スイッチ[1]ON時にtest.pngを
+ *                                 # バトラーグラフィックに指定します。
  *
  * 注意！　キャッシュされていない画像に変更した場合、JSの非同期読み込みの仕様で
  * 画像がチラついたり、ただしく表示されない場合があります。
@@ -48,21 +55,27 @@
  * @author トリアコンタン
  *
  * @help アクターの歩行、顔、バトルグラフィックを動的に変化させます。
- * HPの残量、特定のステート有効時に変化します。
+ * HPの残量、特定のステート有効時、スイッチON時に変化します。
  *
  * アクターのメモ欄に以下の通り入力してください。
  * <DAG_CHARACTER_HP50:test,2> # HP残量50%以下でtest.pngの2番目の画像を
  *                             # 歩行グラフィックに指定します。(HPは10%刻みで指定)
- * <DAG_FACE_HP10:test,1>      # HP残量10%以下でtest.pngの2番目の画像を
+ * <DAG_FACE_HP10:test,2>      # HP残量10%以下でtest.pngの2番目の画像を
  *                             # 顔グラフィックに指定します。
  * <DAG_BATTLER_HP30:test>     # HP残量30%以下でtest.pngを
  *                             # バトラーグラフィックに指定します。
  * <DAG_CHARACTER_ST5:test,2>  # ステート[5]有効時にtest.pngの2番目の画像を
  *                             # 歩行グラフィックに指定します。
- * <DAG_FACE_ST5:test,1>       # ステート[5]有効時にtest.pngの2番目の画像を
+ * <DAG_FACE_ST5:test,2>       # ステート[5]有効時にtest.pngの2番目の画像を
  *                             # 顔グラフィックに指定します。
  * <DAG_BATTLER_ST5:test>      # ステート[5]有効時にtest.pngを
  *                             # バトラーグラフィックに指定します。
+ * <DAG_CHARACTER_SWITCH1:test, 2> # スイッチ[1]ON時にtest.pngの2番目の画像を
+ *                                 # 歩行グラフィックに指定します。
+ * <DAG_FACE_SWITCH1:test,2>       # スイッチ[1]ON時にtest.pngの2番目の画像を
+ *                                 # 顔グラフィックに指定します。
+ * <DAG_BATTLER_SWITCH1:test>      # スイッチ[1]ON時にtest.pngを
+ *                                 # バトラーグラフィックに指定します。
  *
  * 注意！　キャッシュされていない画像に変更した場合、JSの非同期読み込みの仕様で
  * 画像がチラついたり、ただしく表示されない場合があります。
@@ -128,7 +141,7 @@
 
     //=============================================================================
     // Game_Actor
-    //  リフレッシュ
+    //  アクターグラフィックを動的に差し替えます。
     //=============================================================================
     var _Game_Actor_refresh    = Game_Actor.prototype.refresh;
     Game_Actor.prototype.refresh = function() {
@@ -145,6 +158,7 @@
         this.initCustomGraphic();
         this.refreshCustomGraphicForState();
         this.refreshCustomGraphicForHpRate();
+        this.refreshCustomGraphicForSwitch();
         $gamePlayer.refresh();
     };
 
@@ -165,6 +179,27 @@
             this.setFaceCustom(getMetaValue(actor, 'FACE_HP' + String(hpRate)));
             this.setBattlerCustom(getMetaValue(actor, 'BATTLER_HP' + String(hpRate)));
         }
+    };
+
+    Game_Actor.prototype.refreshCustomGraphicForSwitch = function() {
+        var actor = this.actor();
+        Object.keys(actor.meta).forEach(function(metaName) {
+            metaName.replace(/CHARACTER_SWITCH(\d+)/i, function() {
+                if ($gameSwitches.value(getArgNumber(arguments[1]))) {
+                    this.setCharacterCustom(getMetaValue(actor, arguments[0]));
+                }
+            }.bind(this));
+            metaName.replace(/FACE_SWITCH(\d+)/i, function() {
+                if ($gameSwitches.value(getArgNumber(arguments[1]))) {
+                    this.setFaceCustom(getMetaValue(actor, arguments[0]));
+                }
+            }.bind(this));
+            metaName.replace(/BATTLER_SWITCH(\d+)/i, function() {
+                if ($gameSwitches.value(getArgNumber(arguments[1]))) {
+                    this.setBattlerCustom(getMetaValue(actor, arguments[0]));
+                }
+            }.bind(this));
+        }.bind(this));
     };
 
     Game_Actor.prototype.getSortedStates = function() {
@@ -225,6 +260,36 @@
     var _Game_Actor_battlerName    = Game_Actor.prototype.battlerName;
     Game_Actor.prototype.battlerName = function() {
         return this._battlerNameCustom || _Game_Actor_battlerName.apply(this, arguments);
+    };
+
+    //=============================================================================
+    // Game_Party
+    //  メンバーのグラフィックを動的に差し替えます。
+    //=============================================================================
+    Game_Party.prototype.refreshMemberCustomGraphic = function() {
+        this.members().forEach(function(actor) {
+            actor.refreshCustomGraphic();
+        });
+    };
+
+    //=============================================================================
+    // Scene_Map
+    //  作成時にアクターグラフィックを更新します。
+    //=============================================================================
+    var _Scene_Map_create = Scene_Map.prototype.create;
+    Scene_Map.prototype.create = function() {
+        $gameParty.refreshMemberCustomGraphic();
+        _Scene_Map_create.apply(this, arguments);
+    };
+
+    //=============================================================================
+    // Scene_MenuBase
+    //  作成時にアクターグラフィックを更新します。
+    //=============================================================================
+    var _Scene_MenuBase_create = Scene_MenuBase.prototype.create;
+    Scene_MenuBase.prototype.create = function() {
+        $gameParty.refreshMemberCustomGraphic();
+        _Scene_MenuBase_create.apply(this, arguments);
     };
 })();
 
