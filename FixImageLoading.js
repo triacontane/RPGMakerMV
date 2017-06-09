7c75e77b14ef24a0a644abda6d0475149a2dc9ee
トリアコンタン <triacontanetakenoko@gmail.com>
2017-06-09 22:01:59 +0900

    2.0.0 2017/06/09 本体ver1.5.0に合わせて再作成
    2.0.0 2017/06/09 本体ver1.5.0に合わせて再作成

--- FixImageLoading.js
+++ FixImageLoading.js
@@ -1,11 +1,12 @@
 //=============================================================================
 // FixImageLoading.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2015 Triacontane
+// Copyright (c) 2015-2017 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 2.0.0 2017/06/09 本体ver1.5.0に合わせて再作成
 // 1.1.1 2016/11/20 ロード完了時にframeが更新されない不具合を修正
 //                  ロード中にframeが変更された場合に、ロード完了まで反映を遅らせる仕様を追加
 // 1.1.0 2016/11/16 liply_GC.jsとの競合を解消 by 奏 ねこま様
@@ -24,6 +25,9 @@
  * 一瞬発生するチラつきを防止します。
  * 画像のロードが完了するまで以前に表示していた画像を残します。
  *
+ * 逆に画像を消したいときは明示的にピクチャの消去等を行ってから
+ * 表示してください。
+ *
  * このプラグインにはプラグインコマンドはありません。
  *
  * 利用規約：
@@ -32,52 +36,34 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function () {
+(function() {
     'use strict';
 
-    Object.defineProperty(Sprite.prototype, 'bitmap', {
-        get: function() {
-            return this._bitmap;
-        },
-        set: function(value) {
-            if (this._bitmap === value) return;
-            if (value) {
-                value.__liply_attachedToScene = true;               // added by nekoma
-                if (!this._bitmap) {
-                    this._setBitmapObject(value);
-                }
-                this._changeingBitmap = value;
-                value.addLoadListener(function () {
-                    this._changeingBitmap = null;
-                    if (!this._bitmap) return;
-                    this._setBitmapObject(value);
-                    this._onBitmapLoad();
-                }.bind(this));
-            } else {
-                this._bitmap = null;
-                this.texture.frame = Rectangle.emptyRectangle;      // modified by nekoma
-            }
-        },
-        configurable: true
-    });
-
-    /**
-     * @method _setBitmapObject
-     * @private
-     */
-    Sprite.prototype._setBitmapObject = function(bitmap) {
-        this._bitmap = bitmap;
-        this._bitmap.__liply_attachedToScene = false;   // added by nekoma
-        this.setFrame(0, 0, 0, 0);
+    var _Sprite__renderCanvas = Sprite.prototype._renderCanvas;
+    Sprite.prototype._renderCanvas = function(renderer) {
+        _Sprite__renderCanvas.apply(this, arguments);
+        if (this.isExistLoadingBitmap()) {
+            this._renderCanvas_PIXI(renderer);
+        }
     };
 
-    var _Sprite_setFrame = Sprite.prototype.setFrame;
-    Sprite.prototype.setFrame = function(x, y, width, height) {
-        if (this._changeingBitmap) {
-            this._changeingBitmap.addLoadListener(_Sprite_setFrame.bind(this, x, y, width, height));
-        } else {
-            _Sprite_setFrame.apply(this, arguments);
+    var _Sprite__renderWebGL      = Sprite.prototype._renderWebGL;
+    Sprite.prototype._renderWebGL = function(renderer) {
+        _Sprite__renderWebGL.apply(this, arguments);
+        if (this.isExistLoadingBitmap()) {
+            if (this._isPicture) {
+                this._speedUpCustomBlendModes(renderer);
+                renderer.setObjectRenderer(renderer.plugins.picture);
+                renderer.plugins.picture.render(this);
+            } else {
+                renderer.setObjectRenderer(renderer.plugins.sprite);
+                renderer.plugins.sprite.render(this);
+            }
         }
+    };
+
+    Sprite.prototype.isExistLoadingBitmap = function() {
+        return this.bitmap && !this.bitmap.isReady();
     };
 })();
 
