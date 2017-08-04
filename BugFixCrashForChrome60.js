d062fbfdc6eada271305937080b4081d6c4f686c
トリアコンタン <triacontanetakenoko@gmail.com>
2017-08-05 02:29:17 +0900

    1.1.0 2017/08/05 対応範囲の拡大
    1.1.0 2017/08/05 対応範囲の拡大

--- BugFixCrashForChrome60.js
+++ BugFixCrashForChrome60.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2017/08/05 対応範囲の拡大
 // 1.0.1 2017/07/31 適用バージョンが60固定になっていたので6Xに変更
 // 1.0.0 2017/07/31 初版
 // ----------------------------------------------------------------------------
@@ -42,10 +43,17 @@
  *
  * 根本原因は、forEachの繰り返し途中で、対象配列の要素が削除された場合に、
  * 配列の範囲外(長さを上回るindex)の値（undefined）が渡されることがあるためです。
- * なぜ範囲外の値が渡されるのかは不明なので、こちらは暫定対策となります。
+ * 範囲外の値が渡されるのはChromeのバグの可能性が高いためこちらは暫定対策となります。
+ *
+ * エラー発生時は以下のメッセージが出力されます。
+ * Cannot read property 'update' of undefined
  *
  * 発生を確認したバージョン
  * Google Chrome 60.0.3112.78
+ * Google Chrome 60.0.3112.90
+ *
+ * このプラグインはコアスクリプトを上書きしている箇所が多いので
+ * プラグイン管理画面で極力、上の方に配置してください。
  *
  * このプラグインにはプラグインコマンドはありません。
  *
@@ -65,6 +73,56 @@
     }
 
     Sprite.prototype.update = function() {
+        this.children.forEach(function(child) {
+            if (child && child.update) {
+                child.update();
+            }
+        });
+    };
+
+    Scene_Base.prototype.updateChildren = function() {
+        this.children.forEach(function(child) {
+            if (child && child.update) {
+                child.update();
+            }
+        });
+    };
+
+    TilingSprite.prototype.update = function() {
+        this.children.forEach(function(child) {
+            if (child && child.update) {
+                child.update();
+            }
+        });
+    };
+
+    Tilemap.prototype.update = function() {
+        this.animationCount++;
+        this.animationFrame = Math.floor(this.animationCount / 30);
+        this.children.forEach(function(child) {
+            if (child && child.update) {
+                child.update();
+            }
+        });
+        for (var i=0; i<this.bitmaps.length;i++) {
+            if (this.bitmaps[i]) {
+                this.bitmaps[i].touch();
+            }
+        }
+    };
+
+    Window.prototype.update = function() {
+        if (this.active) {
+            this._animationCount++;
+        }
+        this.children.forEach(function(child) {
+            if (child && child.update) {
+                child.update();
+            }
+        });
+    };
+
+    WindowLayer.prototype.update = function() {
         this.children.forEach(function(child) {
             if (child && child.update) {
                 child.update();
