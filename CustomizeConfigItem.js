fa65631ffe5e86d2f47f5a2e1046c8861576db46
トリアコンタン <triacontanetakenoko@gmail.com>
2017-09-10 21:10:22 +0900

    2.0.0 2017/09/10 ツクールの型指定機能に対応し、各オプション項目を任意の数だけ追加できる機能を追加

--- CustomizeConfigItem.js
+++ CustomizeConfigItem.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 2.0.0 2017/09/10 ツクールの型指定機能に対応し、各オプション項目を任意の数だけ追加できる機能を追加
 // 1.2.3 2017/06/08 1.2.2の修正により起動できなくなっていた問題を修正
 // 1.2.2 2017/05/27 競合の可能性のある記述（Objectクラスへのプロパティ追加）をリファクタリング
 // 1.2.1 2016/12/08 1.2.0の機能追加以降、デフォルト項目で決定ボタンを押すとエラーになっていた現象を修正
@@ -19,52 +20,29 @@
 // [GitHub] : https://github.com/triacontane/
 //=============================================================================
 
-/*:ja
+/*:
  * @plugindesc オプション任意項目作成プラグイン
  * @author トリアコンタン
  *
- * 項目を増やしたい場合は、以下をコピーしてください。
- * -------------------------------------------------------
- * @param スイッチ項目1
- * @desc 項目の情報です。以下の順で指定します。
- * 名称:初期値:スイッチ番号:隠しフラグ:スクリプト
- * @default スイッチ項目1:OFF:0:OFF:
+ * @param 数値項目
+ * @desc 追加する数値項目のオプション項目情報です。
+ * @default
+ * @type struct<NumberData>[]
  *
- * @param 数値項目1
- * @desc 項目の情報です。以下の順で指定します。
- * 名称:初期値:変数番号:隠しフラグ:最小値:最大値:変化値:スクリプト
- * @default 数値項目1:0:0:OFF:0:10:1:
+ * @param 文字項目
+ * @desc 追加する文字項目のオプション項目情報です。
+ * @default
+ * @type struct<StringData>[]
  *
- * @param 音量項目1
- * @desc 項目の情報です。以下の順で指定します。
- * 名称:初期値:変数番号:隠しフラグ:スクリプト
- * @default 音量項目1:0:0:OFF:
+ * @param スイッチ項目
+ * @desc 追加するスイッチ項目のオプション項目情報です。
+ * @default
+ * @type struct<BooleanData>[]
  *
- * @param 文字項目1
- * @desc 項目の情報です。以下の順で指定します。
- * 名称:初期値:変数番号:隠しフラグ:文字配列:スクリプト
- * @default 文字項目1:0:0:OFF:EASY, NORMAL, HARD, VERY HARD:
- *
- * @param スイッチ項目2
- * @desc 項目の情報です。以下の順で指定します。
- * 名称,初期値:スイッチ番号:隠しフラグ:スクリプト
- * @default スイッチ項目2:OFF:0:OFF:
- *
- * @param 数値項目2
- * @desc 項目の情報です。以下の順で指定します。
- * 名称:初期値:変数番号:隠しフラグ:最小値:最大値:変化値:スクリプト
- * @default 数値項目2:0:0:OFF:0:10:1:
- *
- * @param 音量項目2
- * @desc 項目の情報です。以下の順で指定します。
- * 名称:初期値:変数番号:隠しフラグ:スクリプト
- * @default 音量項目2:0:0:OFF:
- *
- * @param 文字項目2
- * @desc 項目の情報です。以下の順で指定します。
- * 名称:初期値:変数番号:隠しフラグ:文字配列:スクリプト
- * @default 文字項目2:0:0:OFF:EASY, NORMAL, HARD, VERY HARD:
- * --------------------------------------------------------
+ * @param 音量項目
+ * @desc 追加する音量項目のオプション項目情報です。
+ * @default
+ * @type struct<VolumeData>[]
  *
  * @help オプション画面に任意の項目を追加します。
  * 項目の種類は、以下の四種類があります。
@@ -78,37 +56,24 @@
  * 隠しフラグを設定すると、オプション画面に表示されなくなります。
  * ゲームを進めないと出現しない項目などに利用できます。
  * 隠しフラグはプラグインコマンドから解除できます。
- * それぞれの値はコロン（:）区切りで指定してください。
  *
  * スクリプトは上級者向け項目です。対象にカーソルを合わせて決定ボタンを
  * 押下すると指定したJavaScriptを実行できます。
  * 主に専用の設定画面などの遷移に使用します。
- *
- * 指定項目「名称:初期値:値が設定されるスイッチ:隠しフラグ:スクリプト」
- * 例：スイッチ項目1:OFF:1:OFF:script
  *
  * ・数値項目：
  * 数値を選択する項目です。指定した番号の変数と値が同期されます。
  * スイッチ項目で指定した内容に加えて、
  * 最小値と最大値および一回の入力で変化する値を指定します。
  *
- * 指定項目「名称:初期値:値が設定される変数:隠しフラグ:最小値:最大値:変化値:スクリプト」
- * 例：数値項目1:0:1:OFF:0:10:1:
- *
  * ・音量項目：
  * 音量を選択する項目です。BGMボリュームなどと同じ仕様で
  * キャラクターごとのボイス音量等に使ってください。
- *
- * 指定項目「名称:初期値:値が設定される変数:隠しフラグ:スクリプト」
- * 例：音量項目1:0:2:OFF:SceneManager.push(Scene_SoundTest);
  *
  * ・文字項目：
  * 文字を選択する項目です。指定した文字の配列から項目を選択します。
  * 選択した文字のインデックス(開始位置は0)が変数に設定されます。
  * 初期値に設定する値もインデックスです。
- *
- * 設定項目「名称:初期値:値が設定される変数:隠しフラグ:文字の配列:スクリプト」
- * 例：文字項目1:0:3:OFF:EASY, NORMAL, HARD, VERY HARD:script
  *
  * プラグインコマンド詳細
  *  イベントコマンド「プラグインコマンド」から実行。
@@ -117,12 +82,129 @@
  *  CC_UNLOCK or
  *  オプション任意項目の隠し解除 [項目名]
  *  　指定した項目の隠しフラグを解除します。
- *  使用例：CC_ITEM_VALID 数値項目1
+ *  使用例：CC_UNLOCK 数値項目1
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
  *  についても制限はありません。
  *  このプラグインはもうあなたのものです。
+ */
+
+/*~struct~NumberData:
+ * @param Name
+ * @desc 項目の名称です。
+ * @default 数値項目1
+ *
+ * @param DefaultValue
+ * @desc 項目の初期値です。
+ * @default 0
+ * @type number
+ *
+ * @param VariableID
+ * @desc 項目の設定内容が格納される変数番号です。
+ * @default 0
+ * @type variable
+ *
+ * @param HiddenFlag
+ * @desc 項目がデフォルトで隠されるようになります。プラグインコマンドの実行で表示されます。
+ * @default false
+ * @type boolean
+ *
+ * @param Script
+ * @desc 項目を決定したときに実行されるスクリプトです。
+ * @default
+ *
+ * @param NumberMin
+ * @desc 項目の最小値です。
+ * @default 0
+ * @type number
+ *
+ * @param NumberMax
+ * @desc 項目の最大値です。
+ * @default 100
+ * @type number
+ *
+ * @param NumberStep
+ * @desc 項目を操作したときの数値の変化量です。
+ * @default 20
+ * @type number
+ */
+/*~struct~BooleanData:
+ * @param Name
+ * @desc 項目の名称です。
+ * @default スイッチ項目1
+ *
+ * @param DefaultValue
+ * @desc 項目の初期値です。
+ * @default false
+ * @type boolean
+ *
+ * @param SwitchID
+ * @desc 項目の設定内容が格納されるスイッチ番号です。
+ * @default 0
+ * @type switch
+ *
+ * @param HiddenFlag
+ * @desc 項目がデフォルトで隠されるようになります。プラグインコマンドの実行で表示されます。
+ * @default false
+ * @type boolean
+ *
+ * @param Script
+ * @desc 項目を決定したときに実行されるスクリプトです。
+ * @default
+ */
+/*~struct~StringData:
+ * @param Name
+ * @desc 項目の名称です。
+ * @default 文字列項目1
+ *
+ * @param DefaultValue
+ * @desc 項目の初期値です。インデックスの数値を指定します。
+ * @default 0
+ * @type number
+ *
+ * @param VariableID
+ * @desc 項目の設定内容が格納される変数番号です。
+ * @default 0
+ * @type variable
+ *
+ * @param HiddenFlag
+ * @desc 項目がデフォルトで隠されるようになります。プラグインコマンドの実行で表示されます。
+ * @default false
+ * @type boolean
+ *
+ * @param Script
+ * @desc 項目を決定したときに実行されるスクリプトです。
+ * @default
+ *
+ * @param StringItems
+ * @desc 項目の設定内容の配列です。
+ * @default
+ * @type string[]
+ */
+/*~struct~VolumeData:
+ * @param Name
+ * @desc 項目の名称です。
+ * @default 音量項目1
+ *
+ * @param DefaultValue
+ * @desc 項目の初期値です。
+ * @default 0
+ * @type number
+ *
+ * @param VariableID
+ * @desc 項目の設定内容が格納される変数番号です。
+ * @default 0
+ * @type variable
+ *
+ * @param HiddenFlag
+ * @desc 項目がデフォルトで隠されるようになります。プラグインコマンドの実行で表示されます。
+ * @default false
+ * @type boolean
+ *
+ * @param Script
+ * @desc 項目を決定したときに実行されるスクリプトです。
+ * @default
  */
 
 (function() {
@@ -143,6 +225,24 @@
         return null;
     };
 
+    var getParamArrayJson = function(paramNames, defaultValue) {
+        var value = getParamString(paramNames) || null;
+        try {
+            value = JSON.parse(value);
+            if (value === null) {
+                value = defaultValue;
+            } else {
+                value = value.map(function(valueData) {
+                    return JSON.parse(valueData);
+                });
+            }
+        } catch (e) {
+            alert(`!!!Plugin param is wrong.!!!\nPlugin:${pluginName}.js\nName:[${paramNames}]\nValue:${value}`);
+            value = defaultValue;
+        }
+        return value;
+    };
+
     var getCommandName = function(command) {
         return (command || '').toUpperCase();
     };
@@ -160,11 +260,24 @@
     var getArgNumber = function(arg, min, max) {
         if (arguments.length < 2) min = -Infinity;
         if (arguments.length < 3) max = Infinity;
-        return (parseInt(arg, 10) || 0).clamp(min, max);
+        return (parseInt(arg) || 0).clamp(min, max);
     };
 
     var getArgBoolean = function(arg) {
-        return (arg || '').toUpperCase() === 'ON';
+        return (arg || '').toUpperCase() === 'TRUE';
+    };
+
+    var getArgJson = function(arg, defaultValue) {
+        try {
+            arg = JSON.parse(arg);
+            if (arg === null) {
+                arg = defaultValue;
+            }
+        } catch (e) {
+            alert(`!!!Plugin param is wrong.!!!\nPlugin:${pluginName}.js\nValue:${arg}`);
+            arg = defaultValue;
+        }
+        return arg;
     };
 
     var iterate = function(that, handler) {
@@ -172,6 +285,15 @@
             handler.call(that, key, that[key], index);
         });
     };
+
+    //=============================================================================
+    // パラメータの取得と整形
+    //=============================================================================
+    var param           = {};
+    param.numberOptions = getParamArrayJson(['NumberOptions', '数値項目'], []);
+    param.stringOptions = getParamArrayJson(['StringOptions', '文字項目'], []);
+    param.switchOptions = getParamArrayJson(['SwitchOptions', 'スイッチ項目'], []);
+    param.volumeOptions = getParamArrayJson(['VolumeOptions', '音量項目'], []);
 
     var localOptionWindowIndex = 0;
 
@@ -182,23 +304,7 @@
     var _Game_Interpreter_pluginCommand      = Game_Interpreter.prototype.pluginCommand;
     Game_Interpreter.prototype.pluginCommand = function(command, args) {
         _Game_Interpreter_pluginCommand.call(this, command, args);
-        try {
-            this.pluginCommandCustomizeConfigItem(command, args);
-        } catch (e) {
-            if ($gameTemp.isPlaytest() && Utils.isNwjs()) {
-                var window = require('nw.gui').Window.get();
-                if (!window.isDevToolsOpen()) {
-                    var devTool = window.showDevTools();
-                    devTool.moveTo(0, 0);
-                    devTool.resizeTo(Graphics.width, Graphics.height);
-                    window.focus();
-                }
-            }
-            console.log('プラグインコマンドの実行中にエラーが発生しました。');
-            console.log('- コマンド名 　: ' + command);
-            console.log('- コマンド引数 : ' + args);
-            console.log('- エラー原因   : ' + e.toString());
-        }
+        this.pluginCommandCustomizeConfigItem(command, args);
     };
 
     Game_Interpreter.prototype.pluginCommandCustomizeConfigItem = function(command, args) {
@@ -218,55 +324,66 @@
     ConfigManager.hiddenInfo   = {};
 
     ConfigManager.getCustomParams = function() {
-        if (this.customParams != null) return this.customParams;
+        if (this.customParams) {
+            return this.customParams;
+        }
         this.customParams = {};
-        var i, result;
-        for (i = 1, result = true; result; i++) {
-            result = this._getCustomParamItem('スイッチ項目', 'Boolean', i);
-        }
-        for (i = 1, result = true; result; i++) {
-            result = this._getCustomParamItem('数値項目', 'Number', i);
-        }
-        for (i = 1, result = true; result; i++) {
-            result = this._getCustomParamItem('音量項目', 'Volume', i);
-        }
-        for (i = 1, result = true; result; i++) {
-            result = this._getCustomParamItem('文字項目', 'String', i);
-        }
+        param.numberOptions.forEach(function(optionItem, index) {
+            this.makeNumberOption(optionItem, index);
+        }, this);
+        param.stringOptions.forEach(function(optionItem, index) {
+            this.makeStringOption(optionItem, index);
+        }, this);
+        param.switchOptions.forEach(function(optionItem, index) {
+            this.makeSwitchOption(optionItem, index);
+        }, this);
+        param.volumeOptions.forEach(function(optionItem, index) {
+            this.makeVolumeOption(optionItem, index);
+        }, this);
         return this.customParams;
     };
 
-    ConfigManager._getCustomParamItem = function(paramBaseName, symbolType, i) {
-        var param = getParamString(paramBaseName + '%1'.format(i)).split(':');
-        if (param.length > 1) {
-            try {
-                var data       = {};
-                data.symbol    = symbolType + '%1'.format(i);
-                data.name      = getArgString(param[0]);
-                data.initValue = symbolType === 'Boolean' ? getArgBoolean(param[1]) : getArgNumber(param[1]);
-                data.variable  = getArgNumber(param[2]);
-                data.hidden    = getArgBoolean(param[3]);
-                data.script    = getArgString(param[param.length - 1]);
-                switch (symbolType) {
-                    case 'Number':
-                        data.min    = getArgNumber(param[4]);
-                        data.max    = getArgNumber(param[5]);
-                        data.offset = getArgNumber(param[6]);
-                        break;
-                    case 'String':
-                        data.values = getArgArrayString(param[4]);
-                        data.min    = 0;
-                        data.max    = data.values.length - 1;
-                        break;
-                }
-                this.customParams[data.symbol] = data;
-            } catch (e) {
-                console.log('パラメータの指定に不正があります。: ' + param + ' : ' + e.toString());
-                return false;
-            }
-            return true;
-        }
-        return false;
+    ConfigManager.makeNumberOption = function(optionItem, index) {
+        var data    = this.makeCommonOption(optionItem, index, 'Number');
+        data.min    = getArgNumber(optionItem.NumberMin);
+        data.max    = getArgNumber(optionItem.NumberMax);
+        data.offset = getArgNumber(optionItem.NumberStep);
+        this.pushOptionData(data);
+    };
+
+    ConfigManager.makeStringOption = function(optionItem, index) {
+        var data    = this.makeCommonOption(optionItem, index, 'String');
+        data.values = getArgJson(optionItem.StringItems);
+        data.min    = 0;
+        data.max    = data.values.length - 1;
+        this.pushOptionData(data);
+    };
+
+    ConfigManager.makeSwitchOption = function(optionItem, index) {
+        var data       = this.makeCommonOption(optionItem, index, 'Switch');
+        data.initValue = getArgBoolean(optionItem.DefaultValue);
+        data.variable  = getArgNumber(optionItem.SwitchID);
+        this.pushOptionData(data);
+    };
+
+    ConfigManager.makeVolumeOption = function(optionItem, index) {
+        var data = this.makeCommonOption(optionItem, index, 'Volume');
+        this.pushOptionData(data);
+    };
+
+    ConfigManager.makeCommonOption = function(optionItem, index, type) {
+        var data       = {};
+        data.symbol    = `${type}${index + 1}`;
+        data.name      = optionItem.Name;
+        data.hidden    = getArgBoolean(optionItem.HiddenFlag);
+        data.script    = optionItem.Script;
+        data.initValue = getArgNumber(optionItem.DefaultValue);
+        data.variable  = getArgNumber(optionItem.VariableID, 0);
+        return data;
+    };
+
+    ConfigManager.pushOptionData = function(data) {
+        this.customParams[data.symbol] = data;
     };
 
     var _ConfigManager_makeData = ConfigManager.makeData;
@@ -291,7 +408,7 @@
             } else {
                 this[symbol] = this.readOther(config, symbol, item);
             }
-            this.hiddenInfo[symbol] = (config.hiddenInfo != null ? config.hiddenInfo[symbol] : item.hidden);
+            this.hiddenInfo[symbol] = (config.hiddenInfo ? config.hiddenInfo[symbol] : item.hidden);
         }.bind(this));
     };
 
