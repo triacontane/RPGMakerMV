19c76e7fe491bb6093dd78c2b311610cce23ae88
トリアコンタン <triacontanetakenoko@gmail.com>
2017-09-15 22:42:57 +0900

    1.1.0 2017/09/15 型指定機能に対応

--- FloatVariables.js
+++ FloatVariables.js
@@ -6,13 +6,51 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2017/09/15 型指定機能に対応
 // 1.0.0 2016/07/30 初版
 // ----------------------------------------------------------------------------
-// [Blog]   : http://triacontane.blogspot.jp/
+// [Blog]   : https://triacontane.blogspot.jp/
 // [Twitter]: https://twitter.com/triacontane/
 // [GitHub] : https://github.com/triacontane/
 //=============================================================================
 
+/*:
+ * @plugindesc FloatVariablesPlugin
+ * @author triacontane
+ *
+ * @param FloatVariableStart
+ * @desc 小数演算を行う変数の開始位置です。
+ * @default 0
+ * @type variable
+ *
+ * @param FloatVariableEnd
+ * @desc 小数演算を行う変数の終了位置です。
+ * @default 0
+ * @type variable
+ *
+ * @help FloatVariables.js
+ *
+ * 指定した範囲内の変数を小数演算可能にします。
+ *
+ * ・通常(整数演算)の場合
+ * 3 / 2 = 1
+ *
+ * ・小数演算の場合
+ * 3 / 2 = 1.5
+ *
+ * 小数が代入されている変数をイベントコマンドのオペランドとして
+ * 使用すると予期しない問題が発生する可能性があります。
+ * (HPに0.5を加算するなど)
+ *
+ * 注意して利用してください。
+ *
+ * このプラグインにはプラグインコマンドはありません。
+ *
+ * 利用規約：
+ *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
+ *  についても制限はありません。
+ *  このプラグインはもうあなたのものです。
+ */
 /*:
  * @plugindesc 変数の小数演算プラグイン
  * @author トリアコンタン
@@ -20,12 +58,15 @@
  * @param 小数変数開始位置
  * @desc 小数演算を行う変数の開始位置です。
  * @default 0
+ * @type variable
  *
  * @param 小数変数終了位置
  * @desc 小数演算を行う変数の終了位置です。
  * @default 0
+ * @type variable
  *
- * @help
+ * @help FloatVariables.js
+ *
  * 指定した範囲内の変数を小数演算可能にします。
  *
  * ・通常(整数演算)の場合
