923224b0c7ef4a89fb3a845b8d6ec05547357346
triacontane <triacontanetakenoko@gmail.com>
2017-12-09 01:25:28 +0900

    1.0.0 2017/12/09 初版

---
+++ WindowBlinkStop.js
@@ -0,0 +1,53 @@
+//=============================================================================
+// WindowBlinkStop.js
+// ----------------------------------------------------------------------------
+// Copyright (c) 2015-2017 Triacontane
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+// ----------------------------------------------------------------------------
+// Version
+// 1.0.0 2017/12/09 初版
+// ----------------------------------------------------------------------------
+// [Blog]   : https://triacontane.blogspot.jp/
+// [Twitter]: https://twitter.com/triacontane/
+// [GitHub] : https://github.com/triacontane/
+//=============================================================================
+
+/*:
+ * @plugindesc WindowBlinkStopPlugin
+ * @author triacontane
+ *
+ * @help WindowBlinkStop.js
+ *
+ * 選択中のウィンドウカーソルの点滅を停止します。
+ *
+ * このプラグインにはプラグインコマンドはありません。
+ *
+ * This plugin is released under the MIT License.
+ */
+/*:ja
+ * @plugindesc ウィンドウ点滅停止プラグイン
+ * @author トリアコンタン
+ *
+ * @help WindowBlinkStop.js
+ *
+ * 選択中のウィンドウカーソルの点滅を停止します。
+ *
+ * このプラグインにはプラグインコマンドはありません。
+ *
+ * 利用規約：
+ *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
+ *  についても制限はありません。
+ *  このプラグインはもうあなたのものです。
+ */
+
+(function() {
+    'use strict';
+
+    var _Window__updateCursor = Window.prototype._updateCursor;
+    Window.prototype._updateCursor = function() {
+        this._animationCount = 0;
+        _Window__updateCursor.apply(this, arguments);
+    };
+})();
+
