d836721a94461d5b2da24c0a510d97b66cc9fff6
トリアコンタン <triacontanetakenoko@gmail.com>
2018-09-25 16:50:03 +0900

    1.1.0 2018/09/25 ミュート切り替え時に即座に音声が反映されるよう修正

--- AudioThroughIfMute.js
+++ AudioThroughIfMute.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2018/09/25 ミュート切り替え時に即座に音声が反映されるよう修正
 // 1.0.1 2018/09/25 ヘルプ修正
 // 1.0.0 2017/09/15 初版
 // ----------------------------------------------------------------------------
@@ -94,13 +95,66 @@
     param.audioThroughSwitchId = getParamNumber(['AudioThroughSwitchId', '音声無効スイッチ番号'], 0);
 
     //=============================================================================
+    // Window_Options
+    //  ミュート状態からの復帰処理を追記します。
+    //=============================================================================
+    var _Window_Options_setConfigValue = Window_Options.prototype.setConfigValue;
+    Window_Options.prototype.setConfigValue = function(symbol, volume) {
+        _Window_Options_setConfigValue.apply(this, arguments);
+        AudioManager.restoreMuteIfNeed();
+    };
+
+    //=============================================================================
+    // Window_Options
+    //  ミュート状態の更新処理を追記します。
+    //=============================================================================
+    var _Game_Switches_setValue = Game_Switches.prototype.setValue;
+    Game_Switches.prototype.setValue = function(switchId, value) {
+        _Game_Switches_setValue.apply(this, arguments);
+        if (switchId === param.audioThroughSwitchId) {
+            if (value) {
+                AudioManager.applyMute();
+            } else {
+                AudioManager.restoreMuteIfNeed();
+            }
+        }
+    };
+
+    //=============================================================================
+    // AudioManager
+    //  ミュート時の音声ファイルのロードを行わないよう変更します。
+    //=============================================================================
+    AudioManager.restoreMuteIfNeed = function() {
+        if (!this.isBgmMute() && this._currentBgm) {
+            this.replayBgm(this._currentBgm);
+        }
+        if (!this.isBgsMute() && this._currentBgs) {
+            this.replayBgs(this._currentBgs);
+        }
+    };
+
+    AudioManager.applyMute = function() {
+        var prevCurrentBgm = this._currentBgm;
+        var prevCurrentBgs = this._currentBgs;
+        AudioManager.stopAll();
+        this._currentBgm = prevCurrentBgm;
+        this._currentBgs = prevCurrentBgs;
+    };
+
+    //=============================================================================
     // AudioManager
     //  ミュート時の音声ファイルのロードを行わないよう変更します。
     //=============================================================================
     var _AudioManager_playBgm = AudioManager.playBgm;
     AudioManager.playBgm = function(bgm, pos) {
         if (this.isBgmMute()) {
+            this.stopBgm();
+            this.updateCurrentBgm(bgm, pos);
+            this._muteBgm = true;
             return;
+        } else if (this._muteBgm) {
+            this._muteBgm = false;
+            this._currentBgm = null;
         }
         _AudioManager_playBgm.apply(this, arguments);
     };
@@ -108,7 +162,13 @@
     var _AudioManager_playBgs = AudioManager.playBgs;
     AudioManager.playBgs = function(bgs, pos) {
         if (this.isBgsMute()) {
+            this.stopBgs();
+            this.updateCurrentBgs(bgs, pos);
+            this._muteBgs = true;
             return;
+        } else if (this._muteBgs) {
+            this._muteBgs = false;
+            this._currentBgs = null;
         }
         _AudioManager_playBgs.apply(this, arguments);
     };
@@ -116,6 +176,7 @@
     var _AudioManager_playMe = AudioManager.playMe;
     AudioManager.playMe = function(me) {
         if (this.isMeMute()) {
+            this.stopMe();
             return;
         }
         _AudioManager_playMe.apply(this, arguments);
@@ -124,6 +185,7 @@
     var _AudioManager_playSe = AudioManager.playSe;
     AudioManager.playSe = function(se) {
         if (this.isSeMute()) {
+            this.stopSe();
             return;
         }
         _AudioManager_playSe.apply(this, arguments);
