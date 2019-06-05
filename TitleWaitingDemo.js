24a56ee2d56c709a62d74cff79426973a4d67164
triacontane <triacontanetakenoko@gmail.com>
2019-06-05 23:31:49 +0900

    1.0.0 2019/06/05 初版

---
+++ TitleWaitingDemo.js
@@ -0,0 +1,144 @@
+/*=============================================================================
+ TitleWaitingDemo.js
+----------------------------------------------------------------------------
+ (C)2019 Triacontane
+ This software is released under the MIT License.
+ http://opensource.org/licenses/mit-license.php
+----------------------------------------------------------------------------
+ Version
+ 1.0.0 2019/06/05 初版
+----------------------------------------------------------------------------
+ [Blog]   : https://triacontane.blogspot.jp/
+ [Twitter]: https://twitter.com/triacontane/
+ [GitHub] : https://github.com/triacontane/
+=============================================================================*/
+
+/*:
+ * @plugindesc TitleWaitingDemoPlugin
+ * @author triacontane
+ *
+ * @param mapId
+ * @desc 移動先のマップIDです。
+ * @default 1
+ * @type number
+ *
+ * @param mapX
+ * @desc 移動先のX座標です。
+ * @default 1
+ * @type number
+ *
+ * @param mapY
+ * @desc 移動先のY座標です。
+ * @default 1
+ * @type number
+ *
+ * @param waitSecond
+ * @desc タイトル画面で待機する秒数です。
+ * @default 20
+ * @type number
+ *
+ * @help TitleWaitingDemo.js
+ *
+ * タイトル画面を表示したまま一定時間待機することで
+ * 指定したマップからニューゲーム扱いでゲーム開始します。
+ *
+ * デモ画面や隠し要素に利用できます。
+ *　
+ * このプラグインにはプラグインコマンドはありません。
+ *
+ * This plugin is released under the MIT License.
+ */
+/*:ja
+ * @plugindesc タイトル待機デモ表示プラグイン
+ * @author トリアコンタン
+ *
+ * @param mapId
+ * @text マップID
+ * @desc 移動先のマップIDです。
+ * @default 1
+ * @type number
+ *
+ * @param mapX
+ * @text X座標
+ * @desc 移動先のX座標です。
+ * @default 1
+ * @type number
+ *
+ * @param mapY
+ * @text Y座標
+ * @desc 移動先のY座標です。
+ * @default 1
+ * @type number
+ *
+ * @param waitSecond
+ * @text 待機秒数
+ * @desc タイトル画面で待機する秒数です。
+ * @default 20
+ * @type number
+ *
+ * @help TitleWaitingDemo.js
+ *
+ * タイトル画面を表示したまま一定時間待機することで
+ * 指定したマップからニューゲーム扱いでゲーム開始します。
+ *
+ * デモ画面や隠し要素に利用できます。
+ *　
+ * このプラグインにはプラグインコマンドはありません。
+ *
+ * 利用規約：
+ *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
+ *  についても制限はありません。
+ *  このプラグインはもうあなたのものです。
+ */
+
+(function() {
+    'use strict';
+
+    /**
+     * Create plugin parameter. param[paramName] ex. param.commandPrefix
+     * @param pluginName plugin name(EncounterSwitchConditions)
+     * @returns {Object} Created parameter
+     */
+    var createPluginParameter = function(pluginName) {
+        var paramReplacer = function(key, value) {
+            if (value === 'null') {
+                return value;
+            }
+            if (value[0] === '"' && value[value.length - 1] === '"') {
+                return value;
+            }
+            try {
+                return JSON.parse(value);
+            } catch (e) {
+                return value;
+            }
+        };
+        var parameter     = JSON.parse(JSON.stringify(PluginManager.parameters(pluginName), paramReplacer));
+        PluginManager.setParameters(pluginName, parameter);
+        return parameter;
+    };
+
+    var param = createPluginParameter('TitleWaitingDemo');
+
+    var _Scene_Title_start = Scene_Title.prototype.start;
+    Scene_Title.prototype.start = function() {
+        _Scene_Title_start.apply(this, arguments);
+        this._waitingDemoFrame = 0;
+    };
+
+    var _Scene_Title_update = Scene_Title.prototype.update;
+    Scene_Title.prototype.update = function() {
+        _Scene_Title_update.apply(this, arguments);
+        this._waitingDemoFrame++;
+        if (this._waitingDemoFrame > param.waitSecond * 60 && !this._startWaitingDemo) {
+            this.startWaitingDemo();
+            this._startWaitingDemo = true;
+        }
+    };
+
+    Scene_Title.prototype.startWaitingDemo = function() {
+        this.commandNewGame();
+        $gamePlayer.reserveTransfer(param.mapId, param.mapX, param.mapY);
+        this._commandWindow.open();
+    };
+})();
