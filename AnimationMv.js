47ed32576b7c78d229e0a65b87dfcd0746353156
トリアコンタン <triacontanetakenoko@gmail.com>
2020-08-16 16:06:54 +0900

    1.1.0 2020/06/16 MVプロジェクトを指定するだけで必要ファイルを自動コピーできる機能を追加

--- AnimationMv.js
+++ AnimationMv.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.1.0 2020/06/16 MVプロジェクトを指定するだけで必要ファイルを自動コピーできる機能を追加
  1.0.0 2020/06/15 初版
 ----------------------------------------------------------------------------
  [Blog]   : https://triacontane.blogspot.jp/
@@ -15,34 +16,6 @@
 
 /*:
  * @target MZ
- * @plugindesc AnimationMvPlugin
- * @author triacontane
- * @base PluginCommonBase
- * @orderAfter PluginCommonBase
- * @url https://raw.githubusercontent.com/triacontane/RPGMakerMZ/master/js/plugins/AnimationMv.js
- *
- * @param AnimationList
- * @desc 使用するアニメーション素材のリストです。登録しておけば未使用素材削除の対象から外れます。
- * @default []
- * @dir img/animations/
- * @type file[]
- *
- * @help AnimationMv.js
- *
- * RPGツクールMVのアニメーション再生方式とMZの再生方式との併用を可能にします。
- *
- * MVのエディタでアニメーションを作成後、データファイル『Animation.json』を
- * コピーして『data/mv』フォルダ配下に配置してください。
- * また『img/animations』フォルダ以下にMV規格のアニメーション画像を
- * 配置してください。
- *
- * MZ側のデータベースで『パーティクルエフェクト』『効果音』『フラッシュ』を
- * 全て空の状態(新規作成の状態)で作成すると、MVのアニメーションを再生します。
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
- * @target MZ
  * @plugindesc MVアニメーションプラグイン
  * @author トリアコンタン
  * @base PluginCommonBase
@@ -50,23 +23,36 @@
  * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/AnimationMv.js
  *
  * @param AnimationList
- * @text アニメーション素材のリスト
+ * @text アニメーションリスト
  * @desc 使用するアニメーション素材のリストです。登録しておけば未使用素材削除の対象から外れます。
  * @default []
  * @dir img/animations/
  * @type file[]
  *
+ * @param MvProjectPathText
+ * @text MVプロジェクトの絶対パス
+ * @desc アニメーションを編集するMVプロジェクトのパスです。空にすると自動コピーが無効になります。
+ * @default
+ *
+ * @param NoCopyImageFile
+ * @text 画像はコピーしない
+ * @desc アニメーション画像ファイルを自動コピーしなくなります。コピーに時間が掛かっている場合に有効です。
+ * @default false
+ * @type boolean
+ *
  * @help AnimationMv.js
+ * RPGツクールMVのアニメーション再生方式と新しいMZの再生方式との併用できます。
  *
- * RPGツクールMVのアニメーション再生方式とMZの再生方式との併用を可能にします。
- *
- * MVのエディタでアニメーションを作成後、データファイル『Animation.json』を
- * コピーして『data/mv』フォルダ配下に配置してください。
- * また『img/animations』フォルダ以下にMV規格のアニメーション画像を
- * 配置してください。
+ * RPGツクールMVのエディタでアニメーションデータを編集して、
+ * パラメータからMVプロジェクトのパスを指定してください。
+ * テストプレー時に『img/animations』以下の画像と『data/Animations.json』が
+ * MZプロジェクトに自動でコピーされます。
+ * 『Animations.json』は『mv/Animations.json』にコピーされます。
  *
  * MZ側のデータベースで『パーティクルエフェクト』『効果音』『フラッシュ』を
  * 全て空の状態(新規作成の状態)で作成すると、MVのアニメーションを再生します。
+ *
+ * 本プラグインはグローバル変数『$dataMvAnimations』を使用します。
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -76,11 +62,86 @@
 
 (() => {
     'use strict';
+    const script = document.currentScript;
+    const param  = PluginManagerEx.createParameter(script);
 
     const variableName = '$dataMvAnimations';
     const variableSrc = 'mv/Animations.json';
 
+    const node  = {
+        fs  : require('fs'),
+        path: require('path')
+    };
+
+    /**
+     * MvFileCopyUtil
+     *  MVのプロジェクトからファイルをコピーするユーティリティクラスです。
+     */
+    class MvFileCopyUtil {
+        static copyMvAnimationData() {
+            const mvRoot = param.MvProjectPathText;
+            if (!this.isNeedFileCopy() || !mvRoot) {
+                return ;
+            }
+            if (!node.fs.existsSync(mvRoot + '/index.html')) {
+                PluginManagerEx.throwError(`Invalid MV Project Path :${mvRoot}`, script);
+            }
+            this.copyMvAnimationJson(mvRoot);
+            if (!param.NoCopyImageFile) {
+                this.copyMvAnimationImage(mvRoot);
+            }
+        }
+
+        static copyMvAnimationJson(mvRoot) {
+            const srcPath = node.path.join(mvRoot, 'data/');
+            const destPath = this.fileDirectoryPath('data/mv/');
+            StorageManager.fsMkdir(destPath);
+            this.copyFile(srcPath, destPath, 'Animations.json');
+        }
+
+        static copyMvAnimationImage(mvRoot) {
+            const srcPath = node.path.join(mvRoot, 'img/animations/');
+            const destPath = this.fileDirectoryPath('img/animations/');
+            StorageManager.fsMkdir(destPath);
+            this.copyAllFiles(srcPath, destPath, 'Animations.json');
+        }
+
+        static fileDirectoryPath = function(directory) {
+            const base = node.path.dirname(process.mainModule.filename);
+            return node.path.join(base, `${directory}/`);
+        }
+
+        static copyAllFiles(originalPath, targetPath) {
+            const copyFile = this.copyFile.bind(this, originalPath, targetPath);
+            node.fs.readdir(originalPath, function(error, list) {
+                if (error || !list) {
+                    console.warn(error);
+                    return;
+                }
+                list.forEach(function(fileName) {
+                    copyFile(fileName);
+                });
+            });
+        }
+
+        static copyFile(originalPath, targetPath, fileName) {
+            node.fs.copyFileSync(originalPath + fileName, targetPath + fileName);
+        }
+
+        static isNeedFileCopy() {
+            return Utils.isOptionValid('test') || DataManager.isBattleTest() || DataManager.isEventTest();
+        }
+    }
+
     DataManager._databaseFiles.push({ name: variableName, src: variableSrc });
+
+    const _DataManager_loadDatabase = DataManager.loadDatabase;
+    DataManager.loadDatabase = function() {
+        if (Utils.isNwjs()) {
+            MvFileCopyUtil.copyMvAnimationData();
+        }
+        _DataManager_loadDatabase.apply(this, arguments);
+    };
 
     const _DataManager_loadDataFile = DataManager.loadDataFile;
     DataManager.loadDataFile = function(name, src) {
