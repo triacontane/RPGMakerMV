0fb50884c8d05c1b0abc831eb742b496799a49e3
triacontane <triacontanekinoko@gmail.com>
2020-08-18 20:32:16 +0900

    1.0.1 2020/08/18 英語ヘルプ作成

--- GameoverImageChange.js
+++ GameoverImageChange.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.0.1 2020/08/18 英語ヘルプ作成
  1.0.0 2020/08/18 初版
 ----------------------------------------------------------------------------
  [Blog]   : https://triacontane.blogspot.jp/
@@ -14,6 +15,25 @@
 =============================================================================*/
 
 /*:
+ * @plugindesc GameoverImageChangePlugin
+ * @author triacontane
+ *
+ * @param gameoverList
+ * @text List of images and MEs
+ * @desc This is the list that sets the game over image and ME change conditions.
+ * @default []
+ * @type struct<Record>[]
+ *
+ * @help GameoverImageChange.js
+ *
+ * Change the game over image and ME to suit the situation.
+ * Set the image, ME, and conditional switch.
+ * You can set multiple images, and when multiple conditions are met,
+ * the setting above the list takes precedence.
+ *
+ * This plugin is released under the MIT License.
+ */
+/*:ja
  * @plugindesc ゲームオーバー画像変更プラグイン
  * @author トリアコンタン
  *
@@ -38,6 +58,27 @@
  */
 
 /*~struct~Record:
+ * @param ImageFile
+ * @desc This is the game over image that will be displayed when the specified switch is ON.
+ * @default
+ * @require 1
+ * @dir img/system/
+ * @type file
+ *
+ * @param AudioFile
+ * @desc This is the file name of the ME file to be played when the switch specified by the condition is ON.
+ * @default
+ * @require 1
+ * @dir audio/me/
+ * @type file
+ *
+ * @param SwitchId
+ * @desc The switch is used to display the specified image or ME.
+ * @default 1
+ * @type switch
+ */
+
+/*~struct~Record:ja
  * @param ImageFile
  * @text 画像ファイル
  * @desc 条件で指定したスイッチがONのときに表示されるゲームオーバー画像です。指定しなかった場合はデフォルト画像が表示されます。
