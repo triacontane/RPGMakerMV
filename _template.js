0457f9a52ef5b62c20e5df7aafe2bc4116619f2f
トリアコンタン <triacontanetakenoko@gmail.com>
2020-08-20 17:33:40 +0900

    テンプレート登録

---
+++ _template.js
@@ -0,0 +1,68 @@
+/*=============================================================================
+ ${NAME}.js
+----------------------------------------------------------------------------
+ (C)${YEAR} Triacontane
+ This software is released under the MIT License.
+ http://opensource.org/licenses/mit-license.php
+----------------------------------------------------------------------------
+ Version
+ 1.0.0 ${DATE} 初版
+----------------------------------------------------------------------------
+ [Blog]   : https://triacontane.blogspot.jp/
+ [Twitter]: https://twitter.com/triacontane/
+ [GitHub] : https://github.com/triacontane/
+=============================================================================*/
+
+/*:
+ * @plugindesc ${NAME}Plugin
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/${NAME}.js
+ * @base PluginCommonBase
+ * @author triacontane
+ *
+ * @help ${NAME}.js
+ *
+ * This plugin is released under the MIT License.
+ */
+/*:ja
+ * @plugindesc ${NAME}プラグイン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/${NAME}.js
+ * @base PluginCommonBase
+ * @author トリアコンタン
+ *
+ * @param param
+ * @text パラメータ名称
+ * @desc パラメータ説明
+ * @default
+ * @type number
+ *
+ * @command command
+ * @text コマンド名称
+ * @desc コマンド説明
+ *
+ * @arg arg
+ * @text 引数名称
+ * @desc 引数説明
+ * @default
+ * @type number
+ *
+ * @help ${NAME}.js
+ *　
+ * このプラグインにはプラグインコマンドはありません。
+ *
+ * 利用規約：
+ *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
+ *  についても制限はありません。
+ *  このプラグインはもうあなたのものです。
+ */
+
+(()=> {
+    'use strict';
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
+
+    PluginManagerEx.registerCommand(script, 'command', args => {
+
+    });
+})();
