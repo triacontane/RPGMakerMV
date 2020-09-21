afa3849ebd19194448580b59f44e6265d1698e13
トリアコンタン <triacontanetakenoko@gmail.com>
2020-09-21 12:59:47 +0900

    1.1.0 2020/09/21 指定したスイッチがONのときのみプラグインの機能を有効にする設定を追加

--- PlayerShiftTurn.js
+++ PlayerShiftTurn.js
@@ -1,15 +1,16 @@
 //=============================================================================
 // PlayerShiftTurn.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2015 Triacontane
+// (C)2016 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2020/09/21 指定したスイッチがONのときのみプラグインの機能を有効にする設定を追加
 // 1.0.1 2016/07/09 8方向移動系（かつキャラクターの向きは4方向）のプラグインとの競合を解消
 // 1.0.0 2016/01/06 初版
 // ----------------------------------------------------------------------------
-// [Blog]   : http://triacontane.blogspot.jp/
+// [Blog]   : https://triacontane.blogspot.jp/
 // [Twitter]: https://twitter.com/triacontane/
 // [GitHub] : https://github.com/triacontane/
 //=============================================================================
@@ -18,10 +19,20 @@
  * @plugindesc プレイヤーのその場方向転換
  * @author トリアコンタン
  *
- * @param ボタン名称
+ * @param buttonName
+ * @text ボタン名称
  * @desc その場方向転換に使用するボタンです。
- * (shift or control or tab)
  * @default shift
+ * @type combo
+ * @option shift
+ * @option control
+ * @option tab
+ *
+ * @param validSwitchId
+ * @text 有効スイッチ番号
+ * @desc 指定したスイッチがONのときのみプラグインの機能が有効になります。0を指定すると常に有効になります。
+ * @default 0
+ * @type switch
  *
  * @help 指定されたキーを押している間、プレイヤーを移動させずに
  * その場で方向転換します。
@@ -35,24 +46,26 @@
     'use strict';
     var pluginName = 'PlayerShiftTurn';
 
-    var getParamString = function(paramNames) {
-        var value = getParamOther(paramNames);
-        return value == null ? '' : value;
+    var createPluginParameter = function(pluginName) {
+        var paramReplacer = function(key, value) {
+            if (value === 'null') {
+                return value;
+            }
+            if (value[0] === '"' && value[value.length - 1] === '"') {
+                return value;
+            }
+            try {
+                return JSON.parse(value);
+            } catch (e) {
+                return value;
+            }
+        };
+        var parameter     = JSON.parse(JSON.stringify(PluginManager.parameters(pluginName), paramReplacer));
+        PluginManager.setParameters(pluginName, parameter);
+        return parameter;
     };
 
-    var getParamOther = function(paramNames) {
-        if (!Array.isArray(paramNames)) paramNames = [paramNames];
-        for (var i = 0; i < paramNames.length; i++) {
-            var name = PluginManager.parameters(pluginName)[paramNames[i]];
-            if (name) return name;
-        }
-        return null;
-    };
-
-    //=============================================================================
-    // パラメータの取得と整形
-    //=============================================================================
-    var paramButtonName = getParamString(['ButtonName', 'ボタン名称']).toLowerCase();
+    var param = createPluginParameter(pluginName);
 
     //=============================================================================
     // Game_Player
@@ -60,13 +73,17 @@
     //=============================================================================
     var _Game_Player_executeMove = Game_Player.prototype.executeMove;
     Game_Player.prototype.executeMove = function(direction) {
-        if (Input.isPressed(paramButtonName)) {
+        if (Input.isPressed(param.buttonName) && this.isValidShiftTurn()) {
             if (direction === Input.dir4) {
                 this.setDirection(direction);
             }
         } else {
             _Game_Player_executeMove.apply(this, arguments);
         }
+    };
+
+    Game_Player.prototype.isValidShiftTurn = function() {
+        return !param.validSwitchId || $gameSwitches.value(param.validSwitchId);
     };
 })();
 
