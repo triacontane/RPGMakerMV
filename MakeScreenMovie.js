51024527c0273fc2c2d5c18ea3bca15c79356979
triacontane <triacontanekinoko@gmail.com>
2020-10-13 23:58:22 +0900

    1.0.1 2020/10/13 MZで動作するよう修正

--- MakeScreenMovie.js
+++ MakeScreenMovie.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.0.1 2020/10/13 MZで動作するよう修正
  1.0.0 2019/01/05 初版
 ----------------------------------------------------------------------------
  [Blog]   : https://triacontane.blogspot.jp/
@@ -14,109 +15,11 @@
 =============================================================================*/
 
 /*:
- * @plugindesc MakeScreenMoviePlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @param FunkKeyRecord
- * @desc ゲーム画面の録画と停止を行うキーです。この機能による録画はテストプレー時のみ有効です。
- * @default F10
- * @type select
- * @option none
- * @option F1
- * @option F2
- * @option F3
- * @option F4
- * @option F5
- * @option F6
- * @option F7
- * @option F8
- * @option F9
- * @option F10
- * @option F11
- * @option F12
- *
- * @param RecordSwitchId
- * @desc 指定したスイッチがONになると自動で録画を開始します。
- * @default 0
- * @type switch
- *
- * @param FileName
- * @desc 作成した動画のファイル名です。制御文字\V[n]が使えるほか、\Dで日付文字列に変換できます。
- * @default movie_\d
- *
- * @param Location
- * @desc ファイルの出力パスです。相対パス、絶対パスが利用できます。
- * 区切り文字は「/」もしくは「\」で指定してください。
- * @default records
- *
- * @param IncludeAudio
- * @desc 録画した動画に音声データを含めるかどうかを選択できます。
- * @default true
- * @type boolean
- *
- * @param MimeType
- * @desc 生成する動画のMIMEタイプです。通常は自動で問題ありません。サポート外の規格を選択すると自動選択されます。
- * @default 自動
- * @type select
- * @option 自動
- * @option video/webm;codecs=vp9
- * @option video/webm;codecs=vp8
- * @option video/webm;codecs=h264
- * @option video/webm
- *
- * @param RefreshRate
- * @text リフレッシュレート
- * @desc 動画のリフレッシュレートです。-1を指定すると自動で設定されます。0を指定すると静止画になります。
- * @default -1
- * @type number
- * @min -1
- * @max 120
- *
- * @help MakeScreenMovie.js
- *
- * ゲーム画面を録画してwebm形式で保存できます。
- * 指定したファンクションキーを押すか、スイッチがONになると録画開始します。
- * もう一度ファンクションキーを押すか、スイッチがOFFになると録画終了します。
- * 主に進捗や紹介用の動画を作成する際の制作補助プラグインとして使います。
- * ファイル名や保存場所、音声を含めるかどうかを設定できます。
- *
- * テストプレーのみ録画中「●REC」と表示されますが、実際の動画には含まれません。
- *
- * MoviePicture.jsと組み合わせると録画した動画をピクチャとして再生できます。
- * 1. 録画を終了して少し待つ。(ファイルの保存は非同期なので)
- *
- * 2.「変数の操作」の「スクリプト」から以下を実行する。
- * StorageManager.getLatestMovieFilePath();
- *
- * 3.「プラグインコマンド」から以下を実行する。(n: 2.で保存した変数番号)
- * MP_SET_OUTER_MOVIE \v[n]
- *
- * 4.「ピクチャの表示」を実行すると動画が再生される。
- *
- * このプラグインは、ウェブブラウザ実行もしくはver1.6.0より古いツクールMVでは
- * 動作しません。うまく動作しない場合は、F12キーでデベロッパツールを開いて
- * 「MakeScreenMovie.js」と記述されたログが出力されていないかをご確認ください。
- *
- * ・制約事項
- * 長時間の録画は、環境次第でPCの動作が不安定になる場合があります。
- * 録画中は「文章の表示」で表示する文章が全て瞬間表示されます。
- * 予期しない動作が起こった場合、こちらで対応できない場合があります。
- *
- * ・参考にした記事
- * https://qiita.com/ru_shalm/items/0930aedad12c4e100446
- *　
- * このプラグインにはプラグインコマンドはありません。
- *
- * 利用規約：
- *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
- *  についても制限はありません。
- *  このプラグインはもうあなたのものです。
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc ゲーム動画作成プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/MakeScreenMovie.js
+ * @base PluginCommonBase
+ * @author トリアコンタン
  *
  * @param FunkKeyRecord
  * @text 録画ファンクションキー
@@ -189,25 +92,7 @@
  *
  * テストプレーのみ録画中「●REC」と表示されますが、実際の動画には含まれません。
  *
- * MoviePicture.js組み合わせると録画した動画をピクチャとして再生できます。
- * 1. 録画を終了して少し待つ。(ファイルの保存は非同期なので)
- *
- * 2.「変数の操作」の「スクリプト」から以下を実行する。
- * StorageManager.getLatestMovieFilePath();
- *
- * 3.「プラグインコマンド」から以下を実行する。(n: 2.で保存した変数番号)
- * MP_SET_OUTER_MOVIE \v[n]
- *
- * 4.「ピクチャの表示」を実行すると動画が再生される。
- *
- * このプラグインは、ウェブブラウザ実行もしくはver1.6.0より古いツクールMVでは
- * 動作しません。うまく動作しない場合は、F12キーでデベロッパツールを開いて
- * 「MakeScreenMovie.js」と記述されたログが出力されていないかをご確認ください。
- *
- * ・制約事項
- * 長時間の録画は、環境次第でPCの動作が不安定になる場合があります。
- * 録画中は「文章の表示」で表示する文章が全て瞬間表示されます。
- * 予期しない動作が起こった場合、こちらで対応できない場合があります。
+ * あまりに長時間の録画は動作が不安定になる場合があります。
  *
  * ・参考にした記事
  * https://qiita.com/ru_shalm/items/0930aedad12c4e100446
@@ -223,36 +108,13 @@
 (function() {
     'use strict';
 
-    if (!Utils.isNwjs() || Utils.RPGMAKER_VERSION < '1.6.0') {
-        console.warn('MakeScreenMovie.js: Invalid MV version.');
+    if (!Utils.isNwjs()) {
+        console.error('MakeScreenMovie.js: This plugin use only Game.exe');
         return;
     }
 
-    /**
-     * Create plugin parameter. param[paramName] ex. param.commandPrefix
-     * @param pluginName plugin name(EncounterSwitchConditions)
-     * @returns {Object} Created parameter
-     */
-    const createPluginParameter = function(pluginName) {
-        const paramReplacer = function(key, value) {
-            if (value === 'null') {
-                return value;
-            }
-            if (value[0] === '"' && value[value.length - 1] === '"') {
-                return value;
-            }
-            try {
-                return JSON.parse(value);
-            } catch (e) {
-                return value;
-            }
-        };
-        const parameter     = JSON.parse(JSON.stringify(PluginManager.parameters(pluginName), paramReplacer));
-        PluginManager.setParameters(pluginName, parameter);
-        return parameter;
-    };
-
-    const param = createPluginParameter('MakeScreenMovie');
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
 
     const MIME_TYPES = [
         'video/webm;codecs=vp9',
@@ -353,7 +215,7 @@
      * Game_Switches
      * スイッチに連動してレコーダーを更新します。
      */
-    var _Game_Switches_setValue      = Game_Switches.prototype.setValue;
+    const _Game_Switches_setValue      = Game_Switches.prototype.setValue;
     Game_Switches.prototype.setValue = function(variableId, value) {
         _Game_Switches_setValue.apply(this, arguments);
         if (variableId === param.RecordSwitchId) {
@@ -380,7 +242,7 @@
     };
 
     SceneManager.onKeyDownForScreenMovie = function(event) {
-        if (event.keyCode === Input.functionReverseMapper[param.FunkKeyRecord]) {
+        if (event.key === param.FunkKeyRecord) {
             this._screenRecorder.toggle();
         }
     };
@@ -412,7 +274,7 @@
         let filePath = param.Location;
         if (!filePath.match(/^[A-Z]:/)) {
             const path = require('path');
-            filePath   = path.join(path.dirname(this.localFileDirectoryPath()), filePath);
+            filePath   = path.join(path.dirname(process.mainModule.filename), filePath);
         }
         return filePath.match(/\/$/) ? filePath : filePath + '/';
     };
@@ -489,32 +351,5 @@
         const oscillator = audioContext.createOscillator();
         oscillator.connect(destination);
         return destination.stream;
-    };
-
-    /**
-     * Input
-     * ファンクションキーをコードを紐付けます。
-     */
-    Input.functionReverseMapper = {
-        F1 : 112,
-        F2 : 113,
-        F3 : 114,
-        F4 : 115,
-        F5 : 116,
-        F6 : 117,
-        F7 : 118,
-        F8 : 119,
-        F9 : 120,
-        F10: 121,
-        F11: 122,
-        F12: 123
-    };
-
-    const _Window_Message_updateShowFast = Window_Message.prototype.updateShowFast;
-    Window_Message.prototype.updateShowFast = function() {
-        _Window_Message_updateShowFast.apply(this, arguments);
-        if (SceneManager.isScreenRecording()) {
-            this._showFast = true;
-        }
     };
 })();
