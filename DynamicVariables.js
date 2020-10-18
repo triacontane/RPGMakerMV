d2c5f0679d1fcd11d1e3d196e41fac720761b1cf
トリアコンタン <triacontanetakenoko@gmail.com>
2020-10-18 16:55:23 +0900

    1.2.0 2020/10/18 リファクタリングとヘルプの整理

--- DynamicVariables.js
+++ DynamicVariables.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.2.0 2020/10/18 リファクタリングとヘルプの整理
 // 1.1.1 2018/09/27 テストプレー時はパラメータ「例外処理」の値に関係なくスクリプトエラーで異常終了しないよう修正
 // 1.1.0 2018/08/19 イベントページの出現条件および敵キャラの行動パターンで各オブジェクトおよびデータを参照できる機能を追加
 // 1.0.5 2018/01/27 1.0.4の更新でもともと入っていた値の取得方法をvalueに変更
@@ -25,103 +26,59 @@
  * @author triacontane
  *
  * @param DynamicSwitchStart
- * @desc 動的スイッチの開始位置番号です。
+ * @desc The start position number of the dynamic switch.
  * @default 0
  * @type switch
  *
  * @param DynamicSwitchEnd
- * @desc 動的スイッチの終了位置番号です。
+ * @desc The end position number of the dynamic switch.
  * @default 0
  * @type switch
  *
  * @param DynamicVariableStart
- * @desc 動的変数の開始位置番号です。
+ * @desc The start position number of the dynamic variable.
  * @default 0
  * @type variable
  *
  * @param DynamicVariableEnd
- * @desc 動的変数の終了位置番号です。
+ * @desc The end position number of the dynamic variable.
  * @default 0
  * @type variable
  *
  * @param ValidException
- * @desc スクリプト実行時に例外処理を行います。不正なスクリプトで強制終了しなくなる代わりに実行速度が僅かに低下します。
+ * @desc Handles exceptions during script execution.
  * @default false
  * @type boolean
  *
- * @help 指定範囲内の変数、スイッチを参照しようとした際に、
- * 「変数名称」及び「スイッチ名称」をJavaScriptとして評価した結果を返すようにします。
- * JavaScript中では式の簡略化のために以下のローカル変数が利用できます。
+ * @help When a variable or switch in the specified range is referenced
+ * Returns the result of evaluating "variable name" and "switch name" as a script.
+ * The following local variables can be used in the script
  *
- * id        # 処理対象のスイッチ、変数ID
- * value     # 処理対象のスイッチ、変数IDにもともと入っていた値
- * v(n)      # [n]番の変数の値
- * s(n)      # [n]番のスイッチの値
- * max       # Math.maxに変換されます。(例 : max(1, 2) -> 2)
- * min       # Math.minに変換されます。(例 : min(1, 2) -> 1)
- * gPlayer   # $gamePlayerに変換されます。(例 : gPlayer.x -> プレイヤーX座標)
- * dSystem   # $dataSystemsに変換されます。(例：dSystem.startX -> 開始位置X座標)
- * ※ Math.max以外にも一般的なMathモジュールのメソッドが使用可能です。
- *    また、他のゲームオブジェクト、データオブジェクトも同じ記法で参照できます。
- *    いずれも利用には、多少のJavaScriptの知識が必要になります。
+ * id # ID of the switch or variable to be processed
+ * value # the value that was originally in the switch or variable ID to be processed
  *
- * イベントページの出現条件でスイッチを参照する場合、追加で以下の変数が使えます。
- * e         # イベントオブジェクトへの参照
- * d         # イベントデータへの参照
+ * Dynamic variables are valid in all places (*) that refer to variables and switches.
  *
- * 敵キャラの行動パターンでスイッチを参照する場合、追加で以下の変数が使えます。
- * e         # 敵キャラオブジェクトへの参照
- * d         # 敵キャラデータへの参照
+ * Examples of referring to variables and switches
+ * 1. decision processing of the event page
+ * 2. enemy character action decision processing
+ * 3. conditional branching
+ * 4. various operands of event commands.
+ * 5. Other plug-in references
  *
- * 上記以外の状況でeやdを参照するとエラーになるで注意してください。
+ * You can also reference additional dynamic variables and switches in your scripts, but you can also use the
+ * If you try to reference the variable (switch) with the same number, a circular reference occurs and an error occurs.
  *
- * 動的変数は、指定範囲内の変数およびスイッチを参照するすべての箇所(※)で有効です。
- * また、範囲内の変数及びスイッチに対する値の設定は無視されます。
+ * If you try to refer to a variable (switch) with the same number, it will be a circular reference and an error will occur.
+ * Normally, the process of determining the page of events is done when one of the switches or variables is
+ * It only runs when it is changed.
+ * The plugin retains that specification for performance reasons.
+ * If you enter the following in the event memo field, only that event will be executed when
+ * You can change it to always check the page conditions.
  *
- * ※変数・スイッチを参照する例
- * 1. イベントページの決定処理
- * 2. 敵キャラの行動決定処理
- * 3. 条件分岐
- * 4. イベントコマンドの各種オペランド
- * 5. スキルやアイテムの計算式（ただしv[1]で参照した場合は除く）
- * 6. その他プラグインによる参照
- *
- * スクリプト中でさらに動的変数およびスイッチを参照することもできますが
- * 同じ番号の変数 or スイッチを参照しようとすると循環参照となりエラーが発生します。
- *
- * ・設定例1
- * イベントページの「スイッチ」に「名称」が以下のものを設定する。
- * v(1) > 0 || s(1) # 変数[1]が[0]より大きい、もしくはスイッチ[1]がON
- * 以上の条件を満たした場合に対象ページが表示される。
- *
- * ・設定例2
- * イベントページの「スイッチ」に「名称」が以下のものを設定する。
- * v(1) < v(2) && !s(2) # 変数[1]が変数[2]より小さい、かつスイッチ[2]がOFF
- * 以上の条件を満たした場合に対象ページが表示される。
- *
- * ・設定例3
- * イベントページの「スイッチ」に「名称」が以下のものを設定する。
- * gPlayer.x % 2 === 0 # プレイヤーのX座標が偶数
- * 以上の条件を満たした場合に対象ページが表示される。
- *
- * ・使用上の注意
- * 通常、イベントのページを決定する処理は、いずれかのスイッチもしくは変数が
- * 変更されたときにのみ実行されます。本プラグインでは、パフォーマンス上の理由から
- * その仕様を維持します。
- * もし、指定している条件がスイッチおよび変数と完全に無関係な場合(設定例3のケース等)
- * イベントのメモ欄に以下の通り入力すると、そのイベントのみ
- * 常にページの条件チェックを行うように変更できます。
- *
- * <DV常時リフレッシュ>
  * <DVAlwaysRefresh>
  *
- * （参考）基本的なJavaScript演算子
- * ・四則演算    : + - * /
- * ・論理和(Or)  : ||
- * ・論理積(And) : &&
- * ・否定(Not)   : !
- *
- * このプラグインにはプラグインコマンドはありません。
+ * There are no plug-in commands in this plugin.
  *
  * This plugin is released under the MIT License.
  */
@@ -129,95 +86,72 @@
  * @plugindesc 動的変数プラグイン
  * @author トリアコンタン
  *
- * @param 動的スイッチ開始位置
+ * @param DynamicSwitchStart
+ * @text 動的スイッチ開始位置
  * @desc 動的スイッチの開始位置番号です。
  * @default 0
  * @type switch
  *
- * @param 動的スイッチ終了位置
+ * @param DynamicSwitchEnd
+ * @text 動的スイッチ終了位置
  * @desc 動的スイッチの終了位置番号です。
  * @default 0
  * @type switch
  *
- * @param 動的変数開始位置
+ * @param DynamicVariableStart
+ * @text 動的変数開始位置
  * @desc 動的変数の開始位置番号です。
  * @default 0
  * @type variable
  *
- * @param 動的変数終了位置
+ * @param DynamicVariableEnd
+ * @text 動的変数終了位置
  * @desc 動的変数の終了位置番号です。
  * @default 0
  * @type variable
  *
- * @param 例外処理
+ * @param ValidException
+ * @text 例外処理
  * @desc スクリプト実行時に例外処理を行います。不正なスクリプトで強制終了しなくなる代わりに実行速度が僅かに低下します。
  * @default false
  * @type boolean
  *
- * @help 指定範囲内の変数、スイッチを参照しようとした際に、
- * 「変数名称」及び「スイッチ名称」をJavaScriptとして実行した結果を返すよう
- * 変更します。JavaScript中では式の簡略化のために以下のローカル変数が利用できます。
+ * @help 指定範囲内の変数、スイッチを参照したとき
+ * 「変数名称」及び「スイッチ名称」をスクリプトとして評価した結果を返します。
+ * スクリプト中では以下のローカル変数が利用できます。
  *
- * id        # 処理対象のスイッチ、変数ID
- * value     # 処理対象のスイッチ、変数IDにもともと入っていた値
- * v(n)      # [n]番の変数の値
- * s(n)      # [n]番のスイッチの値
- * max       # Math.maxに変換されます。(例 : max(1, 2) -> 2)
- * min       # Math.minに変換されます。(例 : min(1, 2) -> 1)
- * gPlayer   # $gamePlayerに変換されます。(例 : gPlayer.x -> プレイヤーX座標)
- * dSystem   # $dataSystemsに変換されます。(例：dSystem.startX -> 開始位置X座標)
- * ※ Math.max以外にも一般的なMathモジュールのメソッドが使用可能です。
- *    また、他のゲームオブジェクト、データオブジェクトも同じ記法で参照できます。
- *    いずれも利用には、多少のJavaScriptの知識が必要になります。
+ * id    # 処理対象のスイッチ、変数ID
+ * value # 処理対象のスイッチ、変数IDにもともと入っていた値
  *
- * また、以下のスクリプトでスイッチや変数にもともと入っている値を参照できます。
- * this._data[id]
+ * イベントページの出現条件でスイッチを参照する場合、追加で以下の変数が使えます。
+ * e     # イベントオブジェクトへの参照
+ * d     # イベントデータへの参照
  *
- * 動的変数は、指定範囲内の変数およびスイッチを参照するすべての箇所(※)で有効です。
- * また、範囲内の変数及びスイッチに対する値の設定は無視されます。
+ * 敵キャラの行動パターンでスイッチを参照する場合、追加で以下の変数が使えます。
+ * e     # 敵キャラオブジェクトへの参照
+ * d     # 敵キャラデータへの参照
  *
- * ※変数・スイッチを参照する例
+ * 動的変数は、変数およびスイッチを参照するすべての箇所(※)で有効です。
+ *
+ * ※ 変数・スイッチを参照する例
  * 1. イベントページの決定処理
  * 2. 敵キャラの行動決定処理
  * 3. 条件分岐
  * 4. イベントコマンドの各種オペランド
- * 5. スキルやアイテムの計算式（ただしv[1]で参照した場合は除く）
- * 6. その他プラグインによる参照
+ * 5. その他プラグインによる参照
  *
  * スクリプト中でさらに動的変数およびスイッチを参照することもできますが
- * 同じ番号の変数 or スイッチを参照しようとすると循環参照となりエラーが発生します。
- *
- * ・設定例1
- * イベントページの「スイッチ」に「名称」が以下のものを設定する。
- * v(1) > 0 || s(1) # 変数[1]が[0]より大きい、もしくはスイッチ[1]がON
- * 以上の条件を満たした場合に対象ページが表示される。
- *
- * ・設定例2
- * イベントページの「スイッチ」に「名称」が以下のものを設定する。
- * v(1) < v(2) && !s(2) # 変数[1]が変数[2]より小さい、かつスイッチ[2]がOFF
- * 以上の条件を満たした場合に対象ページが表示される。
- *
- * ・設定例3
- * イベントページの「スイッチ」に「名称」が以下のものを設定する。
- * gPlayer.x % 2 === 0 # プレイヤーのX座標が偶数
- * 以上の条件を満たした場合に対象ページが表示される。
+ * 同じ番号の変数(スイッチ)を参照しようとすると循環参照となりエラーが発生します。
  *
  * ・使用上の注意
  * 通常、イベントのページを決定する処理は、いずれかのスイッチもしくは変数が
- * 変更されたときにのみ実行されます。本プラグインでは、パフォーマンス上の理由から
- * その仕様を維持します。
- * もし、指定している条件がスイッチおよび変数と完全に無関係な場合
- * (設定例3のケース等)イベントのメモ欄に以下の通り入力すると、そのイベントのみ
+ * 変更されたときにのみ実行されます。
+ * 本プラグインでは、パフォーマンス上の理由からその仕様を維持します。
+ * イベントのメモ欄に以下の通り入力すると、そのイベントのみ
  * 常にページの条件チェックを行うように変更できます。
  *
  * <DV常時リフレッシュ>
  * <DVAlwaysRefresh>
- *
- * （参考）基本的なJavaScript演算子
- * ・四則演算    : + - * /
- * ・論理和(Or)  : ||
- * ・論理積(And) : &&
- * ・否定(Not)   : !
  *
  * このプラグインにはプラグインコマンドはありません。
  *
@@ -229,29 +163,7 @@
 
 (function() {
     'use strict';
-    var pluginName    = 'DynamicVariables';
     var metaTagPrefix = 'DV';
-
-    var getParamNumber = function(paramNames, min, max) {
-        var value = getParamOther(paramNames);
-        if (arguments.length < 2) min = -Infinity;
-        if (arguments.length < 3) max = Infinity;
-        return (parseInt(value, 10) || 0).clamp(min, max);
-    };
-
-    var getParamBoolean = function(paramNames) {
-        var value = getParamOther(paramNames);
-        return (value || '').toUpperCase() === 'ON' || (value || '').toUpperCase() === 'TRUE';
-    };
-
-    var getParamOther = function(paramNames) {
-        if (!Array.isArray(paramNames)) paramNames = [paramNames];
-        for (var i = 0; i < paramNames.length; i++) {
-            var name = PluginManager.parameters(pluginName)[paramNames[i]];
-            if (name) return name;
-        }
-        return null;
-    };
 
     var getMetaValue = function(object, name) {
         var metaTagName = metaTagPrefix + (name ? name : '');
@@ -267,18 +179,57 @@
         return undefined;
     };
 
-    //=============================================================================
-    // パラメータの取得と整形
-    //=============================================================================
-    var paramDynamicSwitchStart   = getParamNumber(['DynamicSwitchStart', '動的スイッチ開始位置'], 0);
-    var paramDynamicSwitchEnd     = getParamNumber(['DynamicSwitchEnd', '動的スイッチ終了位置'], 0);
-    var paramDynamicVariableStart = getParamNumber(['DynamicVariableStart', '動的変数開始位置'], 0);
-    var paramDynamicVariableEnd   = getParamNumber(['DynamicVariableEnd', '動的変数終了位置'], 0);
-    var paramValidException       = getParamBoolean(['ValidException', '例外処理']);
+    var createPluginParameter = function(pluginName) {
+        var paramReplacer = function(key, value) {
+            if (value === 'null') {
+                return value;
+            }
+            if (value[0] === '"' && value[value.length - 1] === '"') {
+                return value;
+            }
+            try {
+                return JSON.parse(value);
+            } catch (e) {
+                return value;
+            }
+        };
+        var parameter     = JSON.parse(JSON.stringify(PluginManager.parameters(pluginName), paramReplacer));
+        PluginManager.setParameters(pluginName, parameter);
+        return parameter;
+    };
+
+    var param = createPluginParameter('DynamicVariables');
 
     // eval参照用
     var e = null;
     var d = null;
+    var max = Math.max, min = Math.min, abs = Math.abs, floor = Math.floor, pow = Math.pow, random = Math.random;
+    var dActors       = $dataActors;
+    var dClasses      = $dataClasses;
+    var dSkills       = $dataSkills;
+    var dItems        = $dataItems;
+    var dWeapons      = $dataWeapons;
+    var dArmors       = $dataArmors;
+    var dEnemies      = $dataEnemies;
+    var dTroops       = $dataTroops;
+    var dStates       = $dataStates;
+    var dAnimations   = $dataAnimations;
+    var dTilesets     = $dataTilesets;
+    var dCommonEvents = $dataCommonEvents;
+    var dSystem       = $dataSystem;
+    var dMapInfos     = $dataMapInfos;
+    var dMap          = $dataMap;
+    var gTemp         = $gameTemp;
+    var gSystem       = $gameSystem;
+    var gScreen       = $gameScreen;
+    var gTimer        = $gameTimer;
+    var gMessage      = $gameMessage;
+    var gSwitches     = $gameSwitches;
+    var gActors       = $gameActors;
+    var gParty        = $gameParty;
+    var gTroop        = $gameTroop;
+    var gMap          = $gameMap;
+    var gPlayer       = $gamePlayer;
 
     //=============================================================================
     // Game_Variables
@@ -287,7 +238,7 @@
     var _Game_Variables_value      = Game_Variables.prototype.value;
     Game_Variables.prototype.value = function(variableId) {
         var value = _Game_Variables_value.apply(this, arguments);
-        if (variableId >= paramDynamicVariableStart && variableId <= paramDynamicVariableEnd) {
+        if (variableId >= param.DynamicVariableStart && variableId <= param.DynamicVariableEnd) {
             return this.getDynamicValue($dataSystem.variables[variableId], variableId, value);
         } else {
             return value;
@@ -302,36 +253,9 @@
         if (!dynamicScript) {
             return value;
         }
-        var v   = $gameVariables.value.bind($gameVariables), s = $gameSwitches.value.bind($gameSwitches);
-        var max = Math.max, min = Math.min, abs = Math.abs, floor = Math.floor, pow = Math.pow, random = Math.random;
-
-        var dActors       = $dataActors;
-        var dClasses      = $dataClasses;
-        var dSkills       = $dataSkills;
-        var dItems        = $dataItems;
-        var dWeapons      = $dataWeapons;
-        var dArmors       = $dataArmors;
-        var dEnemies      = $dataEnemies;
-        var dTroops       = $dataTroops;
-        var dStates       = $dataStates;
-        var dAnimations   = $dataAnimations;
-        var dTilesets     = $dataTilesets;
-        var dCommonEvents = $dataCommonEvents;
-        var dSystem       = $dataSystem;
-        var dMapInfos     = $dataMapInfos;
-        var dMap          = $dataMap;
-        var gTemp         = $gameTemp;
-        var gSystem       = $gameSystem;
-        var gScreen       = $gameScreen;
-        var gTimer        = $gameTimer;
-        var gMessage      = $gameMessage;
-        var gSwitches     = $gameSwitches;
-        var gActors       = $gameActors;
-        var gParty        = $gameParty;
-        var gTroop        = $gameTroop;
-        var gMap          = $gameMap;
-        var gPlayer       = $gamePlayer;
-        if (paramValidException || $gameTemp.isPlaytest()) {
+        var v = $gameVariables.value.bind($gameVariables);
+        var s = $gameSwitches.value.bind($gameSwitches);
+        if (param.ValidException || $gameTemp.isPlaytest()) {
             try {
                 return eval(dynamicScript);
             } catch (e) {
@@ -350,7 +274,7 @@
     var _Game_Switches_value      = Game_Switches.prototype.value;
     Game_Switches.prototype.value = function(switchId) {
         var value = _Game_Switches_value.apply(this, arguments);
-        if (switchId >= paramDynamicSwitchStart && switchId <= paramDynamicSwitchEnd) {
+        if (switchId >= param.DynamicSwitchStart && switchId <= param.DynamicSwitchEnd) {
             return !!this.getDynamicValue($dataSystem.switches[switchId], switchId, value);
         } else {
             return value;
