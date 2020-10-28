70b85e43871797a890f961b615e1520fe9f2fafe
triacontane <triacontanekinoko@gmail.com>
2020-10-28 22:04:04 +0900

    2.0.3 2020/10/28 マウスポインタを変更したときに画面からポインタを出して戻すと、ポインタが元に戻ってしまう問題を修正(by 奏ねこま様)

--- MousePointerExtend.js
+++ MousePointerExtend.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 2.0.3 2020/10/28 マウスポインタを変更したときに画面からポインタを出して戻すと、ポインタが元に戻ってしまう問題を修正(by 奏ねこま様)
 // 2.0.2 2020/10/26 2.0.0の修正以後、「キー入力で消去」のパラメータが正常に機能していなかった問題を修正
 // 2.0.1 2020/03/24 ポインタに独自画像を使用する場合に発生しうる現象と対策についてヘルプに追記
 // 2.0.0 2019/10/17 型指定機能に対応
@@ -110,55 +111,41 @@
  * @desc カーソル用の独自画像を/pictures/以外に配置したい場合にパス名を指定してください。区切り文字[/]は不要。
  * @default
  *
- * @help Extends the shape and displayability of the mouse pointer.
- * It provides various shape changes according to switch conditions,
- * the use of a pointer to a self-portrait image, and the ability to
- * hide the image with some keypad input.
+ * @help マウスポインタの形状や表示可否を拡張します。
+ * スイッチ条件に応じた多彩な形状変化や、独自画像のポインタ利用
+ * 何らかのキー・パッド入力で非表示化する機能を提供します。
  *
- * The shape type of the pointer can be one of the following strings.
- * auto :
- * none :
- * default :
- * pointer :
- * crosshair :
- * move :
- * text :
- * wait :
- * help :
- * url1 : original1
- * url2 : original2
- * url3 : original3
- * url4 : original4
- * url5 : original5
+ * ポインタの形状タイプは以下の文字列を指定します。
+ * auto : 自動(初期値)
+ * none : 非表示
+ * default : デフォルト
+ * pointer : リンク
+ * crosshair : 十字
+ * move : 移動
+ * text : テキスト
+ * wait : 処理中
+ * help : ヘルプ
+ * url1 : 独自画像1(パラメータ参照)
+ * url2 : 独自画像2(パラメータ参照)
+ * url3 : 独自画像3(パラメータ参照)
+ * url4 : 独自画像4(パラメータ参照)
+ * url5 : 独自画像5(パラメータ参照)
  *
- * ※If you want to specify your own image, please set the image
- * file name as a parameter separately.
- * In addition to the file name, you need to set the extension. (e.g., icon.png)
+ * ※独自画像を指定する場合は、別途画像ファイル名をパラメータに設定してください。
+ * ファイル名に加えて拡張子の設定が必要です。(例：icon.png)
  *
- * When using a proprietary image as a pointer, it has been reported that once the pointer
- * has been moved out of the screen and back in, the point display may not be correct.
- * If this happens, please install a countermeasure plugin based on the following article.
+ * スイッチ条件を満たしたのに画像が表示されない場合は、ファイルパスが間違っているか
+ * 指定した画像をアイコンとして利用できないかのどちらかの可能性が高いです。
  *
- * http://tm.lucky-duet.com/viewtopic.php?f=23&t=9105
+ * 形状変化用のスイッチは3つまで指定可能で複数の条件を満たした場合は
+ * 「1」→「2」→「3」の優先度になります。
  *
- * If the image is not displayed even though the switch conditions are met,
- * the file path may be wrong or the
- * Either the specified image is not available as an icon, or it is highly likely.
- * The animation cursor (.ani) cannot be used.
- * Also, GIF files can be used but will not be animated.
+ * また、「キー入力で消去」を有効にするとキーもしくはパッド入力により
+ * ポインタを一時的に画面から消去できます。
+ * マウス主体の操作を行わない場合にポインタが邪魔にならないための仕様です。
+ * マウス関連のイベントが発生すると再びポインタが表示されます。
  *
- * Multiple switches for shape change can be specified, and when
- * multiple conditions are met, the
- * The smaller number ("1" → "2" → "3"...) takes precedence over the smaller
- * number ("1" → "2" → "3"...).  ) has priority over the smaller number.
- *
- * In addition, when "Erase by key input" is enabled, you can use the key or pad
- * input to erase the data.
- * The pointer can be temporarily erased from the screen.
- * This is a specification to keep the pointer out of the way when the mouse is not used.
- * The pointer is displayed again when a mouse-related event occurs.
- *
- * There are no plugin commands in this plugin.
+ * このプラグインにはプラグインコマンドはありません。
  *
  * This plugin is released under the MIT License.
  */
@@ -456,12 +443,6 @@
  * ※独自画像を指定する場合は、別途画像ファイル名をパラメータに設定してください。
  * ファイル名に加えて拡張子の設定が必要です。(例：icon.png)
  *
- * ポインタに独自画像を使う場合、一度ポインタを画面外に出して戻したときに
- * ポイントの表示がおかしくなる問題が報告されています。
- * 発生した場合は以下の記事に基づき対策プラグインの導入をお願いします。
- *
- * http://tm.lucky-duet.com/viewtopic.php?f=23&t=9105
- *
  * スイッチ条件を満たしたのに画像が表示されない場合は、ファイルパスが間違っているか
  * 指定した画像をアイコンとして利用できないかのどちらかの可能性が高いです。
  * アニメーションカーソル(.ani)は使用できません。
@@ -606,7 +587,40 @@
     };
 
     Graphics.updateMousePointer = function() {
-        document.body.style.cursor = this._hiddenPointer ? 'none' : this._PointerType;
+        // modified by nekoma start.
+        // document.body.style.cursor = this._hiddenPointer ? 'none' : this._PointerType;
+        if (!document.getElementById('MousePointer')) {
+            let canvas = document.getElementById('GameCanvas');
+            var span = document.createElement('span');
+            span.id = 'MousePointer';
+            span.style.position = 'absolute';
+            span.style.left = '0px';
+            span.style.top = '0px';
+            span.style.right = '0px';
+            span.style.bottom = '0px';
+            span.style.width = (Number(canvas.style.width.replace('px', '')) - 16) + 'px';
+            span.style.height = (Number(canvas.style.height.replace('px', '')) - 16) + 'px';
+            span.style.margin = 'auto';
+            span.style.zIndex = 999;
+            span.oncontextmenu = function(){ return false; };
+            document.body.appendChild(span);
+        }
+        document.getElementById('MousePointer').style.cursor = this._hiddenPointer ? 'none' : this._PointerType;
+        // modified by nekoma end.
     };
-})();
 
+    // added by nekoma start.
+    var windowResizeTimeoutId = null;
+    window.addEventListener('resize', event => {
+        let span = document.getElementById('MousePointer');
+        if (span) {
+            clearTimeout(windowResizeTimeoutId);
+            windowResizeTimeoutId = setTimeout(() => {
+                let canvas = document.getElementById('GameCanvas');
+                span.style.width = (Number(canvas.style.width.replace('px', '')) - 16) + 'px';
+                span.style.height = (Number(canvas.style.height.replace('px', '')) - 16) + 'px';
+            }, 200);
+        }
+    });
+    // added by nekoma end.
+})();
