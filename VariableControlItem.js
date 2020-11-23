c25b3abc87eb6661acdd369e31a3c5a25236e73a
トリアコンタン <triacontanetakenoko@gmail.com>
2020-11-23 12:16:42 +0900

    1.3.0 2020/11/23 変数操作の実行条件に実行者を指定できる機能を追加

--- VariableControlItem.js
+++ VariableControlItem.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.3.0 2020/11/23 変数操作の実行条件に実行者を指定できる機能を追加
 // 1.2.0 2020/11/22 MZで動作するよう全面的に修正
 // 1.1.1 2017/04/19 範囲が「なし」の場合も操作できるよう修正
 // 1.1.0 2016/10/21 加算と代入を別々のメモ欄で設定できるよう変更
@@ -29,6 +30,8 @@
  * <VCIVarNumber:3>
  * <VCISetValue:5>
  * <VCIAddValue:5>
+ * <VCISubject:actor>
+ * <VCISubject:enemy>
  *
  * The base plugin "PluginCommonBase.js" is required to use this plugin.
  * The "PluginCommonBase.js" is here.
@@ -52,6 +55,10 @@
  * <VCISetValue:5>  # 同上
  * <VCI加算値:5>    # 指定した変数に値[5]を加算します。
  * <VCIAddValue:5>  # 同上
+ * <VCI実行者:actor>   # 実行者がアクターの場合のみ変数操作します。
+ * <VCISubject:actor> # 同上
+ * <VCI実行者:enemy>   # 実行者が敵キャラの場合のみ変数操作します。
+ * <VCISubject:enemy> # 同上
  * ※加算値に負の値を指定すると減算になります。
  *
  * 設定値は、制御文字を適用した上でJavaScript計算式として評価されます。
@@ -77,7 +84,7 @@
     // Game_Action
     //  行動が成功した場合、変数の操作を実行します。
     //=============================================================================
-    const _Game_Action_applyItemUserEffect      = Game_Action.prototype.applyItemUserEffect;
+    const _Game_Action_applyItemUserEffect = Game_Action.prototype.applyItemUserEffect;
     Game_Action.prototype.applyItemUserEffect = function(target) {
         _Game_Action_applyItemUserEffect.apply(this, arguments);
         if (!this.isForNone()) {
@@ -85,7 +92,7 @@
         }
     };
 
-    const _Game_Action_applyGlobal      = Game_Action.prototype.applyGlobal;
+    const _Game_Action_applyGlobal = Game_Action.prototype.applyGlobal;
     Game_Action.prototype.applyGlobal = function(target) {
         _Game_Action_applyGlobal.apply(this, arguments);
         if (this.isForNone()) {
@@ -98,20 +105,38 @@
     };
 
     Game_Action.prototype.applyVariableControl = function() {
-        const varNumberStr = this.findMetaForVariableControl( ['VarNumber', '変数番号']);
+        if (!this.isVariableControlSubject()) {
+            return;
+        }
+        const varNumberStr = this.findMetaForVariableControl(['VarNumber', '変数番号']);
         if (varNumberStr) {
             const varNumber = parseInt(varNumberStr);
-            const setValue   = this.findMetaForVariableControl( ['SetValue', '代入値']);
+            const setValue = this.findMetaForVariableControl(['SetValue', '代入値']);
             if (setValue) {
                 $gameVariables.setValue(varNumber, eval(setValue));
                 return;
             }
-            const addValue   = this.findMetaForVariableControl(['AddValue', '加算値']);
+            const addValue = this.findMetaForVariableControl(['AddValue', '加算値']);
             if (addValue) {
                 const originalValue = $gameVariables.value(varNumber);
                 $gameVariables.setValue(varNumber, originalValue + eval(addValue));
             }
         }
+    };
+
+    Game_Action.prototype.isVariableControlSubject = function() {
+        let subject = this.findMetaForVariableControl(['Subject', '実行者']);
+        if (!subject && subject === true) {
+            return true;
+        }
+        subject = subject.toLowerCase();
+        if (subject === 'actor' && this._subjectEnemyIndex >= 0) {
+            return false;
+        }
+        if (subject === 'enemy' && this._subjectActorId > 0) {
+            return false;
+        }
+        return true;
     };
 
     Game_Action.prototype.findMetaForVariableControl = function(tags) {
