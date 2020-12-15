1a939f68e1f2d7d23aba34d41cd9c20c2f1d9765
triacontane <triacontanekinoko@gmail.com>
2020-12-15 23:00:07 +0900

    1.1.0 2020/12/15 MZ版として全面的に修正

--- TraitAnimation.js
+++ TraitAnimation.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.1.0 2020/12/15 MZ版として全面的に修正
  1.0.0 2020/12/06 初版
 ----------------------------------------------------------------------------
  [Blog]   : https://triacontane.blogspot.jp/
@@ -15,6 +16,9 @@
 
 /*:
  * @plugindesc TraitAnimationPlugin
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/TraitAnimation.js
+ * @base PluginCommonBase
  * @author triacontane
  *
  * @help TraitAnimation.js
@@ -27,6 +31,9 @@
  */
 /*:ja
  * @plugindesc 特徴によるアニメーションプラグイン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/TraitAnimation.js
+ * @base PluginCommonBase
  * @author トリアコンタン
  *
  * @help TraitAnimation.js
@@ -36,6 +43,11 @@
  * ※ アクター、職業、武器、防具、敵キャラ、ステート
  * <TraitAnimation:1> // アニメーションID[1]を再生し続けます。
  * <特徴アニメ:1>     // 同上
+ *
+ * なお、Effekseerを使用したアニメーションを同時に複数再生すると
+ * パフォーマンスに影響が出る場合があるのでご注意ください。
+ * ロンチプラグイン『AnimationMv.js』を使ったMV方式の
+ * アニメーションの利用も可能です。
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -47,48 +59,13 @@
     'use strict';
 
     /**
-     * Get database meta information.
-     * @param object Database item
-     * @param name Meta name
-     * @returns {String} meta value
-     */
-    var getMetaValue = function(object, name) {
-        return object.meta.hasOwnProperty(name) ? convertEscapeCharacters(object.meta[name]) : null;
-    };
-
-    /**
-     * Get database meta information.(for multi language)
-     * @param object Database item
-     * @param names Meta name array (for multi language)
-     * @returns {String} meta value
-     */
-    var getMetaValues = function(object, names) {
-        var metaValue;
-        names.some(function(name) {
-            metaValue = getMetaValue(object, name);
-            return metaValue !== null;
-        });
-        return metaValue;
-    };
-
-    /**
-     * Convert escape characters.(require any window object)
-     * @param text Target text
-     * @returns {String} Converted text
-     */
-    var convertEscapeCharacters = function(text) {
-        var windowLayer = SceneManager._scene._windowLayer;
-        return windowLayer ? windowLayer.children[0].convertEscapeCharacters(text.toString()) : text;
-    };
-
-    /**
      * Game_BattlerBase
      * 特徴の情報を取得します。
      */
     Game_BattlerBase.prototype.findTraitAnimation = function() {
-        var animationId = 0;
+        let animationId = 0;
         this.traitObjects().forEach(function(obj) {
-            var meta = getMetaValues(obj, ['TraitAnimation', '特徴アニメ']);
+            const meta = PluginManagerEx.findMetaValue(obj, ['TraitAnimation', '特徴アニメ']);
             if (meta) {
                 animationId = parseInt(meta);
             }
@@ -103,32 +80,86 @@
         }
     };
 
-    /**
-     * Sprite_Battler
-     * 特徴アニメーションを再生します。
-     */
-    var _Sprite_Battler_updateAnimation = Sprite_Battler.prototype.updateAnimation
-    Sprite_Battler.prototype.updateAnimation = function() {
-        _Sprite_Battler_updateAnimation.apply(this, arguments);
-        this.updateTraitAnimation();
+    Game_BattlerBase.prototype.updateTraitAnimation = function() {
+        const data = this.findTraitAnimation();
+        if (!data || !$dataAnimations[data.id]) {
+            return;
+        }
+        $gameTemp.requestTraitAnimation([this], data.id, data.mirror);
     };
 
-    Sprite_Battler.prototype.updateTraitAnimation = function() {
-        var sprite = this._traitAnimationSprite;
-        if (sprite && !sprite.isPlaying()) {
-            sprite.remove();
-            this._traitAnimationSprite = null;
+    const _BattleManager_update = BattleManager.update;
+    BattleManager.update = function(timeActive) {
+        _BattleManager_update.apply(this, arguments);
+        $gameParty.members().forEach(battler => battler.updateTraitAnimation());
+        $gameTroop.members().forEach(battler => battler.updateTraitAnimation());
+    };
+
+    Game_Temp.prototype.requestTraitAnimation = function(targets, animationId, mirror) {
+        this.requestAnimation(targets, animationId, mirror);
+        this._animationQueue[this._animationQueue.length - 1].trait = true;
+    }
+
+    const _Spriteset_Base_initialize = Spriteset_Base.prototype.initialize;
+    Spriteset_Base.prototype.initialize = function() {
+        _Spriteset_Base_initialize.apply(this, arguments);
+        this._traitAnimationSprites = [];
+    };
+
+    const _Spriteset_Base_updateAnimations = Spriteset_Base.prototype.updateAnimations;
+    Spriteset_Base.prototype.updateAnimations = function() {
+        _Spriteset_Base_updateAnimations.apply(this, arguments);
+        for (const sprite of this._traitAnimationSprites) {
+            if (!sprite.isPlaying()) {
+                this.removeAnimation(sprite);
+                this._traitAnimationSprites.remove(sprite);
+            }
         }
+    };
+
+    const _Spriteset_Base_createAnimation = Spriteset_Base.prototype.createAnimation;
+    Spriteset_Base.prototype.createAnimation = function(request) {
+        this._createTraitAnimation = request.trait;
+        if (request.trait && this.isTraitAnimationPlaying(request)) {
+            return;
+        }
+        _Spriteset_Base_createAnimation.apply(this, arguments);
+        this._createTraitAnimation = false;
+    };
+
+    Spriteset_Base.prototype.isTraitAnimationPlaying = function(request) {
+        const targetSprites = this.makeTargetSprites(request.targets);
+        return targetSprites.some(item => item.hasTraitAnimationSprite());
+    };
+
+    const _Spriteset_Base_createAnimationSprite = Spriteset_Base.prototype.createAnimationSprite;
+    Spriteset_Base.prototype.createAnimationSprite = function(targets, animation, mirror, delay) {
+        _Spriteset_Base_createAnimationSprite.apply(this, arguments);
+        if (this._createTraitAnimation) {
+            this.createTraitAnimationSprite(targets);
+        }
+    };
+
+    Spriteset_Base.prototype.createTraitAnimationSprite = function(targets) {
+        const sprite = this._animationSprites.pop();
+        const targetSprites = this.makeTargetSprites(targets);
+        targetSprites.forEach(item => item.setTraitAnimationSprite(sprite));
+        this._traitAnimationSprites.push(sprite);
+    };
+
+    Sprite_Battler.prototype.setTraitAnimationSprite = function(sprite) {
+        this._traitAnimationSprite = sprite;
+    };
+
+    Sprite_Battler.prototype.hasTraitAnimationSprite = function() {
         if (!this._traitAnimationSprite) {
-            this.setupTraitAnimation();
-        }
-    };
-
-    Sprite_Battler.prototype.setupTraitAnimation = function() {
-        var data = this._battler.findTraitAnimation();
-        if (data) {
-            this.startAnimation($dataAnimations[data.id], data.mirror, 0);
-            this._traitAnimationSprite = this._animationSprites.pop();
+            return false;
+        } else {
+            const playing = this._traitAnimationSprite.isPlaying();
+            if (!playing) {
+                this._traitAnimationSprite = null;
+            }
+            return playing;
         }
     };
 })();
