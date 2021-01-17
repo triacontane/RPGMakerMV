defe47b09582b39451b05f976b30d444e74340bf
トリアコンタン <triacontanetakenoko@gmail.com>
2021-01-17 15:19:03 +0900

    1.1.0 2021/01/17 MZ向けにリファクタリング

--- FastForwardCustomize.js
+++ FastForwardCustomize.js
@@ -1,11 +1,12 @@
 //=============================================================================
 // FastForwardCustomize.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2015-2017 Triacontane
+// (C)2017 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2021/01/17 MZ向けにリファクタリング
 // 1.0.0 2017/07/01 初版
 // ----------------------------------------------------------------------------
 // [Blog]   : https://triacontane.blogspot.jp/
@@ -15,41 +16,53 @@
 
 /*:
  * @plugindesc FastForwardCustomizePlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/FastForwardCustomize.js
+ * @base PluginCommonBase
+ * @author triacontane
  *
- * @param EventSpeedVariableId
- * @desc イベント高速化時の速度を取得する変数番号です。変数値が0なら高速化禁止、数値が大きくなると速度も速くなります。
+ * @param eventSpeedVariableId
+ * @desc Variable number to get the speed at event acceleration.
  * @default 0
  * @type variable
  *
- * @help イベント高速化時の速度を調整できます。
- * 指定した変数の値によって高速化時の速度が変化します。
- * 変数値によって以下の通り変化します。
- * 0  : 高速化が禁止されます。
- * 1  : デフォルトの速度です。
- * 2- : 数値が高いほど高速になります。異常に高い値を設定すると処理落ちします。
+ * @help FastForwardCustomize.js
  *
- * このプラグインにはプラグインコマンドはありません。
+ * This function allows you to adjust the fast forward of event acceleration.
+ * The speed at which the event is accelerated changes depending on the value of the specified variable.
+ * The speed changes as follows depending on the value of the variable.
+ * 0 : Fast forward is disabled.
+ * 1 : This is the default speed.
+ * 2+ : The higher the value, the faster the speed. If an abnormally high value is set, processing will fail.
  *
  * This plugin is released under the MIT License.
  */
 /*:ja
  * @plugindesc イベント高速化調整プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/FastForwardCustomize.js
+ * @base PluginCommonBase
+ * @author トリアコンタン
  *
- * @param イベント速度変数
+ * @param eventSpeedVariableId
+ * @text イベント速度変数
  * @desc イベント高速化時の速度を取得する変数番号です。変数値が0なら高速化禁止、数値が大きくなると速度も速くなります。
  * @default 0
  * @type variable
  *
- * @help イベント高速化時の速度を調整できます。
+ * @help FastForwardCustomize.js
+ *
+ * イベント高速化時の速度を調整できます。
  * 指定した変数の値によって高速化時の速度が変化します。
  * 変数値によって以下の通り変化します。
  * 0  : 高速化が禁止されます。
  * 1  : デフォルトの速度です。
- * 2- : 数値が高いほど高速になります。異常に高い値を設定すると処理落ちします。
+ * 2+ : 数値が高いほど高速になります。異常に高い値を設定すると処理落ちします。
  *
- * このプラグインにはプラグインコマンドはありません。
+ * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
+ * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
+ * 以下のフォルダに格納されています。
+ * dlc/BasicResources/plugins/official
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -59,35 +72,11 @@
 
 (function() {
     'use strict';
-    var pluginName    = 'FastForwardCustomize';
 
-    //=============================================================================
-    // ローカル関数
-    //  プラグインパラメータやプラグインコマンドパラメータの整形やチェックをします
-    //=============================================================================
-    var getParamString = function(paramNames) {
-        if (!Array.isArray(paramNames)) paramNames = [paramNames];
-        for (var i = 0; i < paramNames.length; i++) {
-            var name = PluginManager.parameters(pluginName)[paramNames[i]];
-            if (name) return name;
-        }
-        return '';
-    };
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
 
-    var getParamNumber = function(paramNames, min, max) {
-        var value = getParamString(paramNames);
-        if (arguments.length < 2) min = -Infinity;
-        if (arguments.length < 3) max = Infinity;
-        return (parseInt(value) || 0).clamp(min, max);
-    };
-
-    //=============================================================================
-    // パラメータの取得と整形
-    //=============================================================================
-    var param       = {};
-    param.eventSpeedVariableId = getParamNumber(['EventSpeedVariableId', 'イベント速度変数']);
-
-    var _Scene_Map_updateMainMultiply = Scene_Map.prototype.updateMainMultiply;
+    const _Scene_Map_updateMainMultiply = Scene_Map.prototype.updateMainMultiply;
     Scene_Map.prototype.updateMainMultiply = function() {
         _Scene_Map_updateMainMultiply.apply(this, arguments);
         if (this.isFastForward()) {
@@ -96,13 +85,13 @@
     };
 
     Scene_Map.prototype.updateMainForMoreFast = function() {
-        var updateCount = this.getFastSpeed() - 1;
-        for (var i = 0; i < updateCount; i++) {
+        const updateCount = this.getFastSpeed() - 1;
+        for (let i = 0; i < updateCount; i++) {
             this.updateMain();
         }
     };
 
-    var _Scene_Map_isFastForward = Scene_Map.prototype.isFastForward;
+    const _Scene_Map_isFastForward = Scene_Map.prototype.isFastForward;
     Scene_Map.prototype.isFastForward = function() {
         return _Scene_Map_isFastForward.apply(this, arguments) && this.getFastSpeed() > 0;
     };
