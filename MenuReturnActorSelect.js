712e1bed0368057dd7ad77cd89d165f9ac822ec4
triacontane <triacontanekinoko@gmail.com>
2021-03-16 00:19:58 +0900

    1.0.1 2021/03/16 MZ向けに修正

--- MenuReturnActorSelect.js
+++ MenuReturnActorSelect.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.0.1 2021/03/16 MZ向けに修正
  1.0.0 2021/03/16 初版
 ----------------------------------------------------------------------------
  [Blog]   : https://triacontane.blogspot.jp/
@@ -15,6 +16,8 @@
 
 /*:
  * @plugindesc MenuReturnActorSelectPlugin
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/MenuReturnActorSelect.js
  * @author triacontane
  *
  * @help MenuReturnActorSelect.js
@@ -28,6 +31,8 @@
  */
 /*:ja
  * @plugindesc メニュー詳細画面からアクター選択に戻るプラグイン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/MenuReturnActorSelect.js
  * @author トリアコンタン
  *
  * @help MenuReturnActorSelect.js
@@ -48,19 +53,19 @@
 
     Scene_Menu._selectPersonal = false;
 
-    var _Scene_Menu_onPersonalOk = Scene_Menu.prototype.onPersonalOk;
+    const _Scene_Menu_onPersonalOk = Scene_Menu.prototype.onPersonalOk;
     Scene_Menu.prototype.onPersonalOk = function() {
         _Scene_Menu_onPersonalOk.apply(this, arguments);
         Scene_Menu._selectPersonal = true;
     };
 
-    var _Scene_Menu_onPersonalCancel = Scene_Menu.prototype.onPersonalCancel;
+    const _Scene_Menu_onPersonalCancel = Scene_Menu.prototype.onPersonalCancel;
     Scene_Menu.prototype.onPersonalCancel = function() {
         _Scene_Menu_onPersonalCancel.apply(this, arguments);
         Scene_Menu._selectPersonal = false;
     };
 
-    var _Scene_Menu_start = Scene_Menu.prototype.start;
+    const _Scene_Menu_start = Scene_Menu.prototype.start;
     Scene_Menu.prototype.start = function() {
         _Scene_Menu_start.apply(this, arguments);
         if (Scene_Menu._selectPersonal) {
