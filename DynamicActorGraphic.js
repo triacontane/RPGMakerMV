629954f988c62ca06fa604140d8efe297d060038
トリアコンタン <triacontanetakenoko@gmail.com>
2021-03-21 03:22:21 +0900

    2.0.0 2021/03/21 MZ向けに全面的に修正

--- DynamicActorGraphic.js
+++ DynamicActorGraphic.js
@@ -1,11 +1,12 @@
 //=============================================================================
 // DynamicActorGraphic.js
 // ----------------------------------------------------------------------------
-// (C) 2016 Triacontane
+// (C)2016 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 2.0.0 2021/03/21 MZ向けに全面的に修正
 // 1.2.3 2020/06/06 必要ない場合はプレイヤーをリフレッシュしないよう修正
 // 1.2.2 2020/04/21 NpcFollower.jsと併用したときに発生する循環参照を解消
 // 1.2.1 2018/11/18 ヘルプの記述を修正
@@ -22,87 +23,61 @@
 
 /*:
  * @plugindesc DynamicActorGraphicPlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/DynamicActorGraphic.js
+ * @base PluginCommonBase
+ * @author triacontane
  *
- * @help アクターの歩行、顔、バトルグラフィックを動的に変化させます。
- * HPの残量、特定のステート有効時、スイッチON時に変化します。
+ * @param list
+ * @text List
+ * @desc List of image files and conditions to be changed.
+ * @default []
+ * @type struct<ACTOR>[]
  *
- * スイッチによる変動はメニュー画面の切り替えもしくはマップ移動後に有効になります。
+ * @command REFRESH
+ * @text Refresh
+ * @desc Image changes made by the switch will be reflected immediately.
  *
- * アクターのメモ欄に以下の通り入力してください。
- * <DAG_CHARACTER_HP50:test,2> # HP残量50%以下でtest.pngの2番目の画像を
- *                             # 歩行グラフィックに指定します。(10%刻みで指定)
- * <DAG_FACE_HP10:test,2>      # HP残量10%以下でtest.pngの2番目の画像を
- *                             # 顔グラフィックに指定します。
- * <DAG_BATTLER_HP30:test>     # HP残量30%以下でtest.pngを
- *                             # バトラーグラフィックに指定します。
- * <DAG_CHARACTER_MP50:test,2> # MP残量50%以下でtest.pngの2番目の画像を
- *                             # 歩行グラフィックに指定します。(10%刻みで指定)
- * <DAG_FACE_MP10:test,2>      # MP残量10%以下でtest.pngの2番目の画像を
- *                             # 顔グラフィックに指定します。
- * <DAG_BATTLER_MP30:test>     # MP残量30%以下でtest.pngを
- *                             # バトラーグラフィックに指定します。
- * <DAG_CHARACTER_ST5:test,2>  # ステート[5]有効時にtest.pngの2番目の画像を
- *                             # 歩行グラフィックに指定します。
- * <DAG_FACE_ST5:test,2>       # ステート[5]有効時にtest.pngの2番目の画像を
- *                             # 顔グラフィックに指定します。
- * <DAG_BATTLER_ST5:test>      # ステート[5]有効時にtest.pngを
- *                             # バトラーグラフィックに指定します。
- * <DAG_CHARACTER_SWITCH1:test, 2> # スイッチ[1]ON時にtest.pngの2番目の画像を
- *                                 # 歩行グラフィックに指定します。
- * <DAG_FACE_SWITCH1:test,2>       # スイッチ[1]ON時にtest.pngの2番目の画像を
- *                                 # 顔グラフィックに指定します。
- * <DAG_BATTLER_SWITCH1:test>      # スイッチ[1]ON時にtest.pngを
- *                                 # バトラーグラフィックに指定します。
+ * @help Dynamically changes the actor's walk, face,
+ * and battle graphics according to the conditions.
+ * Set the image and conditions from the parameters.
+ * Changes when HP is remaining, when certain states are enabled,
+ * and when switches are turned on.
  *
- * 注意！　キャッシュされていない画像に変更した場合、JSの非同期読み込みの仕様で
- * 画像がチラついたり、ただしく表示されない場合があります。
- * 当プラグインでは特に対策は行っていないので、専用の対策プラグインを使うか
- * 事前にイベントコマンドなどでキャッシュを行っていただくようお願いします。
+ * The change by the switch will take effect after switching
+ * the menu screen or moving the map.
+ * If you want it to take effect immediately,
+ * you can refresh it from the plugin command.
  *
- * This plugin is released under the MIT License.
+ * MIT License
  */
 /*:ja
  * @plugindesc アクターグラフィックの動的設定プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/DynamicActorGraphic.js
+ * @base PluginCommonBase
+ * @author トリアコンタン
  *
- * @help アクターの歩行、顔、バトルグラフィックを動的に変化させます。
+ * @param list
+ * @text 設定リスト
+ * @desc 変更する画像ファイルと条件のリストです。条件を満たした画像が複数ある場合は、リストの下が優先されます。
+ * @default []
+ * @type struct<ACTOR>[]
+ *
+ * @command REFRESH
+ * @text リフレッシュ
+ * @desc スイッチによる画像変更を即座に反映させます。
+ *
+ * @help アクターの歩行、顔、バトルグラフィックを条件に応じて動的に変化させます。
+ * パラメータから画像と条件を設定してください。
  * HPの残量、特定のステート有効時、スイッチON時に変化します。
+ * スイッチによる変化はメニュー画面の切り替えまたはマップ移動後に有効になります。
+ * すぐに反映させたい場合はプラグインコマンドからリフレッシュしてください。
  *
- * スイッチによる変動はメニュー画面の切り替えもしくはマップ移動後に有効になります。
- *
- * アクターのメモ欄に以下の通り入力してください。
- * <DAG_CHARACTER_HP50:test,2> # HP残量50%以下でtest.pngの2番目の画像を
- *                             # 歩行グラフィックに指定します。(10%刻みで指定)
- * <DAG_FACE_HP10:test,2>      # HP残量10%以下でtest.pngの2番目の画像を
- *                             # 顔グラフィックに指定します。
- * <DAG_BATTLER_HP30:test>     # HP残量30%以下でtest.pngを
- *                             # バトラーグラフィックに指定します。
- * <DAG_CHARACTER_MP50:test,2> # MP残量50%以下でtest.pngの2番目の画像を
- *                             # 歩行グラフィックに指定します。(10%刻みで指定)
- * <DAG_FACE_MP10:test,2>      # MP残量10%以下でtest.pngの2番目の画像を
- *                             # 顔グラフィックに指定します。
- * <DAG_BATTLER_MP30:test>     # MP残量30%以下でtest.pngを
- *                             # バトラーグラフィックに指定します。
- * <DAG_CHARACTER_ST5:test,2>  # ステート[5]有効時にtest.pngの2番目の画像を
- *                             # 歩行グラフィックに指定します。
- * <DAG_FACE_ST5:test,2>       # ステート[5]有効時にtest.pngの2番目の画像を
- *                             # 顔グラフィックに指定します。
- * <DAG_BATTLER_ST5:test>      # ステート[5]有効時にtest.pngを
- *                             # バトラーグラフィックに指定します。
- * <DAG_CHARACTER_SWITCH1:test, 2> # スイッチ[1]ON時にtest.pngの2番目の画像を
- *                                 # 歩行グラフィックに指定します。
- * <DAG_FACE_SWITCH1:test,2>       # スイッチ[1]ON時にtest.pngの2番目の画像を
- *                                 # 顔グラフィックに指定します。
- * <DAG_BATTLER_SWITCH1:test>      # スイッチ[1]ON時にtest.pngを
- *                                 # バトラーグラフィックに指定します。
- *
- * 注意！　キャッシュされていない画像に変更した場合、JSの非同期読み込みの仕様で
- * 画像がチラついたり、ただしく表示されない場合があります。
- * 当プラグインでは特に対策は行っていないので、専用の対策プラグインを使うか
- * 事前にイベントコマンドなどでキャッシュを行っていただくようお願いします。
- *
- * このプラグインにはプラグインコマンドはありません。
+ * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
+ * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
+ * 以下のフォルダに格納されています。
+ * dlc/BasicResources/plugins/official
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -110,60 +85,91 @@
  *  このプラグインはもうあなたのものです。
  */
 
+/*~struct~ACTOR:
+ *
+ * @param id
+ * @text アクターID
+ * @desc 対象となるアクターIDです。
+ * @default 0
+ * @type actor
+ *
+ * @param name
+ * @text 名称
+ * @desc 識別用の名称です。特に意味はありません。
+ * @default
+ *
+ * @param faceImage
+ * @text フェイス画像
+ * @desc 条件を満たしたときに変更するフェイスグラフィックです。
+ * @default
+ * @type file
+ * @dir img/faces
+ *
+ * @param faceIndex
+ * @text フェイス番号
+ * @desc 条件を満たしたときに変更するフェイスグラフィックのインデックスです。0から7までの値を指定します。
+ * @default 0
+ * @type number
+ *
+ * @param characterImage
+ * @text キャラクター画像
+ * @desc 条件を満たしたときに変更するキャラクターグラフィックです。
+ * @default
+ * @type file
+ * @dir img/characters
+ *
+ * @param characterIndex
+ * @text キャラクター番号
+ * @desc 条件を満たしたときに変更するフェイスグラフィックのインデックスです。0から7までの値を指定します。
+ * @default 0
+ * @type number
+ *
+ * @param battlerImage
+ * @text バトラー画像
+ * @desc 条件を満たしたときに変更するバトラーグラフィックです。
+ * @default
+ * @type file
+ * @dir img/sv_actors
+ *
+ * @param hpRate
+ * @text HP条件
+ * @desc HPが指定した割合(%)を下回った場合にグラフィック変更
+ * @default 0
+ * @type number
+ * @max 100
+ *
+ * @param state
+ * @text ステート条件
+ * @desc 指定したステートが有効になった場合にグラフィック変更
+ * @default 0
+ * @type state
+ *
+ * @param switchId
+ * @text スイッチ条件
+ * @desc 指定したスイッチがONの場合にグラフィック変更
+ * @default 0
+ * @type switch
+ *
+ */
+
 (function() {
     'use strict';
-    var metaTagPrefix = 'DAG_';
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
+    if (!param.list) {
+        return;
+    }
 
-    //=============================================================================
-    // ローカル関数
-    //  プラグインパラメータやプラグインコマンドパラメータの整形やチェックをします
-    //=============================================================================
-    var getArgNumber = function(arg, min, max) {
-        if (arguments.length < 2) min = -Infinity;
-        if (arguments.length < 3) max = Infinity;
-        return (parseInt(arg) || 0).clamp(min, max);
-    };
-
-    var getArgArrayString = function(args) {
-        var values = args.split(',');
-        for (var i = 0; i < values.length; i++) {
-            values[i] = values[i].trim();
-        }
-        return values;
-    };
-
-    var getMetaValue = function(object, name) {
-        var metaTagName = metaTagPrefix + name;
-        return object.meta.hasOwnProperty(metaTagName) ? convertEscapeCharacters(object.meta[metaTagName]) : undefined;
-    };
-
-    var convertEscapeCharacters = function(text) {
-        if (text == null) text = '';
-        text = text.replace(/\\/g, '\x1b');
-        text = text.replace(/\x1b\x1b/g, '\\');
-        text = text.replace(/\x1bV\[(\d+)\]/gi, function() {
-            return $gameVariables.value(parseInt(arguments[1], 10));
-        }.bind(this));
-        text = text.replace(/\x1bV\[(\d+)\]/gi, function() {
-            return $gameVariables.value(parseInt(arguments[1], 10));
-        }.bind(this));
-        text = text.replace(/\x1bN\[(\d+)\]/gi, function() {
-            var actor = parseInt(arguments[1], 10) >= 1 ? $gameActors.actor(parseInt(arguments[1], 10)) : null;
-            return actor ? actor.name() : '';
-        }.bind(this));
-        text = text.replace(/\x1bP\[(\d+)\]/gi, function() {
-            var actor = parseInt(arguments[1], 10) >= 1 ? $gameParty.members()[parseInt(arguments[1], 10) - 1] : null;
-            return actor ? actor.name() : '';
-        }.bind(this));
-        text = text.replace(/\x1bG/gi, TextManager.currencyUnit);
-        return text;
-    };
+    PluginManagerEx.registerCommand(script, 'REFRESH', args => {
+        $gameParty.refreshMemberCustomGraphic();
+        $gameTemp.requestBattleRefresh();
+    });
 
     //=============================================================================
     // Game_Actor
     //  アクターグラフィックを動的に差し替えます。
     //=============================================================================
-    var _Game_Actor_refresh    = Game_Actor.prototype.refresh;
+    const _Game_Actor_refresh    = Game_Actor.prototype.refresh;
     Game_Actor.prototype.refresh = function() {
         _Game_Actor_refresh.apply(this, arguments);
         this.refreshCustomGraphic();
@@ -176,71 +182,37 @@
 
     Game_Actor.prototype.refreshCustomGraphic = function() {
         this.initCustomGraphic();
-        this.refreshCustomGraphicForState();
-        this.refreshCustomGraphicForHpRate();
-        this.refreshCustomGraphicForMpRate();
-        this.refreshCustomGraphicForSwitch();
+        param.list.filter(item => this.isValidCustomGraphic(item))
+            .forEach(item => this.setCustomGraphic(item));
         if (this.isChangeCharacterImage()) {
             $gamePlayer.requestRefresh();
         }
-    };
-
-    Game_Actor.prototype.refreshCustomGraphicForState = function() {
-        var actor = this.actor();
-        this.getSortedStates().forEach(function(stateId) {
-            this.setCharacterCustom(getMetaValue(actor, 'CHARACTER_ST' + String(stateId)));
-            this.setFaceCustom(getMetaValue(actor, 'FACE_ST' + String(stateId)));
-            this.setBattlerCustom(getMetaValue(actor, 'BATTLER_ST' + String(stateId)));
-        }, this);
-    };
-
-    Game_Actor.prototype.refreshCustomGraphicForHpRate = function() {
-        var actor = this.actor();
-        for (var hpRate = 10; hpRate <= 100; hpRate += 10) {
-            if (this.hpRate() > hpRate / 100) continue;
-            this.setCharacterCustom(getMetaValue(actor, 'CHARACTER_HP' + String(hpRate)));
-            this.setFaceCustom(getMetaValue(actor, 'FACE_HP' + String(hpRate)));
-            this.setBattlerCustom(getMetaValue(actor, 'BATTLER_HP' + String(hpRate)));
+        if (this.isChangeFaceImage()) {
+            $gameTemp.requestBattleRefresh();
         }
     };
 
-    Game_Actor.prototype.refreshCustomGraphicForMpRate = function() {
-        var actor = this.actor();
-        for (var mpRate = 10; mpRate <= 100; mpRate += 10) {
-            if (this.mpRate() > mpRate / 100) continue;
-            this.setCharacterCustom(getMetaValue(actor, 'CHARACTER_MP' + String(mpRate)));
-            this.setFaceCustom(getMetaValue(actor, 'FACE_MP' + String(mpRate)));
-            this.setBattlerCustom(getMetaValue(actor, 'BATTLER_MP' + String(mpRate)));
+    Game_Actor.prototype.isValidCustomGraphic = function(item) {
+        const conditions = []
+        conditions.push(() => item.id === this.actorId());
+        conditions.push(() => !item.hpRate || item.hpRate / 100 >= this.hpRate());
+        conditions.push(() => !item.state || this.isStateAffected(item.state));
+        conditions.push(() => !item.switchId || $gameSwitches.value(item.switchId));
+        return conditions.every(condition => condition());
+    };
+
+    Game_Actor.prototype.setCustomGraphic = function(item) {
+        if (item.faceImage) {
+            this._faceNameCustom  = item.faceImage;
+            this._faceIndexCustom = item.faceIndex;
         }
-    };
-
-    Game_Actor.prototype.refreshCustomGraphicForSwitch = function() {
-        var actor = this.actor();
-        Object.keys(actor.meta).forEach(function(metaName) {
-            metaName.replace(/CHARACTER_SWITCH(\d+)/i, function() {
-                if ($gameSwitches.value(getArgNumber(arguments[1]))) {
-                    this.setCharacterCustom(getMetaValue(actor, arguments[0]));
-                }
-            }.bind(this));
-            metaName.replace(/FACE_SWITCH(\d+)/i, function() {
-                if ($gameSwitches.value(getArgNumber(arguments[1]))) {
-                    this.setFaceCustom(getMetaValue(actor, arguments[0]));
-                }
-            }.bind(this));
-            metaName.replace(/BATTLER_SWITCH(\d+)/i, function() {
-                if ($gameSwitches.value(getArgNumber(arguments[1]))) {
-                    this.setBattlerCustom(getMetaValue(actor, arguments[0]));
-                }
-            }.bind(this));
-        }.bind(this));
-    };
-
-    Game_Actor.prototype.getSortedStates = function() {
-        return this._states.clone().sort(this.compareOrderStateIdPriority.bind(this));
-    };
-
-    Game_Actor.prototype.compareOrderStateIdPriority = function(stateIdA, stateIdB) {
-        return $dataStates[stateIdB].priority - $dataStates[stateIdA].priority;
+        if (item.characterImage) {
+            this._characterNameCustom  = item.characterImage;
+            this._characterIndexCustom = item.characterIndex;
+        }
+        if (item.battlerImage) {
+            this._battlerNameCustom = item.battlerImage;
+        }
     };
 
     Game_Actor.prototype.initCustomGraphic = function() {
@@ -255,46 +227,31 @@
         return this._characterNameCustom !== null || this._characterIndexCustom !== null;
     };
 
-    Game_Actor.prototype.setCharacterCustom = function(metaValue) {
-        if (!metaValue || this._characterNameCustom) return;
-        var metaValueArray       = getArgArrayString(metaValue);
-        this._characterNameCustom  = metaValueArray[0];
-        this._characterIndexCustom = getArgNumber(metaValueArray[1], 0, 7);
+    Game_Actor.prototype.isChangeFaceImage = function() {
+        return this._faceNameCustom !== null || this._faceIndexCustom !== null;
     };
 
-    Game_Actor.prototype.setFaceCustom = function(metaValue) {
-        if (!metaValue || this._faceNameCustom) return;
-        var metaValueArray  = getArgArrayString(metaValue);
-        this._faceNameCustom  = metaValueArray[0];
-        this._faceIndexCustom = getArgNumber(metaValueArray[1], 0, 7);
-    };
-
-    Game_Actor.prototype.setBattlerCustom = function(metaValue) {
-        if (!metaValue || this._battlerNameCustom) return;
-        this._battlerNameCustom  = metaValue;
-    };
-
-    var _Game_Actor_characterName    = Game_Actor.prototype.characterName;
+    const _Game_Actor_characterName    = Game_Actor.prototype.characterName;
     Game_Actor.prototype.characterName = function() {
         return this._characterNameCustom || _Game_Actor_characterName.apply(this, arguments);
     };
 
-    var _Game_Actor_characterIndex    = Game_Actor.prototype.characterIndex;
+    const _Game_Actor_characterIndex    = Game_Actor.prototype.characterIndex;
     Game_Actor.prototype.characterIndex = function() {
         return this._characterIndexCustom || _Game_Actor_characterIndex.apply(this, arguments);
     };
 
-    var _Game_Actor_faceName    = Game_Actor.prototype.faceName;
+    const _Game_Actor_faceName    = Game_Actor.prototype.faceName;
     Game_Actor.prototype.faceName = function() {
         return this._faceNameCustom || _Game_Actor_faceName.apply(this, arguments);
     };
 
-    var _Game_Actor_faceIndex    = Game_Actor.prototype.faceIndex;
+    const _Game_Actor_faceIndex    = Game_Actor.prototype.faceIndex;
     Game_Actor.prototype.faceIndex = function() {
         return this._faceIndexCustom || _Game_Actor_faceIndex.apply(this, arguments);
     };
 
-    var _Game_Actor_battlerName    = Game_Actor.prototype.battlerName;
+    const _Game_Actor_battlerName    = Game_Actor.prototype.battlerName;
     Game_Actor.prototype.battlerName = function() {
         return this._battlerNameCustom || _Game_Actor_battlerName.apply(this, arguments);
     };
@@ -303,7 +260,7 @@
     // Game_Player
     //  リフレッシュ要求に応じてリフレッシュします。
     //=============================================================================
-    var _Game_Player_update = Game_Player.prototype.update;
+    const _Game_Player_update = Game_Player.prototype.update;
     Game_Player.prototype.update = function() {
         _Game_Player_update.apply(this, arguments);
         if (this._needsRefresh) {
@@ -331,7 +288,7 @@
     // Game_Map
     //  場所移動時にアクターグラフィックを更新します。
     //=============================================================================
-    var _Game_Map_setup = Game_Map.prototype.setup;
+    const _Game_Map_setup = Game_Map.prototype.setup;
     Game_Map.prototype.setup = function(mapId) {
         $gameParty.refreshMemberCustomGraphic();
         _Game_Map_setup.apply(this, arguments);
@@ -341,7 +298,7 @@
     // Scene_Map
     //  作成時にアクターグラフィックを更新します。
     //=============================================================================
-    var _Scene_Map_create = Scene_Map.prototype.create;
+    const _Scene_Map_create = Scene_Map.prototype.create;
     Scene_Map.prototype.create = function() {
         $gameParty.refreshMemberCustomGraphic();
         _Scene_Map_create.apply(this, arguments);
@@ -351,11 +308,9 @@
     // Scene_MenuBase
     //  作成時にアクターグラフィックを更新します。
     //=============================================================================
-    var _Scene_MenuBase_create = Scene_MenuBase.prototype.create;
+    const _Scene_MenuBase_create = Scene_MenuBase.prototype.create;
     Scene_MenuBase.prototype.create = function() {
         $gameParty.refreshMemberCustomGraphic();
         _Scene_MenuBase_create.apply(this, arguments);
     };
 })();
-
-
