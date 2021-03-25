032c35902a7a9bdd7e11d1f1a1b3e48b1777cfa0
triacontane <triacontanekinoko@gmail.com>
2021-03-25 15:54:19 +0900

    1.1.0 2021/03/25 MZで動作するよう修正

--- EventNoLock.js
+++ EventNoLock.js
@@ -1,46 +1,69 @@
 //=============================================================================
 // EventNoLock.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2016 Triacontane
+// (C)2016 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2021/03/25 MZで動作するよう修正
 // 1.0.1 2017/02/07 端末依存の記述を削除
 // 1.0.0 2016/12/08 初版
 // ----------------------------------------------------------------------------
-// [Blog]   : http://triacontane.blogspot.jp/
+// [Blog]   : https://triacontane.blogspot.jp/
 // [Twitter]: https://twitter.com/triacontane/
 // [GitHub] : https://github.com/triacontane/
 //=============================================================================
 
 /*:
  * @plugindesc EventNoLockPlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/EventNoLock.js
+ * @base PluginCommonBase
+ * @author triacontane
  *
- * @help イベントを起動したときに一時的に
- * プレイヤーの方向を向く処理（イベントロック）を無効化します。
- * ロックを無効にしたいイベントのメモ欄に以下の通り記述してください。
+ * @param defaultDisable
+ * @desc Disables the locking of all events without specifying a memo field.
+ * @default false
+ * @type boolean
  *
- * <ENLロック無効>
- * <ENLDisableLock>
+ * @help EventNoLock.js
  *
- * このプラグインにはプラグインコマンドはありません。
+ * Disables the process of turning the player's direction
+ * when the event starts (event lock).
+ * Write the following in the memo field of the event
+ * you want to disable the lock for
+ * Disables the locking of all events without
+ * specifying a memo field.
  *
- * This plugin is released under the MIT License.
+ * <DisableLock>
+ *
  */
 /*:ja
  * @plugindesc イベントロック無効化プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/EventNoLock.js
+ * @base PluginCommonBase
+ * @author トリアコンタン
  *
- * @help イベントを起動したときに一時的に
- * プレイヤーの方向を向く処理（イベントロック）を無効化します。
+ * @param defaultDisable
+ * @text デフォルトで無効
+ * @desc メモ欄の指定なしで全てのイベントのロックを無効化します。
+ * @default false
+ * @type boolean
+ *
+ * @help EventNoLock.js
+ *
+ * イベント起動時にプレイヤーの方向を向く処理（イベントロック）を無効化します。
  * ロックを無効にしたいイベントのメモ欄に以下の通り記述してください。
  *
- * <ENLロック無効>
- * <ENLDisableLock>
+ * <ロック無効>
+ * <DisableLock>
  *
- * このプラグインにはプラグインコマンドはありません。
+ * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
+ * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
+ * 以下のフォルダに格納されています。
+ * dlc/BasicResources/plugins/official
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -50,33 +73,24 @@
 
 (function() {
     'use strict';
-    var metaTagPrefix = 'ENL';
-
-    var getMetaValue = function(object, name) {
-        var metaTagName = metaTagPrefix + (name ? name : '');
-        return object.meta.hasOwnProperty(metaTagName) ? object.meta[metaTagName] : undefined;
-    };
-
-    var getMetaValues = function(object, names) {
-        if (!Array.isArray(names)) return getMetaValue(object, names);
-        for (var i = 0, n = names.length; i < n; i++) {
-            var value = getMetaValue(object, names[i]);
-            if (value !== undefined) return value;
-        }
-        return undefined;
-    };
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
 
     //=============================================================================
     // Game_Event
     //  イベントロックを無効にします。
     //=============================================================================
-    var _Game_Event_lock = Game_Event.prototype.lock;
+    const _Game_Event_lock = Game_Event.prototype.lock;
     Game_Event.prototype.lock = function() {
         _Game_Event_lock.apply(this, arguments);
-        var disableLock = getMetaValues(this.event(), ['ロック無効', 'DisableLock']);
-        if (disableLock) {
+        if (this.isValidNoLock()) {
             this.setDirection(this._prelockDirection);
         }
+    };
+
+    Game_Event.prototype.isValidNoLock = function() {
+        return param.defaultDisable ||
+            PluginManagerEx.findMetaValue(this.event(), ['ロック無効', 'DisableLock']);
     };
 })();
 
