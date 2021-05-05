109988041a8851d79f824cf1a84e8b688d9562dd
トリアコンタン <triacontanetakenoko@gmail.com>
2021-05-05 20:25:41 +0900

    1.1.0 2021/05/05 MZ版を作成

--- PlayerGraphicChange.js
+++ PlayerGraphicChange.js
@@ -1,47 +1,39 @@
 //=============================================================================
 // PlayerGraphicChange.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2016 Triacontane
+// (C)2016 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2021/05/05 MZ版を作成
 // 1.0.0 2016/11/09 初版
 // ----------------------------------------------------------------------------
-// [Blog]   : http://triacontane.blogspot.jp/
+// [Blog]   : https://triacontane.blogspot.jp/
 // [Twitter]: https://twitter.com/triacontane/
 // [GitHub] : https://github.com/triacontane/
 //=============================================================================
 
-/*:
- * @plugindesc PlayerGraphicChangePlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @help プレイヤー画像をアクターとは無関係に設定できるようになります。
- *
- * プレイヤーに対して一度でも「移動ルートの指定」から「画像の変更」を
- * 行うと、その変更が場所移動やセーブなどを行った後も継続するようになります。
- *
- * 変更したグラフィックを元に戻す場合は、イベントコマンドの「スクリプト」から
- * 以下のスクリプトを実行してください。
- *
- * $gamePlayer.resetCustomizeGraphic();
- *
- * This plugin is released under the MIT License.
- */
 /*:ja
  * @plugindesc プレイヤーの画像変更プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/PlayerGraphicChange.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
  *
- * @help プレイヤー画像をアクターとは無関係に設定できるようになります。
+ * @command RESET
+ * @text プレイヤー画像リセット
+ * @desc 変更したプレイヤー画像を元に戻します。
+ *
+ * @help PlayerGraphicChange.js
+ *
+ * プレイヤー画像をアクターとは無関係に設定できるようになります。
  *
  * プレイヤーに対して一度でも「移動ルートの指定」から「画像の変更」を
  * 行うと、その変更が場所移動やセーブなどを行った後も継続するようになります。
  *
- * 変更したグラフィックを元に戻す場合は、イベントコマンドの「スクリプト」から
- * 以下のスクリプトを実行してください。
- *
- * $gamePlayer.resetCustomizeGraphic();
+ * 変更したグラフィックを元に戻す場合は、プラグインコマンドを実行してください。
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -49,14 +41,19 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+(()=> {
     'use strict';
+    const script = document.currentScript;
+
+    PluginManagerEx.registerCommand(script, 'RESET', args => {
+        $gamePlayer.resetCustomizeGraphic();
+    });
 
     //=============================================================================
     // Game_Player
     //  プレイヤーに対するグラフィック変更処理を永続化します。
     //=============================================================================
-    var _Game_Player_refresh = Game_Player.prototype.refresh;
+    const _Game_Player_refresh = Game_Player.prototype.refresh;
     Game_Player.prototype.refresh = function() {
         this._refreshing = true;
         _Game_Player_refresh.apply(this, arguments);
