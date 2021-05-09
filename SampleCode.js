ffc503df5263e7cbc3363dbd69ea1adb070f5fd8
トリアコンタン <triacontanetakenoko@gmail.com>
2021-05-09 14:38:03 +0900

    ひな形を整形

--- SampleCode.js
+++ SampleCode.js
@@ -1,10 +1,7 @@
-(function() {
-    'use strict';
-    var _TouchInput__onMouseMove = TouchInput._onMouseMove;
-    TouchInput._onMouseMove = function(event) {
-        _TouchInput__onMouseMove.apply(this, arguments);
-        this.mouseX = Graphics.pageToCanvasX(event.pageX);
-        this.mouseY = Graphics.pageToCanvasY(event.pageY);
-    };
+/*:
+ * @plugindesc サンプルコード
+ * @target MZ
+ */
+(()=> {
 })();
 
