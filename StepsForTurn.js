ba542ae489f75064a6fd438711fee0192f3a63a3
triacontane <triacontanekinoko@gmail.com>
2021-05-20 00:48:01 +0900

    1.0.0 2021/05/20 初版

---
+++ StepsForTurn.js
@@ -0,0 +1,71 @@
+/*=============================================================================
+ StepsForTurn.js
+----------------------------------------------------------------------------
+ (C)2021 Triacontane
+ This software is released under the MIT License.
+ http://opensource.org/licenses/mit-license.php
+----------------------------------------------------------------------------
+ Version
+ 1.0.0 2021/05/20 初版
+----------------------------------------------------------------------------
+ [Blog]   : https://triacontane.blogspot.jp/
+ [Twitter]: https://twitter.com/triacontane/
+ [GitHub] : https://github.com/triacontane/
+=============================================================================*/
+
+/*:
+ * @plugindesc ターン経過歩数変更プラグイン
+ * @author トリアコンタン
+ *
+ * @param turn
+ * @text ターン経過歩数
+ * @desc 1ターン経過歩数です。
+ * @default 20
+ *
+ * @help StepsForTurn.js
+ *
+ * 1ターン経過と認識される歩数(通常20歩)を変更できます。
+ * 主にステートによるスリップダメージの頻度が変わります。
+ * パラメータから調整してください。
+ *　
+ * このプラグインにはプラグインコマンドはありません。
+ *
+ * 利用規約：
+ *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
+ *  についても制限はありません。
+ *  このプラグインはもうあなたのものです。
+ */
+
+(function() {
+    'use strict';
+
+    /**
+     * Create plugin parameter. param[paramName] ex. param.commandPrefix
+     * @param pluginName plugin name(EncounterSwitchConditions)
+     * @returns {Object} Created parameter
+     */
+    var createPluginParameter = function(pluginName) {
+        var paramReplacer = function(key, value) {
+            if (value === 'null') {
+                return value;
+            }
+            if (value[0] === '"' && value[value.length - 1] === '"') {
+                return value;
+            }
+            try {
+                return JSON.parse(value);
+            } catch (e) {
+                return value;
+            }
+        };
+        var parameter     = JSON.parse(JSON.stringify(PluginManager.parameters(pluginName), paramReplacer));
+        PluginManager.setParameters(pluginName, parameter);
+        return parameter;
+    };
+
+    var param = createPluginParameter('StepsForTurn');
+
+    Game_Actor.prototype.stepsForTurn = function() {
+        return param.turn;
+    };
+})();
