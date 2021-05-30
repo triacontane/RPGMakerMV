6a8348dbe82f71cdbef7a42d1dd45328efb44204
トリアコンタン <triacontanetakenoko@gmail.com>
2021-05-30 14:02:33 +0900

    1.0.1 2021/05/30 メッセージウィンドウなどに表示されるページ送り画像のアニメーションが止まってしまう問題を修正

--- WindowBlinkStop.js
+++ WindowBlinkStop.js
@@ -1,11 +1,12 @@
 //=============================================================================
 // WindowBlinkStop.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2015-2017 Triacontane
+// (C)2017 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.0.1 2021/05/30 メッセージウィンドウなどに表示されるページ送り画像のアニメーションが止まってしまう問題を修正
 // 1.0.0 2017/12/09 初版
 // ----------------------------------------------------------------------------
 // [Blog]   : https://triacontane.blogspot.jp/
@@ -46,8 +47,10 @@
 
     var _Window__updateCursor = Window.prototype._updateCursor;
     Window.prototype._updateCursor = function() {
+        var prevCount = this._animationCount;
         this._animationCount = 0;
         _Window__updateCursor.apply(this, arguments);
+        this._animationCount = prevCount;
     };
 })();
 
