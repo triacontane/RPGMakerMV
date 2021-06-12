5f27da134b1678df3a82c49d70216b2b84fe7d0e
トリアコンタン <triacontanetakenoko@gmail.com>
2021-06-12 14:21:54 +0900

    1.1.4 2021/06/12 ラベルのZ座標をイベントのプライオリティとは無関係に6で設定するよう変更

--- EventLabel.js
+++ EventLabel.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.1.4 2021/06/12 ラベルのZ座標をイベントのプライオリティとは無関係に6で設定するよう変更
  1.1.3 2021/05/07 動的生成したイベントを消去したときにラベルが残ってしまう競合を修正
  1.1.2 2020/09/26 1.1.0でイベントIDを0で指定していた場合に表示されない問題を修正
  1.1.1 2020/09/26 ラベル表示条件のヘルプを書き忘れていたので追加
@@ -203,7 +204,7 @@
     };
 
     Game_Event.prototype.findLabelZ = function() {
-        return this.screenZ() + 1;
+        return 6;
     };
 
     Game_Event.prototype.findLabelName = function() {
