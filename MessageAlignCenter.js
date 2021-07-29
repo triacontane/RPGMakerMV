f4a5a799df8b487a5b9ae5bb10faaa069582311b
triacontane <triacontanekinoko@gmail.com>
2021-07-29 17:23:54 +0900

    1.0.1 2021/07/29 リファクタリング

--- MessageAlignCenter.js
+++ MessageAlignCenter.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.0.1 2021/07/29 リファクタリング
  1.0.0 2021/07/28 初版
 ----------------------------------------------------------------------------
  [Blog]   : https://triacontane.blogspot.jp/
@@ -23,12 +24,12 @@
  *
  * 以下の制御文字を行の先頭に記述すると揃えを変更できます。
  * 文章の表示などのメッセージのほか、スキルの説明文などにも反映されます。
- * \ac 中央揃え
- * \ar 右揃え
+ * \ac : 中央揃え
+ * \ar : 右揃え
  *
  * メッセージのプレビューには反映されません。
- * 必ず行の先頭に記述してください。
- * 右読み言語には現バージョンでは対応しません。
+ * 制御文字は必ず行の先頭に記述してください。
+ * 右読み言語には現バージョンでは対応していません。
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -39,6 +40,25 @@
 (() => {
     'use strict';
 
+    const _Window_Base_processEscapeCharacter = Window_Base.prototype.processEscapeCharacter;
+    Window_Base.prototype.processEscapeCharacter = function(code, textState) {
+        if (textState.drawing) {
+            switch (code) {
+                case "AC":
+                    textState.x += this.findInnerSpace(textState) / 2;
+                    break;
+                case "AR":
+                    textState.x += this.findInnerSpace(textState);
+                    break;
+            }
+        }
+        _Window_Base_processEscapeCharacter.apply(this, arguments);
+    };
+
+    Window_Base.prototype.findInnerSpace = function(textState) {
+        return this.innerWidth - this.textSizeEx(this.findLineText(textState)).width - textState.startX;
+    };
+
     Window_Base.prototype.findLineText = function(textState) {
         let text = '';
         let index = textState.index;
@@ -46,22 +66,5 @@
             text += textState.text[index++];
         }
         return text;
-    };
-
-    const _Window_Base_processEscapeCharacter = Window_Base.prototype.processEscapeCharacter;
-    Window_Base.prototype.processEscapeCharacter = function(code, textState) {
-        switch (code) {
-            case "AC":
-                textState.x += this.findInnerSpace(textState) / 2;
-                break;
-            case "AR":
-                textState.x += this.findInnerSpace(textState);
-                break;
-        }
-        _Window_Base_processEscapeCharacter.apply(this, arguments);
-    };
-
-    Window_Base.prototype.findInnerSpace = function(textState) {
-        return this.innerWidth - this.textSizeEx(this.findLineText(textState)).width - textState.startX;
     };
 })();
