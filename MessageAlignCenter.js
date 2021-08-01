62561d209755f1a971ae267edff37729b9ce0f9f
トリアコンタン <triacontanetakenoko@gmail.com>
2021-08-01 20:01:54 +0900

    1.1.0 2021/08/01 他プラグインと制御文字が被ったときに備えて、パラメータから制御文字を変更できる機能を追加

--- MessageAlignCenter.js
+++ MessageAlignCenter.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.1.0 2021/08/01 他プラグインと制御文字が被ったときに備えて、パラメータから制御文字を変更できる機能を追加
  1.0.1 2021/07/29 リファクタリング
  1.0.0 2021/07/28 初版
 ----------------------------------------------------------------------------
@@ -18,7 +19,19 @@
  * @plugindesc メッセージの中央揃えプラグイン
  * @target MZ
  * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/MessageAlignCenter.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
  * @author トリアコンタン
+ *
+ * @param escapeCharacterCenter
+ * @text 中央揃えの制御文字
+ * @desc メッセージを中央揃えにする制御文字です。通常はそのままで使えますが、他プラグインと競合する場合は変更してください。
+ * @default ac
+ *
+ * @param escapeCharacterRight
+ * @text 右揃えの制御文字
+ * @desc メッセージを右揃えにする制御文字です。通常はそのままで使えますが、他プラグインと競合する場合は変更してください。
+ * @default ar
  *
  * @help MessageAlignCenter.js
  *
@@ -31,6 +44,11 @@
  * 制御文字は必ず行の先頭に記述してください。
  * 右読み言語には現バージョンでは対応していません。
  *
+ * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
+ * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
+ * 以下のフォルダに格納されています。
+ * dlc/BasicResources/plugins/official
+ *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
  *  についても制限はありません。
@@ -39,20 +57,30 @@
 
 (() => {
     'use strict';
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
 
     const _Window_Base_processEscapeCharacter = Window_Base.prototype.processEscapeCharacter;
     Window_Base.prototype.processEscapeCharacter = function(code, textState) {
         if (textState.drawing) {
             switch (code) {
-                case "AC":
+                case this.findEscapeCenter():
                     textState.x += this.findInnerSpace(textState) / 2;
                     break;
-                case "AR":
+                case this.findEscapeRight():
                     textState.x += this.findInnerSpace(textState);
                     break;
             }
         }
         _Window_Base_processEscapeCharacter.apply(this, arguments);
+    };
+
+    Window_Base.prototype.findEscapeCenter = function() {
+        return (param.escapeCharacterCenter || 'AC').toUpperCase();
+    };
+
+    Window_Base.prototype.findEscapeRight = function() {
+        return (param.escapeCharacterRight || 'AR').toUpperCase();
     };
 
     Window_Base.prototype.findInnerSpace = function(textState) {
