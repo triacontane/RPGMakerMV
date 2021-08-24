b0c87d5e48ad6a408bf1dea175133a323b160fcf
triacontane <triacontanekinoko@gmail.com>
2021-08-25 00:28:03 +0900

    1.2.0 2021/08/25 対象ステートを範囲指定できるパラメータを追加

--- StateAfterDeath.js
+++ StateAfterDeath.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.2.0 2021/08/25 対象ステートを範囲指定できるパラメータを追加
  1.1.0 2021/05/28 MZ用にリファクタリング
  1.0.1 2018/08/12 継続ステートが戦闘不能後にターン数で解除されなくなっていた問題を修正
  1.0.0 2018/08/12 初版
@@ -27,6 +28,18 @@
  * @desc 戦闘不能後も継続するステートの配列の一覧です。
  * @type state[]
  * @default []
+ *
+ * @param stateIdStart
+ * @text 対象ステート(範囲設定開始)
+ * @desc 戦闘不能後も継続するステートを範囲指定したい場合の開始IDです。
+ * @type state
+ * @default 0
+ *
+ * @param stateIdEnd
+ * @text 対象ステート(範囲設定終了)
+ * @desc 戦闘不能後も継続するステートを範囲指定したい場合の終了IDです。
+ * @type state
+ * @default 0
  *
  * @help StateAfterDeath.js
  *
@@ -51,6 +64,18 @@
  * @type state[]
  * @default []
  *
+ * @param stateIdStart
+ * @text 対象ステート(範囲設定開始)
+ * @desc 戦闘不能後も継続するステートを範囲指定したい場合の開始IDです。
+ * @type state
+ * @default 0
+ *
+ * @param stateIdEnd
+ * @text 対象ステート(範囲設定終了)
+ * @desc 戦闘不能後も継続するステートを範囲指定したい場合の終了IDです。
+ * @type state
+ * @default 0
+ *
  * @help StateAfterDeath.js
  *
  * 戦闘不能後も解除されず継続するステートを作成できます。
@@ -74,7 +99,11 @@
 
     const _Game_BattlerBase_die      = Game_BattlerBase.prototype.die;
     Game_BattlerBase.prototype.die = function() {
-        const stillStates     = this._states.filter(stateId=> param.states.includes(stateId));
+        const deathStates = param.states.clone();
+        for (let id = param.stateIdStart; id <= param.stateIdEnd; id++) {
+            deathStates.push(id);
+        }
+        const stillStates     = this._states.filter(stateId=> deathStates.includes(stateId));
         const stillStateTurns = {};
         stillStates.forEach(stateId => stillStateTurns[stateId] = this._stateTurns[stateId]);
         _Game_BattlerBase_die.apply(this, arguments);
