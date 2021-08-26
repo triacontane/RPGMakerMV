2ffd756daceee63a21451998ebd4bd3861dae095
triacontane <triacontanekinoko@gmail.com>
2021-08-26 23:10:19 +0900

    1.4.0 2021/08/26 プラグインの機能を一時無効化するスイッチを追加

--- PlayerPointerTurn.js
+++ PlayerPointerTurn.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.4.0 2021/08/26 プラグインの機能を一時無効化するスイッチを追加
 // 1.3.0 2021/06/20 MZで動作するよう修正
 // 1.2.0 2021/06/20 移動中も常にポインタの方を向く機能を追加
 // 1.1.0 2018/02/10 PD_8DirDash.jsと連携した場合、8方向に対応する機能を追加しました。
@@ -29,6 +30,12 @@
  * @desc 移動中も常にポインタの方を向きます。
  * @default false
  * @type boolean
+ *
+ * @param invalidSwitch
+ * @text 無効スイッチ
+ * @desc 指定したスイッチがONのときプラグインの機能が無効になります。
+ * @default 0
+ * @type switch
  *
  * @help 移動可能な場合にプレイヤーが
  * マウスポインタの方を向きます。
@@ -66,6 +73,9 @@
     };
 
     Game_Player.prototype.turnToPointer = function() {
+        if ($gameSwitches.value(param.invalidSwitch)) {
+            return;
+        }
         const tx = TouchInput.x, ty = TouchInput.y, sx = this.screenX(), sy = this.screenY();
         const dir = Math.abs(tx - sx) > Math.abs(ty - sy) ? (tx > sx ? 6 : 4) : (ty > sy ? 2 : 8);
         this.setDirection(dir);
