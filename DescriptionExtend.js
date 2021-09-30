f10450ca1e9b4aab0c81111f172b04f11f22a774
triacontane <triacontanekinoko@gmail.com>
2021-10-01 01:26:23 +0900

    1.2.0 2021/10/01 MZで動作するよう修正

--- DescriptionExtend.js
+++ DescriptionExtend.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.2.0 2021/10/01 MZで動作するよう修正
 // 1.1.0 2018/05/22 プラグインの機能を無効化するスイッチを追加
 // 1.0.0 2018/05/20 初版
 // ----------------------------------------------------------------------------
@@ -15,40 +16,12 @@
 //=============================================================================
 
 /*:
- * @plugindesc DescriptionExtendPlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @param swapDescription
- * @desc 元の説明文を無視してメモ欄の値で置き換えます。OFFの場合は元の説明文の次行に表示されます。
- * @default true
- * @type boolean
- *
- * @param helpLines
- * @desc ヘルプウィンドウの高さを変更したい場合は指定してください。0の場合は何もしません。
- * @default 0
- * @type number
- *
- * @param validSwitch
- * @desc 指定した番号のスイッチがONのときのみプラグインが有効になります。0の場合は常に有効になります。
- * @default 0
- * @type switch
- *
- * @param notePrefix
- * @desc 他のプラグインとメモ欄もしくはプラグインコマンドの名称が被ったときに指定する接頭辞です。通常は指定不要です。
- * @default
- *
- * @help DescriptionExtend.js
- *
- * ヘルプウィンドウの説明欄を拡張します。3行目以降を表示できるようになります。
- * メモ欄に以下の通り設定してください。
- * <ExtendDesc:aaa> // [aaa]を追加表示します。
- * <拡張説明:aaa>   // 同上
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc 説明拡張プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/DescriptionExtend.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
  *
  * @param swapDescription
  * @text 説明置き換え
@@ -68,11 +41,6 @@
  * @default 0
  * @type switch
  *
- * @param notePrefix
- * @text メモ欄接頭辞
- * @desc 他のプラグインとメモ欄もしくはプラグインコマンドの名称が被ったときに指定する接頭辞です。通常は指定不要です。
- * @default
- *
  * @help DescriptionExtend.js
  *
  * ヘルプウィンドウの説明欄を拡張します。3行目以降を表示できるようになります。
@@ -80,7 +48,10 @@
  * <ExtendDesc:aaa> // [aaa]を追加表示します。
  * <拡張説明:aaa>   // 同上
  *
- * このプラグインにはプラグインコマンドはありません。
+ * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
+ * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
+ * 以下のフォルダに格納されています。
+ * dlc/BasicResources/plugins/official
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -88,87 +59,32 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+(()=> {
     'use strict';
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
 
-    /**
-     * Get database meta information.
-     * @param object Database item
-     * @param name Meta name
-     * @returns {String} meta value
-     */
-    var getMetaValue = function(object, name) {
-        var tagName = param.notePrefix + name;
-        return object.meta.hasOwnProperty(tagName) ? convertEscapeCharacters(object.meta[tagName]) : null;
+    const _Scene_MenuBase_helpAreaHeight = Scene_MenuBase.prototype.helpAreaHeight;
+    Scene_MenuBase.prototype.helpAreaHeight = function() {
+        const lineNumber = param.helpLines;
+        if (lineNumber > 0) {
+            return this.calcWindowHeight(lineNumber, false);
+        } else {
+            return _Scene_MenuBase_helpAreaHeight.apply(this, arguments);
+        }
     };
-
-    /**
-     * Get database meta information.(for multi language)
-     * @param object Database item
-     * @param names Meta name array (for multi language)
-     * @returns {String} meta value
-     */
-    var getMetaValues = function(object, names) {
-        var metaValue;
-        names.some(function(name) {
-            metaValue = getMetaValue(object, name);
-            return metaValue !== null;
-        });
-        return metaValue;
-    };
-
-    /**
-     * Convert escape characters.(require any window object)
-     * @param text Target text
-     * @returns {String} Converted text
-     */
-    var convertEscapeCharacters = function(text) {
-        var windowLayer = SceneManager._scene._windowLayer;
-        return windowLayer ? windowLayer.children[0].convertEscapeCharacters(text.toString()) : text;
-    };
-
-    /**
-     * Create plugin parameter. param[paramName] ex. param.commandPrefix
-     * @param pluginName plugin name(EncounterSwitchConditions)
-     * @returns {Object} Created parameter
-     */
-    var createPluginParameter = function(pluginName) {
-        var paramReplacer = function(key, value) {
-            if (value === 'null') {
-                return value;
-            }
-            if (value[0] === '"' && value[value.length - 1] === '"') {
-                return value;
-            }
-            try {
-                return JSON.parse(value);
-            } catch (e) {
-                return value;
-            }
-        };
-        var parameter     = JSON.parse(JSON.stringify(PluginManager.parameters(pluginName), paramReplacer));
-        PluginManager.setParameters(pluginName, parameter);
-        return parameter;
-    };
-
-    var param = createPluginParameter('DescriptionExtend');
 
     /**
      * Window_Help
      * 拡張説明を追記します。
      */
-    var _Window_Help_initialize = Window_Help.prototype.initialize;
-    Window_Help.prototype.initialize = function(numLines) {
-        _Window_Help_initialize.call(this, numLines || param.helpLines);
-    };
-
-    var _Window_Help_setItem = Window_Help.prototype.setItem;
+    const _Window_Help_setItem = Window_Help.prototype.setItem;
     Window_Help.prototype.setItem = function(item) {
         _Window_Help_setItem.apply(this, arguments);
         if (!item || !this.isValidDescriptionExtend()) {
             return;
         }
-        var extendText = getMetaValues(item, ['拡張説明', 'ExtendDesc']);
+        const extendText = PluginManagerEx.findMetaValue(item, ['拡張説明', 'ExtendDesc']);
         if (extendText) {
             this.setText((param.swapDescription ? '' : this._text + '\n') + extendText);
         }
