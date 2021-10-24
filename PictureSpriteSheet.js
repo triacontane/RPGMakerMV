20f8e1321fa37dc8af6b4c4ca38b34f94182e9c6
トリアコンタン <triacontanetakenoko@gmail.com>
2021-10-24 15:40:16 +0900

    1.1.0 2021/10/24 MZで動作するよう修正

--- PictureSpriteSheet.js
+++ PictureSpriteSheet.js
@@ -1,86 +1,94 @@
 //=============================================================================
 // PictureSpriteSheet.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2015-2017 Triacontane
+// (C)2016 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2021/10/24 MZで動作するよう修正
 // 1.0.1 2017/02/07 端末依存の記述を削除
 // 1.0.0 2016/12/31 初版
 // ----------------------------------------------------------------------------
-// [Blog]   : http://triacontane.blogspot.jp/
+// [Blog]   : https://triacontane.blogspot.jp/
 // [Twitter]: https://twitter.com/triacontane/
 // [GitHub] : https://github.com/triacontane/
 //=============================================================================
 
 /*:
- * @plugindesc PictureSpriteSheetPlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
+ * @plugindesc ピクチャのスプライトシートプラグイン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/PictureSpriteSheet.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
  *
- * @help ピクチャを戦闘アニメーション画像のように横N * 縦Nのスプライトシートにします。
+ * @command SET_CELL_SIZE
+ * @text セルサイズ設定
+ * @desc 次の表示するピクチャのセル数を指定してます。「ピクチャの表示」直前に実行してください。
+ *
+ * @arg row
+ * @text 縦のセル数
+ * @desc 縦方向のセルの数です。
+ * @default 1
+ * @type number
+ * @min 1
+ *
+ * @arg column
+ * @text 横のセル数
+ * @desc 横方向のセルの数です。
+ * @default 1
+ * @type number
+ * @min 1
+ *
+ * @command SET_CELL
+ * @text セル番号指定
+ * @desc 指定したピクチャ番号のセル番号を設定します。
+ *
+ * @arg pictureId
+ * @text ピクチャ番号
+ * @desc 操作対象のピクチャ番号です。
+ * @default 1
+ * @type number
+ * @min 1
+ *
+ * @arg cellNumber
+ * @text セル番号
+ * @desc 設定したいセル番号です。指定の仕方はヘルプを参照してください。
+ * @default 0
+ * @type number
+ * @min 0
+ *
+ * @command ADD_CELL
+ * @text セル番号加算
+ * @desc 指定したピクチャ番号のセル番号をひとつ加算します。
+ *
+ * @arg pictureId
+ * @text ピクチャ番号
+ * @desc 操作対象のピクチャ番号です。
+ * @default 1
+ * @type number
+ * @min 1
+ *
+ * @help PictureSpriteSheet.js
+ *
+ * ピクチャを戦闘アニメーション画像のように横N * 縦Nのスプライトシートにします。
  * セル画像を切り替えてアニメーションを行う場合に、パフォーマンスを改善して
  * かつ非同期によるチラつきが発生しなくなります。
  *
- * ピクチャのアニメーションプラグインと類似していますが、
- * こちらはパフォーマンスを重視しています。連結した画像があまりに
- * 大きい(5000ピクセル以上)場合は正常に表示されない可能性があります。
+ * 連結した画像があまりに大きい(縦横いずれかが4096ピクセル以上)場合は
+ * 正常に表示されない可能性があります。
+ * プラグインコマンドからセル番号を指定してます。
  *
- * プラグインコマンド詳細
- *  イベントコマンド「プラグインコマンド」から実行。
- *  （パラメータの間は半角スペースで区切る）
- *
- * PSS_セルサイズ設定 4 3 # 次の表示するピクチャのセルを横[4] 縦[3]にします。
- * PSS_SET_CELL_SIZE 4 3  # 同上(※1)
- *
- * ※1 必ず「ピクチャの表示」直前に実行してください。
- * また、ピクチャ番号の指定は不要です。
- *
- * PSS_セル指定 1 4 # ピクチャ番号[1]のピクチャのセル番号を[4]にします。
- * PSS_SET_CELL 1 4 # 同上(※2)
- *
- * ※2 セル番号は[0]から開始で以下の凡例に従って指定してください。
+ * セル番号は[0]から開始で以下の凡例に従って指定してください。
  * [0] [1] [2]
  * [3] [4] [5]
  * [6] [7] [8]
  *
- * PSS_セル加算 1 # ピクチャ番号[1]のピクチャのセル番号を1加算します。
- * PSS_ADD_CELL 1 # 同上
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
- * @plugindesc ピクチャのスプライトシート化プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
- *
- * @help ピクチャを戦闘アニメーション画像のように横N * 縦Nのスプライトシートにします。
- * セル画像を切り替えてアニメーションを行う場合に、パフォーマンスを改善して
- * かつ非同期によるチラつきが発生しなくなります。
- *
- * ピクチャのアニメーションプラグインと類似していますが、
- * こちらはパフォーマンスを重視しています。連結した画像があまりに
- * 大きい(5000ピクセル以上)場合は正常に表示されない可能性があります。
- *
- * プラグインコマンド詳細
- *  イベントコマンド「プラグインコマンド」から実行。
- *  （パラメータの間は半角スペースで区切る）
- *
- * PSS_セルサイズ設定 4 3 # 次の表示するピクチャのセルを横[4] 縦[3]にします。
- * PSS_SET_CELL_SIZE 4 3  # 同上(※1)
- *
- * ※1 必ず「ピクチャの表示」直前に実行してください。
- * また、ピクチャ番号の指定は不要です。
- *
- * PSS_セル指定 1 4 # ピクチャ番号[1]のピクチャのセル番号を[4]にします。
- * PSS_SET_CELL 1 4 # 同上(※2)
- *
- * ※2 セル番号は[0]から開始で以下の凡例に従って指定してください。
- * [0] [1] [2]
- * [3] [4] [5]
- * [6] [7] [8]
- *
- * PSS_セル加算 1 # ピクチャ番号[1]のピクチャのセル番号を1加算します。
- * PSS_ADD_CELL 1 # 同上
+ * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
+ * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
+ * 以下のフォルダに格納されています。
+ * dlc/BasicResources/plugins/official
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -88,74 +96,27 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+(()=> {
     'use strict';
+    const script = document.currentScript;
 
-    //=============================================================================
-    // ローカル関数
-    //  プラグインパラメータやプラグインコマンドパラメータの整形やチェックをします
-    //=============================================================================
-    var getArgNumber = function(arg, min, max) {
-        if (arguments.length < 2) min = -Infinity;
-        if (arguments.length < 3) max = Infinity;
-        return (parseInt(arg) || 0).clamp(min, max);
-    };
+    PluginManagerEx.registerCommand(script, 'SET_CELL_SIZE', args => {
+        $gameScreen.setPictureSpriteSheetSize(args.column, args.row);
+    });
 
-    var convertEscapeCharacters = function(text) {
-        if (text == null) text = '';
-        var windowLayer = SceneManager._scene._windowLayer;
-        return windowLayer ? windowLayer.children[0].convertEscapeCharacters(text) : text;
-    };
-
-    var convertAllArguments = function(args) {
-        for (var i = 0; i < args.length; i++) {
-            args[i] = convertEscapeCharacters(args[i]);
-        }
-        return args;
-    };
-
-    //=============================================================================
-    // パラメータの取得と整形
-    //=============================================================================
-    var pluginCommandMap = new Map([
-        ['PSS_セルサイズ設定', 'setCellSizeForPss'],
-        ['PSS_SET_CELL_SIZE', 'setCellSizeForPss'],
-        ['PSS_セル指定', 'setCellForPss'],
-        ['PSS_SET_CELL', 'setCellForPss'],
-        ['PSS_セル加算', 'addCellForPss'],
-        ['PSS_ADD_CELL', 'addCellForPss']
-    ]);
-
-    //=============================================================================
-    // Game_Interpreter
-    //  プラグインコマンドを追加定義します。
-    //=============================================================================
-    var _Game_Interpreter_pluginCommand    = Game_Interpreter.prototype.pluginCommand;
-    Game_Interpreter.prototype.pluginCommand = function(command, args) {
-        _Game_Interpreter_pluginCommand.apply(this, arguments);
-        var pluginCommand = pluginCommandMap.get(command.toUpperCase());
-        if (pluginCommand) {
-            this[pluginCommand](convertAllArguments(args));
-        }
-    };
-
-    Game_Interpreter.prototype.setCellSizeForPss = function(args) {
-        $gameScreen.setPictureSpriteSheetSize(getArgNumber(args[0], 1), getArgNumber(args[1], 1));
-    };
-
-    Game_Interpreter.prototype.setCellForPss = function(args) {
-        var picture = $gameScreen.picture(getArgNumber(args[0], 1));
+    PluginManagerEx.registerCommand(script, 'SET_CELL', args => {
+        const picture = $gameScreen.picture(args.pictureId);
         if (picture) {
-            picture.setSheetCellIndex(getArgNumber(args[1], 0));
+            picture.setSheetCellIndex(args.cellNumber);
         }
-    };
+    });
 
-    Game_Interpreter.prototype.addCellForPss = function(args) {
-        var picture = $gameScreen.picture(getArgNumber(args[0], 1));
+    PluginManagerEx.registerCommand(script, 'ADD_CELL', args => {
+        const picture = $gameScreen.picture(args.pictureId);
         if (picture) {
             picture.addSheetCellIndex();
         }
-    };
+    });
 
     //=============================================================================
     // Game_Screen
@@ -166,7 +127,7 @@
     };
 
     Game_Screen.prototype.getPictureSpriteSheetSize = function() {
-        var result = this._pictureSpriteSheetSize;
+        const result = this._pictureSpriteSheetSize;
         if (result) {
             this._pictureSpriteSheetSize = undefined;
         }
@@ -177,11 +138,11 @@
     // Game_Picture
     //  セルサイズを取得して、スプライトシート化します。
     //=============================================================================
-    var _Game_Picture_show    = Game_Picture.prototype.show;
+    const _Game_Picture_show    = Game_Picture.prototype.show;
     Game_Picture.prototype.show = function(name, origin, x, y, scaleX,
                                            scaleY, opacity, blendMode) {
         _Game_Picture_show.apply(this, arguments);
-        var spriteSheetSize = $gameScreen.getPictureSpriteSheetSize();
+        const spriteSheetSize = $gameScreen.getPictureSpriteSheetSize();
         if (spriteSheetSize) {
             this._sheetCol = spriteSheetSize[0];
             this._sheetRow = spriteSheetSize[1];
@@ -220,9 +181,9 @@
     };
 
     Game_Picture.prototype.getSheetCellPosition = function() {
-        var index = this.getSheetCellIndex();
-        var colIndex = index % this._sheetCol;
-        var rowIndex = Math.floor(index / this._sheetCol);
+        const index = this.getSheetCellIndex();
+        const colIndex = index % this._sheetCol;
+        const rowIndex = Math.floor(index / this._sheetCol);
         return [colIndex, rowIndex];
     };
 
@@ -230,7 +191,7 @@
     // Sprite_Picture
     //  スプライトシート化を実装します。
     //=============================================================================
-    var _Sprite_Picture_update    = Sprite_Picture.prototype.update;
+    const _Sprite_Picture_update    = Sprite_Picture.prototype.update;
     Sprite_Picture.prototype.update = function() {
         _Sprite_Picture_update.apply(this, arguments);
         if (this.visible && this.picture().isUsingSpriteSheet()) {
@@ -239,9 +200,9 @@
     };
 
     Sprite_Picture.prototype.updateFrameForPss = function() {
-        var position = this.picture().getSheetCellPosition();
-        var width    = this.getCellWidth();
-        var height   = this.getCellHeight();
+        const position = this.picture().getSheetCellPosition();
+        const width    = this.getCellWidth();
+        const height   = this.getCellHeight();
         this.setFrame(position[0] * width, position[1] * height, width, height);
     };
 
