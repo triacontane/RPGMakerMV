1daa12b0a804c37910b652ed9a87362c1dd8de4b
triacontane <triacontanekinoko@gmail.com>
2021-11-10 09:36:16 +0900

    2.1.0 2021/11/10 フェードインを有効にしているとメニュー画面のウィンドウでも適用される問題を修正 一部のパラメータの初期値を修正、ヘルプを微修正

--- DestinationWindow.js
+++ DestinationWindow.js
@@ -1,11 +1,13 @@
 //=============================================================================
 // DestinationWindow.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2015-2017 Triacontane
+// (C)2017 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 2.1.0 2021/11/10 フェードインを有効にしているとメニュー画面のウィンドウでも適用される問題を修正
+//                  一部のパラメータの初期値を修正、ヘルプを微修正
 // 2.0.3 2020/12/13 非表示状態でメニューからマップへ戻ると1フレームだけ行動目標が表示される不具合を修正
 // 2.0.2 2020/10/12 フォント設定が効かない不具合を修正
 // 2.0.1 2020/10/11 言語設定によってアイコン設定の型が違う問題を修正
@@ -19,138 +21,22 @@
 // 1.1.0 2017/05/02 メニュー画面にも表示できる機能を追加
 // 1.0.0 2017/05/02 初版
 // ----------------------------------------------------------------------------
-// [Blog]   : http://triacontane.blogspot.jp/
+// [Blog]   : https://triacontane.blogspot.jp/
 // [Twitter]: https://twitter.com/triacontane/
 // [GitHub] : https://github.com/triacontane/
 //=============================================================================
 
 /*:
- * @plugindesc DestinationWindowPlugin
- * @target MZ
- * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master
- * @base PluginCommonBase
- * @author triacontane
- *
- * @param ShowingSwitchId
- * @desc 行動目標ウィンドウが表示されるスイッチIDです。
- * @default 1
- * @type switch
- *
- * @param CloseEventRunning
- * @desc イベントが実行されている間はウィンドウを閉じます。
- * @default true
- * @type boolean
- *
- * @param WindowX
- * @desc ウィンドウのX座標です。
- * @default 24
- * @type number
- *
- * @param WindowY
- * @desc ウィンドウのY座標です。
- * @default 24
- * @type number
- *
- * @param WindowWidth
- * @desc ウィンドウの横幅です。
- * @default 320
- * @type number
- *
- * @param WindowOpacity
- * @desc ウィンドウの不透明度です。
- * @default 255
- * @type number
- *
- * @param WindowSkin
- * @desc ウィンドウスキンのファイル名(img/system)です。拡張子不要。
- * @default
- * @require 1
- * @dir img/system/
- * @type file
- *
- * @param FadeFrame
- * @desc ウィンドウのフェードイン、フェードアウト時間(フレーム数)です。
- * @default 8
- * @type number
- *
- * @param FontSize
- * @desc ウィンドウのフォントサイズです。
- * @default 22
- * @type number
- *
- * @param ShowingInMenu
- * @desc 行動目標ウィンドウをメニュー画面にも表示します。ただし座標やサイズは自働で整形されます。
- * @default false
- * @type boolean
- *
- * @param AutoAdjust
- * @desc 指定した文字列がウィンドウに収まらない場合に自働で調整します。ただし一部の制御文字が使用不可となります。
- * @default true
- * @type boolean
- *
- * @param ShowingFrames
- * @desc 行動目標ウィンドウの表示フレーム数です。0を指定すると常時表示されます。
- * @default 0
- * @type number
- *
- * @param TextAlign
- * @desc 文字列の揃えです。
- * @default 0
- * @type select
- * @option Left
- * @value 0
- * @option Center
- * @value 1
- * @option Right
- * @value 2
- *
- * @param NoDestinationMaps
- * @desc 非表示にしたいマップIDリスト
- * @default []
- * @type number[]
- *
- * @command SET_DESTINATION
- * @text 目標設定
- * @desc 行動目標を設定します。
- *
- * @arg destination
- * @text 行動目標
- * @desc 行動目標です。
- * @type multiline_string
- * @default
- *
- * @arg icon
- * @text アイコン
- * @desc 行動目標の先頭に表示するアイコン番号です。
- * @type string
- * @default
- *
- * @help マップ中に行動目標ウィンドウを表示します。
- * 制御文字を含めた好きな文字列を表示できるので様々な用途に使えます。
- * 表示する内容はプラグインコマンドで、表示可否はスイッチで制御します。
- *
- * 自動調整を有効にした場合、文字列がウィンドウに収まらない場合に自働で調整します。
- * ただし、以下の制御文字が無効になります。
- * \i[n]、\c[n]、\{、\}
- *
- * 複数行の目標を表示したい場合は、文章中に改行を挿入してください。
- *
- * 以下のいずれかの条件を満たすマップでは、行動目標ウィンドウは非表示になります。
- * - プラグインパラメータ NoDestinationWindowMapIds で指定したIDをを持つ
- * - マップのメモ欄に <noDestinationWindow> と記述されている
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc 行動目標ウィンドウプラグイン
  * @target MZ
- * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/DestinationWindow.js
  * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
  * @author トリアコンタン
  *
  * @param 表示スイッチID
- * @desc 行動目標ウィンドウが表示されるスイッチIDです。
- * @default 1
+ * @desc 行動目標ウィンドウが表示されるスイッチIDです。0を指定した場合、無条件で表示されます。
+ * @default 0
  * @type switch
  *
  * @param イベント中は閉じる
@@ -200,9 +86,9 @@
  * @default false
  * @type boolean
  *
- * @param 自働調整
- * @desc 指定した文字列がウィンドウに収まらない場合に自働で調整します。ただし一部の制御文字が使用不可となります。
- * @default true
+ * @param 自動調整
+ * @desc 指定した文字列がウィンドウに収まらない場合に自動で調整します。ただし一部の制御文字が使用不可となります。
+ * @default false
  * @type boolean
  *
  * @param 表示フレーム数
@@ -222,7 +108,8 @@
  * @value 2
  *
  * @param NoDestinationWindowMapIds
- * @desc 非表示にしたいマップIDリスト
+ * @text 非表示マップIDリスト
+ * @desc 非表示にしたいマップIDのリスト
  * @default []
  * @type number[]
  *
@@ -246,15 +133,20 @@
  * 制御文字を含めた好きな文字列を表示できるので様々な用途に使えます。
  * 表示する内容はプラグインコマンドで、表示可否はスイッチで制御します。
  *
- * 自動調整を有効にした場合、文字列がウィンドウに収まらない場合に自働で調整します。
+ * 自動調整を有効にした場合、文字列がウィンドウに収まるよう自動調整します。
  * ただし、以下の制御文字が無効になります。
  * \i[n]、\c[n]、\{、\}
  *
  * 複数行の目標を表示したい場合は、文章中に改行を挿入してください。
  *
- * 以下のいずれかの条件を満たすマップでは、行動目標ウィンドウは非表示になります。
- * - プラグインパラメータ NoDestinationWindowMapIds で指定したIDをを持つ
+ * 以下のいずれかの条件を満たすマップでは、行動目標は非表示になります。
+ * - プラグインパラメータ NoDestinationWindowMapIds で指定したIDを持つ
  * - マップのメモ欄に <noDestinationWindow> と記述されている
+ *
+ * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
+ * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
+ * 以下のフォルダに格納されています。
+ * dlc/BasicResources/plugins/official
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -264,40 +156,40 @@
 
 (function() {
     'use strict';
-    var pluginName    = 'DestinationWindow';
+    const pluginName    = 'DestinationWindow';
 
     //=============================================================================
     // ローカル関数
     //  プラグインパラメータやプラグインコマンドパラメータの整形やチェックをします
     //=============================================================================
-    var getParamString = function(paramNames) {
+    const getParamString = function(paramNames) {
         if (!Array.isArray(paramNames)) paramNames = [paramNames];
-        for (var i = 0; i < paramNames.length; i++) {
-            var name = PluginManager.parameters(pluginName)[paramNames[i]];
+        for (let i = 0; i < paramNames.length; i++) {
+            const name = PluginManager.parameters(pluginName)[paramNames[i]];
             if (name) return name;
         }
         return '';
     };
 
-    var getParamNumber = function(paramNames, min, max) {
-        var value = getParamString(paramNames);
+    const getParamNumber = function(paramNames, min, max) {
+        const value = getParamString(paramNames);
         if (arguments.length < 2) min = -Infinity;
         if (arguments.length < 3) max = Infinity;
         return (parseInt(value) || 0).clamp(min, max);
     };
 
-    var getParamBoolean = function(paramNames) {
-        var value = getParamString(paramNames);
+    const getParamBoolean = function(paramNames) {
+        const value = getParamString(paramNames);
         return value.toUpperCase() === 'ON' || value.toUpperCase() === 'TRUE';
     };
 
-    var getParamNumberArray = function(paramNames) {
-        var value = PluginManager.parameters(pluginName)[paramNames];
+    const getParamNumberArray = function(paramNames) {
+        const value = PluginManager.parameters(pluginName)[paramNames];
         return JSON.parse(value)
             .map(function(e){ return Number(JSON.parse(e)); }, this);
     };
 
-    var getArgNumber = function(arg, min, max) {
+    const getArgNumber = function(arg, min, max) {
         if (arguments.length < 2) min = -Infinity;
         if (arguments.length < 3) max = Infinity;
         return (parseInt(arg) || 0).clamp(min, max);
@@ -306,7 +198,7 @@
     //=============================================================================
     // パラメータの取得と整形
     //=============================================================================
-    var param                       = {};
+    const param                       = {};
     param.showingSwitchId           = getParamNumber(['ShowingSwitchId', '表示スイッチID'], 0);
     param.windowX                   = getParamNumber(['WindowX', 'ウィンドウX座標']);
     param.windowY                   = getParamNumber(['WindowY', 'ウィンドウY座標']);
@@ -322,15 +214,11 @@
     param.textAlign                 = getParamNumber(['TextAlign', '文字列揃え'], 0);
     param.noDestinationWindowMapIds = getParamNumberArray(['NoDestinationWindowMapIds']);
 
-    var _extractMetadata = DataManager.extractMetadata;
+    const _extractMetadata = DataManager.extractMetadata;
     DataManager.extractMetadata = function(data) {
         _extractMetadata.call(this, data);
         if (this.isMapObject(data)) {
-            if (data.meta.noDestinationWindow) {
-                data.noDestinationWindow = true;
-            } else {
-                data.noDestinationWindow = false;
-            }
+            data.noDestinationWindow = data.meta.noDestinationWindow;
         }
     };
 
@@ -401,7 +289,7 @@
     // Scene_Map
     //  行動目標ウィンドウを生成します。
     //=============================================================================
-    var _Scene_Map_createMapNameWindow      = Scene_Map.prototype.createMapNameWindow;
+    const _Scene_Map_createMapNameWindow      = Scene_Map.prototype.createMapNameWindow;
     Scene_Map.prototype.createMapNameWindow = function() {
         this.createDestinationWindow();
         _Scene_Map_createMapNameWindow.apply(this, arguments);
@@ -420,7 +308,7 @@
     // Scene_Menu
     //  メニュー画面にも表示できるようにします。
     //=============================================================================
-    var _Scene_Menu_create      = Scene_Menu.prototype.create;
+    const _Scene_Menu_create      = Scene_Menu.prototype.create;
     Scene_Menu.prototype.create = function() {
         _Scene_Menu_create.apply(this, arguments);
         if (param.showingInMenu) {
@@ -447,7 +335,7 @@
     };
 
     Scene_Menu.prototype.destinationWindowRect = function() {
-        var x, y, width, height;
+        let x, y, width, height;
         x = this._commandWindow.x;
         if (this._commandWindow.maxCols() === 1) {
             width  = this._commandWindow.width;
@@ -527,8 +415,8 @@
     };
 
     Window_Destination.prototype.updateText = function() {
-        var textList  = $gameSystem.getDestination();
-        var iconIndex = getArgNumber(this.convertEscapeCharacters($gameSystem.getDestinationIcon()), 0);
+        const textList  = $gameSystem.getDestination();
+        const iconIndex = getArgNumber(this.convertEscapeCharacters($gameSystem.getDestinationIcon()), 0);
         if (textList.length !== this._textList.length) {
             this.height = this.fittingHeight(textList.length);
             this.createContents();
@@ -547,8 +435,8 @@
 
     Window_Destination.prototype.drawDestination = function(lineNumber) {
         this.contents.clearRect(0, lineNumber * this.lineHeight(), this.contentsWidth(), this.lineHeight());
-        var x = this.getContentsX();
-        var y = lineNumber * this.lineHeight() + this.lineHeight() / 2 - this.contents.fontSize / 2 - 4;
+        let x = this.getContentsX();
+        const y = lineNumber * this.lineHeight() + this.lineHeight() / 2 - this.contents.fontSize / 2 - 4;
         if (this._iconIndex > 0 && lineNumber === 0) {
             this.drawIcon(this._iconIndex, x, y);
             x += ImageManager.iconWidth;
@@ -565,11 +453,11 @@
         if (param.textAlign === 0) {
             return 0;
         }
-        var width = param.autoAdjust ? this.textWidth(this._text) : this.drawTextEx(this._text, 0, -this.lineHeight());
+        let width = param.autoAdjust ? this.textWidth(this._text) : this.drawTextEx(this._text, 0, -this.lineHeight());
         if (this._iconIndex > 0) {
             width += ImageManager.iconWidth;
         }
-        var division = (param.textAlign === 1 ? 2 : 1);
+        const division = (param.textAlign === 1 ? 2 : 1);
         return this.contentsWidth() / division - width / division;
     };
 
@@ -583,7 +471,12 @@
     };
 
     Window_Destination.prototype.isVisible = function() {
-        return $gameSwitches.value(param.showingSwitchId) && !this.isEventRunning() && this.isExistText() && !this.isOverFrame() && !this.isNoDestinationWindowMap();
+        return this.isValidSwitch() && !this.isEventRunning() &&
+            this.isExistText() && !this.isOverFrame() && !this.isNoDestinationWindowMap();
+    };
+
+    Window_Destination.prototype.isValidSwitch = function() {
+        return !param.showingSwitchId || $gameSwitches.value(param.showingSwitchId);
     };
 
     Window_Destination.prototype.isOverFrame = function() {
@@ -615,5 +508,9 @@
 
     Window_DestinationMenu.prototype.isOverFrame = function() {
         return false;
+    };
+
+    Window_DestinationMenu.prototype.getFadeValue = function() {
+        return 255;
     };
 })();
