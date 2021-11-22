0d98e46421e8ad40390824b96594d67f44d0f14c
トリアコンタン <triacontanetakenoko@gmail.com>
2021-11-22 15:00:23 +0900

    1.1.0 2021/11/22 MZで動作するよう修正

--- ItemNameEscape.js
+++ ItemNameEscape.js
@@ -1,88 +1,60 @@
 //=============================================================================
 // ItemNameEscape.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2015 Triacontane
+// (C)2015 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2021/11/22 MZで動作するよう修正
 // 1.0.1 2015/12/24 マップデータが歯抜けになっている場合に発生するエラーを対応
 // 1.0.0 2015/12/20 初版
 // ----------------------------------------------------------------------------
-// [Blog]   : http://triacontane.blogspot.jp/
+// [Blog]   : https://triacontane.blogspot.jp/
 // [Twitter]: https://twitter.com/triacontane/
 // [GitHub] : https://github.com/triacontane/
 //=============================================================================
 
 /*:
- * @plugindesc Plugin of item name escape
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @help Plugin of item name escape
- * Usable escape code : \V, \N, \P, \G
- * Target : name, description
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc 項目名の制御文字適用プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/ItemNameEscape.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
  *
- * @help データベースの項目に制御文字が使えるようになります。
+ * @help ItemNameEscape.js
+ * 
+ * データベースの項目に制御文字が使えるようになります。
  * 利用可能な制御文字は以下の通りです。
  * 　\V, \N, \P, \G
  *
  * 対象項目は項目は以下の通りです。
  * 　「名称」「説明文」
  *
- * このプラグインにはプラグインコマンドはありません。
+ * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
+ * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
+ * 以下のフォルダに格納されています。
+ * dlc/BasicResources/plugins/official
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
  *  についても制限はありません。
  *  このプラグインはもうあなたのものです。
  */
-(function () {
+(()=> {
     'use strict';
-
-    //=============================================================================
-    // ローカル関数
-    //  プラグインパラメータやプラグインコマンドパラメータの整形やチェックをします
-    //=============================================================================
-    var convertEscapeCharacters = function(text) {
-        if (text == null) text = '';
-        text = text.replace(/\\/g, '\x1b');
-        text = text.replace(/\x1b\x1b/g, '\\');
-        text = text.replace(/\x1bV\[(\d+)\]/gi, function() {
-            return $gameVariables.value(parseInt(arguments[1], 10));
-        }.bind(window));
-        text = text.replace(/\x1bV\[(\d+)\]/gi, function() {
-            return $gameVariables.value(parseInt(arguments[1], 10));
-        }.bind(window));
-        text = text.replace(/\x1bN\[(\d+)\]/gi, function() {
-            var n = parseInt(arguments[1]);
-            var actor = n >= 1 ? $gameActors.actor(n) : null;
-            return actor ? actor.name() : '';
-        }.bind(window));
-        text = text.replace(/\x1bP\[(\d+)\]/gi, function() {
-            var n = parseInt(arguments[1]);
-            var actor = n >= 1 ? $gameParty.members()[n - 1] : null;
-            return actor ? actor.name() : '';
-        }.bind(window));
-        text = text.replace(/\x1bG/gi, TextManager.currencyUnit);
-        return text;
-    };
 
     //=============================================================================
     // DataManager
     //  データベースの「名称」と「説明文」に制御文字を適用します。
     //=============================================================================
-    var _DataManager_onLoad = DataManager.onLoad;
+    const _DataManager_onLoad = DataManager.onLoad;
     DataManager.onLoad = function(object) {
         _DataManager_onLoad.apply(this, arguments);
         if (Array.isArray(object) && object !== $dataMapInfos) {
-            for (var i = 1; i < object.length; i++) {
-                var data = object[i];
+            for (let i = 1; i < object.length; i++) {
+                const data = object[i];
                 if (data != null) {
                     if (data.name && data.name.match(/\\/g)) data.preName = data.name;
                     if (data.description && data.description.match(/\\/g)) data.preDescription = data.description;
@@ -92,10 +64,10 @@
     };
 
     DataManager.databaseEscape = function() {
-        for (var i = 0; i < this._databaseFiles.length; i++) {
-            var object = window[this._databaseFiles[i].name];
+        for (let i = 0; i < this._databaseFiles.length; i++) {
+            const object = window[this._databaseFiles[i].name];
             if (Array.isArray(object) && object !== $dataMapInfos) {
-                for (var j = 1; j < object.length; j++) {
+                for (let j = 1; j < object.length; j++) {
                     if (object[j] != null) this.convertName(object[j]);
                 }
             }
@@ -103,15 +75,19 @@
     };
 
     DataManager.convertName = function(data) {
-        if (data.preName != null) data.name = convertEscapeCharacters(data.preName);
-        if (data.preDescription != null) data.description = convertEscapeCharacters(data.preDescription);
+        if (data.preName != null) {
+            data.name = PluginManagerEx.convertEscapeCharacters(data.preName);
+        }
+        if (data.preDescription != null) {
+            data.description = PluginManagerEx.convertEscapeCharacters(data.preDescription);
+        }
     };
 
     //=============================================================================
     // Scene_Boot
     //  ゲーム開始時にデータベースの制御文字を適用する処理を追加定義します。
     //=============================================================================
-    var _Scene_Boot_start = Scene_Boot.prototype.start;
+    const _Scene_Boot_start = Scene_Boot.prototype.start;
     Scene_Boot.prototype.start = function() {
         _Scene_Boot_start.call(this);
         DataManager.databaseEscape();
@@ -121,7 +97,7 @@
     // Game_Map
     //  マップリフレッシュ時にデータベースの制御文字を適用する処理を追加定義します。
     //=============================================================================
-    var _Game_Map_refresh = Game_Map.prototype.refresh;
+    const _Game_Map_refresh = Game_Map.prototype.refresh;
     Game_Map.prototype.refresh = function() {
         _Game_Map_refresh.call(this);
         DataManager.databaseEscape();
