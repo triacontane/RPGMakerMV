4be3b925c0854bd775c850345f6cd68e48511088
トリアコンタン <triacontanetakenoko@gmail.com>
2022-01-07 15:53:05 +0900

    1.2.0 2022/01/07 MZで動作するよう修正

--- InitialState.js
+++ InitialState.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.2.0 2022/01/07 MZで動作するよう修正
 // 1.1.0 2018/07/08 複数の初期ステートを設定できる機能を追加
 // 1.0.1 2017/02/07 端末依存の記述を削除
 // 1.0.0 2017/01/12 初版
@@ -16,36 +17,23 @@
 //=============================================================================
 
 /*:
- * @plugindesc InitialStatePlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @help 敵キャラに初期状態でステートを付与します。
- *
- * 敵キャラのメモ欄に以下の通り記述してください。
- *
- * <ISステート:3> # 戦闘開始時にステートID[3]が自動で付与されます。
- * <ISState:3>    # 同上
- *
- * 複数のステートを設定したい場合は、カンマ区切りでIDを指定してください。
- * 例：<IS_ステート:3,4,5>
- *
- * このプラグインにはプラグインコマンドはありません。
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc 初期ステートプラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/InitialState.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
  *
- * @help 敵キャラに初期状態でステートを付与します。
+ * @help InitialState.js
  *
+ * 敵キャラに初期状態でステートを付与します。
  * 敵キャラのメモ欄に以下の通り記述してください。
  *
- * <IS_ステート:3> # 戦闘開始時にステートID[3]が自動で付与されます。
- * <IS_State:3>    # 同上
+ * <初期ステート:3>   # 戦闘開始時にステートID[3]が自動で付与されます。
+ * <InitialState:3> # 同上
  *
  * 複数のステートを設定したい場合は、カンマ区切りでIDを指定してください。
- * 例：<IS_ステート:3,4,5>
+ * 例：<InitialState:3,4,5>
  *
  * このプラグインにはプラグインコマンドはありません。
  *
@@ -55,50 +43,21 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+(()=> {
     'use strict';
-    var metaTagPrefix = 'IS_';
 
-    var parseArgs = function(arg) {
-        return JSON.parse('[' + convertEscapeCharacters(arg) + ']');
-    };
-
-    var getMetaValue = function(object, name) {
-        var metaTagName = metaTagPrefix + (name ? name : '');
-        return object.meta.hasOwnProperty(metaTagName) ? object.meta[metaTagName] : undefined;
-    };
-
-    var getMetaValues = function(object, names) {
-        if (!Array.isArray(names)) return getMetaValue(object, names);
-        for (var i = 0, n = names.length; i < n; i++) {
-            var value = getMetaValue(object, names[i]);
-            if (value !== undefined) return value;
-        }
-        return undefined;
-    };
-
-    var convertEscapeCharacters = function(text) {
-        if (text == null) text = '';
-        var windowLayer = SceneManager._scene._windowLayer;
-        return windowLayer ? windowLayer.children[0].convertEscapeCharacters(text) : text;
-    };
-
-    var _Game_Enemy_setup = Game_Enemy.prototype.setup;
+    const _Game_Enemy_setup = Game_Enemy.prototype.setup;
     Game_Enemy.prototype.setup = function(enemyId, x, y) {
         _Game_Enemy_setup.apply(this, arguments);
         this.setupInitialState();
     };
 
     Game_Enemy.prototype.setupInitialState = function() {
-        var stateList = getMetaValues(this.enemy(), ['State', 'ステート']);
+        const stateList = PluginManagerEx.findMetaValue(this.enemy(), ['初期ステート', 'InitialState']);
         if (!stateList) {
             return;
         }
-        parseArgs(stateList).forEach(function(state) {
-            if (state > 0) {
-                this.addState(state);
-            }
-        }, this);
+        String(stateList).split(',').forEach(state => this.addState(parseInt(state)));
     };
 })();
 
