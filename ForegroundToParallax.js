d6b22b3d9009e24348a2656e58d14c978205131d
triacontane <triacontanekinoko@gmail.com>
2022-01-18 22:12:52 +0900

    1.1.0 2022/01/18 指定したマップのみ遠景化する機能を追加

--- ForegroundToParallax.js
+++ ForegroundToParallax.js
@@ -1,11 +1,12 @@
 //=============================================================================
 // ForegroundToParallax.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2015-2017 Triacontane
+// (C)2017 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2022/01/18 指定したマップのみ遠景化する機能を追加
 // 1.0.0 2017/06/19 初版
 // ----------------------------------------------------------------------------
 // [Blog]   : https://triacontane.blogspot.jp/
@@ -14,25 +15,15 @@
 //=============================================================================
 
 /*:
- * @plugindesc ForegroundToParallaxPlugin
- * @author triacontane
- *
- * @help 準公式プラグイン「Foreground.js」で表示する近景を遠景に切り替えます。
- * もともとの遠景の後ろ側に表示されるようになります。
- * 二種類の遠景を同時に表示させたい場合などにご使用ください。
- *
- * プラグイン管理画面でForeground.jsよりも下に配置してください。
- *
- * Foreground.jsはサンプルゲームなどに収録されています。
- *
- * このプラグインにはプラグインコマンドはありません。
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc Foregroundの遠景化プラグイン
  * @author トリアコンタン
  *
+ * @param validNoteTag
+ * @text 指定した場合のみ有効
+ * @desc ONにするとタグ<fgToParallax>を記載したマップでのみ遠景化が有効になります。
+ * @default false
+ * @type boolean
+ *
  * @help 準公式プラグイン「Foreground.js」で表示する近景を遠景に切り替えます。
  * もともとの遠景の後ろ側に表示されるようになります。
  * 二種類の遠景を同時に表示させたい場合などにご使用ください。
@@ -40,6 +31,10 @@
  * プラグイン管理画面でForeground.jsよりも下に配置してください。
  *
  * Foreground.jsはサンプルゲームなどに収録されています。
+ *
+ * パラメータ「指定した場合のみ有効」をONにすると、以下のタグを記載したマップのみ
+ * 遠景化されます。
+ * <fgToParallax>
  *
  * このプラグインにはプラグインコマンドはありません。
  *
@@ -52,9 +47,33 @@
 (function() {
     'use strict';
 
+    var createPluginParameter = function(pluginName) {
+        var paramReplacer = function(key, value) {
+            if (value === 'null') {
+                return value;
+            }
+            if (value[0] === '"' && value[value.length - 1] === '"') {
+                return value;
+            }
+            try {
+                return JSON.parse(value);
+            } catch (e) {
+                return value;
+            }
+        };
+        var parameter     = JSON.parse(JSON.stringify(PluginManager.parameters(pluginName), paramReplacer));
+        PluginManager.setParameters(pluginName, parameter);
+        return parameter;
+    };
+
+    var param = createPluginParameter('ForegroundToParallax');
+
     var _Spriteset_Map_createForeground = Spriteset_Map.prototype.createForeground;
     Spriteset_Map.prototype.createForeground = function() {
         _Spriteset_Map_createForeground.apply(this, arguments);
+        if (param.validNoteTag && $dataMap && !!$dataMap.meta.fgToParallax) {
+            return;
+        }
         this._baseSprite.removeChild(this._foreground);
         var newIndex = this._baseSprite.getChildIndex(this._parallax);
         this._baseSprite.addChildAt(this._foreground, newIndex);
