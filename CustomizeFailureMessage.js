e30c37c7546270bc48875695494eba00cdac480f
トリアコンタン <triacontanetakenoko@gmail.com>
2022-01-23 15:48:07 +0900

    1.1.0 2022/01/23 有効な効果を与えられなかった場合にもカスタム失敗メッセージを表示するよう変更

--- CustomizeFailureMessage.js
+++ CustomizeFailureMessage.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2022/01/23 有効な効果を与えられなかった場合にもカスタム失敗メッセージを表示するよう変更
 // 1.0.1 2018/02/11 失敗メッセージ表示後に別のスキルで失敗した場合、もとの失敗メッセージが表示される不具合を修正
 // 1.0.0 2016/07/31 初版
 // ----------------------------------------------------------------------------
@@ -82,6 +83,19 @@
     var _Window_BattleLog_displayMiss = Window_BattleLog.prototype.displayMiss;
     Window_BattleLog.prototype.displayMiss = function(target) {
         _Window_BattleLog_displayMiss.apply(this, arguments);
+        this.displayCustomFailure(target);
+    };
+
+
+    var _Window_BattleLog_displayFailure = Window_BattleLog.prototype.displayFailure;
+    Window_BattleLog.prototype.displayFailure = function(target) {
+        _Window_BattleLog_displayFailure.apply(this, arguments);
+        if (target.result().isHit() && !target.result().success) {
+            this.displayCustomFailure(target);
+        }
+    };
+
+    Window_BattleLog.prototype.displayCustomFailure = function(target) {
         var fmt = target.getFailureMessage();
         if (fmt) {
             for (var i = this._methods.length - 1; i >= 0; i--) {
