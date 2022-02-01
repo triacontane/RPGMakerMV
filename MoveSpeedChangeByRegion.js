54013a4611b165cc29424dd5232ab255eada4809
triacontane <triacontanekinoko@gmail.com>
2022-02-01 20:29:11 +0900

    1.4.0 2022/02/01 速度変化をイベントに適用するかどうかを設定できるパラメータ、メモ欄を追加

--- MoveSpeedChangeByRegion.js
+++ MoveSpeedChangeByRegion.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.4.0 2022/02/01 速度変化をイベントに適用するかどうかを設定できるパラメータ、メモ欄を追加
 // 1.3.1 2022/01/27 ヘルプの記述を修正
 // 1.3.0 2022/01/27 MZで動作するよう修正
 // 1.2.0 2021/11/21 1.1.0のメモ欄の指定方法変更
@@ -56,6 +57,12 @@
  * @default 1
  * @type number
  *
+ * @param eventNoApply
+ * @text イベントには適用しない
+ * @desc 有効にするとイベントには速度補正を適用しなくなります。
+ * @default false
+ * @type boolean
+ *
  * @help MoveSpeedChangeByRegion.js
  *
  * 指定した地形もしくはリージョンに乗っている間だけキャラクターの移動速度を
@@ -71,6 +78,13 @@
  * 現在の速度から相対的に指定する場合は[+][-]の記号を付けてください。
  * ・リージョン[1]のとき速度が[2]段階あがる
  * <速度リージョン指定:1,+2>
+ *
+ * イベントの速度補正を個別に設定する場合、メモ欄に以下の通り指定してください。
+ * メモ欄の設定はパラメータの設定より優先されます。
+ * <速度地形無効>
+ * <NoMoveSpeedRegion>
+ * <速度地形有効>
+ * <MoveSpeedRegion>
  *
  * このプラグインにはプラグインコマンドはありません。
  *
@@ -103,13 +117,16 @@
     //=============================================================================
     const _Game_CharacterBase_realMoveSpeed      = Game_CharacterBase.prototype.realMoveSpeed;
     Game_CharacterBase.prototype.realMoveSpeed = function() {
-        const speed = _Game_CharacterBase_realMoveSpeed.apply(this, arguments) +
-            this.changeSpeedByTerrainTags() + this.changeSpeedByRegions();
+        const originalSpeed = _Game_CharacterBase_realMoveSpeed.apply(this, arguments);
+        if (!this.isValidMoveSpeedChange()) {
+            return originalSpeed;
+        }
+        const speed = originalSpeed + this.changeSpeedByTerrainTags() + this.changeSpeedByRegions();
         return this.changeSpeedByNote(speed);
     };
 
     Game_CharacterBase.prototype.changeSpeedByNote = function(speed) {
-        if (!(this instanceof Game_Player) && !(this instanceof Game_Follower)) {
+        if (!this.isValidMoveSpeedChangeByNote()) {
             return speed;
         }
         $gameParty.battleMembers().forEach(function(member) {
@@ -125,6 +142,14 @@
             }, this);
         }, this);
         return speed;
+    };
+
+    Game_CharacterBase.prototype.isValidMoveSpeedChange = function() {
+        return true;
+    };
+
+    Game_CharacterBase.prototype.isValidMoveSpeedChangeByNote = function() {
+        return false;
     };
 
     Game_CharacterBase.prototype.findSpeedByNote = function(noteText, id, defaultSpeed) {
@@ -168,12 +193,38 @@
         return param.deltaSpeed;
     };
 
+    const _Game_Event_initialize = Game_Event.prototype.initialize;
+    Game_Event.prototype.initialize = function(mapId, eventId) {
+        _Game_Event_initialize.apply(this, arguments);
+        if (PluginManagerEx.findMetaValue(this.event(), ['MoveSpeedRegion', '速度地形有効'])) {
+            this._moveSpeedByRegion = true;
+        } else if (PluginManagerEx.findMetaValue(this.event(), ['NoMoveSpeedRegion', '速度地形無効'])) {
+            this._moveSpeedByRegion = false;
+        }
+    };
+
+    Game_Event.prototype.isValidMoveSpeedChange = function() {
+        if (this._moveSpeedByRegion !== undefined) {
+            return this._moveSpeedByRegion;
+        } else {
+            return !param.eventNoApply;
+        }
+    };
+
+    Game_Player.prototype.isValidMoveSpeedChangeByNote = function() {
+        return true;
+    };
+
     //=============================================================================
     // Game_Follower
     //  実移動速度を再定義
     //=============================================================================
     Game_Follower.prototype.realMoveSpeed = function() {
         return _Game_CharacterBase_realMoveSpeed.apply(this, arguments);
+    };
+
+    Game_Follower.prototype.isValidMoveSpeedChangeByNote = function() {
+        return true;
     };
 })();
 
