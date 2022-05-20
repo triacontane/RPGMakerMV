b5ed1c6e7f09dd182e4d122735b3c4cc48f200bd
トリアコンタン <triacontanetakenoko@gmail.com>
2022-05-20 23:49:39 +0900

    1.2.0 2022/05/20 MZで動作するよう修正

--- MessageUnlockBusy.js
+++ MessageUnlockBusy.js
@@ -1,35 +1,50 @@
 //=============================================================================
 // MessageUnlockBusy.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2015 Triacontane
+// (C)2015 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.2.0 2022/05/20 MZで動作するよう修正
 // 1.1.1 2016/08/23 プラグインコマンドが小文字でも機能するよう修正
 // 1.1.0 2016/08/23 一度ロックを解除したあとで再度、ロック状態に戻すプラグインコマンドを追加
 // 1.0.0 2016/07/20 初版
 // ----------------------------------------------------------------------------
-// [Blog]   : http://triacontane.blogspot.jp/
+// [Blog]   : https://triacontane.blogspot.jp/
 // [Twitter]: https://twitter.com/triacontane/
 // [GitHub] : https://github.com/triacontane/
 //=============================================================================
 
 /*:
- * @plugindesc メッセージのロック解除プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @plugindesc メッセージの入力待ち解除プラグイン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/MessageUnlockBusy.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
  *
- * @help メッセージ中に制御文字「\UL」を挿入すると、
+ * @param escapeCode
+ * @text 制御文字
+ * @desc 本プラグインの制御文字をデフォルトのULから変更する場合に指定します。
+ * @default
+ *
+ * @command RE_WAIT
+ * @text 再ウェイト
+ * @desc \UL実行後に再度、入力待ち状態に戻します。
+ *
+ * @help MessageUnlockBusy.js
+ *
+ * メッセージ中に制御文字「\UL」を挿入すると、
  * メッセージウィンドウを表示したまま次のイベント命令に移行するようになります。
+ * ポーズサインは表示されたままです。
  *
- * さらにプラグインコマンドを実行することで、再度実行待ち状態に戻すこともできます。
+ * プラグインコマンドから再度、入力待ち状態に戻すこともできます。
  *
- * プラグインコマンド詳細
- *  イベントコマンド「プラグインコマンド」から実行。
- *  （パラメータの間は半角スペースで区切る）
- *
- * MUB_再ウェイト # 再度メッセージ待ち状態に戻します。
- * MUB_RE_WAIT    # 同上
+ * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
+ * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
+ * 以下のフォルダに格納されています。
+ * dlc/BasicResources/plugins/official
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -37,22 +52,22 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+(()=> {
     'use strict';
-    var metaTagPrefix = 'MUB_';
-    var setting = {
-        unlockCode:'UL'
-    };
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
 
-    var getCommandName = function(command) {
-        return (command || '').toUpperCase();
-    };
+    PluginManagerEx.registerCommand(script, 'RE_WAIT', function(args) {
+        if ($gameMessage.isBusy()) {
+            this.setWaitMode('message');
+        }
+    });
 
     //=============================================================================
     // Game_Message
     //  ウェイト解除処理を追加定義します。
     //=============================================================================
-    var _Game_Message_clear = Game_Message.prototype.clear;
+    const _Game_Message_clear = Game_Message.prototype.clear;
     Game_Message.prototype.clear = function() {
         _Game_Message_clear.apply(this, arguments);
         this._interpreter = null;
@@ -68,44 +83,7 @@
         }
     };
 
-    //=============================================================================
-    // Game_Interpreter
-    //  プラグインコマンドを追加定義します。
-    //=============================================================================
-    var _Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
-    Game_Interpreter.prototype.pluginCommand = function(command, args) {
-        _Game_Interpreter_pluginCommand.apply(this, arguments);
-        var commandPrefix = new RegExp('^' + metaTagPrefix, 'i');
-        if (!command.match(commandPrefix)) return;
-        try {
-            this.pluginCommandMessageUnlockBusy(command.replace(commandPrefix, ''), args);
-        } catch (e) {
-            if ($gameTemp.isPlaytest() && Utils.isNwjs()) {
-                var window = require('nw.gui').Window.get();
-                if (!window.isDevToolsOpen()) {
-                    var devTool = window.showDevTools();
-                    devTool.moveTo(0, 0);
-                    devTool.resizeTo(window.screenX + window.outerWidth, window.screenY + window.outerHeight);
-                    window.focus();
-                }
-            }
-            console.log('プラグインコマンドの実行中にエラーが発生しました。');
-            console.log('- コマンド名 　: ' + command);
-            console.log('- コマンド引数 : ' + args);
-            console.log('- エラー原因   : ' + e.stack || e.toString());
-        }
-    };
-
-    Game_Interpreter.prototype.pluginCommandMessageUnlockBusy = function(command, args) {
-        switch (getCommandName(command)) {
-            case '再ウェイト' :
-            case 'RE_WAIT' :
-                if ($gameMessage.isBusy()) this.setWaitMode('message');
-                break;
-        }
-    };
-
-    var _Game_Interpreter_command101 = Game_Interpreter.prototype.command101;
+    const _Game_Interpreter_command101 = Game_Interpreter.prototype.command101;
     Game_Interpreter.prototype.command101 = function() {
         if (!$gameMessage.isBusy()) {
             $gameMessage.setInterpreter(this);
@@ -117,10 +95,11 @@
     // Window_Message
     //  制御文字を実装します。
     //=============================================================================
-    var _Window_Message_processEscapeCharacter = Window_Message.prototype.processEscapeCharacter;
+    const _Window_Message_processEscapeCharacter = Window_Message.prototype.processEscapeCharacter;
     Window_Message.prototype.processEscapeCharacter = function(code, textState) {
+        const ulCode = param.escapeCode || 'UL';
         switch (code) {
-            case setting.unlockCode:
+            case ulCode:
                 $gameMessage.setWaitMode('');
                 break;
             default:
