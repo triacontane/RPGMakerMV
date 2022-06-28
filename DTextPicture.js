1a86093fbeceec8303dcb03eab5ad3ac8b8e18a8
triacontane <triacontanekinoko@gmail.com>
2022-06-28 13:22:20 +0900

    1.20.4 2022/06/28 ウィンドウカーソルの矩形指定表示が指定したパラメータ通りに表示されていなかった問題を修正

--- DTextPicture.js
+++ DTextPicture.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.20.4 2022/06/28 ウィンドウカーソルの矩形指定表示が指定したパラメータ通りに表示されていなかった問題を修正
 // 1.20.3 2021/04/29 プラグインコマンドでフォントサイズの指定に制御文字が使えなくなっていた問題を修正
 // 1.20.2 2021/02/09 NRP_EvalPluginCommand.jsと併用したとき、D_TEXTの制御文字を変換対象外にするよう修正
 // 1.20.1 2021/02/08 色調変更したピクチャを消去し、同一の番号で動的文字列ピクチャを作成したとき文字列ピクチャが表示されない場合がある問題を修正
@@ -374,10 +375,10 @@
                 var windowRect = null;
                 if (getArgBoolean(args[1])) {
                     windowRect = {
-                        x     : getArgNumber(args[3] || '', 0),
-                        y     : getArgNumber(args[4] || '', 0),
-                        width : getArgNumber(args[5] || '', 0),
-                        height: getArgNumber(args[6] || '', 0)
+                        x     : getArgNumber(args[2] || '', 0),
+                        y     : getArgNumber(args[3] || '', 0),
+                        width : getArgNumber(args[4] || '', 0),
+                        height: getArgNumber(args[5] || '', 0)
                     };
                 }
                 $gameScreen.setDTextWindowCursor(getArgNumber(args[0], 0), windowRect);
@@ -657,8 +658,8 @@
         var rect = picture.getWindowCursor();
         if (rect) {
             var width  = rect.width || this._frameWindow.contentsWidth();
-            var height = rect.width || this._frameWindow.contentsHeight();
-            this._frameWindow.setCursorRect(0, 0, width, height);
+            var height = rect.height || this._frameWindow.contentsHeight();
+            this._frameWindow.setCursorRect(rect.x || 0, rect.y || 0, width, height);
             this._frameWindow.active = picture.getWindowCursorActive();
         } else {
             this._frameWindow.setCursorRect(0, 0, 0, 0);
