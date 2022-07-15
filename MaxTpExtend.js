609d83cd19129084c35229f79935257a75a869e0
triacontane <triacontanekinoko@gmail.com>
2022-07-16 00:42:43 +0900

    1.1.0 2022/07/16 TP拡張のタグの対象にスキルを追加

--- MaxTpExtend.js
+++ MaxTpExtend.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.1.0 2022/07/16 TP拡張のタグの対象にスキルを追加
  1.0.1 2021/06/22 競合が起こりやすい記述を変更
  1.0.0 2020/05/08 初版
 ----------------------------------------------------------------------------
@@ -43,7 +44,7 @@
  * 特徴を有するデータベース(※)のメモ欄に以下の通り記述してください。
  * <MaxTp:30>  // 最大TPが30加算されます。
  * <最大TP:30> // 同上
- * ※アクター、職業、武器、防具、敵キャラ、ステート
+ * ※アクター、職業、武器、防具、敵キャラ、ステート、スキル
  *
  * 負の値も設定できます。実際のTPは基本値の100に対して指定した
  * メモ欄全ての値が合算されます。制御文字\v[n]で変数の値を参照できます。
@@ -104,12 +105,21 @@
     };
 
     Game_BattlerBase.prototype.findMaxTpExtend = function() {
-        return this.traitObjects().reduce(function(tp, traitObj) {
+        return this.findMaxTpTraits().reduce(function(tp, traitObj) {
             var meta = getMetaValues(traitObj, ['最大TP', 'MaxTp']);
             if (meta) {
                 tp = tp + parseInt(meta) || 0;
             }
             return tp;
         }, 0);
+    };
+
+    Game_BattlerBase.prototype.findMaxTpTraits = function() {
+        return this.traitObjects();
+    };
+
+    Game_Actor.prototype.findMaxTpTraits = function() {
+        const traits = Game_BattlerBase.prototype.findMaxTpTraits.apply(this, arguments);
+        return traits.concat(this.skills());
     };
 })();
