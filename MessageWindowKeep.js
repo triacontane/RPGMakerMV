2881026fac70b36920fea211e32f79fbd9e425a0
triacontane <triacontanekinoko@gmail.com>
2022-07-18 16:31:38 +0900

    1.1.0 2022/07/18 MV版を作成

--- MessageWindowKeep.js
+++ MessageWindowKeep.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.1.0 2022/07/18 MV版を作成
  1.0.0 2022/01/18 初版
 ----------------------------------------------------------------------------
  [Blog]   : https://triacontane.blogspot.jp/
@@ -15,10 +16,6 @@
 
 /*:
  * @plugindesc メッセージウィンドウの維持プラグイン
- * @target MZ
- * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/MessageWindowKeep.js
- * @base PluginCommonBase
- * @orderAfter PluginCommonBase
  * @author トリアコンタン
  *
  * @param keepSwitch
@@ -32,11 +29,6 @@
  * 指定したスイッチがONのあいだ、メッセージウィンドウを閉じずに残します。
  * スイッチがOFFになると閉じられます。
  * その他、メニュー画面の開閉でも閉じるので基本的にはイベント中に制御します。
- *　
- * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
- * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
- * 以下のフォルダに格納されています。
- * dlc/BasicResources/plugins/official
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -44,12 +36,31 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(() => {
+(function() {
     'use strict';
-    const script = document.currentScript;
-    const param = PluginManagerEx.createParameter(script);
 
-    const _Window_Message_checkToNotClose = Window_Message.prototype.checkToNotClose;
+    var createPluginParameter = function(pluginName) {
+        var paramReplacer = function(key, value) {
+            if (value === 'null') {
+                return value;
+            }
+            if (value[0] === '"' && value[value.length - 1] === '"') {
+                return value;
+            }
+            try {
+                return JSON.parse(value);
+            } catch (e) {
+                return value;
+            }
+        };
+        var parameter     = JSON.parse(JSON.stringify(PluginManager.parameters(pluginName), paramReplacer));
+        PluginManager.setParameters(pluginName, parameter);
+        return parameter;
+    };
+
+    var param = createPluginParameter('MessageWindowKeep');
+
+    var _Window_Message_checkToNotClose = Window_Message.prototype.checkToNotClose;
     Window_Message.prototype.checkToNotClose = function() {
         _Window_Message_checkToNotClose.apply(this, arguments);
         if (!this.doesContinue() && this.isOpen()) {
@@ -57,7 +68,7 @@
         }
     };
 
-    const _Window_Message_doesContinue = Window_Message.prototype.doesContinue;
+    var _Window_Message_doesContinue = Window_Message.prototype.doesContinue;
     Window_Message.prototype.doesContinue = function() {
         const result = _Window_Message_doesContinue.apply(this, arguments);
         return result || $gameSwitches.value(param.keepSwitch);
