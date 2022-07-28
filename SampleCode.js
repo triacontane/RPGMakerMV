b1544e40146f31b09c36d4068afcf3e3b650b967
triacontane <triacontanekinoko@gmail.com>
2022-07-28 23:30:31 +0900

    1.0.0b 2022/07/28 ヘルプ追記

--- SampleCode.js
+++ SampleCode.js
@@ -5,17 +5,26 @@
 =============================================================================*/
 
 /*:
- * @plugindesc サンプルコード
+ * @plugindesc アイテム選択の処理で隠しアイテムの個数を表示
  * @target MZ
  * @author トリアコンタン
  *
  * @help
  *
- *
- *
- * MV/MZ両用
+ * イベントコマンド『アイテム選択の処理』で
+ * 隠しアイテムの個数を表示するよう仕様変更します。
  */
 (()=> {
 
+    const _Window_EventItem_needsNumber = Window_EventItem.prototype.needsNumber;
+    Window_EventItem.prototype.needsNumber = function() {
+        const result = _Window_EventItem_needsNumber.apply(this, arguments);
+        const itypeId = $gameMessage.itemChoiceItypeId();
+        if (itypeId >= 3) {
+            return true;
+        } else {
+            return result;
+        }
+    };
 })();
 
