b6a12bfe4e7623e702d1571f7d05705482fa5e75
triacontane <triacontanekinoko@gmail.com>
2022-10-14 07:38:18 +0900

    1.2.0 2022/10/14 MZで動作するよう修正

--- ChangeMapTouchPolicy.js
+++ ChangeMapTouchPolicy.js
@@ -1,11 +1,12 @@
 //=============================================================================
 // ChangeMapTouchPolicy.js
 // ----------------------------------------------------------------------------
-// (C)2015-2018 Triacontane
+// (C)2015 Triacontane
 // This plugin is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.2.0 2022/10/14 MZで動作するよう修正
 // 1.1.0 2018/03/01 パラメータの型指定機能に対応。マップタッチ移動時の強制ダッシュを無効にする機能を追加。
 // 1.0.1 2016/09/11 レベル「簡易」の時の押し続け判定を変更
 // 1.0.0 2015/11/18 初版
@@ -16,42 +17,12 @@
 //=============================================================================
 
 /*:
- * @plugindesc Plugin that change policy of 'map touch'
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @param initPolicyLevel
- * @desc Initial value of policy level
- * @option 1 : invalid
- * @value 1
- * @option 2 : simple
- * @value 2
- * @option 3 : normal
- * @value 3
- * @option 4 : excessive
- * @value 4
- *
- * @param invalidForceDash
- * @desc マップタッチ移動時の強制ダッシュを無効にしてShiftキーを押している間のみダッシュします。スマホ版は除く。
- * @default false
- * @type boolean
- *
- * @help Plugin that change policy of 'map touch'
- * There are four stages
- *
- *  1 : 'map touch' become invalid
- *  2 : 'map touch' become simple(move one step)
- *  3 : 'map touch' become normal(default policy)
- *  4 : 'map touch' become excessive(auto track to pointer)
- *
- * Plugin Command
- *  CHANGE_MTP [policy level] : change policy of 'map touch'
- *  ex：CHANGE_MTP 3
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc マップタッチ仕様変更プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/ChangeMapTouchPolicy.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
  *
  * @param initPolicyLevel
  * @text 仕様レベル初期値
@@ -73,102 +44,60 @@
  * @default false
  * @type boolean
  *
- * @help マップをタッチした際の移動の仕方を4種類から変更できます。
+ * @help ChangeMapTouchPolicy.js
+ *
+ * マップをタッチした際の移動の仕方を4種類から変更できます。
  * パラメータの「仕様レベル初期値」を設定してください。
  * 1 : 無効「マップをタッチしてもプレイヤーは動きません」
  * 2 : 簡易「マップをタッチするとプレイヤーが一マスだけ動きます」
  * 　　（指定位置へは移動しません。タッチし続けると移動を続けます）
  * 3 : 通常「マップをタッチするとその位置へプレイヤーが動きます」
  * 　　（もともとの仕様です）
- * 4 : 過剰「一度でもマップクリックすると以後、プレイヤーがポインタを追跡します」
- * 　　（もう一度クリックすると移動を止めます）
+ * 4 : 過剰「一度でもマップタッチすると以後、プレイヤーがポインタを追跡します」
+ * 　　（もう一度タッチすると移動を止めます）
  *
- * プラグインコマンド詳細
- *   イベントコマンド「プラグインコマンド」から実行。
- *   （引数の間は半角スペースで区切る）
+ * 仕様レベルを後から変更したい場合は、パラメータに制御文字\v[n]を
+ * 指定してください。
  *
- *  CHANGE_MTP [仕様レベル] : マップタッチの仕様レベルを変更します。
- *  例：CHANGE_MTP 3
+ * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
+ * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
+ * 以下のフォルダに格納されています。
+ * dlc/BasicResources/plugins/official
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
  *  についても制限はありません。
  *  このプラグインはもうあなたのものです。
  */
-(function () {
 
-    var getArgNumber = function (arg, min, max) {
-        if (arguments.length < 2) min = -Infinity;
-        if (arguments.length < 3) max = Infinity;
-        return (parseInt(convertEscapeCharacters(arg)) || 0).clamp(min, max);
-    };
-
-    var convertEscapeCharacters = function(text) {
-        if (text == null) text = '';
-        var windowLayer = SceneManager._scene._windowLayer;
-        return windowLayer ? windowLayer.children[0].convertEscapeCharacters(text) : text;
-    };
-
-    /**
-     * Create plugin parameter. param[paramName] ex. param.commandPrefix
-     * @param pluginName plugin name(EncounterSwitchConditions)
-     * @returns {Object} Created parameter
-     */
-    var createPluginParameter = function(pluginName) {
-        var paramReplacer = function(key, value) {
-            if (value === 'null') {
-                return value;
-            }
-            if (value[0] === '"' && value[value.length - 1] === '"') {
-                return value;
-            }
-            try {
-                return JSON.parse(value);
-            } catch (e) {
-                return value;
-            }
-        };
-        var parameter     = JSON.parse(JSON.stringify(PluginManager.parameters(pluginName), paramReplacer));
-        PluginManager.setParameters(pluginName, parameter);
-        return parameter;
-    };
-
-    var param = createPluginParameter('ChangeMapTouchPolicy');
-
-
-    //=============================================================================
-    // Game_Interpreter
-    //  プラグインコマンド[CHANGE_MTP]などを追加定義します。
-    //=============================================================================
-    var _Game_Interpreter_pluginCommand      = Game_Interpreter.prototype.pluginCommand;
-    Game_Interpreter.prototype.pluginCommand = function (command, args) {
-        _Game_Interpreter_pluginCommand.apply(this, arguments);
-        if (command.toUpperCase() === 'CHANGE_MTP') {
-            $gameSystem._mapTouchPolicy = getArgNumber(args[0], 1, 4);
-        }
-    };
+(()=> {
+    'use strict';
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
 
     //=============================================================================
     // Game_Temp
     //  移動開始フラグを追加定義します。
     //=============================================================================
-    var _Game_Temp_initialize = Game_Temp.prototype.initialize;
+    const _Game_Temp_initialize = Game_Temp.prototype.initialize;
     Game_Temp.prototype.initialize = function() {
         _Game_Temp_initialize.apply(this, arguments);
         this._moveStart = false;
     };
 
-    //=============================================================================
-    // Game_System
-    //  仕様レベルの初期値を追加定義します。
-    //=============================================================================
-    var _Game_System_initialize = Game_System.prototype.initialize;
-    Game_System.prototype.initialize = function() {
-        _Game_System_initialize.apply(this, arguments);
-        this._mapTouchPolicy = param.initPolicyLevel;
+    Game_Temp.prototype.getMoveStart = function() {
+        return this._moveStart;
     };
 
-    var _Game_Player_updateDashing = Game_Player.prototype.updateDashing;
+    Game_Temp.prototype.setMoveStart = function(value) {
+        this._moveStart = value;
+    };
+
+    Game_Temp.prototype.toggleMoveStart = function() {
+        this._moveStart = !this._moveStart;
+    };
+
+    const _Game_Player_updateDashing = Game_Player.prototype.updateDashing;
     Game_Player.prototype.updateDashing = function() {
         _Game_Player_updateDashing.apply(this, arguments);
         if (!param.invalidForceDash || Utils.isMobileDevice() || this.isMoving()) {
@@ -183,16 +112,18 @@
     // Scene_Map
     //  仕様レベルによってマップタッチの挙動を変更します。
     //=============================================================================
-    var _Scene_Map_isMapTouchOk = Scene_Map.prototype.isMapTouchOk;
+    const _Scene_Map_isMapTouchOk = Scene_Map.prototype.isMapTouchOk;
     Scene_Map.prototype.isMapTouchOk = function() {
-        var result = $gameSystem._mapTouchPolicy !== 1 && _Scene_Map_isMapTouchOk.call(this);
-        if (!result) $gameTemp._moveStart = false;
+        const result = param.initPolicyLevel !== 1 && _Scene_Map_isMapTouchOk.call(this);
+        if (!result) {
+            $gameTemp.setMoveStart(false);
+        }
         return result;
     };
 
-    var _Scene_Map_processMapTouch = Scene_Map.prototype.processMapTouch;
+    const _Scene_Map_processMapTouch = Scene_Map.prototype.processMapTouch;
     Scene_Map.prototype.processMapTouch = function() {
-        switch ($gameSystem._mapTouchPolicy) {
+        switch (param.initPolicyLevel) {
             case 2:
                 this.processMapTouchLevel2();
                 break;
@@ -207,11 +138,11 @@
 
     Scene_Map.prototype.processMapTouchLevel2 = function() {
         if (TouchInput.isTriggered() || TouchInput.isLongPressed()) {
-            var px = $gamePlayer.x;
-            var py = $gamePlayer.y;
-            var deltaX = $gameMap.deltaX($gameMap.canvasToMapX(TouchInput.x), px);
-            var deltaY = $gameMap.deltaY($gameMap.canvasToMapY(TouchInput.y), py);
-            var d = 0;
+            let px = $gamePlayer.x;
+            let py = $gamePlayer.y;
+            const deltaX = $gameMap.deltaX($gameMap.canvasToMapX(TouchInput.x), px);
+            const deltaY = $gameMap.deltaY($gameMap.canvasToMapY(TouchInput.y), py);
+            let d = 0;
             if (Math.abs(deltaX) < Math.abs(deltaY)) {
                 if (deltaY > 0) d = 2;
                 if (deltaY < 0) d = 8;
@@ -246,29 +177,21 @@
                         break;
                 }
                 $gameTemp.setDestination(px, py);
-                $gameTemp._moveStart = true;
+                $gameTemp.setMoveStart(true);
             }
         }
     };
 
     Scene_Map.prototype.processMapTouchLevel4 = function() {
-        var x = $gameMap.canvasToMapX(TouchInput.x);
-        var y = $gameMap.canvasToMapY(TouchInput.y);
-        if (TouchInput.isTriggered()) $gameTemp._moveStart = !$gameTemp._moveStart;
-        if ($gameTemp._moveStart) {
+        const x = $gameMap.canvasToMapX(TouchInput.x);
+        const y = $gameMap.canvasToMapY(TouchInput.y);
+        if (TouchInput.isTriggered()) {
+            $gameTemp.toggleMoveStart();
+        }
+        if ($gameTemp.getMoveStart()) {
             $gameTemp.setDestination(x, y);
         } else {
             $gameTemp.clearDestination();
         }
-    };
-
-    //=============================================================================
-    // TouchInput
-    //  ポインタ移動時にマウス位置の記録を常に行うように元の処理を上書き
-    //=============================================================================
-    TouchInput._onMouseMove = function(event) {
-        var x = Graphics.pageToCanvasX(event.pageX);
-        var y = Graphics.pageToCanvasY(event.pageY);
-        this._onMove(x, y);
     };
 })();
