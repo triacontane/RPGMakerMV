9ba2761dc96878d2d912422a9d67f9d4e749a47c
triacontane <triacontanekinoko@gmail.com>
2023-02-01 21:50:13 +0900

    1.0.0 2023/02/01 初版

---
+++ FixEventAfterTransfer.js
@@ -0,0 +1,47 @@
+/*=============================================================================
+ FixEventAfterTransfer.js
+----------------------------------------------------------------------------
+ (C)2023 Triacontane
+ This software is released under the MIT License.
+ http://opensource.org/licenses/mit-license.php
+----------------------------------------------------------------------------
+ Version
+ 1.0.0 2023/02/01 初版
+----------------------------------------------------------------------------
+ [Blog]   : https://triacontane.blogspot.jp/
+ [Twitter]: https://twitter.com/triacontane/
+ [GitHub] : https://github.com/triacontane/
+=============================================================================*/
+
+/*:
+ * @plugindesc 場所移動後のイベント取得修正プラグイン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/FixEventAfterTransfer.js
+ * @author トリアコンタン
+ *
+ * @help FixEventAfterTransfer.js
+ *
+ * イベント中に場所移動したあとも『移動ルートの指定』など
+ * キャラクターを指定するイベントコマンドを正常に動かせるようにします。
+ * （デフォルト仕様では無視されます）
+ * 動作対象は移動後のマップの指定したIDのイベントです。
+ *
+ * 利用規約：
+ *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
+ *  についても制限はありません。
+ *  このプラグインはもうあなたのものです。
+ */
+
+(() => {
+    'use strict';
+
+    const _Game_Interpreter_character = Game_Interpreter.prototype.character;
+    Game_Interpreter.prototype.character = function(param) {
+        const character = _Game_Interpreter_character.apply(this, arguments);
+        if (character === null && !this.isOnCurrentMap() && !$gameParty.inBattle()) {
+            return $gameMap.event(param > 0 ? param : this._eventId);
+        } else {
+            return character;
+        }
+    };
+})();
