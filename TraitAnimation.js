282197e6197ca1184daa2e8cadcca9b29aebc174
triacontane <triacontanekinoko@gmail.com>
2023-04-11 23:50:04 +0900

    1.1.0 2021/04/11 複数のアニメーションを同時に表示できるよう修正

--- TraitAnimation.js
+++ TraitAnimation.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.1.0 2021/04/11 複数のアニメーションを同時に表示できるよう修正
  1.0.0 2020/12/06 初版
 ----------------------------------------------------------------------------
  [Blog]   : https://triacontane.blogspot.jp/
@@ -36,6 +37,10 @@
  * ※ アクター、職業、武器、防具、敵キャラ、ステート
  * <TraitAnimation:1> // アニメーションID[1]を再生し続けます。
  * <特徴アニメ:1>     // 同上
+ *
+ * 同一バトラーに複数のアニメーションを表示させることも可能ですが
+ * パフォーマンスにはご注意ください。
+ * また、同一のデータベースに対して同一のメモ欄は指定できません。
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -86,27 +91,29 @@
      * 特徴の情報を取得します。
      */
     Game_BattlerBase.prototype.findTraitAnimation = function() {
-        var animationId = 0;
-        this.traitObjects().forEach(function(obj) {
-            var meta = getMetaValues(obj, ['TraitAnimation', '特徴アニメ']);
+        const animations = [];
+        this.traitObjects().forEach(obj => {
+            const meta = getMetaValues(obj, ['TraitAnimation', '特徴アニメ']);
             if (meta) {
-                animationId = parseInt(meta);
+                animations.push({
+                    id : parseInt(meta),
+                    mirror: this.isActor()
+                });
             }
         });
-        if (animationId > 0) {
-            return {
-                id : animationId,
-                mirror: this.isActor()
-            }
-        } else {
-            return null;
-        }
+        return animations;
     };
 
     /**
      * Sprite_Battler
      * 特徴アニメーションを再生します。
      */
+    const _Sprite_Battler_initialize = Sprite_Battler.prototype.initialize;
+    Sprite_Battler.prototype.initialize = function(battler) {
+        _Sprite_Battler_initialize.apply(this, arguments);
+        this._traitAnimationSprite = [];
+    };
+
     var _Sprite_Battler_updateAnimation = Sprite_Battler.prototype.updateAnimation
     Sprite_Battler.prototype.updateAnimation = function() {
         _Sprite_Battler_updateAnimation.apply(this, arguments);
@@ -114,21 +121,31 @@
     };
 
     Sprite_Battler.prototype.updateTraitAnimation = function() {
-        var sprite = this._traitAnimationSprite;
-        if (sprite && !sprite.isPlaying()) {
-            sprite.remove();
-            this._traitAnimationSprite = null;
-        }
-        if (!this._traitAnimationSprite) {
-            this.setupTraitAnimation();
-        }
+        this._traitAnimationSprite = this._traitAnimationSprite.filter(function(sprite) {
+            if (!sprite.isPlaying()) {
+                sprite.remove();
+            }
+            return sprite.isPlaying();
+        });
+        this.setupTraitAnimation();
+    };
+
+    Sprite_Battler.prototype.hasTraitAnimation = function(id) {
+        return this._traitAnimationSprite.some(function(sprite) {
+            return sprite.animationId === id;
+        });
     };
 
     Sprite_Battler.prototype.setupTraitAnimation = function() {
-        var data = this._battler.findTraitAnimation();
-        if (data) {
+        var list = this._battler.findTraitAnimation();
+        list.forEach(function(data) {
+            if (this.hasTraitAnimation(data.id)) {
+                return;
+            }
             this.startAnimation($dataAnimations[data.id], data.mirror, 0);
-            this._traitAnimationSprite = this._animationSprites.pop();
-        }
+            var sprite = this._animationSprites.pop();
+            sprite.animationId = data.id;
+            this._traitAnimationSprite.push(sprite);
+        }, this);
     };
 })();
