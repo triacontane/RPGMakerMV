920bf413e46e398c84ce1081a2264576e24656c7
triacontane <triacontanekinoko@gmail.com>
2023-06-17 18:12:20 +0900

    1.1.0 2023/06/17 MZ用にリファクタリング

--- FixCommonOnBattleEnd.js
+++ FixCommonOnBattleEnd.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.1.0 2023/06/17 MZ用にリファクタリング
  1.0.0 2020/04/05 初版
 ----------------------------------------------------------------------------
  [Blog]   : https://triacontane.blogspot.jp/
@@ -14,27 +15,15 @@
 =============================================================================*/
 
 /*:
- * @plugindesc FixCommonOnBattleEndPlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @help FixCommonOnBattleEnd.js
- *
- * When you stab your opponent in the back and the battle ends,
- * you can also use the skill
- * Change the specification so that the common event is executed.
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc 戦闘終了時のコモンイベント実行修正プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/FixCommonOnBattleEnd.js
+ * @author トリアコンタン
  *
  * @help FixCommonOnBattleEnd.js
  *
  * 相手にトドメを刺して戦闘終了したときもスキルで指定した
  * コモンイベントが実行されるように仕様変更します。
- *　
- * このプラグインにはプラグインコマンドはありません。
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -42,14 +31,16 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+(() => {
     'use strict';
 
-    var _BattleManager_checkBattleEnd = BattleManager.checkBattleEnd;
+    const _BattleManager_checkBattleEnd = BattleManager.checkBattleEnd;
     BattleManager.checkBattleEnd = function() {
+        const result = _BattleManager_checkBattleEnd.apply(this, arguments);
         if ($gameTemp.isCommonEventReserved()) {
             return false;
+        } else {
+            return result;
         }
-        return _BattleManager_checkBattleEnd.apply(this, arguments);
     }
 })();
