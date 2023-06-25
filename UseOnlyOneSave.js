34b8b4e036cb247b5a2622dfc4e04d0e6523da32
triacontane <triacontanekinoko@gmail.com>
2023-06-25 14:36:44 +0900

    1.2.0 2023/06/25 MZで動作するよう修正

--- UseOnlyOneSave.js
+++ UseOnlyOneSave.js
@@ -1,11 +1,12 @@
 //=============================================================================
 // UseOnlyOneSave.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2015-2017 Triacontane
+// (C)2017 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.2.0 2023/06/25 MZで動作するよう修正
 // 1.1.4 2018/01/06 1.1.3の修正でセーブ画面に遷移するとゲームが停止する不具合を修正
 // 1.1.3 2018/01/05 マップ画面からスクリプトでロード実行されたときに、エラーが発生する場合がある問題を修正
 // 1.1.2 2017/12/02 MenuCommonEvent.jsとの競合を解消
@@ -21,25 +22,14 @@
 //=============================================================================
 
 /*:
- * @plugindesc UseOnlyOneSavePlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @param SaveMessage
- * @desc メニュー画面でのセーブ実行時に表示するメッセージです。指定しない場合は表示されません。
- * @default プレイデータをセーブしました。
- *
- * @help UseOnlyOneSave.js
- *
- * ロードもしくはセーブ時に各画面を経由せず、ファイル1のみを
- * 常に使用するように変更します。
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc 単一セーブデータプラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/UseOnlyOneSave.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
  *
- * @param セーブメッセージ
+ * @param saveMessage
+ * @text セーブメッセージ
  * @desc メニュー画面でのセーブ実行時に表示するメッセージです。指定しない場合は表示されません。
  * @default プレイデータをセーブしました。
  *
@@ -47,6 +37,8 @@
  *
  * ロードもしくはセーブ時に各画面を経由せず、ファイル1のみを
  * 常に使用するように変更します。
+ * オートセーブを有効にしている場合は、ロード時に最新データが
+ * ロード対象となります。
  *
  * このプラグインにはプラグインコマンドはありません。
  *
@@ -56,41 +48,26 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+(()=> {
     'use strict';
-    var pluginName    = 'UseOnlyOneSave';
-
-    var getParamString = function(paramNames) {
-        if (!Array.isArray(paramNames)) paramNames = [paramNames];
-        for (var i = 0; i < paramNames.length; i++) {
-            var name = PluginManager.parameters(pluginName)[paramNames[i]];
-            if (name !== undefined) return name;
-        }
-        alert('Fail to load plugin parameter of ' + pluginName);
-        return null;
-    };
-
-    //=============================================================================
-    // パラメータの取得と整形
-    //=============================================================================
-    var param       = {};
-    param.saveMessage = getParamString(['SaveMessage', 'セーブメッセージ']);
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
 
     //=============================================================================
     // SceneManager
     //  セーブ画面もしくはロード画面への遷移を中止します。
     //=============================================================================
-    var _SceneManager_goto = SceneManager.goto;
+    const _SceneManager_goto = SceneManager.goto;
     SceneManager.goto = function(sceneClass) {
-        var result = this.cutSaveScene(sceneClass);
+        const result = this.cutSaveScene(sceneClass);
         if (!result) {
             _SceneManager_goto.apply(this, arguments);
         }
     };
 
-    var _SceneManager_push = SceneManager.push;
+    const _SceneManager_push = SceneManager.push;
     SceneManager.push = function(sceneClass) {
-        var result = this.cutSaveScene(sceneClass);
+        const result = this.cutSaveScene(sceneClass);
         if (!result) {
             _SceneManager_push.apply(this, arguments);
         }
@@ -98,7 +75,7 @@
 
     SceneManager.cutSaveScene = function(sceneClass) {
         if (sceneClass === Scene_Save || sceneClass === Scene_Load) {
-            var sceneFile = new sceneClass();
+            const sceneFile = new sceneClass();
             sceneFile.onSavefileOk();
             if (sceneClass === Scene_Load && this._scene instanceof Scene_Map) {
                 $dataMap.loadFromMap = true;
@@ -109,21 +86,12 @@
         return false;
     };
 
-    //=============================================================================
-    // StorageManager
-    //  バックアップの削除失敗のエラーを無視します。(オンラインストレージ対策)
-    //=============================================================================
-    var _StorageManager_cleanBackup = StorageManager.cleanBackup;
-    StorageManager.cleanBackup = function(savefileId) {
-        try {
-            _StorageManager_cleanBackup.apply(this, arguments);
-        } catch (e) {
-            if (e.code === 'EBUSY') {
-                console.error(e);
-            } else {
-                throw e;
-            }
+    const _Scene_File_isSavefileEnabled = Scene_File.prototype.isSavefileEnabled;
+    Scene_File.prototype.isSavefileEnabled = function(savefileId) {
+        if (!this._listWindow) {
+            return true;
         }
+        return _Scene_File_isSavefileEnabled.apply(this, arguments);
     };
 
     //=============================================================================
@@ -134,7 +102,7 @@
         return !!param.saveMessage;
     };
 
-    var _Scene_Menu_create = Scene_Menu.prototype.create;
+    const _Scene_Menu_create = Scene_Menu.prototype.create;
     Scene_Menu.prototype.create = function() {
         _Scene_Menu_create.apply(this, arguments);
         if (this.isExistHelpNotice()) {
@@ -144,12 +112,31 @@
 
     Scene_Menu.prototype.createSaveNoticeWindow = function() {
         this.createHelpWindow();
-        this._helpWindow.y = SceneManager._boxHeight - this._helpWindow.height;
+        this._helpWindow.y = Graphics.boxHeight - this._helpWindow.height;
         this._helpWindow.openness = 0;
         this._helpWindow.setText(param.saveMessage);
     };
 
-    var _Scene_Menu_commandSave = Scene_Menu.prototype.commandSave;
+    const _Scene_Menu_helpAreaHeight = Scene_Menu.prototype.helpAreaHeight;
+    Scene_Menu.prototype.helpAreaHeight = function() {
+        if (this.isExistHelpNotice()) {
+            return Scene_MenuBase.prototype.helpAreaHeight.call(this);
+        } else {
+            return _Scene_Menu_helpAreaHeight.apply(this, arguments);
+        }
+    };
+
+    const _Scene_Menu_mainAreaHeight = Scene_Menu.prototype.mainAreaHeight;
+    Scene_Menu.prototype.mainAreaHeight = function() {
+        const result = _Scene_Menu_mainAreaHeight.apply(this, arguments);
+        if (this.isExistHelpNotice()) {
+            return result + this.helpAreaHeight();
+        } else {
+            return result;
+        }
+    };
+
+    const _Scene_Menu_commandSave = Scene_Menu.prototype.commandSave;
     Scene_Menu.prototype.commandSave = function() {
         _Scene_Menu_commandSave.apply(this, arguments);
         if (this.isExistHelpNotice()) {
@@ -164,7 +151,7 @@
     };
 
     // Resolve conflict for plugin to override Scene_MenuBase or Scene_Base
-    var _Scene_Menu_update = Scene_Menu.prototype.hasOwnProperty('update') ? Scene_Menu.prototype.update : null;
+    const _Scene_Menu_update = Scene_Menu.prototype.hasOwnProperty('update') ? Scene_Menu.prototype.update : null;
     Scene_Menu.prototype.update = function() {
         if (_Scene_Menu_update) {
             _Scene_Menu_update.apply(this, arguments);
@@ -195,18 +182,26 @@
         // do nothing
     };
 
+    Scene_Load.prototype.savefileId = function() {
+        if (this.isAutosaveEnabled()) {
+            return DataManager.latestSavefileIdIncludeAuto();
+        } else {
+            return Scene_File.prototype.savefileId.apply(this, arguments);
+        }
+    };
+
     //=============================================================================
     // Scene_Title
     //  ロード成功時にBGMを再生します。
     //=============================================================================
-    var _Scene_Title_commandContinue = Scene_Title.prototype.commandContinue;
+    const _Scene_Title_commandContinue = Scene_Title.prototype.commandContinue;
     Scene_Title.prototype.commandContinue = function() {
         this.fadeOutAll();
         _Scene_Title_commandContinue.apply(this, arguments);
         this._loadSuccess = true;
     };
 
-    var _Scene_Title_terminate = Scene_Title.prototype.terminate;
+    const _Scene_Title_terminate = Scene_Title.prototype.terminate;
     Scene_Title.prototype.terminate = function() {
         _Scene_Title_terminate.apply(this, arguments);
         if (this._loadSuccess) {
@@ -218,7 +213,7 @@
     // Scene_Map
     //  マップ画面からロードが実行された場合に、マップデータがロードされるまで処理を中断します
     //=============================================================================
-    var _Scene_Map_updateMain = Scene_Map.prototype.updateMain;
+    const _Scene_Map_updateMain = Scene_Map.prototype.updateMain;
     Scene_Map.prototype.updateMain = function() {
         if ($dataMap.loadFromMap) {
             return;
@@ -230,13 +225,21 @@
     // Game_Interpreter
     //  イベントからセーブ時にインデックスを進めます。
     //=============================================================================
-    var _Game_Interpreter_command352 = Game_Interpreter.prototype.command352;
+    const _Game_Interpreter_command352 = Game_Interpreter.prototype.command352;
     Game_Interpreter.prototype.command352 = function() {
         // Resolves of being saved again when loading.
         if (!$gameParty.inBattle()) {
             this._index++;
         }
         _Game_Interpreter_command352.apply(this, arguments);
+    };
+
+    DataManager.latestSavefileIdIncludeAuto = function() {
+        const globalInfo = this._globalInfo;
+        const validInfo = globalInfo.filter(x => x);
+        const latest = Math.max(...validInfo.map(x => x.timestamp));
+        const index = globalInfo.findIndex(x => x && x.timestamp === latest);
+        return index > 0 ? index : 0;
     };
 })();
 
