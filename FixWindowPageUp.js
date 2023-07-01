6e716cba5126eddcccdf84c41408bfd5651885d1
triacontane <triacontanekinoko@gmail.com>
2023-07-01 13:17:36 +0900

    1.1.0 2023/07/01 ページダウンボタンを押したときにウィンドウが1ページに収まっていてもカーソルが一番下まで移動するよう修正

--- FixWindowPageUp.js
+++ FixWindowPageUp.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.1.0 2023/07/01 ページダウンボタンを押したときにウィンドウが1ページに収まっていてもカーソルが一番下まで移動するよう修正
  1.0.0 2023/06/29 初版
 ----------------------------------------------------------------------------
  [Blog]   : https://triacontane.blogspot.jp/
@@ -21,8 +22,10 @@
  *
  * @help FixWindowPageUp.js
  *　
- * ウィンドウでページアップボタンを押したときに
- * カーソルが一番上まで移動するようになります。
+ * ウィンドウでページアップボタンを押したときにカーソルが一番上まで
+ * 移動するようになります。
+ * また、ページダウンボタンを押したときにウィンドウが1ページに収まっていても
+ * カーソルが一番下まで移動するようになります。
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -39,6 +42,17 @@
         if (this.topRow() === 0) {
             this.smoothScrollUp(this.maxPageRows());
             this.select(0);
+        }
+    };
+
+    const _Window_Selectable_cursorPagedown = Window_Selectable.prototype.cursorPagedown;
+    Window_Selectable.prototype.cursorPagedown = function() {
+        _Window_Selectable_cursorPagedown.apply(this, arguments);
+        if (this.topRow() === 0) {
+            const index = this.index();
+            const maxItems = this.maxItems();
+            this.smoothScrollDown(this.maxPageRows());
+            this.select(Math.min(index + this.maxPageItems(), maxItems - 1));
         }
     };
 })();
