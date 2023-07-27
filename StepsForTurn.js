407d63410f2333533f011ca431ececb40a8f907e
triacontane <triacontanekinoko@gmail.com>
2023-07-27 22:43:41 +0900

    1.1.0 2023/07/27 MZ版を作成

--- StepsForTurn.js
+++ StepsForTurn.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.1.0 2023/07/27 MZ版を作成
  1.0.0 2021/05/20 初版
 ----------------------------------------------------------------------------
  [Blog]   : https://triacontane.blogspot.jp/
@@ -28,13 +29,22 @@
  * @type number
  * @min 1
  *
+ * @param turnVariable
+ * @text ターン経過歩数変数
+ * @desc 1ターン経過歩数を格納する変数番号です。指定した場合、直接指定より優先されます。
+ * @default 0
+ * @type variable
+ *
  * @help StepsForTurn.js
  *
  * 1ターン経過と認識される歩数(通常20歩)を変更できます。
  * 主にステートによるスリップダメージの頻度が変わります。
  * パラメータから調整してください。
  *　
- * このプラグインにはプラグインコマンドはありません。
+ * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
+ * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
+ * 以下のフォルダに格納されています。
+ * dlc/BasicResources/plugins/official
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -44,12 +54,20 @@
 
 (()=> {
     'use strict';
-
     const script = document.currentScript;
     const param = PluginManagerEx.createParameter(script);
 
     const _Game_Actor_stepsForTurn = Game_Actor.prototype.stepsForTurn;
     Game_Actor.prototype.stepsForTurn = function() {
-        return param.turn || _Game_Actor_stepsForTurn.apply(this, arguments);
+        const originalTurn = _Game_Actor_stepsForTurn.apply(this, arguments);
+        return this.findStepsForTurnCustom() || originalTurn;
+    };
+
+    Game_Actor.prototype.findStepsForTurnCustom = function() {
+        if (param.turnVariable > 0) {
+            return $gameVariables.value(param.turnVariable);
+        } else {
+            return param.turn;
+        }
     };
 })();
