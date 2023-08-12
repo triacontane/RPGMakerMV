1207408dca0b142073825be022187db55bfc42a9
triacontane <triacontanekinoko@gmail.com>
2023-08-12 13:56:26 +0900

    1.2.0 2023/08/12 MZで動作するよう修正

--- ExpForOutsideParty.js
+++ ExpForOutsideParty.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.2.0 2023/08/12 MZで動作するよう修正
  1.1.0 2018/09/16 一度もパーティに加わっていないアクターも対しても経験値が加算される場合がある問題を修正
                   パーティ外アクターのレベルアップ時にメッセージを表示するかどうかを選択できる機能を追加
  1.0.1 2018/09/06 アクターが抜けている状態でセーブしたデータに対して本プラグインを適用して戦闘終了するとエラーになる問題を修正
@@ -17,32 +18,12 @@
 =============================================================================*/
 
 /*:
- * @plugindesc ExpForOutsidePartyPlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @param expPercent
- * @desc パーティ外の仲間が受け取ることのできる経験値の割合(%)です。
- * @default 100
- * @type number
- *
- * @param showLevelUpMessage
- * @desc パーティ外の仲間がレベルアップしたときにメッセージを表示するかどうかを選択します。
- * @default true
- * @type boolean
- *
- * @help ExpForOutsideParty.js
- *
- * パーティ外のアクターに対しても戦闘で得た経験値のうち一定量を
- * 取得可能にします。
- * ただし、本プラグインを適用前のセーブデータには使用できません。
- *　
- * このプラグインにはプラグインコマンドはありません。
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc パーティ外メンバーの経験値取得プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/ExpForOutsideParty.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
  *
  * @param expPercent
  * @text 経験値取得割合
@@ -60,9 +41,22 @@
  *
  * パーティ外のアクターに対しても戦闘で得た経験値のうち一定量を
  * 取得可能にします。
- * ただし、本プラグインを適用前のセーブデータには使用できません。
+ * パーティ外のアクターとは以下を指します。
+ * ・戦闘に参加していなかった控えメンバー
+ * ・一度でもパーティに加わったことがあるが、現在はパーティにいないメンバー
+ *
+ * なお、本プラグインを適用前のセーブデータには使用できません。
+ *
+ * 経験値の割合(%)をメモ欄から指定することもできます。
+ * アクター、職業、武器、防具、ステートのメモ欄に以下の通り記述してください。
+ * 重複して定義されていた場合は大きい値が優先されます。
+ * <OutsidePartyExpRate:50> # 経験値取得割合を50%に設定
+ * <パーティ外経験値レート:50> # 同上
  *　
- * このプラグインにはプラグインコマンドはありません。
+ * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
+ * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
+ * 以下のフォルダに格納されています。
+ * dlc/BasicResources/plugins/official
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -70,40 +64,16 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+(()=> {
     'use strict';
-
-    /**
-     * Create plugin parameter. param[paramName] ex. param.commandPrefix
-     * @param pluginName plugin name(EncounterSwitchConditions)
-     * @returns {Object} Created parameter
-     */
-    var createPluginParameter = function(pluginName) {
-        var paramReplacer = function(key, value) {
-            if (value === 'null') {
-                return value;
-            }
-            if (value[0] === '"' && value[value.length - 1] === '"') {
-                return value;
-            }
-            try {
-                return JSON.parse(value);
-            } catch (e) {
-                return value;
-            }
-        };
-        var parameter     = JSON.parse(JSON.stringify(PluginManager.parameters(pluginName), paramReplacer));
-        PluginManager.setParameters(pluginName, parameter);
-        return parameter;
-    };
-
-    var param = createPluginParameter('ExpForOutsideParty');
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
 
     /**
      * BattleManager.gainExp
      * EXPを加算します。
      */
-    var _BattleManager_gainExp = BattleManager.gainExp;
+    const _BattleManager_gainExp = BattleManager.gainExp;
     BattleManager.gainExp = function() {
         _BattleManager_gainExp.apply(this, arguments);
         $gameActors.gainExpWithoutParty(this._rewards.exp);
@@ -115,17 +85,14 @@
      * @param exp EXP
      */
     Game_Actors.prototype.gainExpWithoutParty = function(exp) {
-        var partyMember = $gameParty.allMembers();
-        this._data.filter(function(actor) {
-            return actor && !partyMember.contains(actor) && actor.isInPartyAtLeastOnce();
-        }).forEach(function(actor) {
-            actor.gainExp(exp);
-        });
+        const partyMember = $gameParty.allMembers();
+        this._data.filter(actor => actor && !partyMember.contains(actor) && actor.isInPartyAtLeastOnce())
+            .forEach(actor => actor.gainExp(exp));
     };
 
-    var _Game_Actor_shouldDisplayLevelUp = Game_Actor.prototype.shouldDisplayLevelUp;
+    const _Game_Actor_shouldDisplayLevelUp = Game_Actor.prototype.shouldDisplayLevelUp;
     Game_Actor.prototype.shouldDisplayLevelUp = function() {
-        if (!$gameParty.allMembers().contains(this)) {
+        if (!$gameParty.battleMembers().includes(this)) {
             return param.showLevelUpMessage;
         } else {
             return _Game_Actor_shouldDisplayLevelUp.apply(this, arguments);
@@ -144,26 +111,33 @@
      * Game_Actor.prototype.benchMembersExpRate
      * パーティ外のメンバーのレートを返します。
      */
-    var _Game_Actor_benchMembersExpRate = Game_Actor.prototype.benchMembersExpRate;
+    const _Game_Actor_benchMembersExpRate = Game_Actor.prototype.benchMembersExpRate;
     Game_Actor.prototype.benchMembersExpRate = function() {
-        if (!$gameParty.allMembers().contains(this)) {
-            return param.expPercent / 100;
+        const rate = _Game_Actor_benchMembersExpRate.apply(this, arguments);
+        if (!$gameParty.battleMembers().includes(this)) {
+            const customRate = this.findOutsideCustomExpRate();
+            return (customRate || param.expPercent) / 100;
         } else {
-            return _Game_Actor_benchMembersExpRate.apply();
+            return rate;
         }
     };
 
-    var _Game_Party_addActor = Game_Party.prototype.addActor;
+    Game_Actor.prototype.findOutsideCustomExpRate = function() {
+        return this.traitObjects()
+            .map(obj => PluginManagerEx.findMetaValue(obj, ['OutsidePartyExpRate', 'パーティ外経験値レート']))
+            .filter(value => !!value)
+            .sort().pop();
+    };
+
+    const _Game_Party_addActor = Game_Party.prototype.addActor;
     Game_Party.prototype.addActor = function(actorId) {
         _Game_Party_addActor.apply(this, arguments);
         $gameActors.actor(actorId).addParty();
     };
 
-    var _Game_Party_setupStartingMembers = Game_Party.prototype.setupStartingMembers;
+    const _Game_Party_setupStartingMembers = Game_Party.prototype.setupStartingMembers;
     Game_Party.prototype.setupStartingMembers = function() {
         _Game_Party_setupStartingMembers.apply(this, arguments);
-        this._actors.forEach(function(actorId) {
-            $gameActors.actor(actorId).addParty();
-        });
+        this._actors.forEach(actorId => $gameActors.actor(actorId).addParty());
     };
 })();
