64b9d1b9b55ac576852e9feaa2bc445475c63fa7
triacontane <triacontanekinoko@gmail.com>
2023-09-07 08:27:25 +0900

    1.1.0 2023/09/07 割り込みコモンを並列処理扱いで実行できる機能を追加

--- CommonInterceptor.js
+++ CommonInterceptor.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2023/09/07 割り込みコモンを並列処理扱いで実行できる機能を追加
 // 1.0.3 2022/10/09 リファクタリング
 // 1.0.2 2020/09/04 ロードコモンが正常に呼ばれていなかった問題を修正
 // 1.0.1 2020/08/23 MZ用にヘルプを修正
@@ -39,6 +40,12 @@
  * @desc メニュー画面を閉じた時に呼ばれるコモンイベントID
  * @default 0
  * @type common_event
+ *
+ * @param parallel
+ * @text 並列処理
+ * @desc 割り込みコモンを並列処理で実行する場合はONにしてください。
+ * @default false
+ * @type boolean
  *
  * @help CommonInterceptor.js
  *
@@ -89,7 +96,27 @@
     const _Game_Map_setupStartingEvent = Game_Map.prototype.setupStartingEvent;
     Game_Map.prototype.setupStartingEvent = function() {
         const result = _Game_Map_setupStartingEvent.apply(this, arguments);
-        return result || this.setupInterceptorCommonEvent();
+        if (!param.parallel) {
+            return result || this.setupInterceptorCommonEvent();
+        } else {
+            return result;
+        }
+    };
+
+    const _Game_Map_updateInterpreter = Game_Map.prototype.updateInterpreter;
+    Game_Map.prototype.updateInterpreter = function() {
+        _Game_Map_updateInterpreter.apply(this, arguments);
+        if (param.parallel) {
+            this.setupInterceptorCommonEventParallel();
+        }
+    };
+
+    Game_Map.prototype.setupInterceptorCommonEventParallel = function() {
+        const commonId = param[DataManager._interceptorType];
+        if (commonId) {
+            this.setupDynamicCommon(commonId);
+            DataManager._interceptorType = null;
+        }
     };
 
     Game_Map.prototype.setupInterceptorCommonEvent = function() {
