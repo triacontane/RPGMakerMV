8c2420b37f449354ef80e94b5221d890cc2e024d
triacontane <triacontanekinoko@gmail.com>
2023-09-17 21:52:06 +0900

    1.1.0 2023/09/17 MZ向けにリファクタリング

--- HiddenParameter.js
+++ HiddenParameter.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2023/09/17 MZ向けにリファクタリング
 // 1.0.0 2018/04/03 初版
 // ----------------------------------------------------------------------------
 // [Blog]   : https://triacontane.blogspot.jp/
@@ -14,26 +15,27 @@
 //=============================================================================
 
 /*:
- * @plugindesc HiddenParameterPlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
+ * @plugindesc パラメータ非表示プラグイン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/HiddenParameter.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
+ *
+ * @param hiddenParams
+ * @text 非表示パラメータ
+ * @desc 非表示にするパラメータのリストです。
+ * @default []
+ * @type struct<PARAM>[]
  *
  * @help HiddenParameter.js
  *
  * 装備品もしくはステータスウィンドウから
  * パラメータの表示を除外します。
+ * 非表示にするだけで、パラメータ自体は有効です。
  *
- * このプラグインにはプラグインコマンドはありません。
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
- * @plugindesc パラメータ隠蔽プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
- *
- * @help HiddenParameter.js
- *
- * 装備品もしくはステータスウィンドウから
- * パラメータの表示を除外します。
+ * 戦闘画面やショップ画面の表示には
+ * 影響を与えません。
  *
  * このプラグインにはプラグインコマンドはありません。
  *
@@ -43,14 +45,59 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
-    'use strict';
+/*~struct~PARAM:
+ * @param actorId
+ * @desc 対象となるアクターIDです。0を指定すると全アクターが対象になります。
+ * @default 0
+ * @type actor
+ *
+ * @param paramId
+ * @desc 対象となるパラメータIDです。0を指定すると全パラメータが対象になります。
+ * @default 0
+ * @type select
+ * @option 最大HP
+ * @value 0
+ * @option 最大MP
+ * @value 1
+ * @option 攻撃力
+ * @value 2
+ * @option 防御力
+ * @value 3
+ * @option 魔法力
+ * @value 4
+ * @option 魔法防御
+ * @value 5
+ * @option 敏捷性
+ * @value 6
+ * @option 運
+ * @value 7
+ *
+ */
 
-    Window_EquipStatus.prototype.windowWidth = function() {
-        return 0;
+(()=> {
+    'use strict';
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
+
+    TextManager.isVisibleParam = function(paramId, actorId) {
+        return !param.hiddenParams.some(data => {
+            return (data.actorId === 0 || data.actorId === actorId) &&
+                (data.paramId === 0 || data.paramId === paramId);
+        });
     };
 
-    Window_Status.prototype.drawBlock3 = function(y) {
-        this.drawEquipments(48, y);
+    const _Window_StatusParams_drawItem = Window_StatusParams.prototype.drawItem;
+    Window_StatusParams.prototype.drawItem = function(index) {
+        const paramId = index + 2;
+        if (TextManager.isVisibleParam(paramId, this._actor.actorId())) {
+            _Window_StatusParams_drawItem.apply(this, arguments);
+        }
+    };
+
+    const _Window_EquipStatus_drawItem = Window_EquipStatus.prototype.drawItem;
+    Window_EquipStatus.prototype.drawItem = function(x, y, paramId) {
+        if (TextManager.isVisibleParam(paramId, this._actor?.actorId())) {
+            _Window_EquipStatus_drawItem.apply(this, arguments);
+        }
     };
 })();
