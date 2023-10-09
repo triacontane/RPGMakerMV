c6a8ec51e28b43e71f963d66f32cbc1a27bd9435
triacontane <triacontanekinoko@gmail.com>
2023-10-09 23:24:19 +0900

    1.1.0 2023/10/09 プラグインの機能を一時的に無効にできるスイッチを追加

--- WindowOneTapDecision.js
+++ WindowOneTapDecision.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.1.0 2023/10/09 プラグインの機能を一時的に無効にできるスイッチを追加
  1.0.0 2023/09/26 初版
 ----------------------------------------------------------------------------
  [Blog]   : https://triacontane.blogspot.jp/
@@ -17,12 +18,25 @@
  * @plugindesc ワンタップで項目決定プラグイン
  * @target MZ
  * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/WindowOneTapDecision.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
  * @author トリアコンタン
+ *
+ * @param disableSwitchId
+ * @text 無効スイッチ番号
+ * @desc 指定した番号のスイッチがONのとき、プラグインの機能が無効になります。
+ * @default 0
+ * @type switch
  *
  * @help WindowOneTapDecision.js
  *
  * モバイルデバイス操作時に、ウィンドウの項目をワンタップで
  * 決定できるよう修正します。
+ *
+ * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
+ * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
+ * 以下のフォルダに格納されています。
+ * dlc/BasicResources/plugins/official
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -32,12 +46,15 @@
 
 (() => {
     'use strict';
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
 
     const _Window_Selectable_processTouch = Window_Selectable.prototype.processTouch;
     Window_Selectable.prototype.processTouch = function() {
         const hitIndex = this.hitIndex();
         if (Utils.isMobileDevice() && this.isOpenAndActive() &&
-            hitIndex !== this.index() && hitIndex >= 0 && TouchInput.isTriggered()) {
+            hitIndex !== this.index() && hitIndex >= 0 && TouchInput.isTriggered() &&
+            !$gameSwitches.value(param.disableSwitchId)) {
             this.select(hitIndex);
         }
         _Window_Selectable_processTouch.apply(this, arguments);
