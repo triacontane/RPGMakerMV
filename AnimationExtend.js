a7c9053eafee97df308232b68afd9f36fbd23861
triacontane <triacontanekinoko@gmail.com>
2023-11-21 20:32:47 +0900

    1.2.0 2023/11/21 MZで動作するよう修正

--- AnimationExtend.js
+++ AnimationExtend.js
@@ -1,11 +1,12 @@
 //=============================================================================
 // AnimationExtend.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2015-2017 Triacontane
+// (C)2017 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.2.0 2023/11/21 MZで動作するよう修正
 // 1.1.0 2017/08/23 アニメーションの拡大率を動的に設定できる機能を追加
 // 1.0.0 2017/04/28 初版
 // ----------------------------------------------------------------------------
@@ -15,57 +16,37 @@
 //=============================================================================
 
 /*:
- * @plugindesc AnimationExtendPlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @param RotationVariableId
- * @desc 回転角(0-360)を取得するための変数番号
- * @default 0
- * @type variable
- *
- * @param FrameVariableId
- * @desc アニメーションのフレーム数(1-)を取得するための変数番号
- * @default 0
- * @type variable
- *
- * @param ScaleVariableId
- * @desc 拡大率(100%)を取得するための変数番号
- * @default 0
- * @type variable
- *
- * @help AnimationExtend.js
- *
- * アニメーションを表示する際の角度やフレーム数を動的に変更できます。
- * パラメータで指定した番号の変数の値がそれぞれ適用されます。
- *
- * このプラグインにはプラグインコマンドはありません。
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc アニメーション設定拡張プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/AnimationExtend.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
  *
- * @param 回転角変数番号
+ * @param rotationVariableId
+ * @text 回転角変数番号
  * @desc 回転角(0-360)を取得するための変数番号
  * @default 0
  * @type variable
  *
- * @param フレーム数変数番号
+ * @param frameVariableId
+ * @text フレーム数変数番号
  * @desc アニメーションのフレーム数(1-)を取得するための変数番号。
  * デフォルトのフレーム数は「4」です。1フレーム=1/60秒
  * @default 0
  * @type variable
  *
- * @param 拡大率変数番号
+ * @param scaleVariableId
+ * @text 拡大率変数番号
  * @desc 拡大率(100%)を取得するための変数番号
  * @default 0
  * @type variable
  *
  * @help AnimationExtend.js
  *
- * アニメーションを表示する際の角度やフレーム数を動的に変更できます。
+ * MV方式のアニメーションを表示する際の角度やフレーム数を動的に変更できます。
  * パラメータで指定した番号の変数の値がそれぞれ適用されます。
+ * MZで採用されたEffekseer方式のアニメーションには適用されません。
  *
  * このプラグインにはプラグインコマンドはありません。
  *
@@ -75,63 +56,36 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+(()=> {
     'use strict';
-    var pluginName = 'AnimationExtend';
-
-    //=============================================================================
-    // ローカル関数
-    //  プラグインパラメータやプラグインコマンドパラメータの整形やチェックをします
-    //=============================================================================
-    var getParamString = function(paramNames) {
-        if (!Array.isArray(paramNames)) paramNames = [paramNames];
-        for (var i = 0; i < paramNames.length; i++) {
-            var name = PluginManager.parameters(pluginName)[paramNames[i]];
-            if (name) return name;
-        }
-        return '';
-    };
-
-    var getParamNumber = function(paramNames, min, max) {
-        var value = getParamString(paramNames);
-        if (arguments.length < 2) min = -Infinity;
-        if (arguments.length < 3) max = Infinity;
-        return (parseInt(value) || 0).clamp(min, max);
-    };
-
-    //=============================================================================
-    // パラメータの取得と整形
-    //=============================================================================
-    var param                = {};
-    param.rotationVariableId = getParamNumber(['RotationVariableId', '回転角変数番号']);
-    param.frameVariableId    = getParamNumber(['FrameVariableId', 'フレーム数変数番号']);
-    param.scaleVariableId    = getParamNumber(['ScaleVariableId', '拡大率変数番号']);
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
 
     //=============================================================================
     // Sprite_Animation
     //  アニメーションの角度を適用します。
     //=============================================================================
-    var _Sprite_Animation_setup      = Sprite_Animation.prototype.setup;
-    Sprite_Animation.prototype.setup = function(target, animation, mirror, delay) {
-        _Sprite_Animation_setup.apply(this, arguments);
-        var customRotation = $gameVariables.value(param.rotationVariableId);
+    const _Sprite_AnimationMV_setup      = Sprite_AnimationMV.prototype.setup;
+    Sprite_AnimationMV.prototype.setup = function(target, animation, mirror, delay) {
+        _Sprite_AnimationMV_setup.apply(this, arguments);
+        const customRotation = $gameVariables.value(param.rotationVariableId);
         if (customRotation > 0) {
             this.rotation = customRotation / (180 / Math.PI);
         }
-        var customScale = $gameVariables.value(param.scaleVariableId);
+        const customScale = $gameVariables.value(param.scaleVariableId);
         if (customScale !== 0) {
             this.scale.x = customScale / 100;
             this.scale.y = customScale / 100;
         }
     };
 
-    var _Sprite_Animation_setupRate = Sprite_Animation.prototype.setupRate;
-    Sprite_Animation.prototype.setupRate = function() {
-        var customRate = $gameVariables.value(param.frameVariableId);
+    const _Sprite_AnimationMV_setupRate = Sprite_AnimationMV.prototype.setupRate;
+    Sprite_AnimationMV.prototype.setupRate = function() {
+        const customRate = $gameVariables.value(param.frameVariableId);
         if (customRate > 0) {
             this._rate = customRate;
         } else {
-            _Sprite_Animation_setupRate.apply(this, arguments);
+            _Sprite_AnimationMV_setupRate.apply(this, arguments);
         }
     };
 })();
