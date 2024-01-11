e43965791699a4d2b33ad33da23b3cd0f0d9be73
triacontane <triacontanekinoko@gmail.com>
2024-01-11 18:00:38 +0900

    1.0.0 2020/04/04 初版

--- SkillCostRateCustomize.js
+++ SkillCostRateCustomize.js
@@ -14,38 +14,25 @@
 =============================================================================*/
 
 /*:
- * @plugindesc SkillCostRateCustomizePlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @help SkillCostRateCustomize.js
- *
- * You can adjust the skill cost multiplier for each specific
- * skill or skill type.
- * Please set the following in the memo column of the
- * database (*) which has the feature as follows.
- *
- * <MpCostRateId1:0.5>   // Increases the MP consumption of ID[1]'s skill by 0.5.
- * <MpCostRateType1:2.0> // Doubles the MP consumption of Skill Type [2].
- * <TpCostRateId1:0.5>   // Increases the TP consumption of ID[1]'s skill by 0.5.
- * <TpCostRateType1:2.0> // Doubles the TP consumption of Skill Type [2].
- *
- * (*)Actors, professions, weapons, armor, enemy characters, and states
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc スキルコスト倍率調整プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/SkillCostRateCustomize.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
+ *
+ * @param skillCostRateList
+ * @text スキルコスト倍率リスト
+ * @desc スキルコスト倍率を調整するリストです。
+ * @default []
+ * @type struct<SkillCostRate>[]
  *
  * @help SkillCostRateCustomize.js
  *
  * 特定のスキルもしくはスキルタイプごとにスキルコスト倍率を調整できます。
  * 特徴を有するデータベース(※)のメモ欄に以下の通り設定してください。
  *
- * <MpCostRateId1:0.5>   // ID[1]のスキルのMP消費を0.5倍にします。
- * <MpCostRateType1:2.0> // スキルタイプ[2]のMP消費を2倍にします。
- * <TpCostRateId1:0.5>   // ID[1]のスキルのTP消費を0.5倍にします。
- * <TpCostRateType1:2.0> // スキルタイプ[2]のTP消費を2倍にします。
+ * <SkillCostRate:id01> // 識別子[id01]の倍率が適用されます。
  *
  * ※アクター、職業、武器、防具、敵キャラ、ステート
  *　
@@ -57,57 +44,77 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+/*~struct~SkillCostRate:
+ * @param id
+ * @text 識別子
+ * @desc メモ欄で指定するタグの値です。<SkillCostRate:id01>の形式で指定します。
+ * @default id01
+ * @type string
+ *
+ * @param skillIdList
+ * @text スキルIDリスト
+ * @desc 倍率を調整するスキルIDのリストです。
+ * @default []
+ * @type skill[]
+ *
+ * @param skillType
+ * @text スキルタイプ
+ * @desc 倍率を調整するスキルタイプ(魔法など)です。
+ * @default 0
+ *
+ * @param costType
+ * @text コストタイプ
+ * @desc 倍率を調整するコストタイプ(MPもしくはTP)です。
+ * @default Mp
+ * @type select
+ * @option MP
+ * @value Mp
+ * @option TP
+ * @value Tp
+ * 
+ * @param rate
+ * @text 倍率
+ * @desc スキルコストの倍率です。
+ * @default 100
+ * @type number
+ */
+
+(()=> {
     'use strict';
-
-    /**
-     * Get database meta information.
-     * @param object Database item
-     * @param name Meta name
-     * @returns {String} meta value
-     */
-    var getMetaValue = function(object, name) {
-        return object.meta.hasOwnProperty(name) ? convertEscapeCharacters(object.meta[name]) : null;
-    };
-
-    /**
-     * Convert escape characters.(require any window object)
-     * @param text Target text
-     * @returns {String} Converted text
-     */
-    var convertEscapeCharacters = function(text) {
-        var windowLayer = SceneManager._scene._windowLayer;
-        return windowLayer ? windowLayer.children[0].convertEscapeCharacters(text.toString()) : text;
-    };
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
+    if (!param.skillCostRateList) {
+        param.skillCostRateList = [];
+    }
 
     /**
      * Game_BattlerBase
      * IDおよびタイプごとにコスト倍率を計算します。
      */
-    var _Game_BattlerBase_skillMpCost = Game_BattlerBase.prototype.skillMpCost;
+    const _Game_BattlerBase_skillMpCost = Game_BattlerBase.prototype.skillMpCost;
     Game_BattlerBase.prototype.skillMpCost = function(skill) {
-        var result = _Game_BattlerBase_skillMpCost.apply(this, arguments);
+        const result = _Game_BattlerBase_skillMpCost.apply(this, arguments);
         return this.applyCostRateCustomize(result, 'Mp', skill);
     };
 
-    var _Game_BattlerBase_skillTpCost = Game_BattlerBase.prototype.skillTpCost;
+    const _Game_BattlerBase_skillTpCost = Game_BattlerBase.prototype.skillTpCost;
     Game_BattlerBase.prototype.skillTpCost = function(skill) {
-        var result =  _Game_BattlerBase_skillTpCost.apply(this, arguments);
+        const result =  _Game_BattlerBase_skillTpCost.apply(this, arguments);
         return this.applyCostRateCustomize(result, 'Tp', skill);
     };
 
     Game_BattlerBase.prototype.applyCostRateCustomize = function(originalRate, mpOrTp, skill) {
-        var rateById = this.findCostRateCustomize(mpOrTp, 'Id', skill);
-        var rateByType = this.findCostRateCustomize(mpOrTp, 'Type', skill);
-        return Math.floor(originalRate * rateById * rateByType);
+        const rate = this.findCostRateCustomize(mpOrTp, skill);
+        return Math.floor(originalRate * rate);
     };
 
-    Game_BattlerBase.prototype.findCostRateCustomize = function(mpOrTp, idOrType, skill) {
-        var id = idOrType === 'Id' ? skill.id : skill.stypeId;
-        var tag = `${mpOrTp}CostRate${idOrType}${id}`;
-        return this.traitObjects().reduce(function(prev, obj) {
-            var rate = getMetaValue(obj, tag);
-            return rate ? parseFloat(rate) * prev : prev;
+    Game_BattlerBase.prototype.findCostRateCustomize = function(mpOrTp, skill) {
+        const rateList = param.skillCostRateList.filter(data => {
+            return (data.skillIdList.includes(skill.id) || data.skillType === skill.stypeId) && data.costType === mpOrTp;
+        });
+        return this.traitObjects().reduce((prev, obj) => {
+            const rateData = rateList.find(data => data.id === obj.meta['SkillCostRate']);
+            return rateData ? prev * rateData.rate / 100 : prev;
         }, 1.0);
     };
 })();
