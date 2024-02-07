641594cef9277071c2443bf10df88e8b656fd4f7
triacontane <triacontanekinoko@gmail.com>
2024-02-07 21:41:24 +0900

    1.0.0 2024/02/07 MZ向けに仕様を再検討

--- EventItemCondition.js
+++ EventItemCondition.js
@@ -1,16 +1,12 @@
 //=============================================================================
 // EventItemCondition.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2015-2016 Triacontane
+// (C)2016 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
-// 1.2.0 2018/07/13 パラメータの型指定機能に対応。条件を満たしたアイテムの文字色を変える機能を追加
-// 1.1.1 2017/05/04 1.1.0で<EICScript:s>タグが正しく機能しない場合がある問題を修正
-// 1.1.0 2017/05/04 ウィンドウを表示中にリフレッシュする機能を追加
-// 1.0.1 2017/01/12 メモ欄の値が空で設定された場合にエラーが発生するかもしれない問題を修正
-// 1.0.0 2016/09/16 初版
+// 1.0.0 2024/02/07 MZ向けに仕様を再検討
 // ----------------------------------------------------------------------------
 // [Blog]   : http://triacontane.blogspot.jp/
 // [Twitter]: https://twitter.com/triacontane/
@@ -18,51 +14,16 @@
 //=============================================================================
 
 /*:
- * @plugindesc EventItemConditionPlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @param DefaultVisible
- * @desc メモ欄が指定されていないアイテムの表示可否です。OFFにするとメモ欄に指定がないアイテムは表示されません。
- * @default true
- * @type boolean
- *
- * @param RefreshSwitchId
- * @desc 指定したIDのスイッチがONになるとウィンドウを再描画します。描画後、スイッチは自動でOFFになります。
- * @default 0
- * @type switch
- *
- * @param TextColor
- * @text テキストカラー番号
- * @desc 指定すると条件を満たしたときに文字色が変化します。(条件を満たさないときは通常表示となります)
- * @default 0
- * @type number
- * @min 0
- * @max 20
- *
- * @help イベントコマンド「アイテム選択の処理」において
- * 表示可否の条件をアイテムごとに設定できます。
- * 対象アイテムのメモ欄に以下の通り設定してください。
- *
- * <EICSwitch:1>     # スイッチ[1]がONのときのみ表示対象
- * <EICScript:s>     # 評価結果がtrueのときのみ表示
- * スクリプト中で不等号を使いたい場合、以下のように記述してください。
- * < → &lt;
- * > → &gt;
- * 例：<EICスクリプト:\v[1] &gt; 10> // 変数[1]が10より大きい場合
- *
- * 「条件を満たさないアイテムを非表示」ではなく
- * 「条件を満たしたアイテムの文字色を変える」仕様にもできます。
- * その場合は、パラメータ「テキストカラー番号」を指定してください。
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc アイテム選択の表示条件プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/EventItemCondition.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
  *
  * @param DefaultVisible
  * @text デフォルト表示可否
- * @desc メモ欄が指定されていないアイテムの表示可否です。OFFにするとメモ欄に指定がないアイテムは表示されません。
+ * @desc パラメータが設定されていないアイテムの表示可否です。無効にすると設定がないアイテムはすべて表示されません。
  * @default true
  * @type boolean
  *
@@ -72,32 +33,21 @@
  * @default 0
  * @type switch
  *
- * @param TextColor
- * @text テキストカラー番号
- * @desc 指定すると条件を満たしたときに文字色が変化します。(条件を満たさないときは通常表示となります)
- * @default 0
- * @type number
- * @min 0
- * @max 20
+ * @param ConditionList
+ * @text 表示条件リスト
+ * @desc アイテムごとの表示条件を設定します。
+ * @default []
+ * @type struct<Condition>[]
  *
- * @help イベントコマンド「アイテム選択の処理」において
+ * @help EventItemCondition.js
+ *
+ * イベントコマンド「アイテム選択の処理」において
  * 表示可否の条件をアイテムごとに設定できます。
- * 対象アイテムのメモ欄に以下の通り設定してください。
  *
- * <EICスイッチ:1>   # スイッチ[1]がONのときのみ表示対象
- * <EICSwitch:1>     # 同上
- * <EICスクリプト:s> # 評価結果がtrueのときのみ表示
- * <EICScript:s>     # 同上
- * スクリプト中で不等号を使いたい場合、以下のように記述してください。
- * < → &lt;
- * > → &gt;
- * 例：<EICスクリプト:\v[1] &gt; 10> // 変数[1]が10より大きい場合
- *
- * 「条件を満たさないアイテムを非表示」ではなく
- * 「条件を満たしたアイテムの文字色を変える」仕様にもできます。
- * その場合は、パラメータ「テキストカラー番号」を指定してください。
- *
- * このプラグインにはプラグインコマンドはありません。
+ * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
+ * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
+ * 以下のフォルダに格納されています。
+ * dlc/BasicResources/plugins/official
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -105,109 +55,59 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+/*~struct~Condition:
+ * @param itemId
+ * @text アイテムID
+ * @desc 条件を設定するアイテムのIDです。
+ * @default 1
+ * @type item
+ *
+ * @param switchId
+ * @text スイッチID
+ * @desc 条件となるスイッチのIDです。
+ * @default 0
+ * @type switch
+ *
+ * @param script
+ * @text スクリプト
+ * @desc 条件となるスクリプトです。
+ * @default
+ * @type multiline_string
+ */
+
+(()=> {
     'use strict';
-    var metaTagPrefix = 'EIC';
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
 
-    var getArgString = function(arg, upperFlg) {
-        arg = convertEscapeCharacters(arg);
-        return upperFlg ? arg.toUpperCase() : arg;
-    };
-
-    var getArgNumber = function(arg, min, max) {
-        if (arguments.length < 2) min = -Infinity;
-        if (arguments.length < 3) max = Infinity;
-        return (parseInt(convertEscapeCharacters(arg), 10) || 0).clamp(min, max);
-    };
-
-    var getMetaValue = function(object, name) {
-        var metaTagName = metaTagPrefix + (name ? name : '');
-        return object.meta.hasOwnProperty(metaTagName) ? object.meta[metaTagName] : undefined;
-    };
-
-    var getMetaValues = function(object, names) {
-        if (!Array.isArray(names)) return getMetaValue(object, names);
-        for (var i = 0, n = names.length; i < n; i++) {
-            var value = getMetaValue(object, names[i]);
-            if (value !== undefined) return value;
-        }
-        return undefined;
-    };
-
-    var convertEscapeCharacters = function(text) {
-        if (text == null || text === true) text = '';
-        text            = text.replace(/&gt;?/gi, '>');
-        text            = text.replace(/&lt;?/gi, '<');
-        var windowLayer = SceneManager._scene._windowLayer;
-        return windowLayer ? windowLayer.children[0].convertEscapeCharacters(text) : text;
-    };
-
-    /**
-     * Create plugin parameter. param[paramName] ex. param.commandPrefix
-     * @param pluginName plugin name(EncounterSwitchConditions)
-     * @returns {Object} Created parameter
-     */
-    var createPluginParameter = function(pluginName) {
-        var paramReplacer = function(key, value) {
-            if (value === 'null') {
-                return value;
-            }
-            if (value[0] === '"' && value[value.length - 1] === '"') {
-                return value;
-            }
-            try {
-                return JSON.parse(value);
-            } catch (e) {
-                return value;
-            }
-        };
-        var parameter     = JSON.parse(JSON.stringify(PluginManager.parameters(pluginName), paramReplacer));
-        PluginManager.setParameters(pluginName, parameter);
-        return parameter;
-    };
-
-    var param = createPluginParameter('EventItemCondition');
-
-    //=============================================================================
-    // Window_EventItem
-    //  アイテム選択ウィンドウに条件を設定します。
-    //=============================================================================
-    var _Window_EventItem_includes      = Window_EventItem.prototype.includes;
+    const _Window_EventItem_includes      = Window_EventItem.prototype.includes;
     Window_EventItem.prototype.includes = function(item) {
-        var result = _Window_EventItem_includes.apply(this, arguments);
-        if (result && !param.TextColor) {
-            this._existCondition = false;
-            result = this.isOkEventItem(item);
-            if (!this._existCondition) {
-                result = param.DefaultVisible;
-            }
-        }
-        return result;
+        return _Window_EventItem_includes.apply(this, arguments) && this.isOkEventItem(item);
+    };
+
+    Window_EventItem.prototype.findEventItemCondition = function(item) {
+        return param.ConditionList.find(condition => condition.itemId === item.id);
     };
 
     Window_EventItem.prototype.isOkEventItem = function(item) {
-        return this.isOkEventItemSwitch(item) && this.isOkEventItemScript(item);
-    };
-
-    Window_EventItem.prototype.isOkEventItemSwitch = function(item) {
-        var metaValue = getMetaValues(item, ['スイッチ', 'Switch']);
-        if (metaValue) {
-            this._existCondition = true;
-            return $gameSwitches.value(getArgNumber(metaValue, 1));
+        const condition = this.findEventItemCondition(item);
+        if (condition) {
+            return this.isOkEventItemSwitch(condition) && this.isOkEventItemScript(condition);
+        } else {
+            return param.DefaultVisible;
         }
-        return true;
     };
 
-    Window_EventItem.prototype.isOkEventItemScript = function(item) {
-        var metaValue = getMetaValues(item, ['スクリプト', 'Script']);
-        if (metaValue) {
-            this._existCondition = true;
-            return eval(getArgString(metaValue));
-        }
-        return true;
+    Window_EventItem.prototype.isOkEventItemSwitch = function(condition) {
+        return !condition.switchId || $gameSwitches.value(condition.switchId);
+
     };
 
-    var _Window_EventItem_update      = Window_EventItem.prototype.update;
+    Window_EventItem.prototype.isOkEventItemScript = function(condition) {
+        return !condition.script || eval(condition.script);
+    };
+
+    const _Window_EventItem_update      = Window_EventItem.prototype.update;
     Window_EventItem.prototype.update = function() {
         _Window_EventItem_update.apply(this, arguments);
         this.updateAutoRefresh();
@@ -219,29 +119,6 @@
             this.refresh();
             this.select(0);
         }
-    };
-
-    var _Window_EventItem_drawItemName = Window_EventItem.prototype.drawItemName;
-    Window_EventItem.prototype.drawItemName = function(item, x, y, width) {
-        this._existCondition = false;
-        var needChange = param.TextColor > 0 && this.isOkEventItem(item) && this._existCondition;
-        if (needChange) {
-            this.changeTextColor(this.textColor(param.TextColor));
-            this._textColorResetDiaabled = true;
-        }
-        _Window_EventItem_drawItemName.apply(this, arguments);
-        if (needChange) {
-            this._textColorResetDiaabled = false;
-            this.resetTextColor();
-        }
-    };
-
-    var _Window_EventItem_resetTextColor = Window_EventItem.prototype.resetTextColor;
-    Window_EventItem.prototype.resetTextColor = function() {
-        if (this._textColorResetDiaabled) {
-            return;
-        }
-        _Window_EventItem_resetTextColor.apply(this, arguments);
     };
 })();
 
