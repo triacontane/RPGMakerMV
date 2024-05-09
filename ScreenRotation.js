a6066ecfc093aea8bd46bbaee288081a91c3f1ad
triacontane <triacontanekinoko@gmail.com>
2024-05-09 21:27:25 +0900

    1.1.0 2024/05/09 MZ対応版を作成

--- ScreenRotation.js
+++ ScreenRotation.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2024/05/09 MZ対応版を作成
 // 1.0.0 2017/01/08 初版
 // ----------------------------------------------------------------------------
 // [Blog]   : http://triacontane.blogspot.jp/
@@ -14,76 +15,72 @@
 //=============================================================================
 
 /*:
- * @plugindesc ScreenRotationPlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @param ThroughWindow
- * @desc ウィンドウが重なったときに透過表示します。(ON/OFF)
- * 複数のウィンドウ表示中に画面を回転させる場合に表示がおかしくなった場合のみONにしてください。
- * @default OFF
- *
- * @help 画面を自由に回転させることができます。
- * プラグインコマンドから実行してください。速さと対象角度を指定可能です。
- *
- * 回転時は、画面外のキャラクターグラフィックを一時的に非表示にします。
- *
- * 注意！
- * Canvasモードで実行中に回転をずっと続けているとパフォーマンスが低下します。
- *
- * プラグインコマンド詳細
- *  イベントコマンド「プラグインコマンド」から実行。
- *  （パラメータの間は半角スペースで区切る）
- *
- * ・画面の回転を開始します。速さと角度(360度単位)を設定できます。
- * 　角度を設定しなかった場合、ずっと回転を続けます。
- * SR_回転開始 2 90       # 速さ[2]で画面を[90]度回転させます。
- * SR_START_ROTATION 2 90 # 同上
- * SR_回転停止            # 現在の角度で回転を停止します。
- * SR_STOP_ROTATION       # 同上
- * SR_回転リセット        # 回転状態を即座にリセットします。
- * SR_RESET_ROTATION      # 同上
- *
- * ・おまけ機能で画面をズームさせることができます。
- * 　フレーム数を[0]に設定すると、即座に指定した拡大率になります。
- * SR_ズーム開始 300 60 # [60]フレームかけて拡大率を[300]%にします。
- * SR_START_ZOOM 300 60 # 同上
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc 画面回転プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/ScreenRotation.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
+ * 
+ * @command startRotation
+ * @text 画面の回転
+ * @desc 画面を回転させます。
+ * 
+ * @arg speed
+ * @text 速さ
+ * @desc 回転速度です。正の値で右回り、負の値で左回りします。0を指定するとリセットされます。
+ * @default 1
+ * @type number
+ * @min -20
+ * @max 20
+ * 
+ * @arg angle
+ * @text 対象角度
+ * @desc 回転後の角度です。省略すると無限に回転を続けます。
+ * @default
+ * @type number
+ * 
+ * @command stopRotation
+ * @text 画面の回転停止
+ * @desc 画面の回転を停止します。
  *
- * @param ウィンドウ透過
- * @desc ウィンドウが重なったときに透過表示します。(ON/OFF)
- * 複数のウィンドウ表示中に画面を回転させる場合に表示がおかしくなった場合のみONにしてください。
- * @default OFF
+ * @command startZoom
+ * @text 画面のズーム
+ * @desc 画面の表示倍率を変更し、拡大縮小します。
  *
- * @help 画面を自由に回転させることができます。
- * プラグインコマンドから実行してください。速さと対象角度を指定可能です。
+ * @arg scale
+ * @text 拡大率
+ * @desc 拡大率です。100で等倍です。
+ * @default 100
+ * @type number
+ * @min 1
  *
- * 回転時は、画面外のキャラクターグラフィックを一時的に非表示にします。
+ * @arg duration
+ * @text 時間
+ * @desc 倍率変更にかかる時間です。0を指定すると即座に拡大率が変更されます。
+ * @default 60
+ * @type number
  *
- * 注意！
- * Canvasモードで実行中に回転をずっと続けているとパフォーマンスが低下します。
+ * @arg x
+ * @text X座標
+ * @desc 拡大の基準点となるX座標です。未入力の場合、画面の中心になります。
+ * @default
+ * @type number
  *
- * プラグインコマンド詳細
- *  イベントコマンド「プラグインコマンド」から実行。
- *  （パラメータの間は半角スペースで区切る）
+ * @arg y
+ * @text Y座標
+ * @desc 拡大の基準点となるY座標です。未入力の場合、画面の中心になります。
+ * @default
+ * @type number
  *
- * ・画面の回転を開始します。速さと角度(360度単位)を設定できます。
- * 　角度を設定しなかった場合、ずっと回転を続けます。
- * SR_回転開始 2 90       # 速さ[2]で画面を[90]度回転させます。
- * SR_START_ROTATION 2 90 # 同上
- * SR_回転停止            # 現在の角度で回転を停止します。
- * SR_STOP_ROTATION       # 同上
- * SR_回転リセット        # 回転状態を即座にリセットします。
- * SR_RESET_ROTATION      # 同上
+ * @help ScreenRotation.js
+ * 
+ * マップ画面全体を回転させる演出ができます。
+ * プラグインコマンドから実行してください。
  *
- * ・おまけ機能で画面をズームさせることができます。
- * 　フレーム数を[0]に設定すると、即座に指定した拡大率になります。
- * SR_ズーム開始 300 60 # [60]フレームかけて拡大率を[300]%にします。
- * SR_START_ZOOM 300 60 # 同上
+ * なお、画面を回転中はメッセージ表示や選択肢表示などのウィンドウの中身が
+ * 正常に表示されない場合があります。
+ * また、回転時は、画面外のキャラクターグラフィックを一時的に非表示にします。
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -91,112 +88,36 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+(()=> {
     'use strict';
-    var pluginName    = 'ScreenRotation';
-    var metaTagPrefix = 'SR_';
+    const script = document.currentScript;
 
-    //=============================================================================
-    // ローカル関数
-    //  プラグインパラメータやプラグインコマンドパラメータの整形やチェックをします
-    //=============================================================================
-    var getParamString = function(paramNames) {
-        if (!Array.isArray(paramNames)) paramNames = [paramNames];
-        for (var i = 0; i < paramNames.length; i++) {
-            var name = PluginManager.parameters(pluginName)[paramNames[i]];
-            if (name) return name;
-        }
-        return '';
-    };
+    PluginManagerEx.registerCommand(script, 'startRotation', args => {
+        const angle = args.angle === '' ? null : args.angle;
+        $gameScreen.startRotation(args.speed / 2, angle);
+    });
 
-    var getParamBoolean = function(paramNames) {
-        var value = getParamString(paramNames);
-        return value.toUpperCase() === 'ON';
-    };
-
-    var getArgNumber = function(arg, min, max) {
-        if (arguments.length < 2) min = -Infinity;
-        if (arguments.length < 3) max = Infinity;
-        return (parseInt(arg) || 0).clamp(min, max);
-    };
-
-    var convertEscapeCharacters = function(text) {
-        if (text == null) text = '';
-        var windowLayer = SceneManager._scene._windowLayer;
-        return windowLayer ? windowLayer.children[0].convertEscapeCharacters(text) : text;
-    };
-
-    var convertAllArguments = function(args) {
-        for (var i = 0; i < args.length; i++) {
-            args[i] = convertEscapeCharacters(args[i]);
-        }
-        return args;
-    };
-
-    var setPluginCommand = function(commandName, methodName) {
-        pluginCommandMap.set(metaTagPrefix + commandName, methodName);
-    };
-
-    //=============================================================================
-    // パラメータの取得と整形
-    //=============================================================================
-    var param     = {};
-    param.throughWindow = getParamBoolean(['ThroughWindow', 'ウィンドウ透過']);
-
-    var pluginCommandMap = new Map();
-    setPluginCommand('回転開始', 'execStartRotation');
-    setPluginCommand('START_ROTATION', 'execStartRotation');
-    setPluginCommand('回転停止', 'execStopRotation');
-    setPluginCommand('STOP_ROTATION', 'execStopRotation');
-    setPluginCommand('回転リセット', 'execResetRotation');
-    setPluginCommand('RESET_ROTATION', 'execResetRotation');
-    setPluginCommand('ズーム開始', 'execStartZoom');
-    setPluginCommand('START_ZOOM', 'execStartZoom');
-
-    //=============================================================================
-    // Game_Interpreter
-    //  プラグインコマンドを追加定義します。
-    //=============================================================================
-    var _Game_Interpreter_pluginCommand    = Game_Interpreter.prototype.pluginCommand;
-    Game_Interpreter.prototype.pluginCommand = function(command, args) {
-        _Game_Interpreter_pluginCommand.apply(this, arguments);
-        var pluginCommandMethod = pluginCommandMap.get(command.toUpperCase());
-        if (pluginCommandMethod) {
-            this[pluginCommandMethod](convertAllArguments(args));
-        }
-    };
-
-    Game_Interpreter.prototype.execStartRotation = function(args) {
-        $gameScreen.startRotation(getArgNumber(args[0]), args.length > 1 ? getArgNumber(args[1], 0) : undefined);
-    };
-
-    Game_Interpreter.prototype.execStopRotation = function() {
+    PluginManagerEx.registerCommand(script, 'stopRotation', args => {
         $gameScreen.stopRotation();
-    };
+    });
 
-    Game_Interpreter.prototype.execResetRotation = function() {
-        $gameScreen.startRotation(0, 0);
-    };
-
-    Game_Interpreter.prototype.execStartZoom = function(args) {
-        var scale = getArgNumber(args[0]);
-        var duration = getArgNumber(args[1], 0);
-        var x = args.length > 2 ? getArgNumber(args[2]) : SceneManager._screenWidth / 2;
-        var y = args.length > 3 ? getArgNumber(args[3]) : SceneManager._screenHeight / 2;
-        if (duration > 0) {
-            $gameScreen.startZoom(x, y, scale / 100, duration);
+    PluginManagerEx.registerCommand(script, 'startZoom', args => {
+        const x = args.x === '' ? Graphics.width / 2 : args.x;
+        const y = args.y === '' ? Graphics.height / 2 : args.y;
+        if (args.duration > 0) {
+            $gameScreen.startZoom(x, y, args.scale / 100, args.duration);
         } else {
-            $gameScreen.setZoom(x, y, scale / 100);
+            $gameScreen.setZoom(x, y, args.scale / 100);
         }
-    };
+    });
 
     //=============================================================================
     // Game_Map
     //  指定した座標が画面内かどうかを返します。
     //=============================================================================
     Game_Map.prototype.isInnerScreenPosition = function(x, y) {
-        var ax = this.adjustX(x);
-        var ay = this.adjustY(y);
+        const ax = this.adjustX(x);
+        const ay = this.adjustY(y);
         return ax >= -1 && ay >= -1 && ax < this.screenTileX() + 1 && ay < this.screenTileY() + 1;
     };
 
@@ -212,7 +133,7 @@
         this._hiddenOuterScreen = false;
     };
 
-    var _Game_CharacterBase_isTransparent    = Game_CharacterBase.prototype.isTransparent;
+    const _Game_CharacterBase_isTransparent    = Game_CharacterBase.prototype.isTransparent;
     Game_CharacterBase.prototype.isTransparent = function() {
         return _Game_CharacterBase_isTransparent.apply(this, arguments) || this._hiddenOuterScreen;
     };
@@ -221,7 +142,7 @@
     // Game_Screen
     //  画面を回転させます。
     //=============================================================================
-    var _Game_Screen_clear    = Game_Screen.prototype.clear;
+    const _Game_Screen_clear    = Game_Screen.prototype.clear;
     Game_Screen.prototype.clear = function() {
         _Game_Screen_clear.apply(this, arguments);
         this.clearRotation();
@@ -238,6 +159,7 @@
         this._rotationSpeed = 0;
         this._angleTarget   = 0;
         this.updateCharacterHide();
+        this.stopRotation();
     };
 
     Game_Screen.prototype.startRotation = function(speed, angleTarget) {
@@ -247,16 +169,20 @@
         }
         this._rotationSpeed = speed;
         this._angle         = this._angle || 0;
-        this._angleTarget   = angleTarget;
+        if (angleTarget === null) {
+            this._angleTarget = (speed > 0 ? Infinity : -Infinity);
+        } else {
+            this._angleTarget = angleTarget;
+        }
     };
 
-    var _Game_Screen_onBattleStart = Game_Screen.prototype.onBattleStart;
+    const _Game_Screen_onBattleStart = Game_Screen.prototype.onBattleStart;
     Game_Screen.prototype.onBattleStart = function() {
         _Game_Screen_onBattleStart.apply(this, arguments);
         this.clearRotation();
     };
 
-    var _Game_Screen_update    = Game_Screen.prototype.update;
+    const _Game_Screen_update    = Game_Screen.prototype.update;
     Game_Screen.prototype.update = function() {
         _Game_Screen_update.apply(this, arguments);
         this.updateRotation();
@@ -292,7 +218,7 @@
     };
 
     Game_Screen.prototype.iterateAllCharacters = function(callBackFund, args) {
-        var characters = $gameMap.events().concat($gamePlayer);
+        const characters = $gameMap.events().concat($gamePlayer);
         characters.forEach(function(character) {
             callBackFund.apply(character, args);
         });
@@ -300,6 +226,10 @@
 
     Game_Screen.prototype.stopRotation = function() {
         this._rotationSpeed = 0;
+        if (this._angle === 0) {
+            this._angle = undefined;
+            SceneManager.rotateScene(0);
+        }
     };
 
     Game_Screen.prototype.isValidRotation = function() {
@@ -311,8 +241,8 @@
     //  画面の回転処理を実装します。
     //=============================================================================
     SceneManager.rotateScene = function(angle) {
-        var radian = angle * Math.PI / 180;
-        this._scene.rotate(radian, this._screenWidth / 2, this._screenHeight / 2);
+        const radian = angle * Math.PI / 180;
+        this._scene.rotate(radian, Graphics.width / 2, Graphics.height / 2);
     };
 
     //=============================================================================
@@ -320,26 +250,14 @@
     //  画面の回転処理を実装します。
     //=============================================================================
     Scene_Base.prototype.rotate = function(radian, ox, oy) {
-        if (this.rotation === radian) return;
-        var sin     = Math.sin(-radian);
-        var cos     = Math.cos(-radian);
+        if (this.rotation === radian) {
+            return;
+        }
+        const sin     = Math.sin(-radian);
+        const cos     = Math.cos(-radian);
         this.rotation = radian;
         this.x        = Math.floor(ox * -cos + oy * -sin) + ox;
         this.y        = Math.floor(ox * sin + oy * -cos) + oy;
     };
-
-    //=============================================================================
-    // ウィンドウを透過して重なり合ったときの表示を自然にします。
-    //=============================================================================
-    if (param.throughWindow && !WindowLayer.throughWindow) {
-        WindowLayer.throughWindow = true;
-        //=============================================================================
-        //  WindowLayer
-        //   ウィンドウのマスク処理を除去します。
-        //=============================================================================
-        WindowLayer.prototype._maskWindow = function(window) {};
-
-        WindowLayer.prototype._canvasClearWindowRect = function(renderSession, window) {};
-    }
 })();
 
