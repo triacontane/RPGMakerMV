6c2884032c0368c9573af87f377782a052d5745d
triacontane <triacontanekinoko@gmail.com>
2024-07-04 22:16:58 +0900

    1.1.0 2024/07/14 MZで動作するようリファクタリング

--- TimerCountInMenu.js
+++ TimerCountInMenu.js
@@ -1,80 +1,63 @@
 //=============================================================================
 // TimerCountInMenu.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2015-2017 Triacontane
+// (C)2017 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2024/07/14 MZで動作するようリファクタリング
 // 1.0.1 2017/08/04 画面キャプチャにタイマー画像が含まれないよう修正
 // 1.0.0 2017/08/03 初版
 // ----------------------------------------------------------------------------
-// [Blog]   : http://triacontane.blogspot.jp/
+// [Blog]   : https://triacontane.blogspot.jp/
 // [Twitter]: https://twitter.com/triacontane/
 // [GitHub] : https://github.com/triacontane/
 //=============================================================================
 
 /*:
- * @plugindesc TimerCountInMenuPlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @param BehaviorInMenu
- * @desc メニュー画面およびその先の画面でタイマーを動作させます。
- * @default true
- * @type boolean
- *
- * @param BehaviorInNameInput
- * @desc 名前入力画面でタイマーを動作させます。
- * @default true
- * @type boolean
- *
- * @param BehaviorInShop
- * @desc ショップ画面でタイマーを動作させます。
- * @default true
- * @type boolean
- *
- * @param BehaviorInSave
- * @desc セーブ画面でタイマーを動作させます。
- * @default true
- * @type boolean
- *
- * @help メニュー画面などでもタイマーのカウントが進むようになります。
- * パラメータで画面ごとに動作有無を指定できます。
- * カウントが0になると強制的にマップ画面に戻されます。
- *
- * このプラグインにはプラグインコマンドはありません。
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc メニュー画面でのタイマーカウントプラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/TimerCountInMenu.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
  *
- * @param メニュー画面で動作
+ * @param behaviorInMenu
+ * @text メニュー画面で動作
  * @desc メニュー画面およびその先の画面でタイマーを動作させます。
  * @default true
  * @type boolean
  *
- * @param 名前入力画面で動作
+ * @param behaviorInNameInput
+ * @text 名前入力画面で動作
  * @desc 名前入力画面でタイマーを動作させます。
  * @default true
  * @type boolean
  *
- * @param ショップ画面で動作
+ * @param behaviorInShop
+ * @text ショップ画面で動作
  * @desc ショップ画面でタイマーを動作させます。
  * @default true
  * @type boolean
  *
- * @param セーブ画面で動作
+ * @param behaviorInSave
+ * @text セーブ画面で動作
  * @desc セーブ画面でタイマーを動作させます。
  * @default true
  * @type boolean
  *
- * @help メニュー画面などでもタイマーのカウントが進むようになります。
- * パラメータで画面ごとに動作有無を指定できます。
- * カウントが0になると強制的にマップ画面に戻されます。
+ * @param expireToMap
+ * @text 時間切れでマップ移動
+ * @desc 時間切れになった場合、強制的にマップ画面に移動します。
+ * @default true
+ * @type boolean
  *
- * このプラグインにはプラグインコマンドはありません。
+ * @help TimerCountInMenu.js
+ *
+ * メニュー画面やショップ画面でタイマーのカウントが進むようになります。
+ * パラメータで画面ごとに動作有無を指定できます。
+ * 時間切れでマップ画面に移動することもできます。
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -82,36 +65,10 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+(()=> {
     'use strict';
-    var pluginName    = 'TimerCountInMenu';
-
-    //=============================================================================
-    // ローカル関数
-    //  プラグインパラメータやプラグインコマンドパラメータの整形やチェックをします
-    //=============================================================================
-    var getParamString = function(paramNames) {
-        if (!Array.isArray(paramNames)) paramNames = [paramNames];
-        for (var i = 0; i < paramNames.length; i++) {
-            var name = PluginManager.parameters(pluginName)[paramNames[i]];
-            if (name) return name;
-        }
-        return '';
-    };
-
-    var getParamBoolean = function(paramNames) {
-        var value = getParamString(paramNames);
-        return value.toUpperCase() === 'TRUE';
-    };
-
-    //=============================================================================
-    // パラメータの取得と整形
-    //=============================================================================
-    var param                 = {};
-    param.behaviorInMenu      = getParamBoolean(['BehaviorInMenu', 'メニュー画面で動作']);
-    param.behaviorInNameInput = getParamBoolean(['BehaviorInNameInput', '名前入力画面で動作']);
-    param.behaviorInShop      = getParamBoolean(['BehaviorInShop', 'ショップ画面で動作']);
-    param.behaviorInSave      = getParamBoolean(['BehaviorInSave', 'セーブ画面で動作']);
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
 
     //=============================================================================
     // Scene_Base
@@ -141,7 +98,7 @@
     // Scene_Map
     //  キャプチャ作成時にタイマーを描画対象から外します。
     //=============================================================================
-    var _Scene_Map_terminate = Scene_Map.prototype.terminate;
+    const _Scene_Map_terminate = Scene_Map.prototype.terminate;
     Scene_Map.prototype.terminate = function() {
         this._spriteset.setTimerOpacity(0);
         _Scene_Map_terminate.apply(this, arguments);
@@ -152,7 +109,7 @@
     // Scene_MenuBase
     //  タイマーをカウントするシーンの場合はタイマーを作成してカウントを進めます。
     //=============================================================================
-    var _Scene_MenuBase_create = Scene_MenuBase.prototype.create;
+    const _Scene_MenuBase_create = Scene_MenuBase.prototype.create;
     Scene_MenuBase.prototype.create = function() {
         _Scene_MenuBase_create.apply(this, arguments);
         if (this.isCountTimer()) {
@@ -165,7 +122,7 @@
         this.addChild(this._timerSprite);
     };
 
-    var _Scene_MenuBase_update = Scene_MenuBase.prototype.update;
+    const _Scene_MenuBase_update = Scene_MenuBase.prototype.update;
     Scene_MenuBase.prototype.update = function() {
         _Scene_MenuBase_update.apply(this, arguments);
         this.updateTimer();
@@ -181,7 +138,7 @@
     // Scene_Title
     //  タイトル画面を表示するときにタイマーを初期化します。
     //=============================================================================
-    var _Scene_Title_start = Scene_Title.prototype.start;
+    const _Scene_Title_start = Scene_Title.prototype.start;
     Scene_Title.prototype.start = function() {
         _Scene_Title_start.apply(this, arguments);
         if ($gameTimer) {
@@ -201,10 +158,10 @@
     // Game_Timer
     //  タイマーが0になったときに強制的にマップ画面に移動します。
     //=============================================================================
-    var _Game_Timer_onExpire = Game_Timer.prototype.onExpire;
+    const _Game_Timer_onExpire = Game_Timer.prototype.onExpire;
     Game_Timer.prototype.onExpire = function() {
         _Game_Timer_onExpire.apply(this, arguments);
-        if (SceneManager.isCountTimerScene() && !$gameParty.inBattle()) {
+        if (SceneManager.isCountTimerScene() && !$gameParty.inBattle() && param.expireToMap) {
             SoundManager.playCancel();
             SceneManager.goto(Scene_Map);
         }
