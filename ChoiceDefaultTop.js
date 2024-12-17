d55e41d6a49452d7ac3cf80a6e2d1d1d882a1f39
triacontane <triacontanekinoko@gmail.com>
2024-12-17 21:54:59 +0900

    1.0.0a 2024/12/17 不要コード削除

--- ChoiceDefaultTop.js
+++ ChoiceDefaultTop.js
@@ -17,8 +17,6 @@
  * @plugindesc 選択肢の初期表示の先頭固定プラグイン
  * @target MZ
  * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/ChoiceDefaultTop.js
- * @base PluginCommonBase
- * @orderAfter PluginCommonBase
  * @author トリアコンタン
  *
  * @help ChoiceDefaultTop.js
@@ -26,11 +24,6 @@
  * 選択肢の表示でデフォルトを「なし」にした場合に
  * 方向ボタン入力後のカーソル位置を先頭に固定します。
  * （デフォルト動作では上ボタンを押すと末尾が選択されます）
- *　
- * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
- * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
- * 以下のフォルダに格納されています。
- * dlc/BasicResources/plugins/official
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -40,8 +33,6 @@
 
 (() => {
     'use strict';
-    const script = document.currentScript;
-    const param = PluginManagerEx.createParameter(script);
 
     const _Window_ChoiceList_select = Window_ChoiceList.prototype.select;
     Window_ChoiceList.prototype.select = function(index) {
