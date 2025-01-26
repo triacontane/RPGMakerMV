9b8984e2657ac2b592a66065360a3131a31fd9a1
triacontane <triacontanekinoko@gmail.com>
2025-01-26 15:33:01 +0900

    1.0.1 2025/01/26 所持制限アイテムの仕様を初期装備やショップでの購入にも正しく反映するよう修正

--- ItemMaxOnlyOne.js
+++ ItemMaxOnlyOne.js
@@ -6,6 +6,7 @@
  http://opensource.org/licenses/mit-license.php
 ----------------------------------------------------------------------------
  Version
+ 1.0.1 2025/01/26 所持制限アイテムの仕様を初期装備やショップでの購入にも正しく反映するよう修正
  1.0.0 2024/03/01 初版
 ----------------------------------------------------------------------------
  [Blog]   : https://triacontane.blogspot.jp/
@@ -48,6 +49,9 @@
  *
  * アイテムの最大所持数を1つに制限します。
  * 制限した場合、メニュー画面での所持数を削除できます。
+ *
+ * 初期装備として制限アイテムを複数のアクターに指定した場合
+ * 最初のアクター以外は未装備の状態になります。
  *　
  * このプラグインの利用にはベースプラグイン『PluginCommonBase.js』が必要です。
  * 『PluginCommonBase.js』は、RPGツクールMZのインストールフォルダ配下の
@@ -65,20 +69,50 @@
     const script = document.currentScript;
     const param = PluginManagerEx.createParameter(script);
 
+    DataManager.isOnlyOneItem = function(item) {
+        const existMeta = !!item.meta[param.tagName];
+        return !(param.condition === 'hasMeta' && !existMeta || param.condition === 'noMeta' && existMeta);
+    };
+
     const _Game_Party_maxItems = Game_Party.prototype.maxItems;
     Game_Party.prototype.maxItems = function(item) {
-        if (this.isOnlyOneItem(item)) {
+        if (DataManager.isOnlyOneItem(item)) {
             return 1;
         } else {
             return _Game_Party_maxItems.apply(this, arguments);
         }
     };
 
-    Game_Party.prototype.isOnlyOneItem = function(item) {
-        const existMeta = !!item.meta[param.tagName];
-        return !(param.condition === 'hasMeta' && !existMeta || param.condition === 'noMeta' && existMeta);
+    Game_Actor.prototype.hasEquip = function(item) {
+        return this.hasWeapon(item) || this.hasArmor(item);
     };
 
+    Game_Actors.prototype.hasAnyEquip = function(item) {
+        return this._data.some(actor => actor && actor.hasEquip(item));
+    };
+
+    Game_Actors.prototype.isAnyEquipOnlyOneItem = function(item) {
+        return DataManager.isOnlyOneItem(item) && this.hasAnyEquip(item);
+    };
+
+    const _Game_Item_setEquip = Game_Item.prototype.setEquip;
+    Game_Item.prototype.setEquip = function(isWeapon, itemId) {
+        _Game_Item_setEquip.apply(this, arguments);
+        const object = this.object();
+        this._itemId = 0;
+        if (object && !$gameActors.isAnyEquipOnlyOneItem(object)) {
+            this._itemId = itemId;
+        }
+    };
+
+    const _Window_ShopBuy_isEnabled = Window_ShopBuy.prototype.isEnabled
+    Window_ShopBuy.prototype.isEnabled = function(item) {
+        const enabled = _Window_ShopBuy_isEnabled.apply(this, arguments);
+        if (enabled && $gameActors.isAnyEquipOnlyOneItem(item)) {
+            return false;
+        }
+        return enabled;
+    };
 
     const _Window_ItemList_drawItemNumber = Window_ItemList.prototype.drawItemNumber;
     Window_ItemList.prototype.drawItemNumber = function(item, x, y, width) {
