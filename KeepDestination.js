e0cbc8ce119a49cf62615074e3f98278ceb8b1b3
triacontane <triacontanekinoko@gmail.com>
2025-03-11 22:37:13 +0900

    1.1.0 2025/03/11 MZ版としてリファクタリング

--- KeepDestination.js
+++ KeepDestination.js
@@ -1,41 +1,29 @@
 //=============================================================================
 // KeepDestination.js
 // ----------------------------------------------------------------------------
-// (C)2015-2018 Triacontane
+// (C)2018 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2025/03/11 MZ版としてリファクタリング
 // 1.0.0 2018/01/06 初版
 // ----------------------------------------------------------------------------
-// [Blog]   : https://triacontane.blogspot.jp/
-// [Twitter]: https://twitter.com/triacontane/
+// [X]      : https://twitter.com/triacontane/
 // [GitHub] : https://github.com/triacontane/
 //=============================================================================
 
 /*:
- * @plugindesc KeepDestinationPlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @help KeepDestination.js
- *
- * タッチ移動中にイベントが実行された場合でもタッチ移動が
- * 中断されなくなります。
- * ただし、進行方向が通行不可だった場合は停止します。
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc タッチ移動先の保持プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/KeepDestination.js
+ * @author トリアコンタン
  *
  * @help KeepDestination.js
  *
  * タッチ移動中にイベントが実行された場合でもタッチ移動が
  * 中断されなくなります。
  * ただし、進行方向が通行不可だった場合は停止します。
- *
- * このプラグインにはプラグインコマンドはありません。
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -43,18 +31,23 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+(()=> {
     'use strict';
 
     //=============================================================================
     // Scene_Map
     //=============================================================================
-    var _Scene_Map_updateDestination = Scene_Map.prototype.updateDestination;
+    const _Scene_Map_updateDestination = Scene_Map.prototype.updateDestination;
     Scene_Map.prototype.updateDestination = function() {
         if ($gamePlayer.canPassStraight() && !$gamePlayer.isTransferring()) {
             $gameTemp.keepDestination();
+            this._prevTouchCount = this._touchCount;
         }
         _Scene_Map_updateDestination.apply(this, arguments);
+        if (this._prevTouchCount && !this.isMapTouchOk()) {
+            this._touchCount = this._prevTouchCount;
+        }
+        this._prevTouchCount = 0;
         $gameTemp.clearKeepDestination();
     };
 
@@ -69,7 +62,7 @@
         this._keepDestination = false;
     };
 
-    var _Game_Temp_clearDestination = Game_Temp.prototype.clearDestination;
+    const _Game_Temp_clearDestination = Game_Temp.prototype.clearDestination;
     Game_Temp.prototype.clearDestination = function() {
         if (this._keepDestination) {
             return;
