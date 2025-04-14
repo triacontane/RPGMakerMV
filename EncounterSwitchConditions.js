f645a54953902210c50935b16158a4de2886d7b2
triacontane <triacontanekinoko@gmail.com>
2025-04-14 22:34:10 +0900

    1.1.0 2025/04/14 MZで動作するよう修正

--- EncounterSwitchConditions.js
+++ EncounterSwitchConditions.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2025/04/14 MZで動作するよう修正
 // 1.0.0 2018/02/18 初版
 // ----------------------------------------------------------------------------
 // [Blog]   : https://triacontane.blogspot.jp/
@@ -13,33 +14,13 @@
 // [GitHub] : https://github.com/triacontane/
 //=============================================================================
 
-/*:
- * @plugindesc EncounterSwitchConditionsPlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @param validSwitchId
- * @desc このプラグインの機能が有効になるスイッチIDです。0を指定すると常に有効になります。
- * @default 0
- * @type switch
- *
- * @help EncounterSwitchConditions.js
- *
- * マップ設定のエンカウント作成におけるリージョンIDによる出現条件をスイッチに
- * 読み替えます。通常のリージョンIDによる絞り込みは無効となります。
- *
- * 指定例
- * マップ設定でリージョンID[6]を指定
- *
- * 本来のリージョンID[6]の条件は無視され、スイッチ番号[6]がONの場合のみ
- * 指定した敵グループとエンカウントする。
- *
- * このプラグインにはプラグインコマンドはありません。
- *
- * This plugin is released under the MIT License.
- */
 /*:ja
  * @plugindesc スイッチ条件エンカウントプラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/EncounterSwitchConditions.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
  *
  * @param validSwitchId
  * @text 有効スイッチID
@@ -66,48 +47,23 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+(()=> {
     'use strict';
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
 
-    /**
-     * Create plugin parameter. param[paramName] ex. param.commandPrefix
-     * @param pluginName plugin name(EncounterSwitchConditions)
-     * @returns {Object} Created parameter
-     */
-    var createPluginParameter = function(pluginName) {
-        var paramReplacer = function(key, value) {
-            if (value === 'null') {
-                return value;
-            }
-            if (value[0] === '"' && value[value.length - 1] === '"') {
-                return value;
-            }
-            try {
-                return JSON.parse(value);
-            } catch (e) {
-                return value;
-            }
-        };
-        var parameter     = JSON.parse(JSON.stringify(PluginManager.parameters(pluginName), paramReplacer));
-        PluginManager.setParameters(pluginName, parameter);
-        return parameter;
-    };
-
-    var param = createPluginParameter('EncounterSwitchConditions');
-
-    var isValidPlugin = function() {
-        return !param.validSwitchId || $gameSwitches.value(param.validSwitchId);
-    };
-
-    var _Game_Player_meetsEncounterConditions      = Game_Player.prototype.meetsEncounterConditions;
+    const _Game_Player_meetsEncounterConditions      = Game_Player.prototype.meetsEncounterConditions;
     Game_Player.prototype.meetsEncounterConditions = function(encounter) {
-        var result = _Game_Player_meetsEncounterConditions.apply(this, arguments);
-        if (isValidPlugin() && result === encounter.regionSet.contains(this.regionId())) {
-            return encounter.regionSet.some(function(switchId) {
-                return $gameSwitches.value(switchId);
-            });
+        const result = _Game_Player_meetsEncounterConditions.apply(this, arguments);
+        const set = encounter.regionSet;
+        if (this.isValidSwitchEncounter() && set.length > 0) {
+            return set.some(switchId => $gameSwitches.value(switchId));
         }
         return result;
+    };
+
+    Game_Player.prototype.isValidSwitchEncounter = function() {
+        return !param.validSwitchId || $gameSwitches.value(param.validSwitchId);
     };
 })();
 
