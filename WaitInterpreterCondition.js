1afcd5dbbc9d19dc793c9ab02d8fba2c7a770e94
triacontane <triacontanekinoko@gmail.com>
2025-05-24 15:04:38 +0900

    1.1.0 2025/05/23 MZ版として再作成

--- WaitInterpreterCondition.js
+++ WaitInterpreterCondition.js
@@ -1,59 +1,53 @@
 //=============================================================================
 // WaitInterpreterCondition.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2015-2017 Triacontane
+// (C)2017 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2025/05/23 MZ版として再作成
 // 1.0.0 2017/10/14 初版
 // ----------------------------------------------------------------------------
-// [Blog]   : https://triacontane.blogspot.jp/
-// [Twitter]: https://twitter.com/triacontane/
+// [Twitter]: https://x.com/triacontane/
 // [GitHub] : https://github.com/triacontane/
 //=============================================================================
 
 /*:
- * @plugindesc WaitEventInterpreterPlugin
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author triacontane
- *
- * @help WaitInterpreterCondition.js
- *
- * イベント実行において条件を満たすまでウェイトします。
- *
- * プラグインコマンド詳細
- *  イベントコマンド「プラグインコマンド」から実行。
- *  （パラメータの間は半角スペースで区切る）
- *
- * WIC_WAIT_SWITCH 1         # スイッチ[1]がONになるまで待機
- * WIC_ウェイト_スイッチ     # 同上
- * WIC_WAIT_SCRIPT f         # 計算式[f]がtrueになるまで待機
- * WIC_ウェイト_スクリプト f # 同上
- *
- * 計算式では制御文字\v[n]などが使用できます。
- *
- * This plugin is released under the MIT License.
- */
-/*:ja
  * @plugindesc 条件付きウェイトプラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/WaitInterpreterCondition.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
+ *
+ * @command wait_switch
+ * @text スイッチウェイト
+ * @desc 指定したスイッチがONになるまでウェイトします。
+ *
+ * @arg switchId
+ * @text スイッチID
+ * @desc ONになるまでウェイトするスイッチ番号です。
+ * @default 1
+ * @type switch
+ *
+ * @command wait_script
+ * @text スクリプトウェイト
+ * @desc 指定したスクリプトがtrueになるまでウェイトします。
+ *
+ * @arg script
+ * @text スクリプト
+ * @desc 戻り値がtrueを返すまでウェイトするスクリプトです。毎フレーム実行されます。
+ * @default
+ * @type multiline_string
  *
  * @help WaitInterpreterCondition.js
  *
  * イベント実行において条件を満たすまでウェイトします。
- *
- * プラグインコマンド詳細
- *  イベントコマンド「プラグインコマンド」から実行。
- *  （パラメータの間は半角スペースで区切る）
- *
- * WIC_WAIT_SWITCH 1         # スイッチ[1]がONになるまで待機
- * WIC_ウェイト_スイッチ     # 同上
- * WIC_WAIT_SCRIPT f         # 計算式[f]がtrueになるまで待機
- * WIC_ウェイト_スクリプト f # 同上
- *
- * 計算式では制御文字\v[n]などが使用できます。
- *
- * このプラグインにはプラグインコマンドはありません。
+ * 条件にはスイッチとスクリプトが使用できます。
+ * スクリプトでは制御文字\v[n]や\s[n]などが使用できます。
+ * 条件を満たすまでイベントが進まなくなるので、ゲーム進行が
+ * 止まらないように注意してください。
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -61,71 +55,27 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function() {
+(()=> {
     'use strict';
-    var metaTagPrefix = 'WIC_';
+    const script = document.currentScript;
 
-    //=============================================================================
-    // ローカル関数
-    //  プラグインパラメータやプラグインコマンドパラメータの整形やチェックをします
-    //=============================================================================
-    var getArgNumber = function(arg, min, max) {
-        if (arguments.length < 2) min = -Infinity;
-        if (arguments.length < 3) max = Infinity;
-        return (parseInt(arg) || 0).clamp(min, max);
-    };
+    PluginManagerEx.registerCommand(script, 'wait_switch', function(args) {
+        this.execWaitSwitch(args.switchId);
+    });
 
-    var convertEscapeCharacters = function(text) {
-        if (isNotAString(text)) text = '';
-        var windowLayer = SceneManager._scene._windowLayer;
-        return windowLayer ? windowLayer.children[0].convertEscapeCharacters(text) : text;
-    };
+    PluginManagerEx.registerCommand(script, 'wait_script', function(args) {
+        this.execWaitScript(args.script);
+    });
 
-    var isNotAString = function(args) {
-        return String(args) !== args;
-    };
-
-    var concatAllArguments = function(args) {
-        return args.reduce(function(prevValue, arg) {
-            return prevValue + ' ' + arg;
-        }, '');
-    };
-
-    var setPluginCommand = function(commandName, methodName) {
-        pluginCommandMap.set(metaTagPrefix + commandName, methodName);
-    };
-
-    //=============================================================================
-    // パラメータの取得と整形
-    //=============================================================================
-    var pluginCommandMap = new Map();
-    setPluginCommand('WAIT_SWITCH', 'execWaitSwitch');
-    setPluginCommand('ウェイト_スイッチ', 'execWaitSwitch');
-    setPluginCommand('WAIT_SCRIPT', 'execWaitScript');
-    setPluginCommand('ウェイト_スクリプト', 'execWaitScript');
-
-    //=============================================================================
-    // Game_Interpreter
-    //  プラグインコマンドを追加定義します。
-    //=============================================================================
     Game_Interpreter._waitModeCondition = 'condition';
 
-    var _Game_Interpreter_pluginCommand      = Game_Interpreter.prototype.pluginCommand;
-    Game_Interpreter.prototype.pluginCommand = function(command, args) {
-        _Game_Interpreter_pluginCommand.apply(this, arguments);
-        var pluginCommandMethod = pluginCommandMap.get(command.toUpperCase());
-        if (pluginCommandMethod) {
-            this[pluginCommandMethod](args);
-        }
-    };
-
-    Game_Interpreter.prototype.execWaitSwitch = function(args) {
-        this._waitSwitchId = getArgNumber(convertEscapeCharacters(args[0]), 1);
+    Game_Interpreter.prototype.execWaitSwitch = function(id) {
+        this._waitSwitchId = id;
         this.setWaitMode(Game_Interpreter._waitModeCondition);
     };
 
-    Game_Interpreter.prototype.execWaitScript = function(args) {
-        this._waitScript = concatAllArguments(args);
+    Game_Interpreter.prototype.execWaitScript = function(script) {
+        this._waitScript = script;
         this.setWaitMode(Game_Interpreter._waitModeCondition);
     };
 
@@ -152,7 +102,7 @@
     };
 
     Game_Interpreter.prototype.isValidWaitScript = function() {
-        return this._waitScript && !eval(convertEscapeCharacters(this._waitScript));
+        return this._waitScript && !eval(PluginManagerEx.convertEscapeCharacters(this._waitScript));
     };
 })();
 
