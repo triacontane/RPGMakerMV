5d37567fc1c4c5ec2dc6ec837c155c82aec50c5f
triacontane <triacontanekinoko@gmail.com>
2025-12-10 22:36:05 +0900

    2.0.0 2025/12/10 MZ向けに再作成

--- ChangeGameoverCondition.js
+++ ChangeGameoverCondition.js
@@ -1,34 +1,41 @@
 //=============================================================================
 // ChangeGameOverCondition.js
 // ----------------------------------------------------------------------------
-// Copyright (c) 2015 Triacontane
+// (C)2016 Triacontane
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 2.0.0 2025/12/10 MZ向けに再作成
 // 1.1.0 2016/05/11 ゲームオーバーになるアクターIDを個別に指定できる機能を追加
 // 1.0.0 2016/04/21 初版
 // ----------------------------------------------------------------------------
-// [Blog]   : http://triacontane.blogspot.jp/
-// [Twitter]: https://twitter.com/triacontane/
+// [X]: https://x.com/triacontane/
 // [GitHub] : https://github.com/triacontane/
 //=============================================================================
 
 /*:
  * @plugindesc ゲームオーバー条件変更プラグイン
- * @target MZ @url https://github.com/triacontane/RPGMakerMV/tree/mz_master @author トリアコンタン
+ * @target MZ
+ * @url https://github.com/triacontane/RPGMakerMV/tree/mz_master/ChangeGameOverCondition.js
+ * @base PluginCommonBase
+ * @orderAfter PluginCommonBase
+ * @author トリアコンタン
  *
- * @help ゲームオーバー条件をいずれかの味方が戦闘不能になった場合
- * に変更します。
- * 対象となるアクターIDを指定することもできます。
+ * @command SET_VIP
+ * @text ゲームオーバーアクター設定
+ * @desc 指定したアクターが戦闘不能になるとゲームオーバーになります。複数指定可能。
  *
- * 条件を変更するにはスクリプトで以下を実行してください。
+ * @arg actorIdList
+ * @text アクターIDリスト
+ * @desc ゲームオーバー条件とするアクターIDのリストです。複数指定でいずれかのアクターの戦闘不能でゲームオーバーです。
+ * @default []
+ * @type actor[]
  *
- * $gameParty.setVip(1);   // アクターID[1]が戦闘不能になるとゲームオーバー
- * $gameParty.setVipAll(); // いずれか一人のアクターが戦闘不能になるとゲームオーバー
- * $gameParty.removeVip(); // ゲームオーバーの設定を解除
+ * @help ChangeGameOverCondition.js
  *
- * このプラグインにはプラグインコマンドはありません。
+ * 指定したアクターが戦闘不能になったときに
+ * ゲームオーバーになるよう仕様を変更できます。
  *
  * 利用規約：
  *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
@@ -36,38 +43,43 @@
  *  このプラグインはもうあなたのものです。
  */
 
-(function () {
+(()=> {
     'use strict';
+    const script = document.currentScript;
+
+    PluginManagerEx.registerCommand(script, 'SET_VIP', args => {
+        const actorIdList = args.actorIdList;
+        if (!actorIdList || actorIdList.length === 0) {
+            $gameParty.removeVip();
+        } else {
+            $gameParty.setVipList(actorIdList);
+        }
+    });
 
     var _Game_Party_initialize = Game_Party.prototype.initialize;
     Game_Party.prototype.initialize = function() {
         _Game_Party_initialize.apply(this, arguments);
-        this._vipActor = null;
-        this._vipAll   = true;
+        this._vipActorList = null;
     };
 
-    Game_Party.prototype.setVip = function(id) {
-        this._vipActor = id;
-        this._vipAll   = false;
-    };
-
-    Game_Party.prototype.setVipAll = function() {
-        this._vipAll   = true;
+    Game_Party.prototype.setVipList = function(list) {
+        this._vipActorList = list;
     };
 
     Game_Party.prototype.removeVip = function() {
-        this._vipActor = null;
-        this._vipAll   = false;
+        this._vipActorList = null;
     };
 
-    var _Game_Party_isAllDead = Game_Party.prototype.isAllDead;
+    const _Game_Party_isAllDead = Game_Party.prototype.isAllDead;
     Game_Party.prototype.isAllDead = function() {
-        var result = _Game_Party_isAllDead.apply(this, arguments);
-        if (!result && this.deadMembers().length > 0 && (this.inBattle() || !this.isEmpty())) {
-            return this._vipAll ? true :
-                this._vipActor ? this.deadMembers().contains($gameActors.actor(this._vipActor)) : false;
+        const isAllDead = _Game_Party_isAllDead.apply(this, arguments);
+        if (!isAllDead && this.deadMembers().length > 0 && (this.inBattle() || !this.isEmpty())) {
+            return this._vipActorList && this._vipActorList.some(actorId => {
+                const actor = $gameActors.actor(actorId);
+                return actor && actor.isDead();
+            });
         }
-        return result;
+        return isAllDead;
     };
 })();
 
